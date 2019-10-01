0bb7d56eddcb3e89f4ec015e0ebc8eb68c6d54c3
David S. CCAO <dsrebro@cookcountyassessor.com>
2019-10-01 15:56:54 +0000

    New script to import Cook County Recorder of Deeds Transfer Files to new CCRDSales table.

---
+++ code.sql/Upload_to_DTBL_CCRDSALES.sql
@@ -0,0 +1,10 @@
+--Imports CCRD Transfer File data from Common drive into a ccrdsales data table, be sure to clean the data first in Excel, e.g. Remove trailing zipcodes, commas in unit no, 
+--and trim left names to 50 characters.  If the bulk insert/import fails, you have to delete all contents and start over from 1999. Also, 2019 has monthly lists. 
+
+BULK INSERT dbo.DTBL_CCRDSALES
+FROM '\\fileserver\ocommon\CCAODATA\data\raw_data\Transfer List CCROD\Transfer List 1999.csv'
+WITH (  
+      DATAFILETYPE = 'char',  
+      FIELDTERMINATOR = ',',  
+      ROWTERMINATOR = '\n'  
+);
