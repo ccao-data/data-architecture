874b80e99b21a2992f00d83898263ba8adb8ced0
David S. CCAO <dsrebro@cookcountyassessor.com>
2019-10-01 15:59:15 +0000

    Update with comment change

--- code.sql/Upload_to_DTBL_CCRDSALES.sql
+++ code.sql/Upload_to_DTBL_CCRDSALES.sql
@@ -1,4 +1,4 @@
---Imports CCRD Transfer File data from Common drive into a ccrdsales data table, be sure to clean the data first in Excel, e.g. Remove trailing zipcodes, commas in unit no, 
+--Imports CCRD Transfer File data (change the year, YYYY.csv, in the line 5 as necessary) from Common drive into a ccrdsales data table, be sure to clean the data first in Excel, e.g. Remove trailing zipcodes, commas in unit no, 
 --and trim left names to 50 characters.  If the bulk insert/import fails, you have to delete all contents and start over from 1999. Also, 2019 has monthly lists. 
 
 BULK INSERT dbo.DTBL_CCRDSALES
