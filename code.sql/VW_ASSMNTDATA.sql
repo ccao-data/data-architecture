eec0c643946dee8955885e37a5c420147da2dde3
sweatyhandshake <wridgeway@cookcountyassessor.com>
2019-10-30 13:32:14 -0500

    just a lot of formatting

--- code.sql/VW_ASSMNTDATA.sql
+++ code.sql/VW_ASSMNTDATA.sql
@@ -1,4 +1,17 @@
-SELECT * FROM VW_RES_UNIVERSE AS UNIVERSE
+ALTER VIEW VW_ASSMNTDATA AS
+
+SELECT 
+
+UNIVERSE.*
+, PROPERTY_ADDRESS, PROPERTY_APT_NO, PROPERTY_CITY, PROPERTY_ZIP, MAILING_ADDRESS, MAILING_CITY, MAILING_ZIP
+, geoid, census_tract, ward, ohare_noise, floodplain, withinmr100, withinmr101300
+, commissioner_dist, reps_dist, senate_dist, tif_agencynum, PUMA, municipality, FIPS
+, midincome, white_perc, black_perc, his_perc, other_perc
+, centroid_x, centroid_y
+, most_recent_sale_date, most_recent_sale_price, DOC_NO, DEED_TYPE
+
+FROM VW_RES_UNIVERSE AS UNIVERSE
+
 LEFT JOIN
 	VW_PINGEO GEO
 	ON UNIVERSE.PIN = GEO.PIN
