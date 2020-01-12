a01e0075a82517b4019af64b7cedb9a735200c71
ccao_rross <rross@cookcountyassessor.com>
2020-01-12 11:47:19 -0600

    making this view incorporate SOPs on ratio studies

--- code.sql/VW_RES_RATIOS.sql
+++ code.sql/VW_RES_RATIOS.sql
@@ -1,31 +1,60 @@
+/* This view provides pre-selected residential sales ratios for the purpose of reporting ratio study statistics.
+This view and its code is goverened by the Standard Operating Procedures on Residential Sales Ratio Studies.
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+DO NOT ALTER THIS VIEW WITHOUT AUTHORIZATION FROM A SENIOR DATA SCIENTISTS OR THE CHIEF DATA OFFICER
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+*/
+
 ALTER VIEW VW_RES_RATIOS AS
 
-SELECT SALES.PIN, sale_price, sale_date, DATE_A_ROLL_CERTIFIED, BOARD_CLOSE_DATE, FMV, FMV / sale_price AS RATIO, HEAD.TAX_YEAR, HEAD.TOWN_CODE, NBHD, [modeling_group]
-
-FROM VW_CLEAN_IDORSALES AS SALES
-
+SELECT
+ T.PIN
+/* --------------- SOPs on Ratio Studies, 3.3.2.1 --------------- */
+ , CASE WHEN T.TAX_YEAR=YEAR(GETDATE()) THEN
+ (fitted_value_6)/S2.sale_price 
+	WHEN T.TAX_YEAR<YEAR(GETDATE()) THEN (fitted_value_6)/S.sale_price 
+	 END AS [RATIO ON PIPELINE RESULT]
+, CASE WHEN T.TAX_YEAR=YEAR(GETDATE()) THEN ((T.HD_ASS_LND + T.HD_ASS_BLD)*10)/S2.sale_price 
+WHEN T.TAX_YEAR<YEAR(GETDATE()) THEN ((T.HD_ASS_LND + T.HD_ASS_BLD)*10)/S.sale_price 
+	END AS [RATIO ON FIRST PASS]
+, CASE WHEN T.TAX_YEAR=YEAR(GETDATE()) THEN ((TB.HD_ASS_LND + TB.HD_ASS_BLD)* 10)/S2.sale_price
+ WHEN T.TAX_YEAR<YEAR(GETDATE()) THEN ((TB.HD_ASS_LND + TB.HD_ASS_BLD)*10)/S.sale_price 
+	END AS [RATIO ON ASSESSOR CERTIFIED]
+, CASE WHEN T.TAX_YEAR=YEAR(GETDATE()) THEN ((BR.HD_ASS_LND + BR.HD_ASS_BLD)* 10)/S2.sale_price 
+WHEN T.TAX_YEAR<YEAR(GETDATE()) THEN ((BR.HD_ASS_LND + BR.HD_ASS_BLD)* 10)/S.sale_price 
+	END AS [RATIO ON BOARD CERTIFIED] 
+/* -------------------------------------------------------------- */
+/* --------------- SOPs on Ratio Studies, 3.1 ------------------- */
+, CASE WHEN T.HD_CLASS IN (202, 203, 204, 205, 206, 207, 208, 209, 210, 234, 278, 295) THEN 'Single_Family'
+  WHEN T.HD_CLASS IN (211, 212) THEN 'Multi-Family'
+  WHEN T.HD_CLASS IN (200, 201, 241) THEN 'Vacant Land'
+  WHEN T.HD_CLASS = 299 AND ((T.HD_PRI_BLD + T.HD_PRI_LND) > 10 OR (T.HD_PRI_BLD + T.HD_PRI_LND) IS NULL) THEN 'Residential Condominium' 
+  ELSE 'Error' END AS [modeling_group] 
+/* -------------------------------------------------------------- */
+, township_name, CAST(T.HD_NBHD AS varchar) AS NBHD, T.TAX_YEAR AS YEAR 
+FROM AS_HEADT AS T
 LEFT JOIN
-
-(SELECT PIN, LEFT(HD_TOWN, 2) AS TOWN_CODE, CAST(HD_NBHD AS varchar) AS NBHD, TAX_YEAR, (HD_ASS_LND + HD_ASS_BLD) * 10 AS FMV,
-	CASE WHEN HD_CLASS IN (202, 203, 204, 205, 206, 207, 208, 209, 210, 234, 278, 295) THEN 'SF'
-	WHEN HD_CLASS IN (211, 212) THEN 'MF'
-	WHEN HD_CLASS IN (200, 201, 241) OR (HD_CLASS = 299 AND ((HD_PRI_BLD + HD_PRI_LND) > 10 OR (HD_PRI_BLD + HD_PRI_LND) IS NULL)) THEN 'NCHARS'
-	ELSE NULL END AS [modeling_group]
-	
-	FROM AS_HEADTB) AS HEAD
-
-ON SALES.PIN = HEAD.PIN
-
+AS_HEADTB AS TB
+ON T.PIN=TB.PIN AND T.TAX_YEAR=TB.TAX_YEAR
 LEFT JOIN
-
-(SELECT CODE, DATE_A_ROLL_CERTIFIED, BOARD_CLOSE_DATE, TAX_YEAR
-	
-	FROM FTBL_CERT_DATES) AS CERTIFIED
-
-ON CERTIFIED.CODE = HEAD.TOWN_CODE AND CERTIFIED.TAX_YEAR = HEAD.TAX_YEAR
-
-WHERE sale_price > 10000
-	AND HEAD.TOWN_CODE IS NOT NULL
-	AND [modeling_group] IS NOT NULL
-	AND (DATEDIFF(DAY, DATE_A_ROLL_CERTIFIED, sale_date) BETWEEN 0 AND 365 OR (YEAR(sale_date) = (SELECT MAX(TAX_YEAR) FROM AS_HEADTB) AND HEAD.TAX_YEAR = (SELECT MAX(TAX_YEAR) FROM AS_HEADT)))
-	AND HEAD.TOWN_CODE + NBHD != '23171'
+AS_HEADBR AS BR
+ON T.PIN=BR.PIN AND T.TAX_YEAR=BR.TAX_YEAR
+LEFT JOIN 
+VW_CLEAN_IDORSALES AS S
+ON T.PIN=S.PIN AND T.TAX_YEAR=YEAR(S.sale_date)
+LEFT JOIN 
+VW_CLEAN_IDORSALES AS S2
+ON T.PIN=S2.PIN AND T.TAX_YEAR=YEAR(S2.sale_date)-1
+LEFT JOIN
+DTBL_MODELVALS AS M
+ON T.PIN=M.PIN AND T.TAX_YEAR=M.TAX_YEAR
+LEFT JOIN
+FTBL_TOWNCODES AS TC
+ON TC.township_code=LEFT(T.HD_TOWN, 2)
+WHERE (1=1)
+	/* SOP 3.5.1 */
+	AND S.sale_price > 10000 AND S2.sale_price > 10000
+	AND (S.sale_price IS NOT NULL OR S2.sale_price IS NOT NULL)
+	/* SOPs 3.1.1 */
+	AND (LEFT(T.HD_TOWN,2) NOT IN (23) AND T.HD_NBHD NOT IN (171))
