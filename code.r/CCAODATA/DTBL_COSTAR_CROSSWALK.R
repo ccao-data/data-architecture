9771846436f7a5635198a58dcf472acc115b002a
HOME-PC\Mike <mwu@cookcountyassessor.com>
2020-10-08 13:43:18 -0500

    After discovered that Match by PIN can be a fairly complicated idea, I Start to work on joining by spatial data.

--- code.r/CCAODATA/DTBL_COSTAR_CROSSWALK.R
+++ code.r/CCAODATA/DTBL_COSTAR_CROSSWALK.R
@@ -8,17 +8,48 @@ library(ccao)
 
 CCAODATA <- dbConnect(odbc(), .connection_string = Sys.getenv("DB_CONFIG_CCAODATA"))
 
-# ------------------ Way 1, match with Latitude/Longevity -----------------
+
+
+# ---------------------- Way 2, match with parcel Pin ID ----------------------
 # SQL PULLS ----
 
-# # Fetch the Lat and Long columns from costarsnapshot
-# query <- "select ID, costar_latitude, costar_longitude   from COSTARSNAPSHOTS"
-# 
-# costar_gis_df <- dbGetQuery(CCAODATA, query) 
-# head(costar_gis_df)
-# # Pull data from shape file in O drive
-# 
-# parcel_shapes <- st_read(dsn = "//fileserver/ocommon/CCAODATA/data/spatial/Historical_Parcels__2019.GeoJSON") %>%
+# Fetch the Costar Parcel pins from costarsnapshot
+query_PIN_costar <- "select costar_parcel_number_1min, costar_parcel_number_2max from COSTARSNAPSHOTS 
+                       where costar_parcel_number_1min is not null and costar_parcel_number_2max is not null and costar_tax_year = '2018'"
+query_PIN_ccao <- "select PIN from AS_HEADT where TAX_YEAR = 2018"
+
+# clean the data before deep pre-processing
+df_costar_parcelPIN <- dbGetQuery(CCAODATA, query_PIN_costar) 
+df_ccao_parcelPIN <- dbGetQuery(CCAODATA, query_PIN_ccao) %>% mutate(PIN = pin_format_pretty(PIN))
+
+head(df_costar_parcelPIN)
+head(df_ccao_parcelPIN)
+# pending on this method, pretty complicated
+
+
+# ------------------ Way 2, match with Latitude/Longevity -----------------
+# SQL PULLS ----
+
+# Fetch the Lat and Long columns from costarsnapshot
+query_gis <- "select ID, costar_latitude, costar_longitude from COSTARSNAPSHOTS where ID is not null and
+             costar_latitude is not null and costar_longitude is not null"
+# create sf spatial points
+df_costar_gis <- dbGetQuery(CCAODATA, query_gis) %>%
+  rename(latitude = costar_latitude,  longitude = costar_longitude ) %>% 
+  # Convert gis data from costar into spatial object can be joined to shape file
+  mutate(costar_sf_point = st_as_sf(x = ., 
+           coords = c('longitude', 'latitude'),
+           crs = "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")) %>%
+  select(ID, latitude, longitude, costar_sf_point)
+
+
+head(df_costar_gis)
+
+
+# Pull data from shape file in O drive
+
+# this is a big extract/PPC commands, be careful when re-run!!!
+# gis_shapes <- st_read(dsn = "//fileserver/ocommon/CCAODATA/data/spatial/Historical_Parcels__2019.GeoJSON") %>%
 #   # format PINs
 #   rename(PIN = PIN10) %>%
 #   filter(!is.na(as.numeric(PIN)) & nchar(PIN) %in% c(10, 14)) %>%
@@ -28,23 +59,12 @@ CCAODATA <- dbConnect(odbc(), .connection_string = Sys.getenv("DB_CONFIG_CCAODAT
 #   filter(SHAPE_Area == max(SHAPE_Area)) %>%
 #   ungroup()
 
-# ---------------------- Way 2, match with parcel Pin ID ----------------------
-# SQL PULLS ----
-
-# Fetch the Costar Parcel pins from costarsnapshot
-query_PIN_costar <- "select costar_parcel_number_1min, costar_parcel_number_2max from COSTARSNAPSHOTS"
-query_PIN_ccao <- "select PIN from AS_HEADT where TAX_YEAR = 2019;"
-
-# clean the data before deep pre-processing
-df_costar_parcelPIN <- dbGetQuery(CCAODATA, query_PIN_costar) 
-df_ccao_parcelPIN <- dbGetQuery(CCAODATA, query_PIN_ccao) %>% mutate(PIN = pin_format_pretty(PIN))
-
-head(df_costar_parcelPIN)
-head(df_ccao_parcelPIN)
+# Join the df_costar_gis with gis_shapes by their spatial points
 
 
+# generate report on the jointed data
 
-
+# ---------------------- Way 3, match by address ---------------------
 
 
 
