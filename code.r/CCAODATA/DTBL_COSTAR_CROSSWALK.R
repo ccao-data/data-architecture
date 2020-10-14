76a0ee0139b3ebe73ee474676c3cc72a30b3d07b
HOME-PC\Mike <mwu@cookcountyassessor.com>
2020-10-14 12:02:58 -0500

    Integrity Check

--- code.r/CCAODATA/DTBL_COSTAR_CROSSWALK.R
+++ code.r/CCAODATA/DTBL_COSTAR_CROSSWALK.R
@@ -20,7 +20,9 @@ df_costar_gis <- dbGetQuery(CCAODATA, query_gis) %>%
   st_as_sf(coords = c("costar_longitude", "costar_latitude"), crs = 4326) %>% st_transform(crs = 3435)
 
 # Read and porcess CCAO GIS data
-df_ccao_gis <- read_sf(dsn = "O:/CCAODATA/data/spatial/Historical_Parcels__2019/Historical_Parcels__2019.shp", layer = 'Historical_Parcels__2019') %>% 
+# df_ccao_gis <- read_sf(dsn = "O:/CCAODATA/data/spatial/Historical_Parcels__2019/Historical_Parcels__2019.shp", layer = 'Historical_Parcels__2019') %>% 
+  df_ccao_gis <- read_sf(dsn = "//fileserver/ocommon/CCAODATA/data/spatial/Historical_Parcels__2019/Historical_Parcels__2019.shp", layer = 'Historical_Parcels__2019') %>% 
+  
           rename(PIN = PIN10) %>%
           filter(!is.na(as.numeric(PIN)) & nchar(PIN) %in% c(10, 14)) %>%
           mutate(PIN = pin_format_pretty(PIN))  %>%
@@ -41,7 +43,14 @@ joined <- st_join(df_costar_gis, df_ccao_gis ) %>%
 joined %>% filter(costar_tax_year== "2018" | costar_tax_year == "2017" | costar_tax_year == "2016") %>% 
   group_by(ID,PIN, costar_tax_year) %>% summarise(Count = n()) %>% filter(Count > 1)
 
+# Integrity Check on the match rate + count
 
+costar_rowcnt <- nrow(df_costar_gis)
+join_rowcnt <- nrow(joined)
+# match rate
+match_rate <- join_rowcnt/costar_rowcnt
+print(paste0("Total Match Count: ",join_rowcnt))
+print(paste0("Match Rate: ",round(match_rate, 2)))
 
 
 
