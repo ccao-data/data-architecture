6e7dec95d03409c580a4531704acf3d4856ea71d
sweatyhandshake <wridgeway@cookcountyassessor.com>
2021-01-05 14:32:02 -0600

    updated to include new validation data

--- code.r/CCAODATA/DTBL_VALIDATED_IC_SALES.R
+++ code.r/CCAODATA/DTBL_VALIDATED_IC_SALES.R
@@ -1,13 +1,37 @@
 # this script exists soley to insert validated IC sales into the SQL server
-# it is a slave to "ccao_ic_cama_dev/code.r/validation_report.R"
 
-CCAODATA <- dbConnect(odbc(),
-                      driver   = "SQL Server",
-                      server   = odbc.credentials("server"),
-                      database = odbc.credentials("database"),
-                      uid      = odbc.credentials("uid"),
-                      pwd      = odbc.credentials("pwd"))
+# connect to SQL server
+CCAODATA <- dbConnect(odbc::odbc(), .connection_string = Sys.getenv("DB_CONFIG_CCAODATAW"))
 
+# ingest old formatted validated sales
+old_sales <- read_xlsx("O:/CCAODATA/results/ad_hoc/validation_report.xlsx",
+                       col_types = c("text", "skip", "text", "text", "skip", "skip")) %>%
+  rename(VALID = VALID_INVALID)
+
+# ingest new sales
+new_sales <- read_xlsx("O:/CCAODATA/data/raw_data/2020 Sales to Validate for DS 11.14.2020.xlsx",
+                       col_types = c("skip", "skip", "skip", "skip", "text", "skip", "text", "text")) %>%
+
+  rename(DEED_NUMBER = DOC_NO)
+
+# combine sales and format for upload
+validated_sales <- bind_rows(old_sales, new_sales) %>%
+
+  mutate(DEED_NUMBER = str_pad(DEED_NUMBER, 11, side = "left", pad = "0"),
+         VALID = as.logical(case_when(VALID %in% c("yes", "Y") ~ 1,
+                                      VALID %in% c("no", "N") ~ 0)),
+         COMMENTS = case_when(is.na(COMMENTS) ~ "",
+                              TRUE ~ COMMENTS)) %>%
+
+  distinct() %>%
+  group_by(DEED_NUMBER, VALID) %>%
+  summarise(COMMENTS = paste(COMMENTS, collapse = "; ")) %>%
+  ungroup() %>%
+
+  mutate(COMMENTS = case_when(substr(COMMENTS, 1, 2) == "; " ~ substr(COMMENTS, 3, nchar(COMMENTS)),
+                              TRUE ~ COMMENTS))
+
+# insert into sql server
 dbWriteTable(CCAODATA, "DTBL_VALIDATED_IC_SALES", validated_sales, overwrite = TRUE)
 
 # disconnect after pulls
