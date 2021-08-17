01868b552643464a09208de2bcc0aa441158ad4e
sweatyhandshake <wridgeway@cookcountyassessor.com>
2021-08-17 17:48:56 -0500

    changed to allow looping through more parameters no longer binds data together for output

--- data_lake_raw/scripts/grab_census_raw.R
+++ data_lake_raw/scripts/grab_census_raw.R
@@ -5,56 +5,95 @@ tidycensus::census_api_key(key = Sys.getenv("CENSUS_API_KEY"))
 
 # ACS ----
 # grab a list of all ACS 5 year tables
-census_tables <- openxlsx::read.xlsx(here("census/documentation/2019_DataProductList.xlsx"), sheet = "2019 Data Product List") %>%
+five_year_census_tables <- openxlsx::read.xlsx(here("census/documentation/2019_DataProductList.xlsx"), sheet = "2019 Data Product List") %>%
   dplyr::filter(Table.Universe == 'Universe: Total population' & Year == '1,5') %>%
   dplyr::pull(Table.ID)
 
-# remove a couple of 5 year tables tables that don't work with the census API, paste on 1 year tables
-census_tables <- c(census_tables[-c(14, 16)],
-                   "B25002", # OCCUPANCY STATUS
-                   "B19083", # GINI INDEX OF INCOME INEQUALITY
-                   "B25013", # TENURE BY EDUCATIONAL ATTAINMENT OF HOUSEHOLDER
-                   "B17019", # POVERTY STATUS IN THE PAST 12 MONTHS OF FAMILIES BY HOUSEHOLD TYPE BY TENURE
-                   "B22003", # RECEIPT OF FOOD STAMPS/SNAP IN THE PAST 12 MONTHS BY POVERTY STATUS IN THE PAST 12 MONTHS FOR HOUSEHOLDS
-                   "B19013", # MEDIAN HOUSEHOLD INCOME IN THE PAST 12 MONTHS (IN 2019 INFLATION-ADJUSTED DOLLARS)
-                   "B25070", # GROSS RENT AS A PERCENTAGE OF HOUSEHOLD INCOME IN THE PAST 12 MONTHS
-                   "B25068"  # BEDROOMS BY GROSS RENT
-                   )
+# remove a couple of 5 year tables tables that don't work with the census API
+five_year_census_tables <- five_year_census_tables[-c(14, 16)]
+
+# 1 year tables
+one_year_census_tables <- c(
+  "B25002", # OCCUPANCY STATUS
+  "B19083", # GINI INDEX OF INCOME INEQUALITY
+  "B25013", # TENURE BY EDUCATIONAL ATTAINMENT OF HOUSEHOLDER
+  "B17019", # POVERTY STATUS IN THE PAST 12 MONTHS OF FAMILIES BY HOUSEHOLD TYPE BY TENURE
+  "B22003", # RECEIPT OF FOOD STAMPS/SNAP IN THE PAST 12 MONTHS BY POVERTY STATUS IN THE PAST 12 MONTHS FOR HOUSEHOLDS
+  "B19013", # MEDIAN HOUSEHOLD INCOME IN THE PAST 12 MONTHS (IN 2019 INFLATION-ADJUSTED DOLLARS)
+  "B25070", # GROSS RENT AS A PERCENTAGE OF HOUSEHOLD INCOME IN THE PAST 12 MONTHS
+  "B25068"  # BEDROOMS BY GROSS RENT
+)
 
 # declare years we'd like to grab census data for
 census_years <- 2010:2019
-names(census_years) <- 2010:2019
 
-# a function to grab a given table for however many years user would like and stack them into one dataframe
-acs_retrieve <- function(table, years) {
+# declare geographies we'd like to query
+geographies <- c(
+  #"block",
+  #"block group",
+  #"congressional_district",
+  #"county",
+  #"county_subdivision",
+  #"puma",
+  #"school_district_elementary",
+  #"school_district_secondary",
+  #"school_district_unified",
+  #"state_representative",
+  #"state_senate",
+  "tract"
+)
 
-  print(table)
+# generate a combination of all years, geographies, and tables
+all_combos <- expand.grid(geography = geographies,
+                          year = census_years,
+                          table = c(one_year_census_tables, five_year_census_tables),
+                          stringsAsFactors = FALSE) %>%
 
-  purrr::map_dfr(
-    years,
-    ~ tidycensus::get_acs(
-      geography = "tract",
-      table = table,
-      output = "wide",
-      state = "IL",
-      county = "Cook",
-      year = .x
-    ),
-    .id = "year"  # when combining results, add id var (name of list item)
-  ) %>%
+  # note which tables are acs1 vs acs5 for API
+  mutate(survey = case_when(table %in% one_year_census_tables ~ "acs1",
+                            table %in% five_year_census_tables ~ "acs5")) %>%
 
-    # Drop margin of error columns and suffix on estimate columns
-    dplyr::select(-ends_with("M", ignore.case = FALSE), -contains("NAME")) %>%
-    dplyr::rename_with(~ str_sub(.x, 1, -2), .cols = ends_with("E", ignore.case = FALSE))
+  # rearrange
+  select(survey, geography, year, table) %>%
+
+  # temporary
+  filter(survey == "acs5")
+
+# loop through all the combos and write the data to parquet files
+for (i in 1:nrow(all_combos)) {
+
+  # skip a file if it already exists
+  if (!file.exists(
+    here(paste0("census/raw/",
+                paste0(all_combos[i, ], collapse = "/"),
+                ".parquet")))) {
+
+    print(paste0("census/raw/", paste0(all_combos[i, ], collapse = "/"), ".parquet"))
+
+    write_parquet(
+
+      tidycensus::get_acs(
+        geography = all_combos$geography[i],
+        table = all_combos$table[i],
+        survey = all_combos$survey[i],
+        output = "wide",
+        state = "IL",
+        county = "Cook",
+        year = all_combos$year[i]
+      ) %>%
+
+        # Drop margin of error columns and suffix on estimate columns
+        dplyr::select(-ends_with("M", ignore.case = FALSE), -contains("NAME")) %>%
+        dplyr::rename_with(~ str_sub(.x, 1, -2), .cols = ends_with("E", ignore.case = FALSE)),
+
+      here(paste0("census/raw/",
+                  all_combos$survey[i], "/",
+                  all_combos$geography[i], "/",
+                  all_combos$year[i], "/",
+                  all_combos$table[i], ".parquet"))
+
+    )
+
+  }
 
 }
-
-# apply function to all desired tables
-acs <- list()
-acs <- lapply(census_tables, acs_retrieve, years = census_years)
-
-# merge tables together by geoid
-acs <- Reduce(function(x, y) merge(x, y, all = TRUE), acs)
-
-# output
-write_parquet(acs, here(paste0("census/raw/acs_tables_", Sys.Date(), ".parquet")))
