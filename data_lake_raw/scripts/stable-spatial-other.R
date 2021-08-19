f6fd3ae1d0b2dca385070f31d1b3ec0519033d83
Dan Snow <dsnow@cookcountyassessor.com>
2021-08-19 14:11:40 -0500

    Add other spatial boundaries

---
+++ data_lake_raw/scripts/stable-spatial-other.R
@@ -0,0 +1,27 @@
+library(dplyr)
+library(here)
+library(purrr)
+library(sf)
+
+oth_path <- here("s3-bucket", "stable", "spatial", "other")
+cur_year <- format(Sys.Date(), "%Y")
+
+# CHICAGO COMMUNITY AREA
+st_read(paste0(
+  "https://data.cityofchicago.org/api/geospatial/",
+  "cauq-8yn6?method=export&format=GeoJSON"
+)) %>%
+  st_write(
+    here(oth_path, "community_area", paste0(cur_year, ".geojson")),
+    delete_dsn = TRUE
+  )
+
+# UNINCORPORATED AREA
+st_read(paste0(
+  "https://datacatalog.cookcountyil.gov/api/geospatial/",
+  "kbr6-dyec?method=export&format=GeoJSON"
+)) %>%
+  st_write(
+    here(oth_path, "unincorporated_area", "2014.geojson"),
+    delete_dsn = TRUE
+  )
