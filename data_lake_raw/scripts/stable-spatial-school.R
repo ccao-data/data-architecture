f0d63be7cfb0aada9216622c0e0231dd4cdb3838
sweatyhandshake <wridgeway@cookcountyassessor.com>
2021-08-25 09:36:30 -0500

    added cps attendance, differeniate between districts/attendance

--- data_lake_raw/scripts/stable-spatial-school.R
+++ data_lake_raw/scripts/stable-spatial-school.R
@@ -5,7 +5,7 @@ library(sf)
 
 school_path <- here("s3-bucket", "stable", "spatial", "school")
 
-# UNIT
+# DISTRICTS - UNIT
 st_read(paste0(
   "https://datacatalog.cookcountyil.gov/api/geospatial/",
   "594e-g5w3?method=export&format=GeoJSON"
@@ -15,12 +15,32 @@ st_read(paste0(
     delete_dsn = TRUE
   )
 
-# ELEMENTARY
+# DISTRICTS - ELEMENTARY
 st_read(paste0(
   "https://datacatalog.cookcountyil.gov/api/geospatial/",
   "an6r-bw5a?method=export&format=GeoJSON"
 )) %>%
   st_write(
     here(school_path, "school_district_elementary", "2016.geojson"),
+    delete_dsn = TRUE
+  )
+
+# CPS ATTENDANCE - ELEMENTARY
+st_read(paste0(
+  "https://data.cityofchicago.org/api/geospatial/",
+  "u959-tya7?method=export&format=GeoJSON"
+)) %>%
+  st_write(
+    here(school_path, "cps_attendance_elementary", "2017-2018.geojson"),
+    delete_dsn = TRUE
+  )
+
+# CPS ATTENDANCE - SECONDARY
+st_read(paste0(
+  "https://data.cityofchicago.org/api/geospatial/",
+  "y9da-bb2y?method=export&format=GeoJSON"
+)) %>%
+  st_write(
+    here(school_path, "cps_attendance_secondary", "2017-2018.geojson"),
     delete_dsn = TRUE
   )
