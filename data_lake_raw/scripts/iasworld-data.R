2a22148f4a13a2d0da757c78e25b956de2a1f7ae
Dan Snow <daniel.snow@cookcountyil.gov>
2021-09-03 10:04:28 -0500

    Update iasworld data script

--- data_lake_raw/scripts/iasworld-data.R
+++ data_lake_raw/scripts/iasworld-data.R
@@ -5,10 +5,10 @@ library(DBI)
 library(dplyr)
 library(here)
 library(glue)
-options(java.parameters = "-Xmx8000m")
+options(java.parameters = "-Xmx16G")
 
 parquet_compression_lib = "snappy"
-parquet_output_path <- here("s3-bucket/iasworld")
+parquet_output_path <- here("s3-bucket", "iasworld", "data")
 iasworld_conn_string <- Sys.getenv("DB_CONFIG_IASWORLD")
 iasworld_drvr_path <- Sys.getenv("DB_CONFIG_IASWORLD_DRV")
 
@@ -17,11 +17,13 @@ iasworld_drvr_path <- Sys.getenv("DB_CONFIG_IASWORLD_DRV")
 # https://www.oracle.com/database/technologies/appdev/jdbc-ucp-21-1-c-downloads.html
 iasworld_conn <- dbConnect(
   drv = JDBC(
-    wdriverClass = "oracle.jdbc.OracleDriver",
+    driverClass = "oracle.jdbc.OracleDriver",
     classPath = iasworld_drvr_path,
   ),
   url = iasworld_conn_string
 )
+
+print("Successfully connected to iasWorld")
 
 # Get all tables
 tables <- dbGetQuery(
@@ -30,19 +32,16 @@ tables <- dbGetQuery(
 ) %>%
   pull(TABLE_NAME)
 
+tables <- c(tables, "ASMT_ALL")
+
 # Get all tables
 for (table in tables) {
-  print(table)
+  print(glue("Pulling {table}"))
   query_result <- dbGetQuery(
     iasworld_conn,
     glue("SELECT * FROM IASWORLD.{table}")
   )
 
-  write_parquet(query_result, glue("s3-bucket/iasworld/{table}.parquet"))
+  print(glue("Writing {table} to parquet"))
+  write_parquet(query_result, glue(parquet_output_path, "/{table}.parquet"))
 }
-
-dbGetQuery(
-  IASWORLD,
-  "SELECT * FROM IASWORLD.ASMT_ALL"
-) %>%
-  write_parquet("s3-bucket/iasworld/ASMT_ALL.parquet")
