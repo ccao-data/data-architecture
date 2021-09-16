1c2a0670c6a0cccdadb84060905689ad88df5955
sweatyhandshake <wridgeway@cookcountyassessor.com>
2021-09-16 11:34:43 -0500

    backup plan for ASMT_HIST

---
+++ data_lake_raw/scripts/iasworld-data-asmt_hist.R
@@ -0,0 +1,66 @@
+library(RJDBC)
+library(glue)
+library(arrow)
+library(lubridate)
+library(here)
+
+# Set Rjava option to increase java mem size. Note that this is required for
+# large queries!
+options(java.parameters = "-Xmx8000m")
+options(scipen = 100)
+
+# Establish connection object, the drv parameter points to the locally stored
+# Oracle JDBC driver from step 2. You can get it here:
+# https://www.oracle.com/database/technologies/appdev/jdbc-ucp-21-1-c-downloads.html
+IASWORLD <- RJDBC::dbConnect(
+  drv = RJDBC::JDBC(
+    driverClass = "oracle.jdbc.OracleDriver",
+    classPath = Sys.getenv("DB_CONFIG_IASWORLD_DRV")
+  ),
+  url = Sys.getenv("DB_CONFIG_IASWORLD")
+)
+
+# set batch size: how many rows would you like to pull at a time?
+batch_size <- 1000000
+
+# generate row numbers for pulls according to batch size
+rows <- seq.int(
+  from = 0,
+  to = as.numeric(dbGetQuery(IASWORLD, "SELECT NUM_ROWS FROM ALL_TABLES WHERE TABLE_NAME = 'ASMT_HIST'")),
+  by = batch_size
+)
+
+# pull according to batch size
+for (i in rows) {
+
+  file_path <- glue("{here('s3-bucket/iasworld/data/ASMT_HIST')}/ASMT_HIST_{i / batch_size + 1}.parquet")
+
+  # skip already completed pulls
+  if (!file.exists(file_path)) {
+
+    print(
+      glue("pulling records {i + 1} through {i + batch_size}")
+    )
+
+    # record start time
+    start <- proc.time()[3]
+
+    # pull and write
+    write_parquet(
+      dbGetQuery(
+        IASWORLD,
+        glue("SELECT *  FROM IASWORLD.ASMT_HIST
+             OFFSET {i} ROWS
+             FETCH NEXT {batch_size} ROWS ONLY")
+      ),
+      file_path
+    )
+
+    # display pull/write time
+    print(
+      glue("pull/write time: {lubridate::seconds_to_period(proc.time()[3] - start)}")
+    )
+
+  }
+
+}
