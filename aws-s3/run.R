c87f8559a405db28fcc4ce31cd3c609dd70f07cb
Dan Snow <daniel.snow@cookcountyil.gov>
2021-11-05 15:17:00 -0500

    Add warehouse scripts to main run script

--- aws-s3/run.R
+++ aws-s3/run.R
@@ -1,8 +1,17 @@
 library(here)
 
-# Run all scripts
+# Run all raw bucket scripts
 lapply(
   list.files(here("scripts-ccao-data-raw-us-east-1"), full.names = TRUE),
+  function(x) {
+    print(paste("Now running:", basename(x)))
+    source(x)
+  }
+)
+
+# Run all warehouse bucket scripts
+lapply(
+  list.files(here("scripts-ccao-data-warehouse-us-east-1"), full.names = TRUE),
   function(x) {
     print(paste("Now running:", basename(x)))
     source(x)
