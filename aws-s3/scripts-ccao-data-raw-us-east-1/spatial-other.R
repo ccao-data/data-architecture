c7228380f93a6e46ad220b8c33f8b2296137fb2a
sweatyhandshake <wridgeway@cookcountyassessor.com>
2021-11-16 14:08:59 -0600

    moving to correct script

--- aws-s3/scripts-ccao-data-raw-us-east-1/spatial-other.R
+++ aws-s3/scripts-ccao-data-raw-us-east-1/spatial-other.R
@@ -58,3 +58,47 @@ if (!aws.s3::object_exists(remote_file_industrial_growth_zone)) {
   aws.s3::put_object(tmp_file_industrial_growth_zone, remote_file_industrial_growth_zone)
   file.remove(tmp_file_industrial_growth_zone)
 }
+
+# BUILDING FOOTPRINTS
+remote_file_footprints <- file.path(
+  AWS_S3_RAW_BUCKET, "spatial", "other",
+  "building_footprint", paste0(current_year, ".geojson"))
+
+if (!aws.s3::object_exists(remote_file_footprints)) {
+
+  temp_file <- tempfile(fileext = ".geojson")
+
+  # Gather building footprints from OSM
+  footprints <- opq("Cook County United States") %>%
+    add_osm_feature(key = "building") %>%
+    osmdata_sf()
+
+  # Only need polygons
+  footprints <- footprints$osm_polygons
+
+  # Transform
+  footprints %>%
+    st_transform(crs = 3435) %>%
+    st_make_valid() %>%
+
+    # Intersect with Cook County border
+    st_intersection(
+      st_transform(
+        st_read(
+          "https://opendata.arcgis.com/datasets/ea127f9e96b74677892722069c984198_1.geojson"
+        ),
+        3435
+      )
+    ) %>%
+    pull(geometry) %>%
+
+    # Upload to AWS
+    st_write(temp_file)
+
+  aws.s3::put_object(temp_file, remote_file_footprints, multipart = TRUE)
+  file.remove(temp_file)
+
+}
+
+# Cleanup
+rm(list = ls())
