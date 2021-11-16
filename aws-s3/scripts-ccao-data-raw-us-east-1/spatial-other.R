9956990afcb06f7e1fc242e41b5c5f8c85469ed7
sweatyhandshake <wridgeway@cookcountyassessor.com>
2021-11-16 17:00:59 -0600

    added QOZs

--- aws-s3/scripts-ccao-data-raw-us-east-1/spatial-other.R
+++ aws-s3/scripts-ccao-data-raw-us-east-1/spatial-other.R
@@ -100,5 +100,38 @@ if (!aws.s3::object_exists(remote_file_footprints)) {
 
 }
 
+# QUALIFIED OPPORTUNITY ZONE
+remote_file_qualified_opportuniy_zone <- file.path(
+  AWS_S3_RAW_BUCKET, "spatial", "other", "qualified_opportuniy_zone",
+  paste0("2019", ".geojson")
+)
+
+# Write file to S3 if it doesn't already exist
+if (!aws.s3::object_exists(remote_file_qualified_opportuniy_zone)) {
+
+  tmp1 <- tempfile(fileext = ".zip")
+  tmp2 <- tempfile()
+  tmp3 <- tempfile(fileext = ".geojson")
+
+  # Grab zipped shapefile
+  download.file(
+    "https://www.cdfifund.gov/sites/cdfi/files/documents/opportunity-zones=8764.-9-10-2019.zip",
+    destfile = tmp1
+  )
+
+  # Unzip shapefile
+  unzip(tmp1, exdir = tmp2, overwrite = T)
+
+  # Only keep Cook County, save as geojson
+  st_read(grep("shp", list.files(tmp2, recursive = TRUE, full.names = TRUE), value = T)) %>%
+    filter(STATENAME == 'Illinois' & COUNTYNAME == 'Cook') %>%
+    st_write(tmp3)
+
+  # Upload, clean
+  aws.s3::put_object(tmp3, remote_file_qualified_opportuniy_zone)
+  file.remove(c(tmp1, tmp3, list.files(tmp2, full.names = TRUE)))
+
+}
+
 # Cleanup
 rm(list = ls())
