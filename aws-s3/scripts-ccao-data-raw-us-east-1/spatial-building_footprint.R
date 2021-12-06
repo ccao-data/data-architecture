314f0d17235b6742c9401bf2630014083e925c38
Dan Snow <daniel.snow@cookcountyil.gov>
2021-12-05 22:49:28 -0600

    Add all raw building footprints

--- aws-s3/scripts-ccao-data-raw-us-east-1/spatial-building_footprint.R
+++ aws-s3/scripts-ccao-data-raw-us-east-1/spatial-building_footprint.R
@@ -6,6 +6,7 @@ library(sf)
 # Script to gather various sources of building footprint data
 AWS_S3_RAW_BUCKET <- Sys.getenv("AWS_S3_RAW_BUCKET")
 current_year <- strftime(Sys.Date(), "%Y")
+current_date <- Sys.Date()
 
 
 # OSM BUILDING FOOTPRINTS
@@ -15,7 +16,7 @@ remote_file_osm <- file.path(
 
 if (!aws.s3::object_exists(remote_file_osm)) {
 
-  temp_file <- tempfile(fileext = ".geojson")
+  temp_file_osm <- tempfile(fileext = ".geojson")
 
   # Gather building footprints from OSM
   footprints <- opq("Cook County United States") %>%
@@ -25,26 +26,75 @@ if (!aws.s3::object_exists(remote_file_osm)) {
   # Only need polygons
   footprints <- footprints$osm_polygons
 
-  # Transform
+  # Filter invalid geoms
   footprints %>%
-    st_transform(crs = 3435) %>%
-    st_make_valid() %>%
-
-    # Intersect with Cook County border
-    st_intersection(
-      st_transform(
-        st_read(
-          "https://opendata.arcgis.com/datasets/ea127f9e96b74677892722069c984198_1.geojson"
-        ),
-        3435
-      )
-    ) %>%
-    pull(geometry) %>%
+    filter(!st_is_empty(geometry), st_is_valid(geometry)) %>%
+    st_cast("MULTIPOLYGON") %>%
+    mutate(pull_date = current_date) %>%
+    select(osm_id, name, height, pull_date, geometry) %>%
 
     # Upload to AWS
-    st_write(temp_file)
+    st_write(temp_file_osm, delete_dsn = TRUE)
 
-  aws.s3::put_object(temp_file, remote_file_osm, multipart = TRUE)
-  file.remove(temp_file)
+  aws.s3::put_object(temp_file_osm, remote_file_osm, multipart = TRUE)
+  file.remove(temp_file_osm)
+}
 
+
+# ESRI / COOK COUNTY FOOTPRINTS
+api_info <- list(
+  "esri_sub" = c("source"   = "https://datacatalog.cookcountyil.gov/api/geospatial/",
+                 "api_url"  = "dh3h-25vu?method=export&format=GeoJSON",
+                 "boundary" = "esri",
+                 "area"     = "suburban_cook",
+                 "year"     = "2008"),
+
+  "esri_chi" = c("source"   = "https://datacatalog.cookcountyil.gov/api/geospatial/",
+                 "api_url"  = "7n8x-uj9u?method=export&format=GeoJSON",
+                 "boundary" = "esri",
+                 "area"     = "chicago",
+                 "year"     = "2008")
+)
+
+# Function to call referenced API, pull requested data, and write it to S3
+pull_and_write <- function(x) {
+
+  tmp_file <- tempfile(fileext = ".geojson")
+  remote_file <- file.path(
+    AWS_S3_RAW_BUCKET, "spatial", "building_footprint",
+    x["boundary"], paste0(x["area"], "-", x["year"], ".geojson")
+  )
+
+  if (!aws.s3::object_exists(remote_file)) {
+    download.file(paste0(x["source"], x["api_url"]), tmp_file)
+    aws.s3::put_object(tmp_file, remote_file)
+    file.remove(tmp_file)
+  }
+}
+
+# Apply function to "api_info"
+lapply(api_info, pull_and_write)
+
+
+# MICROSOFT FOOTPRINTS
+remote_file_microsoft <- file.path(
+  AWS_S3_RAW_BUCKET, "spatial", "building_footprint",
+  "microsoft", "2019.geojson")
+
+if (!aws.s3::object_exists(remote_file_microsoft)) {
+
+  temp_file_zip <- tempfile(fileext = ".zip")
+  ext_file <- file.path(tempdir(), "Illinois.geojson")
+
+  # Download compressed IL footprints file
+  download.file(
+    paste0(
+      "https://usbuildingdata.blob.core.windows.net",
+      "/usbuildings-v2/Illinois.geojson.zip"
+    ),
+    temp_file_zip
+  )
+  unzip(temp_file_zip, exdir = dirname(ext_file))
+  aws.s3::put_object(ext_file, remote_file_microsoft, multipart = TRUE)
+  file.remove(ext_file)
 }
