b112fb8cbcd198dfc88e3fa38592244e1ee401cc
sweatyhandshake <wridgeway@cookcountyassessor.com>
2021-12-09 16:56:33 -0600

    updated to properly bind rows after API pull

--- aws-s3/scripts-ccao-data-raw-us-east-1/school-great_schools.R
+++ aws-s3/scripts-ccao-data-raw-us-east-1/school-great_schools.R
@@ -42,13 +42,21 @@ gather_schools <- function(zipcode, api_key) {
 # Apply function to zip codes
 great_schools <- apply(cook_zips, 1, gather_schools, api_key = API_KEY)
 
+output <- list()
+
 # Condense data into dataframe, limit to Cook County FIPS
-great_schools <- bind_rows(great_schools) %>%
-  filter(fipscounty == "17031")
+for (i in 1:length(great_schools)) {
+
+  output[[i]] <- great_schools[[i]]$schools
+
+}
+
+output <- bind_rows(output) %>%
+  dplyr::filter(fipscounty == "17031")
 
 # Write data if it does not already exist
 if (!aws.s3::object_exists(remote_file)) {
 
-  write_parquet(great_schools, remote_file)
+  write_parquet(output, remote_file)
 
 }
