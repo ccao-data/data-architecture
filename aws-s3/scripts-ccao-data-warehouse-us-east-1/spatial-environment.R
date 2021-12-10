2feb1687bc7b2397644dbcac0e70f3ec08d079ef
sweatyhandshake <wridgeway@cookcountyassessor.com>
2021-12-10 08:12:41 -0600

    Warehouse ETL for fema floodplains

--- aws-s3/scripts-ccao-data-warehouse-us-east-1/spatial-environment.R
+++ aws-s3/scripts-ccao-data-warehouse-us-east-1/spatial-environment.R
@@ -12,6 +12,7 @@ library(tidyr)
 # boundaries and uploads them to S3 as parquet files
 AWS_S3_RAW_BUCKET <- Sys.getenv("AWS_S3_RAW_BUCKET")
 AWS_S3_WAREHOUSE_BUCKET <- Sys.getenv("AWS_S3_WAREHOUSE_BUCKET")
+current_year <- strftime(Sys.Date(), "%Y")
 
 ##### LAKE MICHICAN COASTLINE #####
 remote_file_coastline_raw <- file.path(
@@ -25,5 +26,34 @@ remote_file_coastline_warehouse <- file.path(
 tmp_file_coastline <- tempfile(fileext = ".geojson")
 
 if (!aws.s3::object_exists(remote_file_coastline_warehouse)) {
+
+}
+
+##### FEMA FLOODPLAINS #####
+flood_fema_raw <- file.path(
+  AWS_S3_RAW_BUCKET, "spatial", "environment", "flood_fema",
+  paste0(current_year, ".geojson")
+)
+
+flood_fema_warehouse <- file.path(
+  AWS_S3_WAREHOUSE_BUCKET, "spatial", "environment", "flood_fema",
+  paste0(current_year, ".parquet")
+)
+
+# Write FEMA floodplains to S3 if they don't exist
+if (aws.s3::object_exists(flood_fema_raw) & !aws.s3::object_exists(flood_fema_warehouse)) {
+
+  tmp_file <- tempfile(fileext = ".geojson")
+  aws.s3::save_object(flood_fema_raw, file = tmp_file)
+
+  # All we need to do is recode the special flood hazard area column from boolean to integer
+  # And remove superfluous columns
+  st_read(tmp_file) %>%
+    select(SFHA_TF, geometry) %>%
+    mutate(SFHA_TF = as.integer(as.logical(SFHA_TF))) %>%
+    rename("fema_special_flood_hazard_area" = "SFHA_TF") %>%
+    st_transform(3435) %>%
+    sfarrow::st_write_parquet(flood_fema_warehouse)
+  file.remove(tmp_file)
 
 }
