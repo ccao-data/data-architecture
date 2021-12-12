15ce293da39f844b2e83e2d9bc411337c8c67f2e
Dan Snow <daniel.snow@cookcountyil.gov>
2021-12-12 13:57:36 -0600

    Add num bus stops CTAS

---
+++ aws-s3/scripts-ccao-data-warehouse-us-east-1/proximity-cnt_pin_num_bus_stop.sql
@@ -0,0 +1,55 @@
+-- CTAS to create a table counting the number of bus stops within a half mile
+-- of each PIN
+CREATE TABLE IF NOT EXISTS proximity.cnt_pin_num_bus_stop
+WITH (
+    format='Parquet',
+    write_compression = 'SNAPPY',
+    external_location='s3://ccao-data-warehouse-us-east-1/proximity/cnt_pin_num_bus_stop',
+    partitioned_by = ARRAY['year'],
+    bucketed_by = ARRAY['pin10'],
+    bucket_count = 5
+) AS (
+    WITH pin_locations AS (
+        SELECT DISTINCT x_3435, y_3435
+        FROM spatial.parcel
+        WHERE year >= (SELECT MIN(year) FROM spatial.transit_stop)
+    ),
+    stop_locations AS (
+        SELECT *
+        FROM spatial.transit_stop
+        WHERE route_type = 3
+    ),
+    stop_buffers AS (
+        SELECT
+            p.x_3435,
+            p.y_3435,
+            o.year
+        FROM pin_locations p
+        INNER JOIN stop_locations o
+            ON ST_Contains(
+                ST_Buffer(ST_GeomFromBinary(o.geometry_3435), 2640),
+                ST_Point(p.x_3435, p.y_3435)
+            )
+    ),
+    xy_stop_counts AS (
+        SELECT
+            x_3435,
+            y_3435,
+            year,
+            COUNT(*) AS num_stops_in_half_mile
+        FROM stop_buffers
+        GROUP BY x_3435, y_3435, year
+    )
+    SELECT
+        p.pin10,
+        CASE
+            WHEN xy.num_stops_in_half_mile IS NULL THEN 0
+            ELSE xy.num_stops_in_half_mile END AS num_stops_in_half_mile,
+        p.year
+    FROM spatial.parcel p
+    INNER JOIN xy_stop_counts xy
+        ON p.x_3435 = xy.x_3435
+        AND p.y_3435 = xy.y_3435
+        AND p.year = xy.year
+    WHERE p.year >= (SELECT MIN(year) FROM spatial.transit_stop)
+)
