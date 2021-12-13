586caccfe565df949ab957cdb6f1ac70e2cba6b1
Dan Snow <daniel.snow@cookcountyil.gov>
2021-12-13 11:35:14 -0600

    Add pagesubref to subdivisions

--- aws-s3/scripts-ccao-data-warehouse-us-east-1/spatial-other.R
+++ aws-s3/scripts-ccao-data-warehouse-us-east-1/spatial-other.R
@@ -10,7 +10,8 @@ unincorporated_area_raw <- grep(
   ".geojson",
   file.path(
     AWS_S3_RAW_BUCKET,
-    aws.s3::get_bucket_df(AWS_S3_RAW_BUCKET, prefix = 'spatial/other/unincorporated_area/')$Key
+    aws.s3::get_bucket_df(
+      AWS_S3_RAW_BUCKET, prefix = 'spatial/other/unincorporated_area/')$Key
   ),
   value = TRUE
 )
@@ -25,29 +26,32 @@ clean_unincorporated_area <- function(shapefile_path) {
   st_read(tmp_file) %>%
     filter(st_is_valid(.)) %>%
     select(agencydesc, zonenotes, zoneid, zonedesc, zoneordina, geometry) %>%
-    rename(zoneordinance = zoneordina) %>%
+    rename(zoneordinance = zoneordina, agency_desc = agencydesc) %>%
     rename_with(~ gsub("zone", "zone_", .x)) %>%
-    mutate(geometry_3435 = st_transform(geometry, 3435)) %>%
+    mutate(
+      zone_notes = na_if(zone_notes, ""),
+      geometry_3435 = st_transform(geometry, 3435)
+    ) %>%
     sfarrow::st_write_parquet(
       file.path(
         AWS_S3_WAREHOUSE_BUCKET, "spatial", "other", "unincorporated_area",
         gsub("geojson", "parquet", basename(shapefile_path))
       )
     )
-
 }
 
 # Apply function
 lapply(unincorporated_area_raw, clean_unincorporated_area)
 
-# SUBDIVISIONS
 
+# SUBDIVISIONS
 # Gather paths for subdivision shapefiles
 subdivisions_raw <- grep(
   ".geojson",
   file.path(
     AWS_S3_RAW_BUCKET,
-    aws.s3::get_bucket_df(AWS_S3_RAW_BUCKET, prefix = 'spatial/other/subdivision/')$Key
+    aws.s3::get_bucket_df(
+      AWS_S3_RAW_BUCKET, prefix = 'spatial/other/subdivision/')$Key
   ),
   value = TRUE
 )
@@ -60,16 +64,15 @@ clean_subdivisions <- function(shapefile_path) {
 
   # All we need is geometry column for this data, the other columns aren't useful
   st_read(tmp_file) %>%
-    filter(st_is_valid(.)) %>%
-    select(geometry) %>%
+    filter(st_is_valid(geometry) & !is.na(PAGE_SUBRE)) %>%
     mutate(geometry_3435 = st_transform(geometry, 3435)) %>%
+    select(pagesubref = PAGE_SUBRE, geometry, geometry_3435) %>%
     sfarrow::st_write_parquet(
       file.path(
         AWS_S3_WAREHOUSE_BUCKET, "spatial", "other", "subdivision",
         gsub("geojson", "parquet", basename(shapefile_path))
       )
     )
-
 }
 
 # Apply function
