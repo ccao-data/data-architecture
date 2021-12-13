11c74a387e0d4c55e339cb196548bb140387da5f
sweatyhandshake <wridgeway@cookcountyassessor.com>
2021-12-13 08:09:48 -0600

    ETL for unincorporated areas

--- aws-s3/scripts-ccao-data-warehouse-us-east-1/spatial-other.R
+++ aws-s3/scripts-ccao-data-warehouse-us-east-1/spatial-other.R
@@ -6,10 +6,36 @@ AWS_S3_RAW_BUCKET <- Sys.getenv("AWS_S3_RAW_BUCKET")
 AWS_S3_WAREHOUSE_BUCKET <- Sys.getenv("AWS_S3_WAREHOUSE_BUCKET")
 
 # UNINCORPORATED AREA
-remote_file_unincorporated_area <- file.path(
-  AWS_S3_RAW_BUCKET, "spatial", "other", "unincorporated_area",
-  paste0("2014", ".zip")
+unincorporated_area_raw <- grep(
+  ".geojson",
+  file.path(
+    AWS_S3_RAW_BUCKET,
+    aws.s3::get_bucket_df(AWS_S3_RAW_BUCKET, prefix = 'spatial/other/unincorporated_area/')$Key
+  ),
+  value = TRUE
 )
+
+# Function to extract and transform geometry from shapefiles
+clean_unincorporated_area <- function(shapefile_path) {
+
+  tmp_file <- tempfile(fileext = ".geojson")
+  aws.s3::save_object(shapefile_path, file = tmp_file)
+
+  # Just need to clean up column names and transform
+  st_read(tmp_file) %>%
+    filter(st_is_valid(.)) %>%
+    select(agencydesc, zonenotes, zoneid, zonedesc, zoneordina, geometry) %>%
+    rename(zoneordinance = zoneordina) %>%
+    rename_with(~ gsub("zone", "zone_", .x)) %>%
+    st_transform(3435) %>%
+    sfarrow::st_write_parquet(
+      file.path(
+        AWS_S3_WAREHOUSE_BUCKET, "spatial", "other", "unincorporated_area",
+        gsub("geojson", "parquet", basename(shapefile_path))
+      )
+    )
+
+}
 
 # SUBDIVISIONS
 
