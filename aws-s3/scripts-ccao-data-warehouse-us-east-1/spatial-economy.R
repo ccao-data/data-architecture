d692105ef82b39e717396d34effb63843b63f88c
sweatyhandshake <wridgeway@cookcountyassessor.com>
2021-12-28 08:30:08 -0600

    Consolidated care complete

--- aws-s3/scripts-ccao-data-warehouse-us-east-1/spatial-economy.R
+++ aws-s3/scripts-ccao-data-warehouse-us-east-1/spatial-economy.R
@@ -23,4 +23,53 @@ raw_files <- grep(
   value = TRUE
 )
 
+# Function to clean consolidated care districts
+clean_consolidated_care <- function(shapefile, economic_unit) {
+
+  if (economic_unit == "consolidated_care") {
+
+    return(
+
+      shapefile %>%
+        st_transform(4326) %>%
+        st_make_valid() %>%
+        mutate(AGENCY_DES = str_replace(AGENCY_DES, "PROVISIO", "PROVISO")) %>%
+        group_by(AGENCY_DES, MUNICIPALI) %>%
+        summarise() %>%
+        mutate(geometry_3435 = st_transform(geometry, 3435),
+               centroid = st_centroid(st_transform(geometry, 3435))) %>%
+        cbind(
+          st_coordinates(st_transform(.$centroid, 4326)),
+          st_coordinates(.$centroid)
+        ) %>%
+        group_by(AGENCY_DES) %>%
+        mutate(cc_num = str_pad(cur_group_id(), width = 3, side = "left", pad = "0"),
+               political_boundary = case_when(is.na(MUNICIPALI) ~ "Township",
+                                              TRUE ~ "Municipality"),
+               cc_name = str_squish(
+                 str_to_title(
+                   case_when(is.na(MUNICIPALI) ~ str_replace(AGENCY_DES, "TWP", ""),
+                             TRUE ~ MUNICIPALI)
+                   )
+                 )) %>%
+        select(cc_num, cc_name, political_boundary,
+               lon = X, lat = Y, x_3435 = `X.1`, y_3435 = `Y.1`, geometry, geometry_3435) %>%
+        ungroup()
+    )
+
+  } else {
+
+    return(shapefile)
+
+  }
+
+}
+
 # TODO: Finish economy script
+remote_file <- raw_files[1]
+tmp_file <- tempfile(fileext = ".geojson")
+aws.s3::save_object(remote_file, file = tmp_file)
+
+shapefile <- st_read(tmp_file)
+
+file.remove(tmp_file)
