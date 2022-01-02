d99f0d2b2bc429e1cfcf1384761fbeef106ccc43
Dan Snow <daniel.snow@cookcountyil.gov>
2022-01-01 23:27:06 -0600

    Cleanup proximity view

--- aws-athena/views/default-vw_pin_proximity.sql
+++ aws-athena/views/default-vw_pin_proximity.sql
@@ -7,10 +7,10 @@ SELECT
     bus.num_stops_in_half_mile AS bus_num_stops_in_half_mile,
     sch.num_schools_in_half_mile AS sch_num_schools_in_half_mile,
     sch.avg_rating_in_half_mile AS sch_avg_rating_in_half_mile,
-    bike.gnis_code AS bike_trail_gnis_code,
+    CAST(CAST(bike.gnis_code AS bigint) AS varchar) AS bike_trail_gnis_code,
     bike.name AS bike_trail_name,
     bike.dist_ft AS bike_trail_dist_ft,
-    ceme.gnis_code AS cemetery_gnis_code,
+    CAST(CAST(ceme.gnis_code AS bigint) AS varchar) AS cemetery_gnis_code,
     ceme.name AS cemetery_name,
     ceme.dist_ft AS cemetery_dist_ft,
     ctar.route_id AS cta_route_id,
@@ -19,7 +19,7 @@ SELECT
     ctas.stop_id AS cta_stop_id,
     ctas.stop_name AS cta_stop_name,
     ctas.dist_ft AS cta_stop_dist_ft,
-    hosp.gnis_code AS hospital_gnis_code,
+    CAST(CAST(hosp.gnis_code AS bigint) AS varchar) AS hospital_gnis_code,
     hosp.name AS hospital_name,
     hosp.dist_ft AS hospital_dist_ft,
     lake.dist_ft AS lake_michigan_dist_ft,
@@ -36,7 +36,7 @@ SELECT
     park.osm_id AS park_osm_id,
     park.dist_ft AS park_dist_ft,
     rail.name AS railroad_name,
-    rail.unique_id AS railroad_unique_id,
+    CAST(CAST(rail.unique_id AS bigint) AS varchar) AS railroad_unique_id,
     rail.dist_ft AS railroad_dist_ft,
     water.name AS water_name,
     water.id AS water_id,
@@ -84,4 +84,4 @@ LEFT JOIN proximity.dist_pin_to_railroad rail
 LEFT JOIN proximity.dist_pin_to_water water
     ON pin.pin10 = water.pin10
     AND pin.year = water.year
-WHERE pin.year >= (SELECT MIN(year) FROM spatial.transit_stop);
+WHERE pin.year >= '2013';
