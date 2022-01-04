3d543d1f14f5b91091bc724b6e6e1dd24d4dd681
Dan Snow <daniel.snow@cookcountyil.gov>
2022-01-04 14:37:21 -0600

    Update views to use fill forward CTAS

--- aws-athena/views/default-vw_pin_proximity.sql
+++ aws-athena/views/default-vw_pin_proximity.sql
@@ -4,90 +4,104 @@ CREATE OR REPLACE VIEW default.vw_pin_proximity AS
 SELECT
     pin.pin10,
     pin.year,
-    fc.num_pins_in_half_mile AS total_num_pins_in_half_mile,
-    bus.num_stops_in_half_mile AS bus_num_stops_in_half_mile,
-    sch.num_schools_in_half_mile AS sch_num_schools_in_half_mile,
-    sch.avg_rating_in_half_mile AS sch_avg_rating_in_half_mile,
-    fc.num_foreclosures_in_half_mile_past_5_years AS fc_num_foreclosures_in_half_mile_past_5_years,
-    fc.num_fc_per_1000_props_past_5_years AS fc_num_foreclosures_per_1000_props_past_5_years,
-    bike.gnis_code AS bike_trail_gnis_code,
-    bike.name AS bike_trail_name,
-    bike.dist_ft AS bike_trail_dist_ft,
-    CAST(CAST(ceme.gnis_code AS bigint) AS varchar) AS cemetery_gnis_code,
-    ceme.name AS cemetery_name,
-    ceme.dist_ft AS cemetery_dist_ft,
-    ctar.route_id AS cta_route_id,
-    ctar.route_long_name AS cta_route_name,
-    ctar.dist_ft AS cta_route_dist_ft,
-    ctas.stop_id AS cta_stop_id,
-    ctas.stop_name AS cta_stop_name,
-    ctas.dist_ft AS cta_stop_dist_ft,
-    CAST(CAST(hosp.gnis_code AS bigint) AS varchar) AS hospital_gnis_code,
-    hosp.name AS hospital_name,
-    hosp.dist_ft AS hospital_dist_ft,
-    lake.dist_ft AS lake_michigan_dist_ft,
-    road.osm_id AS road_osm_id,
-    road.name AS road_name,
-    road.dist_ft AS road_dist_ft,
-    metrar.route_id AS metra_route_id,
-    metrar.route_long_name AS metra_route_name,
-    metrar.dist_ft AS metra_route_dist_ft,
-    metras.stop_id AS metra_stop_id,
-    metras.stop_name AS metra_stop_name,
-    metras.dist_ft AS metra_stop_dist_ft,
-    park.name AS park_name,
-    park.osm_id AS park_osm_id,
-    park.dist_ft AS park_dist_ft,
-    rail.name AS railroad_name,
-    CAST(CAST(rail.unique_id AS bigint) AS varchar) AS railroad_unique_id,
-    rail.dist_ft AS railroad_dist_ft,
-    water.name AS water_name,
-    water.id AS water_id,
-    water.dist_ft AS water_dist_ft
+    num_pins_in_half_mile,
+    num_bus_stops_in_half_mile,
+    num_bus_stops_data_year,
+    num_foreclosures_in_half_mile_past_5_years,
+    num_foreclosures_per_1000_props_past_5_years,
+    num_foreclosures_data_year,
+    num_schools_in_half_mile,
+    avg_rating_in_half_mile,
+    num_schools_data_year,
+    nearest_bike_trail_id,
+    nearest_bike_trail_name,
+    nearest_bike_trail_dist_ft,
+    nearest_bike_trail_data_year,
+    nearest_cemetery_gnis_code,
+    nearest_cemetery_name,
+    nearest_cemetery_dist_ft,
+    nearest_cemetery_data_year,
+    nearest_cta_route_id,
+    nearest_cta_route_name,
+    nearest_cta_route_dist_ft,
+    nearest_cta_route_data_year,
+    nearest_cta_stop_id,
+    nearest_cta_stop_name,
+    nearest_cta_stop_dist_ft,
+    nearest_cta_stop_data_year,
+    nearest_hospital_gnis_code,
+    nearest_hospital_name,
+    nearest_hospital_dist_ft,
+    nearest_hospital_data_year,
+    lake_michigan_dist_ft,
+    lake_michigan_data_year,
+    nearest_major_road_osm_id,
+    nearest_major_road_name,
+    nearest_major_road_dist_ft,
+    nearest_major_road_data_year,
+    nearest_metra_route_id,
+    nearest_metra_route_name,
+    nearest_metra_route_dist_ft,
+    nearest_metra_route_data_year,
+    nearest_metra_stop_id,
+    nearest_metra_stop_name,
+    nearest_metra_stop_dist_ft,
+    nearest_metra_stop_data_year,
+    nearest_park_osm_id,
+    nearest_park_name,
+    nearest_park_dist_ft,
+    nearest_park_data_year,
+    nearest_railroad_id,
+    nearest_railroad_name,
+    nearest_railroad_dist_ft,
+    nearest_railroad_data_year,
+    nearest_water_id,
+    nearest_water_name,
+    nearest_water_dist_ft,
+    nearest_water_data_year
 FROM spatial.parcel pin
-LEFT JOIN proximity.cnt_pin_num_bus_stop bus
-    ON pin.pin10 = bus.pin10
-    AND pin.year = bus.year
-LEFT JOIN proximity.cnt_pin_num_foreclosure fc
-    ON pin.pin10 = fc.pin10
-    AND pin.year = fc.year
-LEFT JOIN proximity.cnt_pin_num_school sch
-    ON pin.pin10 = sch.pin10
-    AND pin.year = sch.year
-LEFT JOIN proximity.dist_pin_to_bike_trail bike
-    ON pin.pin10 = bike.pin10
-    AND pin.year = bike.year
-LEFT JOIN proximity.dist_pin_to_cemetery ceme
-    ON pin.pin10 = ceme.pin10
-    AND pin.year = ceme.year
-LEFT JOIN proximity.dist_pin_to_cta_route ctar
-    ON pin.pin10 = ctar.pin10
-    AND pin.year = ctar.year
-LEFT JOIN proximity.dist_pin_to_cta_stop ctas
-    ON pin.pin10 = ctas.pin10
-    AND pin.year = ctas.year
-LEFT JOIN proximity.dist_pin_to_hospital hosp
-    ON pin.pin10 = hosp.pin10
-    AND pin.year = hosp.year
-LEFT JOIN proximity.dist_pin_to_lake_michigan lake
-    ON pin.pin10 = lake.pin10
-    AND pin.year = lake.year
-LEFT JOIN proximity.dist_pin_to_major_road road
-    ON pin.pin10 = road.pin10
-    AND pin.year = road.year
-LEFT JOIN proximity.dist_pin_to_metra_route metrar
-    ON pin.pin10 = metrar.pin10
-    AND pin.year = metrar.year
-LEFT JOIN proximity.dist_pin_to_metra_stop metras
-    ON pin.pin10 = metras.pin10
-    AND pin.year = metras.year
-LEFT JOIN proximity.dist_pin_to_park park
-    ON pin.pin10 = park.pin10
-    AND pin.year = park.year
-LEFT JOIN proximity.dist_pin_to_railroad rail
-    ON pin.pin10 = rail.pin10
-    AND pin.year = rail.year
-LEFT JOIN proximity.dist_pin_to_water water
-    ON pin.pin10 = water.pin10
-    AND pin.year = water.year
-WHERE pin.year >= '2013';
+LEFT JOIN proximity.cnt_pin_num_bus_stop
+    ON pin.pin10 = cnt_pin_num_bus_stop.pin10
+    AND pin.year = cnt_pin_num_bus_stop.year
+LEFT JOIN proximity.cnt_pin_num_foreclosure
+    ON pin.pin10 = cnt_pin_num_foreclosure.pin10
+    AND pin.year = cnt_pin_num_foreclosure.year
+LEFT JOIN proximity.cnt_pin_num_school
+    ON pin.pin10 = cnt_pin_num_school.pin10
+    AND pin.year = cnt_pin_num_school.year
+LEFT JOIN proximity.dist_pin_to_bike_trail
+    ON pin.pin10 = dist_pin_to_bike_trail.pin10
+    AND pin.year = dist_pin_to_bike_trail.year
+LEFT JOIN proximity.dist_pin_to_cemetery
+    ON pin.pin10 = dist_pin_to_cemetery.pin10
+    AND pin.year = dist_pin_to_cemetery.year
+LEFT JOIN proximity.dist_pin_to_cta_route
+    ON pin.pin10 = dist_pin_to_cta_route.pin10
+    AND pin.year = dist_pin_to_cta_route.year
+LEFT JOIN proximity.dist_pin_to_cta_stop
+    ON pin.pin10 = dist_pin_to_cta_stop.pin10
+    AND pin.year = dist_pin_to_cta_stop.year
+LEFT JOIN proximity.dist_pin_to_hospital
+    ON pin.pin10 = dist_pin_to_hospital.pin10
+    AND pin.year = dist_pin_to_hospital.year
+LEFT JOIN proximity.dist_pin_to_lake_michigan
+    ON pin.pin10 = dist_pin_to_lake_michigan.pin10
+    AND pin.year = dist_pin_to_lake_michigan.year
+LEFT JOIN proximity.dist_pin_to_major_road
+    ON pin.pin10 = dist_pin_to_major_road.pin10
+    AND pin.year = dist_pin_to_major_road.year
+LEFT JOIN proximity.dist_pin_to_metra_route
+    ON pin.pin10 = dist_pin_to_metra_route.pin10
+    AND pin.year = dist_pin_to_metra_route.year
+LEFT JOIN proximity.dist_pin_to_metra_stop
+    ON pin.pin10 = dist_pin_to_metra_stop.pin10
+    AND pin.year = dist_pin_to_metra_stop.year
+LEFT JOIN proximity.dist_pin_to_park
+    ON pin.pin10 = dist_pin_to_park.pin10
+    AND pin.year = dist_pin_to_park.year
+LEFT JOIN proximity.dist_pin_to_railroad
+    ON pin.pin10 = dist_pin_to_railroad.pin10
+    AND pin.year = dist_pin_to_railroad.year
+LEFT JOIN proximity.dist_pin_to_water
+    ON pin.pin10 = dist_pin_to_water.pin10
+    AND pin.year = dist_pin_to_water.year
