ba48d7915f3d3c4354967f22a7a71b5a5123318c
sweatyhandshake <wridgeway@cookcountyassessor.com>
2022-01-10 16:09:14 -0600

    Add certified and bor bldg and land values

--- aws-athena/views/default-vw_pin_history.sql
+++ aws-athena/views/default-vw_pin_history.sql
@@ -1,57 +1,112 @@
  -- View containing current and prior years' assessments by PIN in wide format
 CREATE OR replace VIEW default.vw_pin_history
 AS
-  -- CCAO mailed, CCAO final, and BOR final values for each PIN by year
+  -- CCAO mailed_tot, CCAO final, and BOR final values for each PIN by year
   WITH values_by_year
-       AS (SELECT parid,
-                  taxyr,
+       AS (SELECT asmt_all.parid,
+                  asmt_all.taxyr,
                   Max(CASE
-                        WHEN procname = 'CCAOVALUE' THEN valasm3
+                        WHEN asmt_all.procname = 'CCAOVALUE' THEN valasm3
                         ELSE NULL
-                      END) AS ccao_mailed,
+                      END) AS mailed_tot,
                   Max(CASE
-                        WHEN procname = 'CCAOFINAL' THEN valasm3
+                        WHEN asmt_all.procname = 'CCAOFINAL' THEN userval2
                         ELSE NULL
-                      END) AS ccao_final,
+                      END) AS certified_bldg,
                   Max(CASE
-                        WHEN procname = 'BORVALUE' THEN valasm3
+                        WHEN asmt_all.procname = 'CCAOFINAL' THEN userval1
                         ELSE NULL
-                      END) AS bor_final
+                      END) AS certified_land,
+                  Max(CASE
+                        WHEN asmt_all.procname = 'CCAOFINAL' THEN valasm3
+                        ELSE NULL
+                      END) AS certified_tot,
+                  Max(CASE
+                        WHEN asmt_all.procname = 'BORVALUE' THEN userval5
+                        ELSE NULL
+                      END) AS board_bldg,
+                  Max(CASE
+                        WHEN asmt_all.procname = 'BORVALUE' THEN userval4
+                        ELSE NULL
+                      END) AS board_lanD,
+                  Max(CASE
+                        WHEN asmt_all.procname = 'BORVALUE' THEN valasm3
+                        ELSE NULL
+                      END) AS board_tot
            FROM   iasworld.asmt_all
+                  left join iasworld.aprval
+                         ON aprval.parid = asmt_all.parid
+                            AND aprval.taxyr = asmt_all.taxyr
            -- Still working on 2020
-           WHERE  Cast(taxyr AS INT) < Year(current_date) - 2
-           GROUP  BY parid,
-                     taxyr
-           ORDER  BY parid,
-                     taxyr)
+           WHERE  Cast(asmt_all.taxyr AS INT) < Year(current_date) - 2
+           GROUP  BY asmt_all.parid,
+                     asmt_all.taxyr
+           ORDER  BY asmt_all.parid,
+                     asmt_all.taxyr)
   -- Add lagged values for previous two years
   SELECT parid                      AS pin,
          taxyr                      AS year,
-         ccao_mailed,
-         ccao_final,
-         bor_final,
-         Lag(ccao_mailed)
+         mailed_tot,
+         certified_bldg,
+         certified_land,
+         certified_tot,
+         board_bldg,
+         board_land,
+         board_tot,
+         Lag(mailed_tot)
            over(
              PARTITION BY parid
-             ORDER BY parid, taxyr) AS pri1_ccao_mailed,
-         Lag(ccao_final)
+             ORDER BY parid, taxyr) AS oneyr_pri_mailed_tot,
+         Lag(certified_bldg)
            over(
              PARTITION BY parid
-             ORDER BY parid, taxyr) AS pri1_ccao_final,
-         Lag(bor_final)
+             ORDER BY parid, taxyr) AS oneyr_pri_certified_bldg,
+         Lag(certified_land)
            over(
              PARTITION BY parid
-             ORDER BY parid, taxyr) AS pri1_bor_final,
-         Lag(ccao_mailed, 2)
+             ORDER BY parid, taxyr) AS oneyr_pri_certified_land,
+         Lag(certified_tot)
            over(
              PARTITION BY parid
-             ORDER BY parid, taxyr) AS pri2_ccao_mailed,
-         Lag(ccao_final, 2)
+             ORDER BY parid, taxyr) AS oneyr_pri_certified_tot,
+         Lag(board_bldg)
            over(
              PARTITION BY parid
-             ORDER BY parid, taxyr) AS pri2_ccao_final,
-         Lag(bor_final, 2)
+             ORDER BY parid, taxyr) AS oneyr_pri_board_bldg,
+         Lag(board_land)
            over(
              PARTITION BY parid
-             ORDER BY parid, taxyr) AS pri2_bor_final
+             ORDER BY parid, taxyr) AS oneyr_pri_board_land,
+         Lag(board_tot)
+           over(
+             PARTITION BY parid
+             ORDER BY parid, taxyr) AS oneyr_pri_board_tot,
+         Lag(mailed_tot, 2)
+           over(
+             PARTITION BY parid
+             ORDER BY parid, taxyr) AS twoyr_pri_mailed_tot,
+         Lag(certified_bldg, 2)
+           over(
+             PARTITION BY parid
+             ORDER BY parid, taxyr) AS twoyr_pri_certified_bldg,
+         Lag(certified_land, 2)
+           over(
+             PARTITION BY parid
+             ORDER BY parid, taxyr) AS twoyr_pri_certified_land,
+         Lag(certified_tot, 2)
+           over(
+             PARTITION BY parid
+             ORDER BY parid, taxyr) AS twoyr_pri_certified_tot,
+         Lag(board_bldg, 2)
+           over(
+             PARTITION BY parid
+             ORDER BY parid, taxyr) AS twoyr_pri_board_bldg,
+         Lag(board_land, 2)
+           over(
+             PARTITION BY parid
+             ORDER BY parid, taxyr) AS twoyr_pri_board_land,
+         Lag(board_tot, 2)
+           over(
+             PARTITION BY parid
+             ORDER BY parid, taxyr) AS twoyr_pri_board_tot
   FROM   values_by_year
