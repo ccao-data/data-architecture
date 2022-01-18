99a7b4a008c9e2900088cf8e107dde923e441642
sweatyhandshake <wridgeway@cookcountyassessor.com>
2022-01-18 14:13:16 -0600

    rework of sales view. functions much better using "deactivat" column.

--- aws-athena/views/default-vw_pin_sale.sql
+++ aws-athena/views/default-vw_pin_sale.sql
@@ -1,133 +1,111 @@
--- View containing unique, filtered sales
-CREATE OR REPLACE VIEW default.vw_pin_sale AS
--- Township and class of associated PIN
-WITH townclass AS (
-    SELECT DISTINCT
-        parid,
-        class,
-        SUBSTR(nbhd, 1, 2) AS township_code,
-        taxyr
-    FROM iasworld.pardat
-),
--- Indicator to show which sales only have one observation by PIN and sale date
-singletons AS (
-    SELECT
-        parid,
-        saledt,
-        COUNT(*) AS obs
-    FROM iasworld.sales
-    GROUP BY parid, saledt HAVING COUNT(*) = 1
-),
--- Indicator for whether a given transaction number is the first associated with a sale
-drop_extra_transno AS (
-    SELECT
-        parid,
-        price,
-        transno,
-        ROW_NUMBER() OVER(
-            PARTITION BY parid, saledt, price
-            ORDER BY parid, saledt, price
-        ) AS linenum
-    FROM iasworld.sales
-    WHERE transno IS NOT NULL
-),
--- Only keep highest sale price when multiple exist for one pin on a given sale date
-highest_sp AS (
-    SELECT
-        parid,
-        saledt,
-        MAX(price) AS max_price
-    FROM iasworld.sales
-    GROUP BY parid, saledt
-),
--- Remove transaction numbers that show up multiple times
-dedupe_transno AS (
-    SELECT
-        transno,
-        COUNT(transno) AS duplicate_transnos
-    FROM iasworld.sales
-    WHERE transno IS NOT NULL
-    GROUP BY transno
-),
--- 2021 doesn't have transaction numbers
-dedupe_2021 AS (
-    SELECT
-        saledt, price,
-        COUNT(*) AS dupes2021
-    FROM iasworld.sales
-    WHERE SUBSTR(sales.saledt, 1, 4) = '2021'
-    GROUP BY saledt, price
-),
-unique_sales AS (
-    SELECT DISTINCT
-        sales.parid AS pin,
-        SUBSTR(sales.saledt, 1, 4) AS year,
-        townclass.township_code,
-        townclass.class,
-        date_parse(SUBSTR(sales.saledt, 1, 10), '%Y-%m-%d') AS sale_date,
-        CAST(sales.price AS bigint) AS sale_price,
-        LOG(sales.price, 10) AS sale_price_log10,
-        sales.salekey AS sale_key,
-        NULLIF(sales.transno, '') AS doc_no,
-        NULLIF(sales.instrtyp, '') AS deed_type,
-        NULLIF(sales.oldown, '') AS seller_name,
-        NULLIF(sales.own1, '') AS buyer_name,
-        CASE
-        	WHEN sales.saletype = '0' THEN 'LAND'
-        	WHEN sales.saletype = '1' THEN 'LAND AND BUILDING'
-        END AS sale_type
-    FROM iasworld.sales
-    LEFT JOIN townclass
-        ON sales.parid = townclass.parid
-        AND SUBSTR(sales.saledt, 1, 4) = townclass.taxyr
-    LEFT JOIN singletons
-        ON sales.parid = singletons.parid
-        AND sales.saledt = singletons.saledt
-    LEFT JOIN drop_extra_transno
-        ON sales.parid = drop_extra_transno.parid
-        AND sales.price = drop_extra_transno.price
-        AND sales.transno = drop_extra_transno.transno
-    INNER JOIN highest_sp
-        ON sales.parid = highest_sp.parid
-        AND sales.saledt = highest_sp.saledt
-        AND sales.price = highest_sp.max_price
-    LEFT JOIN dedupe_transno
-        ON sales.transno = dedupe_transno.transno
-    LEFT JOIN dedupe_2021
-        ON sales.price = dedupe_2021.price
-        AND sales.saledt = dedupe_2021.saledt
-    -- nopar is number of parcels sold
-    WHERE ((sales.transno IS NOT NULL
-            AND nopar = 1
-            AND linenum = 1)
-        OR obs = 1)
-    AND (duplicate_transnos = 1 OR dupes2021 = 1)
-    AND sales.price > 10000
-    AND sales.parid != '0'
-    AND CAST(SUBSTR(sales.saledt, 1, 4) AS int)
-        BETWEEN 1997 AND YEAR(current_date)
-    -- Exclude quit claims, executor deeds, beneficial interests
-    AND instrtyp NOT IN ('03', '04', '06')
-),
--- Join on lower and upper bounds so that outlier sales can be filtered out
-sale_filter AS (
-    SELECT
-        township_code,
-        class,
-        year,
-        AVG(sale_price_log10) - STDDEV(sale_price_log10) * 4 AS sale_filter_lower_limit,
-        AVG(sale_price_log10) + STDDEV(sale_price_log10) * 4 AS sale_filter_upper_limit,
-        COUNT(*) sale_filter_count
-    FROM unique_sales
-    GROUP BY township_code, class, year
-)
-SELECT
-    unique_sales.*,
-    sale_filter_lower_limit,
-    sale_filter_upper_limit,
-    sale_filter_count
-FROM unique_sales
-LEFT JOIN sale_filter
-    ON unique_sales.township_code = sale_filter.township_code
-    AND unique_sales.class = sale_filter.class
-    AND unique_sales.year = sale_filter.year
+ -- View containing unique, filtered sales
+CREATE OR replace VIEW default.vw_pin_sale
+AS
+  -- Township and class of associated PIN
+  WITH townclass
+       AS (SELECT DISTINCT parid,
+                           class,
+                           Substr(nbhd, 1, 2) AS township_code,
+                           taxyr
+           FROM   iasworld.pardat),
+       -- "nopar" isn't entirely accurate for sales associated with only one parcel, so we create our own counter
+       calculated
+       AS (SELECT instruno,
+                  Count(*) AS nopar_calculated
+           FROM   iasworld.sales
+           WHERE  deactivat IS NULL
+           GROUP  BY instruno),
+       unique_sales
+       AS (SELECT *
+           FROM   (SELECT DISTINCT sales.parid
+                                   AS
+                                           pin,
+                                   Substr(sales.saledt, 1, 4)
+                                   AS
+                                           year,
+                                   townclass.township_code,
+                                   townclass.class,
+                                   Date_parse(Substr(sales.saledt, 1, 10),
+                                   '%Y-%m-%d')
+                                   AS
+                                           sale_date,
+                                   Cast(sales.price AS BIGINT)
+                                   AS
+                                           sale_price,
+                                   Log(sales.price, 10)
+                                   AS
+                                           sale_price_log10,
+                                   sales.salekey
+                                   AS
+                                           sale_key,
+                                   Nullif(sales.instruno, '')
+                                   AS
+                                           doc_no,
+                                   Nullif(sales.instrtyp, '')
+                                   AS
+                                           deed_type,
+                                   Nullif(sales.oldown, '')
+                                   AS
+                                           seller_name,
+                                   Nullif(sales.own1, '')
+                                   AS
+                                           buyer_name,
+                                   CASE
+                                     WHEN sales.saletype = '0' THEN 'LAND'
+                                     WHEN sales.saletype = '1' THEN
+                                     'LAND AND BUILDING'
+                                   END
+                                   AS
+                                           sale_type,
+                                   -- Sales are not entirely unique by pin/date so we group all sales b pin/date
+                                   -- then order then order by descending price and give the top observation a value of 1 for "linenum"
+                                   Row_number()
+                                     over(
+                                       PARTITION BY sales.parid, sales.saledt
+                                       ORDER BY sales.parid, sales.saledt, -1 *
+                                     sales.price ) AS
+                                           linenum
+                   FROM   iasworld.sales
+                          left join calculated
+                                 ON sales.instruno = calculated.instruno
+                          left join townclass
+                                 ON sales.parid = townclass.parid
+                                    AND Substr(sales.saledt, 1, 4) =
+                                        townclass.taxyr
+                   WHERE  sales.instruno IS NOT NULL
+                          -- Indicates whether a record has been deactivated
+                          AND sales.deactivat IS NULL
+                          AND calculated.nopar_calculated = 1
+                          -- "nopar" is number of parcels sold
+                          AND sales.nopar <= 1
+                          AND sales.price > 10000
+                          AND Cast(Substr(sales.saledt, 1, 4) AS INT) BETWEEN
+                              1997 AND Year(current_date)
+                          -- Exclude quit claims, executor deeds, beneficial interests
+                          AND instrtyp NOT IN ( '03', '04', '06' ))
+           -- Only use max price by pin/sale date
+           WHERE  linenum = 1),
+       -- Lower and upper bounds so that outlier sales can be filtered out
+       sale_filter
+       AS (SELECT township_code,
+                  class,
+                  year,
+                  Avg(sale_price_log10) - STDDEV(sale_price_log10) * 4 AS
+                     sale_filter_lower_limit,
+                  Avg(sale_price_log10) + STDDEV(sale_price_log10) * 4 AS
+                     sale_filter_upper_limit,
+                  Count(*)
+                  sale_filter_count
+           FROM   unique_sales
+           GROUP  BY township_code,
+                     class,
+                     year)
+  SELECT unique_sales.*,
+         sale_filter_lower_limit,
+         sale_filter_upper_limit,
+         sale_filter_count
+  FROM   unique_sales
+         left join sale_filter
+                ON unique_sales.township_code = sale_filter.township_code
+                   AND unique_sales.class = sale_filter.class
+                   AND unique_sales.year = sale_filter.year
