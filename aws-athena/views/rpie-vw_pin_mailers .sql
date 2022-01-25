5892fb06056d0e891c420ecd97699f09256b4fd9
sweatyhandshake <wridgeway@cookcountyassessor.com>
2022-01-25 10:36:11 -0600

    Update column names, fix mailing address formatting, add convenience columns

--- aws-athena/views/rpie-vw_pin_mailers .sql
+++ aws-athena/views/rpie-vw_pin_mailers .sql
@@ -2,7 +2,7 @@
 CREATE OR replace VIEW rpie.vw_pin_mailers
 AS
   -- physical parcel addresses
-  WITH parcel_address
+  WITH parcel_addressess
        AS (SELECT parid                                   AS pin,
                   Cast(Cast(taxyr AS INT) + 1 AS VARCHAR) AS year,
                   Cast(adrno AS VARCHAR)
@@ -14,20 +14,38 @@ AS
                   || ' '
                   || adrstr
                   || ' '
-                  || adrsuf                               AS PAR_ADDR,
-                  unitno                                  AS PAR_UNIT,
-                  cityname                                AS PAR_CITY,
-                  'IL'                                    AS PAR_STATE,
+                  || adrsuf                               AS property_address,
+                  unitno                                  AS property_apt_no,
+                  cityname                                AS property_CITY,
+                  'IL'                                    AS property_STATE,
                   zip1
                   || CASE
                        WHEN zip2 IS NOT NULL
                             AND zip2 != '0000' THEN '-'
                                                     || zip2
                        ELSE ''
-                     END                                  AS PAR_ZIP
+                     END                                  AS property_ZIP,
+                  Substr(class, 1, 1)                     AS major_class,
+                  CASE
+                    WHEN Substr(nbhd, 1, 2) IN ( '70', '71', '72', '73',
+                                                 '74', '75', '76', '77' ) THEN
+                    'City'
+                    WHEN Substr(nbhd, 1, 2) IN ( '10', '16', '17', '18',
+                                                 '20', '22', '23', '24',
+                                                 '25', '26', '29', '35', '38' )
+                  THEN
+                    'North'
+                    WHEN Substr(nbhd, 1, 2) IN ( '11', '12', '13', '14',
+                                                 '15', '19', '21', '27',
+                                                 '28', '30', '31', '32',
+                                                 '33', '34', '36', '37', '39' )
+                  THEN
+                    'South'
+                    ELSE NULL
+                  END                                     AS tri
            FROM   iasworld.pardat),
-           -- parcel mailing addresses
-       owner_address
+       -- parcel mailing addresses
+       owner_addressess
        AS (SELECT parid                                   AS pin,
                   Cast(Cast(taxyr AS INT) + 1 AS VARCHAR) AS year,
                   own1
@@ -35,11 +53,11 @@ AS
                        WHEN own2 IS NOT NULL THEN ' '
                                                   || own2
                        ELSE ''
-                     END                                  AS OWN_NAME,
+                     END                                  AS mailing_NAME,
                   CASE
                     WHEN careof IS NOT NULL THEN careof
                     ELSE ''
-                  END                                     AS OWN_CAREOF,
+                  END                                     AS mailing_care_of,
                   CASE
                     WHEN addr1 IS NOT NULL THEN addr1
                                                 || ( CASE
@@ -53,11 +71,13 @@ AS
                                                            || ' '
                               ELSE ''
                             END
+                         || ' '
                          || adrstr
+                         || ' '
                          || adrsuf
-                  END                                     AS OWN_ADR,
-                  cityname                                AS own_city,
-                  statecode                               AS own_state,
+                  END                                     AS mailing_address,
+                  cityname                                AS mailing_city,
+                  statecode                               AS mailing_state,
                   CASE
                     WHEN zip1 IS NOT NULL
                          AND zip1 != '00000' THEN zip1
@@ -67,26 +87,37 @@ AS
                             AND zip2 != '0000' THEN '-'
                                                     || zip2
                        ELSE ''
-                     END                                  AS own_zip
+                     END                                  AS mailing_zip
            FROM   iasworld.owndat)
   SELECT pin_codes.pin,
-         pin_codes.year AS rpie_year,
+         Substr(pin_codes.pin, 1, 2)
+         || '-'
+         || Substr(pin_codes.pin, 3, 2)
+         || '-'
+         || Substr(pin_codes.pin, 5, 3)
+         || '-'
+         || Substr(pin_codes.pin, 8, 3)
+         || '-'
+         || Substr(pin_codes.pin, 11, 4) AS rpie_pin,
+         pin_codes.year                  AS rpie_year,
          pin_codes.rpie_code,
-         par_addr,
-         par_unit,
-         par_city,
-         par_state,
-         par_zip,
-         own_name,
-         own_careof,
-         own_adr,
-         own_city,
-         own_state,
-         own_zip
+         property_address,
+         property_apt_no,
+         property_city,
+         property_state,
+         property_zip,
+         mailing_name,
+         mailing_care_of,
+         mailing_address,
+         mailing_city,
+         mailing_state,
+         mailing_zip,
+         major_class,
+         tri
   FROM   rpie.pin_codes
-         left join parcel_address
-                ON pin_codes.pin = parcel_address.pin
-                   AND pin_codes.year = parcel_address.year
-         left join owner_address
-                ON pin_codes.pin = owner_address.pin
-                   AND pin_codes.year = owner_address.year
+         left join parcel_addressess
+                ON pin_codes.pin = parcel_addressess.pin
+                   AND pin_codes.year = parcel_addressess.year
+         left join owner_addressess
+                ON pin_codes.pin = owner_addressess.pin
+                   AND pin_codes.year = owner_addressess.year
