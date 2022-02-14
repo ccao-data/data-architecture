ab8e30e38ea6de3b574aac9fc1643caad10c4105
sweatyhandshake <wridgeway@cookcountyassessor.com>
2022-02-14 15:07:10 -0600

    Format, use CONCAT_WS

--- aws-athena/views/rpie-vw_pin_mailers.sql
+++ aws-athena/views/rpie-vw_pin_mailers.sql
@@ -2,92 +2,66 @@
 CREATE OR replace VIEW rpie.vw_pin_mailers
 AS
   -- physical parcel addresses
-  WITH parcel_addressess
-       AS (SELECT parid                                   AS pin,
-                  Cast(Cast(taxyr AS INT) + 1 AS VARCHAR) AS rpie_year,
-                  Cast(adrno AS VARCHAR)
-                  || CASE
-                       WHEN adrdir IS NOT NULL THEN ' '
-                                                    || adrdir
-                       ELSE ''
-                     END
-                  || ' '
-                  || adrstr
-                  || ' '
-                  || adrsuf                               AS property_address,
-                  unitno                                  AS property_apt_no,
-                  cityname                                AS property_CITY,
-                  'IL'                                    AS property_STATE,
-                  zip1
-                  || CASE
-                       WHEN zip2 IS NOT NULL
-                            AND zip2 != '0000' THEN '-'
-                                                    || zip2
-                       ELSE ''
-                     END                                  AS property_ZIP,
-                  Substr(class, 1, 1)                     AS major_class,
+  WITH parcel_addressess AS (
+    SELECT
+        parid AS pin,
+        Cast(Cast(taxyr AS INT) + 1 AS VARCHAR) AS rpie_year,
+        -- PIN mailing address from OWNDAT
+        NULLIF(CONCAT_WS(
+        ' ',
+        CAST(adrno AS varchar),
+        adrdir,
+        adrstr,
+        adrsuf), '') AS property_address,
+        unitno AS property_apt_no,
+        cityname AS property_city,
+        'IL' AS property_state,
+        CONCAT_WS(
+        '-',
+        NULLIF(zip1, '00000'),
+        NULLIF(zip2, '0000')) AS property_zip,
+                  Substr(class, 1, 1) AS major_class,
                   CASE
                     WHEN Substr(nbhd, 1, 2) IN ( '70', '71', '72', '73',
-                                                 '74', '75', '76', '77' ) THEN
-                    'City'
+                                                 '74', '75', '76', '77' )
+                    THEN 'City'
                     WHEN Substr(nbhd, 1, 2) IN ( '10', '16', '17', '18',
                                                  '20', '22', '23', '24',
                                                  '25', '26', '29', '35', '38' )
-                  THEN
-                    'North'
+                    THEN 'North'
                     WHEN Substr(nbhd, 1, 2) IN ( '11', '12', '13', '14',
                                                  '15', '19', '21', '27',
                                                  '28', '30', '31', '32',
                                                  '33', '34', '36', '37', '39' )
-                  THEN
-                    'South'
-                    ELSE NULL
-                  END                                     AS tri
-           FROM   iasworld.pardat),
+                  THEN 'South'
+                  ELSE NULL
+                  END AS tri
+      FROM   iasworld.pardat),
        -- parcel mailing addresses
-       owner_addressess
-       AS (SELECT parid                                   AS pin,
-                  Cast(Cast(taxyr AS INT) + 1 AS VARCHAR) AS rpie_year,
-                  own1
-                  || CASE
-                       WHEN own2 IS NOT NULL THEN ' '
-                                                  || own2
-                       ELSE ''
-                     END                                  AS mailing_NAME,
-                  CASE
-                    WHEN careof IS NOT NULL THEN careof
-                    ELSE ''
-                  END                                     AS mailing_care_of,
-                  CASE
-                    WHEN addr1 IS NOT NULL THEN addr1
-                                                || ( CASE
-                    WHEN addr2 IS NOT NULL THEN ' '
-                                                || addr2
-                    ELSE ''
-                                                     END )
-                    ELSE Cast(adrno AS VARCHAR)
-                         || CASE
-                              WHEN adrdir IS NOT NULL THEN adrdir
-                                                           || ' '
-                              ELSE ''
-                            END
-                         || ' '
-                         || adrstr
-                         || ' '
-                         || adrsuf
-                  END                                     AS mailing_address,
-                  cityname                                AS mailing_city,
-                  statecode                               AS mailing_state,
-                  CASE
-                    WHEN zip1 IS NOT NULL
-                         AND zip1 != '00000' THEN zip1
-                  END
-                  || CASE
-                       WHEN zip2 IS NOT NULL
-                            AND zip2 != '0000' THEN '-'
-                                                    || zip2
-                       ELSE ''
-                     END                                  AS mailing_zip
+       owner_addressess AS (
+         SELECT
+          parid AS pin,
+          Cast(Cast(taxyr AS INT) + 1 AS VARCHAR) AS rpie_year,
+          -- PIN mailing address from OWNDAT
+          NULLIF(CONCAT_WS(
+              ' ',
+              own1, own2
+          ), '') AS mailing_name,
+          NULLIF(careof, '') AS mailing_care_of,
+          CASE WHEN NULLIF(addr1, '') IS NOT NULL THEN addr1
+              WHEN NULLIF(addr2, '') IS NOT NULL THEN addr2
+              ELSE NULLIF(CONCAT_WS(' ',
+                  CAST(adrno AS varchar),
+                  adrdir, adrstr, adrsuf,
+                  unitdesc, unitno
+              ), '')
+          END AS mailing_address,
+          cityname AS mailing_city,
+          statecode AS mailing_state,
+          CONCAT_WS(
+              '-',
+              NULLIF(zip1, '00000'),
+              NULLIF(zip2, '0000')) AS mailing_zip
            FROM   iasworld.owndat),
            -- RPIE pin/code combinations
            pin_codes
