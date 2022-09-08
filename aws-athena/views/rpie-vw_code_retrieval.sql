a976183b33faef40a39b15ed8c33e4e610672b26
sweatyhandshake <wridgeway@cookcountyassessor.com>
2022-09-08 11:11:57 -0500

    New rpie view for code retrieval app

---
+++ aws-athena/views/rpie-vw_code_retrieval.sql
@@ -0,0 +1,28 @@
+ -- View containing RPIE codes and parcel physical and mailing addresses
+CREATE OR replace VIEW rpie.vw_code_retrieval
+AS
+WITH address AS (
+  SELECT
+    parid AS pin,
+    own1 AS mailing_name,
+    addr1 AS mailing_addr1,
+    CONCAT_WS(
+        ' ',
+        CONCAT_WS(', ', cityname, statecode),
+        CONCAT_WS('-', zip1, zip2)
+        ) AS mailing_addr2,
+    TAXYR AS year
+  FROM iasworld.owndat
+)
+
+SELECT
+    PC.pin,
+    PC.year,
+    PC.rpie_code,
+    pardat.class,
+    AD.mailing_name,
+    AD.mailing_addr1,
+    AD.mailing_addr2
+FROM rpie.pin_codes PC
+LEFT JOIN address AD ON PC.pin = AD.pin AND PC.year = AD.year
+LEFT JOIN iasworld.pardat ON PC.pin = pardat.parid AND PC.year = pardat.taxyr
