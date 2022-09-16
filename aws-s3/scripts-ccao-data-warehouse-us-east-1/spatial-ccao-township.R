ad346fa111314d0e10bc3d3e38cf365235f7c754
sweatyhandshake <wridgeway@cookcountyassessor.com>
2022-09-16 16:01:43 -0500

    Update script to address new raw file

--- aws-s3/scripts-ccao-data-warehouse-us-east-1/spatial-ccao-township.R
+++ aws-s3/scripts-ccao-data-warehouse-us-east-1/spatial-ccao-township.R
@@ -18,11 +18,13 @@ output_bucket <- file.path(AWS_S3_WAREHOUSE_BUCKET, "spatial", "ccao")
 
 ##### TOWNSHIP #####
 remote_file_town_raw <- file.path(
-  input_bucket, "township", "2019.geojson"
+  input_bucket, "township", "2022.geojson"
 )
 remote_file_town_warehouse <- file.path(
-  output_bucket, "township", "2019.parquet"
+  output_bucket, "township", "2022.parquet"
 )
+
+
 
 if (!aws.s3::object_exists(remote_file_town_warehouse)) {
   tmp_file_town <- tempfile(fileext = ".geojson")
@@ -30,7 +32,16 @@ if (!aws.s3::object_exists(remote_file_town_warehouse)) {
 
   st_read(tmp_file_town) %>%
     st_transform(4326) %>%
-    rename_with(tolower) %>%
+    select("township_name" = "NAME", "geometry") %>%
+    filter(township_name != '') %>%
+    mutate(
+      township_name = str_to_title(township_name),
+      township_name = case_when(
+        township_name %in% c("North", "South", "West") ~ paste(township_name, "Chicago"),
+        TRUE ~ township_name
+      )
+    ) %>%
+    left_join(ccao::town_dict %>% select("township_name", "township_code", "triad_name", "triad_code")) %>%
     mutate(
       geometry_3435 = st_transform(geometry, 3435),
       across(township_code:triad_code, as.character)
