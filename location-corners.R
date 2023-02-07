091ebc721f423ac35da2ffad1e05ce193d1b98c2
Miao Qi <miqi@ccao-datals.ccao.local>
2023-02-07 20:19:31 +0000

    Create network and calculate nodal degree

--- location-corners.R
+++ location-corners.R
@@ -3,19 +3,39 @@ library(osmdata)
 library(tidyverse)
 library(sf)
 library(ggmap)
+library(tidygraph)
+library(sfnetworks)
+library(dbscan)
 
 osm_data <- function(type) {
   opq(bbox = 'cook county illinois', timeout = 1000) %>%
     add_osm_feature(key = 'highway', value = type) %>% 
+    add_osm_feature(key = 'highway', value = '!footway') %>% 
     add_osm_feature(key = 'highway:tag', value = '!alley') %>% 
     osmdata_sf()
 }
 
 highway_type <- available_tags('highway')
 
-# highway_type <- c('mototway', 'trunk', 'primary', 'secondary', 'tertiary', 'residential',
-                  'motorway_link', 'trunk_link', 'primary_link', 'secondary_link', 'tertiary_link')
-
 cook_county <- lapply(highway_type, osm_data)
+
+
+# Combine items in one list
+line <- cook_county[[1]]$osm_lines
+for (i in 2:length(highway_type)){
+  if (is_null(cook_county[[i]]$osm_lines)){
+    i + 1
+  } else{
+  line <- st_join(cook_couty[[i]]$osm_lines, line)
+  }
+}
+
+# Construct the network
+network <- as_sfnetwork(line, directed = FALSE)
+
+degree <- network %>% 
+  activate(nodes) %>% 
+  mutate(degree = centrality_degree()) %>% 
+  filter(degree >= 3)
 
 
