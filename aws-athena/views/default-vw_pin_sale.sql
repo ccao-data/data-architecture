31bf1955a5789334ef667038ee46ef0420ca4fb5
Dan Snow <daniel.snow@cookcountyil.gov>
2023-02-15 18:02:53 +0000

    Add PTAX-203 flag for sale filtering

--- aws-athena/views/default-vw_pin_sale.sql
+++ aws-athena/views/default-vw_pin_sale.sql
@@ -1,35 +1,33 @@
 -- View containing unique, filtered sales
-CREATE OR replace VIEW default.vw_pin_sale
-AS
+CREATE OR replace VIEW default.vw_pin_sale AS
 -- Class of associated PIN
 WITH class AS (
     SELECT
         parid,
         class,
         taxyr
-    FROM iasworld.pardat),
+    FROM iasworld.pardat
+),
 -- Township of associated PIN
 town AS (
     SELECT
         parid,
         substr(TAXDIST, 1, 2) AS township_code,
         taxyr
-    FROM iasworld.legdat),
+    FROM iasworld.legdat
+),
 -- "nopar" isn't entirely accurate for sales associated with only one parcel, so we create our own counter
 calculated AS (
-
     SELECT
         instruno,
         Count(*) AS nopar_calculated
     FROM iasworld.sales
     WHERE deactivat IS NULL
     GROUP BY instruno
-
-    ),
+),
 unique_sales AS (
     SELECT *
     FROM (
-
         SELECT
             sales.parid AS pin,
             Substr(sales.saledt, 1, 4) AS year,
@@ -54,11 +52,11 @@ unique_sales AS (
             -- then order then order by descending price and give the top observation a value of 1 for "max_price"
             Row_number() OVER(
                 PARTITION BY sales.parid, sales.saledt ORDER BY -1 * sales.price
-                ) AS max_price,
+            ) AS max_price,
            -- Some pins sell for the exact same price a few months after they're sold
            -- these sales are unecessary for modeling and may be duplicates
-        Lag(Date_parse(Substr(sales.saledt, 1, 10), '%Y-%m-%d')) OVER(
-            PARTITION BY sales.parid, sales.price ORDER BY sales.saledt
+            Lag(Date_parse(Substr(sales.saledt, 1, 10), '%Y-%m-%d')) OVER(
+                PARTITION BY sales.parid, sales.price ORDER BY sales.saledt
             ) AS same_price_earlier_date
     FROM iasworld.sales
     LEFT JOIN calculated ON sales.instruno = calculated.instruno
@@ -76,19 +74,17 @@ unique_sales AS (
         -- Exclude quit claims, executor deeds, beneficial interests
         AND instrtyp NOT IN ( '03', '04', '06' )
         AND town.township_code IS NOT NULL
-
-        )
+    )
 
     -- Only use max price by pin/sale date
-    WHERE  max_price = 1
+    WHERE max_price = 1
     -- Drop sales for a given pin if it has sold within the last 12 months for the same price
     AND (
         Extract(day FROM sale_date - same_price_earlier_date) > 365
         OR same_price_earlier_date IS NULL
     )
-
 ),
--- Lower and upper bounds so that outlier sales can be filtered out using PTAX203 data
+-- Lower and upper bounds so that outlier sales can be filtered out using PTAX-203 data
 sale_filter AS (
     SELECT
         township_code,
@@ -106,7 +102,6 @@ sale_filter AS (
         is_multisale
 ),
 mydec_sales AS (
-
     SELECT
         replace(document_number, 'D', '') as doc_no,
         replace(line_1_primary_pin, '-', '') as pin,
@@ -134,15 +129,14 @@ mydec_sales AS (
         line_10s_senior_citizens AS "homestead_exemption_senior_citizens",
         line_10s_senior_citizens_assessment_freeze AS "homestead_exemption_senior_citizens_assessment_freeze"
     FROM sale.mydec
-    WHERE is_earliest_within_doc_no = TRUE
+    WHERE is_earliest_within_doc_no
 )
-
 SELECT
     unique_sales.pin,
     unique_sales.year,
     unique_sales.township_code,
     unique_sales.class,
-    --- In the past, mydec sale dates are more precise than iasworld dates which had been truncated
+    --- In the past, mydec sale dates were more precise than iasworld dates which had been truncated
     CASE
         WHEN mydec_date IS NOT NULL AND mydec_date != unique_sales.sale_date THEN mydec_date
         ELSE unique_sales.sale_date END AS sale_date,
@@ -160,9 +154,22 @@ SELECT
     unique_sales.num_parcels_sale,
     unique_sales.buyer_name,
     unique_sales.sale_type,
-    sale_filter_lower_limit,
-    sale_filter_upper_limit,
-    sale_filter_count,
+    sale_filter.sale_filter_lower_limit,
+    sale_filter.sale_filter_upper_limit,
+    sale_filter.sale_filter_count,
+    -- Flag for booting outlier PTAX-203 sales from modeling and reporting. Used in combination with
+    -- sale_filter upper and lower, which finds sales more than 2 SD from the year, town, and class mean
+    (
+      COALESCE(CAST(mydec_sales.is_sale_between_related_individuals_or_corporate_affiliates AS int), 0) +
+      COALESCE(CAST(mydec_sales.is_transfer_of_less_than_100_percent_interest AS int), 0) +
+      COALESCE(CAST(mydec_sales.is_court_ordered_sale AS int), 0) +
+      COALESCE(CAST(mydec_sales.is_sale_in_lieu_of_foreclosure AS int), 0) +
+      COALESCE(CAST(mydec_sales.is_condemnation AS int), 0) +
+      COALESCE(CAST(mydec_sales.is_short_sale AS int), 0) +
+      COALESCE(CAST(mydec_sales.is_bank_reo_real_estate_owned AS int), 0) +
+      COALESCE(CAST(mydec_sales.is_auction_sale AS int), 0) +
+      COALESCE(CAST(mydec_sales.is_seller_buyer_a_financial_institution_or_government_agency AS int), 0)
+    ) > 0 AS sale_filter_ptax_flag,
     mydec_sales.property_advertised,
     mydec_sales.is_installment_contract_fulfilled,
     mydec_sales.is_sale_between_related_individuals_or_corporate_affiliates,
