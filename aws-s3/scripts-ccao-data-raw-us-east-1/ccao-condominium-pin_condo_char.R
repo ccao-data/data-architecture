c3f9c443ec4e54fd916750a5f74f9dac5ce75644
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2023-02-16 18:20:20 +0000

    Raw condo char ingest handles north and south data now

--- aws-s3/scripts-ccao-data-raw-us-east-1/ccao-condominium-pin_condo_char.R
+++ aws-s3/scripts-ccao-data-raw-us-east-1/ccao-condominium-pin_condo_char.R
@@ -14,7 +14,10 @@ AWS_S3_RAW_BUCKET <- Sys.getenv("AWS_S3_RAW_BUCKET")
 output_bucket <- file.path(AWS_S3_RAW_BUCKET, "ccao", "condominium", "pin_condo_char")
 
 # Get local file addresses
-source_paths <- c("//fileserver/ocommon/2022 Data Collection/Condo Project/William Approved Layout North Tri Condo Project FINAL COMPLETED/")
+source_paths <- c(
+  "//fileserver/ocommon/2022 Data Collection/Condo Project/William Approved Layout North Tri Condo Project FINAL COMPLETED/",
+  "//fileserver/ocommon/2023 Data Collection/South Tri Condo Project COMPLETED"
+)
 
 source_files <- grep(
   "completed",
@@ -28,19 +31,29 @@ source_files <- grep(
 
 # Function to retrieve data and write to S3
 read_write <- function(x) {
-  openxlsx::read.xlsx(x, sheet = 1) %>%
-    write_parquet(
-      tolower(
-        str_replace(
-          file.path(
-            output_bucket,
-            str_sub(str_extract(x, '20.*'), 1, 4),
-            paste0(tools::file_path_sans_ext(basename(x)), ".parquet")
-            ),
-          " ", "_"
-          )
+
+  destination <- tolower(
+    str_replace(
+      file.path(
+        output_bucket,
+        str_extract(x, '[0-9]{4}'),
+        paste0(
+          str_extract(x, paste(ccao::town_dict$township_name, collapse = "|")),
+          ".parquet"
         )
-      )
+      ),
+      " ", "_"
+    )
+  )
+
+  if (!aws.s3::object_exists(destination)) {
+
+    openxlsx::read.xlsx(x, sheet = 1) %>%
+      write_parquet(destination)
+
+  }
+
+
 }
 
 # Apply function to foreclosure data
