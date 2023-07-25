6ab76a35b0d6b584eb25f2c721a96de2ad968c42
Jean Cochrane <jean@jeancochrane.com>
2023-07-25 12:12:43 -0500

    Add location models and reorganize dbt spike around schemas

---
+++ dbt/tests/generic/test_column_length.sql
@@ -0,0 +1,41 @@
+-- Test that all columns in a list have the correct length.
+--
+-- Returns columns with the prefix `len_` representing the length of each columns
+-- for all rows where one of the columns has an incorrect length.
+--
+-- If additional columns should be returned along with the length columns,
+-- e.g. to add an identifiable key for the row, use the
+-- additional_select_columns argument.
+{% test column_length(model, columns, length, additional_select_columns=[]) %}
+
+{%- set columns_csv = additional_select_columns | join(', ') %}
+
+{%- set length_columns = [] %}
+{% for column in columns %}
+  {%- set length_columns = length_columns.append([column, 'len_' + column]) %}
+{% endfor %}
+
+{%- set select_columns = additional_select_columns %}
+{% set filter_conditions = [] %}
+
+{% for column, length_column in length_columns %}
+  {%- set select_columns = select_columns.append(
+    'length(' + column + ') as ' + length_column
+  ) %}
+  {%- set filter_conditions = filter_conditions.append(
+    '(' + length_column + ' is not null and ' + length_column + ' > ' + length|string + ')'
+  ) %}
+{% endfor %}
+
+{%- set columns_csv = select_columns | join(', ') %}
+{%- set filter_conditions_str = filter_conditions | join(' or ') %}
+
+with column_lengths as (
+  select {{ columns_csv}}
+  from {{ model }}
+)
+select *
+from column_lengths
+where {{ filter_conditions_str }}
+
+{% endtest %}
