5896cb684908edf97d1fe1ad64f367710654b122
Jean Cochrane <jeancochrane@users.noreply.github.com>
2023-08-03 08:54:00 -0500

    Add sqlfmt pre-commit hook for autoformatting dbt files (#49)

--- dbt/macros/tests/utils.sql
+++ dbt/macros/tests/utils.sql
@@ -1,9 +1,8 @@
 {% macro assert_equals(test_name, value, expected) %}
-    {% if value == expected %}
-        {% do log(test_name ~ " - PASS", info=True) %}
+    {% if value == expected %} {% do log(test_name ~ " - PASS", info=True) %}
     {% else %}
         {% do exceptions.raise_compiler_error(
-            test_name ~ " - FAIL: " ~ value~ " != " ~ expected
+            test_name ~ " - FAIL: " ~ value ~ " != " ~ expected
         ) %}
     {% endif %}
 {% endmacro %}
