27f556cc1bb64ade5c678607064834fc79f53b97
Jean Cochrane <jean@jeancochrane.com>
2023-08-07 10:03:18 -0500

    Clean up cleanup_dbt_resources.sh script for use in CI

--- .github/scripts/cleanup_dbt_resources.sh
+++ .github/scripts/cleanup_dbt_resources.sh
@@ -1,14 +1,27 @@
 #!/usr/bin/env bash
-schemas=$(dbt --quiet list --resource-type model --output json --output-keys schema)
+set -euo pipefail
+
+if [[ "$#" -eq 0 ]]; then
+    echo "Missing first argument representing dbt target"
+    exit 1
+fi
+
+if [ "$1" == "prod" ]; then
+    echo "Target cannot be 'prod'"
+    exit 1
+fi
+
+schemas_json=$(dbt --quiet list --resource-type model --target "$1" \
+    --output json --output-keys schema) || (echo "Error in dbt call" && exit 1)
+schemas=$(echo "$schemas_json"| sort | uniq | jq ' .schema') || (\
+    echo "Error in schema parsing" && exit 1
+)
+
 echo "Deleting the following schemas from Athena:"
 echo
 echo "$schemas"
 
-echo "$schemas" \
-    | sort \
-    | uniq \
-    | jq ' .schema' \
-    | xargs -i bash -c 'aws glue delete-database --name {}'
+echo "$schemas" | xargs -i bash -c 'aws glue delete-database --name {} || exit 255'
 
 echo
 echo "Done!"
