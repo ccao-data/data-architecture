a4da6c984235467bafd5c9637f019e1eb8950ffe
William Ridgeway <william.ridgeway@cookcountyil.gov>
2023-08-07 12:17:44 -0500

    Add stage indicators and deduplicate conditionally rather than through aggregation

--- aws-athena/views/default-vw_pin_value.sql
+++ aws-athena/views/default-vw_pin_value.sql
@@ -1,69 +1,67 @@
--- View containing current and prior years' assessments by PIN in wide format
+-- View containing values from each stage of assessment by PIN and year
 CREATE OR REPLACE VIEW default.vw_pin_value AS
--- CCAO mailed_tot, CCAO final, and BOR final values for each PIN by year
-SELECT
-    parid AS pin,
-    taxyr AS year,
-    -- Mailed values
-    MAX(
+WITH stage_values AS (
+    SELECT
+        parid AS pin,
+        taxyr AS year,
+        -- Mailed values
         CASE
             WHEN procname = 'CCAOVALUE' AND taxyr < '2020' THEN ovrvalasm2
             WHEN procname = 'CCAOVALUE' AND taxyr >= '2020' THEN valasm2
-        END
-    ) AS mailed_bldg,
-    MAX(
+        END AS mailed_bldg,
         CASE
             WHEN procname = 'CCAOVALUE' AND taxyr < '2020' THEN ovrvalasm1
             WHEN procname = 'CCAOVALUE' AND taxyr >= '2020' THEN valasm1
-        END
-    ) AS mailed_land,
-    MAX(
+        END AS mailed_land,
         CASE
             WHEN procname = 'CCAOVALUE' AND taxyr < '2020' THEN ovrvalasm3
             WHEN procname = 'CCAOVALUE' AND taxyr >= '2020' THEN valasm3
-        END
-    ) AS mailed_tot,
-    -- Assessor certified values
-    MAX(
+        END AS mailed_tot,
+        -- Assessor certified values
         CASE
             WHEN procname = 'CCAOFINAL' AND taxyr < '2020' THEN ovrvalasm2
             WHEN procname = 'CCAOFINAL' AND taxyr >= '2020' THEN valasm2
-        END
-    ) AS certified_bldg,
-    MAX(
+        END AS certified_bldg,
         CASE
             WHEN procname = 'CCAOFINAL' AND taxyr < '2020' THEN ovrvalasm1
             WHEN procname = 'CCAOFINAL' AND taxyr >= '2020' THEN valasm1
-        END
-    ) AS certified_land,
-    MAX(
+        END AS certified_land,
         CASE
             WHEN procname = 'CCAOFINAL' AND taxyr < '2020' THEN ovrvalasm3
             WHEN procname = 'CCAOFINAL' AND taxyr >= '2020' THEN valasm3
-        END
-    ) AS certified_tot,
-    -- Board certified values
-    MAX(
+        END AS certified_tot,
+        -- Board certified values
         CASE
             WHEN procname = 'BORVALUE' AND taxyr < '2020' THEN ovrvalasm2
             WHEN procname = 'BORVALUE' AND taxyr >= '2020' THEN valasm2
-        END
-    ) AS board_bldg,
-    MAX(
+        END AS board_bldg,
         CASE
             WHEN procname = 'BORVALUE' AND taxyr < '2020' THEN ovrvalasm1
             WHEN procname = 'BORVALUE' AND taxyr >= '2020' THEN valasm1
-        END
-    ) AS board_land,
-    MAX(
+        END AS board_land,
         CASE
             WHEN procname = 'BORVALUE' AND taxyr < '2020' THEN ovrvalasm3
             WHEN procname = 'BORVALUE' AND taxyr >= '2020' THEN valasm3
-        END
-    ) AS board_tot
-FROM iasworld.asmt_all
-WHERE procname IN ('CCAOVALUE', 'CCAOFINAL', 'BORVALUE')
-    AND rolltype != 'RR'
-    AND deactivat IS NULL
-    AND valclass IS NULL
-GROUP BY parid, taxyr
+        END AS board_tot
+    FROM iasworld.asmt_all
+    WHERE procname IN ('CCAOVALUE', 'CCAOFINAL', 'BORVALUE')
+        AND cur = 'Y'
+        AND rolltype != 'RR'
+        AND deactivat IS NULL
+        AND valclass IS NULL
+)
+
+SELECT
+    stage_values.*,
+    -- Current stage indicator
+    CASE
+        WHEN stage_values.board_tot IS NOT NULL THEN 'BOARD CERTIFIED'
+        WHEN stage_values.certified_tot IS NOT NULL THEN 'ASSESSOR CERTIFIED'
+        WHEN stage_values.mailed_tot IS NOT NULL THEN 'MAILED'
+    END AS stage_name,
+    CASE
+        WHEN stage_values.board_tot IS NOT NULL THEN 3
+        WHEN stage_values.certified_tot IS NOT NULL THEN 2
+        WHEN stage_values.mailed_tot IS NOT NULL THEN 1
+    END AS stage_num
+FROM stage_values
