a6f876a3b76c8f642ec8d0772f0fe6c6ecad7667
Jean Cochrane <jean@jeancochrane.com>
2023-08-24 17:46:29 -0500

    Move where cluase in unique_combination_of_columns macro and add it to docstring

--- dbt/tests/generic/test_unique_combination_of_columns.sql
+++ dbt/tests/generic/test_unique_combination_of_columns.sql
@@ -1,14 +1,17 @@
 -- Test that a given set of columns are unique, with an optional
--- threshold indicating an allowable number of duplicates.
+-- threshold indicating an allowable number of duplicates and an optional
+-- filter clause to restrict the set of rows that should be unique.
 --
 -- For example, test that a given PIN has been sold no more than
--- twice in one year.
+-- twice in one year, for only active rows.
 --
 -- The duplicate threshold defaults to 1, in which case this is a standard
--- uniqueness test.
+-- uniqueness test. The where clause defaults to null, which indicates
+-- that the full set of rows should be unique on the given columns.
 --
 -- Adapted from dbt_utils.unique_combination_of_columns, and adjusted to add the
--- optional duplicate threshold and to only report one row for each dupe.
+-- optional duplicate threshold, to add the optional where clause, and to only
+-- report one row for each dupe.
 {% test unique_combination_of_columns(
     model, combination_of_columns, allowed_duplicates=0, where=null
 ) %}
@@ -17,8 +20,8 @@
 
     select {{ columns_csv }}, count(*) as num_duplicates
     from {{ model }}
+    {% if where %} where ({{ where }}) {% endif %}
     group by {{ columns_csv }}
     having count(*) > {{ allowed_duplicates }} + 1
-    {%- if where -%} where ({{ where }}) {%- endif -%}
 
 {% endtest %}
