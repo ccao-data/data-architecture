8e72252edde3b2292df5d7d1d27f8bdf20efc89a
Jean Cochrane <jean@jeancochrane.com>
2023-08-25 13:06:35 -0500

    Switch to using builtin config.where filter for iasworld tests

--- dbt/tests/generic/test_unique_combination_of_columns.sql
+++ dbt/tests/generic/test_unique_combination_of_columns.sql
@@ -1,26 +1,22 @@
 -- Test that a given set of columns are unique, with an optional
--- threshold indicating an allowable number of duplicates and an optional
--- filter clause to restrict the set of rows that should be unique.
+-- threshold indicating an allowable number of duplicates.
 --
 -- For example, test that a given PIN has been sold no more than
--- twice in one year, for only active rows.
+-- twice in one year.
 --
 -- The duplicate threshold defaults to 1, in which case this is a standard
--- uniqueness test. The where clause defaults to null, which indicates
--- that the full set of rows should be unique on the given columns.
+-- uniqueness test.
 --
 -- Adapted from dbt_utils.unique_combination_of_columns, and adjusted to add the
--- optional duplicate threshold, to add the optional where clause, and to only
--- report one row for each dupe.
+-- optional duplicate threshold and to only report one row for each dupe.
 {% test unique_combination_of_columns(
-    model, combination_of_columns, allowed_duplicates=0, where=null
+    model, combination_of_columns, allowed_duplicates=0
 ) %}
 
     {%- set columns_csv = combination_of_columns | join(", ") %}
 
     select {{ columns_csv }}, count(*) as num_duplicates
     from {{ model }}
-    {% if where %} where ({{ where }}) {% endif %}
     group by {{ columns_csv }}
     having count(*) > {{ allowed_duplicates }} + 1
 
