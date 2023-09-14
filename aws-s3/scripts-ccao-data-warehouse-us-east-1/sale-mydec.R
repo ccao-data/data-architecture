7a0159aaff793657f90413d257ac8e5cc90ef8ef
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2023-09-14 21:31:44 +0000

    Fix some dplyr weirdness with rename_with and make sure sales values are integers rather than characters

--- aws-s3/scripts-ccao-data-warehouse-us-east-1/sale-mydec.R
+++ aws-s3/scripts-ccao-data-warehouse-us-east-1/sale-mydec.R
@@ -35,38 +35,43 @@ files <- grep(
 
 # Function to make sure mydec data can be stacked across years
 clean_up <- function(x) {
-
   read_parquet(x) %>%
     mutate(across(where(is.Date), as.character)) %>%
-    rename_with(~ str_replace_all(.x, c("\\?" = "", "Step 4 - " = "", "Step 3 - " = ""))) %>%
-    rename_with(~"Legal Description 1", ends_with("Legal Description"))
-
+    rename_with(
+      ~ str_replace_all(.x, c("\\?" = "", "Step 4 - " = "", "Step 3 - " = ""))
+    ) %>%
+    rename_with(
+      ~ paste0(.x, " 1", recycle0 = TRUE),
+      .cols = ends_with("Legal Description")
+    )
 }
 
 # MyDec columns we want to use, tend to be different across duplicate sales...
-mydec_vars <- c("line_7_property_advertised",
-                "line_10a",
-                "line_10b",
-                "line_10c",
-                "line_10d",
-                "line_10e",
-                "line_10f",
-                "line_10g",
-                "line_10h",
-                "line_10i",
-                "line_10j",
-                "line_10k",
-                "line_10l",
-                "line_10m",
-                "line_10n",
-                "line_10o",
-                "line_10p",
-                "line_10q",
-                "line_10s",
-                "line_10s",
-                "line_10s_generalalternative",
-                "line_10s_senior_citizens",
-                "line_10s_senior_citizens_assessment_freeze")
+mydec_vars <- c(
+  "line_7_property_advertised",
+  "line_10a",
+  "line_10b",
+  "line_10c",
+  "line_10d",
+  "line_10e",
+  "line_10f",
+  "line_10g",
+  "line_10h",
+  "line_10i",
+  "line_10j",
+  "line_10k",
+  "line_10l",
+  "line_10m",
+  "line_10n",
+  "line_10o",
+  "line_10p",
+  "line_10q",
+  "line_10s",
+  "line_10s",
+  "line_10s_generalalternative",
+  "line_10s_senior_citizens",
+  "line_10s_senior_citizens_assessment_freeze"
+)
 
 # Load raw files, cleanup, then write to warehouse S3
 map(files, clean_up) %>%
@@ -75,48 +80,59 @@ map(files, clean_up) %>%
     str_replace_all(
       str_squish(
         str_replace_all(.x, "[[:punct:]]", "")
-      ), " ", "_")
+      ), " ", "_"
+    )
   )) %>%
-  mutate(across(where(is.character), str_squish)) %>%
-  na_if("") %>%
-  na_if("NULL") %>%
+  mutate(
+    across(where(is.character), str_squish),
+    across(where(is.character), ~ na_if(.x, "")),
+    across(where(is.character), ~ na_if(.x, "NULL")),
+    across(ends_with("consideration"), as.integer)
+  ) %>%
   filter(!is.na(document_number) & line_1_county == "Cook") %>%
   mutate(document_number = str_replace_all(document_number, "D", "")) %>%
   group_by(document_number) %>%
-  # because MyDec variables can be different across duplicate sales doc #s, we'll take the max values
+  # Because MyDec variables can be different across duplicate sales doc #s,
+  # we'll take the max values
   mutate(across(all_of(mydec_vars), ~ max(.x, na.rm = TRUE))) %>%
-  distinct(document_number,
-                     line_7_property_advertised,
-                     line_10a,
-                     line_10b,
-                     line_10c,
-                     line_10d,
-                     line_10e,
-                     line_10f,
-                     line_10g,
-                     line_10h,
-                     line_10i,
-                     line_10j,
-                     line_10k,
-                     line_10l,
-                     line_10m,
-                     line_10n,
-                     line_10o,
-                     line_10p,
-                     line_10q,
-                     line_10s,
-                     line_10s,
-                     line_10s_senior_citizens,
-                     line_10s_senior_citizens_assessment_freeze, .keep_all = TRUE) %>%
-  # Data isn't unique by document number, or even transaction date and document number
-  # so we arrange by transaction date and then recorded date within document number and
-  # create an indicator for the first row within duplicated document numbers
+  distinct(
+    document_number,
+    line_7_property_advertised,
+    line_10a,
+    line_10b,
+    line_10c,
+    line_10d,
+    line_10e,
+    line_10f,
+    line_10g,
+    line_10h,
+    line_10i,
+    line_10j,
+    line_10k,
+    line_10l,
+    line_10m,
+    line_10n,
+    line_10o,
+    line_10p,
+    line_10q,
+    line_10s,
+    line_10s,
+    line_10s_senior_citizens,
+    line_10s_senior_citizens_assessment_freeze,
+    .keep_all = TRUE
+  ) %>%
+  # Data isn't unique by document number, or even transaction date and document
+  # number so we arrange by transaction date and then recorded date within
+  # document number and create an indicator for the first row within duplicated
+  # document numbers
   arrange(line_4_instrument_date, date_recorded, .by_group = TRUE) %>%
-  mutate(year_of_sale = as.character(lubridate::year(line_4_instrument_date)),
-         declaration_id = as.character(declaration_id),
-         is_earliest_within_doc_no = case_when(
-           1:n() == 1 ~ TRUE,
-           TRUE ~ FALSE
-         )) %>%
+  mutate(
+    year_of_sale = as.character(lubridate::year(line_4_instrument_date)),
+    declaration_id = as.character(declaration_id),
+    is_earliest_within_doc_no = case_when(
+      1:n() == 1 ~ TRUE,
+      TRUE ~ FALSE
+    )
+  ) %>%
   group_by(year_of_sale) %>%
   write_partitions_to_s3(output_bucket, is_spatial = FALSE, overwrite = TRUE)
