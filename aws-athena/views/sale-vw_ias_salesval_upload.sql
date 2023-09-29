485bbd6df0c1290766bc13f893322e6bae4d4efa
Michael Wagner <wagnerleemichael@gmail.com>
2023-09-29 14:45:03 +0000

    Remove analyst column

--- aws-athena/views/sale-vw_ias_salesval_upload.sql
+++ aws-athena/views/sale-vw_ias_salesval_upload.sql
@@ -11,7 +11,6 @@ SELECT
     ias_sales.salekey,
     sf.sv_is_outlier,
     sf.sv_outlier_type,
-    CAST(NULL AS VARCHAR) AS analyst_override, -- Should we assume this has been created already in ias world and pull this column from iasworld.sale?
     sf.run_id
 FROM ias_sales 
-JOIN {{ ref('sale.flag') }} sf ON ias_sales.instruno_clean = sf.meta_sale_document_num;
+INNER JOIN {{ ref('sale.flag') }} sf ON ias_sales.instruno_clean = sf.meta_sale_document_num;
