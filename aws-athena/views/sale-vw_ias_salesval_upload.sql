6c957572d3a08dc225a65dcb6666e3371bf7dbbb
Michael Wagner <wagnerleemichael@gmail.com>
2023-10-05 19:06:01 +0000

    Update view to take highest version

--- aws-athena/views/sale-vw_ias_salesval_upload.sql
+++ aws-athena/views/sale-vw_ias_salesval_upload.sql
@@ -5,6 +5,14 @@ WITH ias_sales AS (
     FROM {{ source('iasworld', 'sales') }}
     WHERE cur = 'Y'
     AND deactivat IS NULL
+),
+
+max_version AS (
+    SELECT 
+        meta_sale_document_num,
+        MAX(version) AS max_version
+    FROM {{ source('sale', 'flag') }}
+    GROUP BY meta_sale_document_num
 )
 
 SELECT 
@@ -13,4 +21,8 @@ SELECT
     sf.sv_outlier_type,
     sf.run_id
 FROM ias_sales 
-INNER JOIN {{ source('sale', 'flag') }} sf ON ias_sales.instruno_clean = sf.meta_sale_document_num;
+INNER JOIN {{ source('sale', 'flag') }} sf 
+    ON ias_sales.instruno_clean = sf.meta_sale_document_num
+INNER JOIN max_version mv
+    ON sf.meta_sale_document_num = mv.meta_sale_document_num
+    AND sf.version = mv.max_version;
