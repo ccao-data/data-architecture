667a8c4d1daa6ebd9d5172241ca22cbe26070394
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2023-10-26 20:13:35 +0000

    Revert commercial valuation changes

--- aws-s3/scripts-ccao-data-warehouse-us-east-1/ccao-commercial_valuation.R
+++ aws-s3/scripts-ccao-data-warehouse-us-east-1/ccao-commercial_valuation.R
@@ -2,6 +2,8 @@
 # in order to compile a single aggregated dataset of commercial valuation data.
 
 library(ccao)
+library(DBI)
+library(noctua)
 library(dplyr)
 library(openxlsx)
 library(readr)
@@ -113,79 +115,79 @@ temp <- list.files(
   grep(pattern = "Other", invert = TRUE, value = TRUE) %>%
   list.files(pattern = ".xlsx", full.names = TRUE) %>%
   map(function(x) {
+
     # Unfortunately, people are still working on some of these sheets which
     # means this script will error out when a file is open - `possibly` here
     # avoids that. Don't consider data final if an error is caught.
-    safe_gSN <- possibly(.f = getSheetNames, otherwise = NULL)
+    safe_gSN = possibly(.f = getSheetNames, otherwise = NULL)
 
     crossing(sheet = safe_gSN(x), file = x)
+
   }, .progress = TRUE) %>%
   bind_rows() %>%
   filter(str_detect(sheet, "Summary", negate = TRUE), !is.na(sheet)) %>%
-  # Ingest the sheets, clean, and bind them ----
-  pmap(function(...) {
-    data <- tibble(...)
 
-    read.xlsx(data$file, sheet = data$sheet) %>%
-      mutate(file = data$file, sheet = data$sheet) %>%
-      rename_with(tolower) %>%
-      set_names(str_replace_all(names(.), renames)) %>%
-      select(-starts_with("X"), -contains("age2")) %>%
-      mutate(
-        across(.cols = everything(), as.character)
-      )
-  }, .progress = TRUE) %>%
+  # Ingest the sheets, clean, and bind them ----
+pmap(function(...) {
+
+  data <- tibble(...)
+
+  read.xlsx(data$file, sheet = data$sheet) %>%
+    mutate(file = data$file, sheet = data$sheet) %>%
+    rename_with(tolower) %>%
+    set_names(str_replace_all(names(.), renames)) %>%
+    select(-starts_with("X"), -contains("age2")) %>%
+    mutate(
+      across(.cols = everything(), as.character)
+    )
+
+}, .progress = TRUE) %>%
   bind_rows() %>%
   filter(check.numeric(excesslandval)) %>%
   select(where(~ !all(is.na(.x)))) %>%
+
   # Add useful information to output and clean-up columns ----
-  mutate(
-    year = str_extract(file, "[0-9]{4}"),
-    township = str_replace_all(
-      str_extract(
-        file,
-        str_remove_all(
-          paste(ccao::town_dict$township_name, collapse = "|"), " "
-          )
-      ),
-      c(
-        "ElkGrove" = "Elk Grove",
-        "HydePark" = "Hyde Park",
-        "LakeView" = "Lake View",
-        "NewTrier" = "New Trier",
-        "NorthChicago" = "North Chicago",
-        "NorwoodPark" = "Norwood Park",
-        "OakPark" = "Oak Park",
-        "SouthChicago" = "South Chicago",
-        "RiverForest" = "River Forest",
-        "RogersPark" = "Rogers Park",
-        "WestChicago" = "West Chicago"
-      )
+mutate(
+  year = str_extract(file, "[0-9]{4}"),
+  township = str_replace_all(
+    str_extract(
+      file,
+      str_remove_all(paste(ccao::town_dict$township_name, collapse = "|"), " ")
     ),
-    across(.cols = everything(), ~ na_if(.x, "N/A")),
-    # Ignore known character columns for parse_number
-    across(.cols = !char_cols, parse_number),
-    # Columns that can be numeric should be
-    across(where(~ all(check.numeric(.x))), as.numeric),
-    yearbuilt = case_when(
-      is.na(yearbuilt) & age < 1000 ~ (year - age),
-      TRUE ~ yearbuilt
-    ),
-    tot_units = coalesce(tot_units, tot_apts, `boatslips`, `mobilehomepads`),
-    # marketvalue = coalesce(
-    # finalmarketvalue, marketvalue, marketvalue_incl_excessland
-    # ),
-    # Don't stack pin numbers when "Thru" is present in PIN list
-    across(.cols = c(pins, `class(es)`), ~ case_when(
-      grepl("thru", .x, ignore.case = TRUE) ~ str_squish(.x),
-      .x == "0" ~ NA,
-      TRUE ~ str_replace_all(str_squish(.x), " ", ", ")
-    )),
-    taxdist = as.character(taxdist),
-    across(int_cols, as.integer)
-  ) %>%
+    c("ElkGrove" = "Elk Grove",
+      "HydePark" = "Hyde Park",
+      "LakeView" = "Lake View",
+      "NewTrier" = "New Trier",
+      "NorthChicago" = "North Chicago",
+      "NorwoodPark" = "Norwood Park",
+      "OakPark" = "Oak Park",
+      "SouthChicago" = "South Chicago",
+      "RiverForest" = "River Forest",
+      "RogersPark" = "Rogers Park",
+      "WestChicago" = "West Chicago")
+  ),
+  across(.cols = everything(), ~ na_if(.x, "N/A")),
+  # Ignore known character columns for parse_number
+  across(.cols = !char_cols, parse_number),
+  # Columns that can be numeric should be
+  across(where(~ all(check.numeric(.x))), as.numeric),
+  yearbuilt = case_when(
+    is.na(yearbuilt) & age < 1000 ~ (year - age),
+    TRUE ~ yearbuilt
+  ),
+  tot_units = coalesce(tot_units, tot_apts, `boatslips`, `mobilehomepads`),
+  #marketvalue = coalesce(finalmarketvalue, marketvalue, marketvalue_incl_excessland),
+  # Don't stack pin numbers when "Thru" is present in PIN list
+  across(.cols = c(pins, `class(es)`), ~ case_when(
+    grepl("thru", .x, ignore.case = TRUE) ~ str_squish(.x),
+    .x == "0" ~ NA,
+    TRUE ~ str_replace_all(str_squish(.x), " ", ", ")
+  )),
+  taxdist = as.character(taxdist),
+  across(int_cols, as.integer)
+) %>%
   # Remove empty columns
-  select(where(~ !(all(is.na(.)) | all(. == "")))) %>%
+  select(where(~!(all(is.na(.)) | all(. == "")))) %>%
   # Remove pre-declared columns
   select(!all_of(remove_cols) & !starts_with("market")) %>%
   select(all_of(sort(names(.)))) %>%
@@ -193,3 +195,5 @@ temp <- list.files(
   relocate(c(file, sheet), .after = last_col()) %>%
   group_by(year) %>%
   write_partitions_to_s3(output_bucket, is_spatial = FALSE, overwrite = TRUE)
+
+
