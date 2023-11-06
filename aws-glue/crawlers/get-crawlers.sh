74bd59d202db92f8f9219be4a316d9ae01c28a49
Dan Snow <31494343+dfsnow@users.noreply.github.com>
2023-11-06 15:24:00 -0600

    Remove deprecated Glue assets (#215)

--- aws-glue/crawlers/get-crawlers.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Script to save crawler configs from Glue to JSON file
-aws glue list-crawlers | jq -r .[][] |
-while read file; do
-    aws glue get-crawler --name ${file} > ${file}.json
-done
