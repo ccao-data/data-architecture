7b13ac40f3cf531e615ffc35fe9b59eb973c0c69
Michael Wagner <wagnerleemichael@gmail.com>
2023-11-13 21:11:42 +0000

    Change query to new optimized standard

--- aws-athena/ctas/proximity-dist_pin_to_secondary_road.sql
+++ aws-athena/ctas/proximity-dist_pin_to_secondary_road.sql
@@ -1,4 +1,4 @@
--- CTAS to create a table of distance to the nearest secondary road for each PIN
+-- CTAS to create a table of distance to the nearest major road for each PIN
 {{
     config(
         materialized='table',
@@ -8,84 +8,16 @@
     )
 }}
 
-WITH distinct_pins AS (
-    SELECT DISTINCT
-        x_3435,
-        y_3435
-    FROM {{ source('spatial', 'parcel') }}
-),
-
-distinct_years AS (
-    SELECT DISTINCT year
-    FROM {{ source('spatial', 'parcel') }}
-),
-
-secondary_road_location AS (
-    SELECT
-        fill_years.pin_year,
-        fill_data.*
-    FROM (
-        SELECT
-            dy.year AS pin_year,
-            '2022' AS fill_year
-        FROM {{ source('spatial', 'secondary_road') }} AS df
-        CROSS JOIN distinct_years AS dy
-        WHERE dy.year >= df.year
-    ) AS fill_years
-    LEFT JOIN {{ source('spatial', 'secondary_road') }} AS fill_data
-        ON fill_years.fill_year = fill_data.year
-),
-
-distances AS (
-    SELECT
-        dp.x_3435,
-        dp.y_3435,
-        loc.osm_id,
-        loc.name,
-        loc.pin_year,
-        loc.year,
-        ST_DISTANCE(
-            ST_POINT(dp.x_3435, dp.y_3435),
-            ST_GEOMFROMBINARY(loc.geometry_3435)
-        ) AS distance
-    FROM distinct_pins AS dp
-    CROSS JOIN secondary_road_location AS loc
-),
-
-xy_to_secondary_road_dist AS (
-    SELECT
-        d1.x_3435,
-        d1.y_3435,
-        d1.osm_id,
-        d1.name,
-        d1.pin_year,
-        d1.year,
-        d2.dist_ft
-    FROM distances AS d1
-    INNER JOIN (
-        SELECT
-            x_3435,
-            y_3435,
-            pin_year,
-            MIN(distance) AS dist_ft
-        FROM distances
-        GROUP BY x_3435, y_3435, pin_year
-    ) AS d2
-        ON d1.x_3435 = d2.x_3435
-        AND d1.y_3435 = d2.y_3435
-        AND d1.pin_year = d2.pin_year
-        AND d1.distance = d2.dist_ft
-)
-
 SELECT
     pcl.pin10,
-    ARBITRARY(xy.osm_id) AS nearest_secondary_road_osm_id,
-    ARBITRARY(xy.name) AS nearest_secondary_road_name,
-    ARBITRARY(xy.dist_ft) AS nearest_secondary_road_dist_ft,
-    ARBITRARY(xy.year) AS nearest_secondary_road_data_year,
+    ARBITRARY(xy.osm_id) AS nearest_major_road_osm_id,
+    ARBITRARY(xy.name) AS nearest_major_road_name,
+    ARBITRARY(xy.dist_ft) AS nearest_major_road_dist_ft,
+    ARBITRARY(xy.year) AS nearest_major_road_data_year,
     pcl.year
 FROM {{ source('spatial', 'parcel') }} AS pcl
-INNER JOIN xy_to_secondary_road_dist AS xy
+INNER JOIN
+    ( {{ dist_to_nearest_geometry(source('spatial', 'secondary_road')) }} ) AS xy
     ON pcl.x_3435 = xy.x_3435
     AND pcl.y_3435 = xy.y_3435
     AND pcl.year = xy.pin_year
