fb0b509328756d275fb972197d78e913d4829288
Michael Wagner <wagnerleemichael@gmail.com>
2023-11-14 17:09:10 +0000

    Adjust queries to iterate through years

--- aws-s3/scripts-ccao-data-raw-us-east-1/spatial-environment-major_road.R
+++ aws-s3/scripts-ccao-data-raw-us-east-1/spatial-environment-major_road.R
@@ -15,25 +15,33 @@ output_bucket <- file.path(AWS_S3_RAW_BUCKET, "spatial", "environment")
 current_year <- strftime(Sys.Date(), "%Y")
 
 ##### Major roads #####
-# Query OpenStreetMap API for Secondary roads in Cook
-remote_file <- file.path(
-  output_bucket, "major_road",
-  paste0("year=", current_year),
-  paste0("major_road-", current_year, ".parquet")
-)
+# Query OpenStreetMap API for major roads in Cook
+# Create a sequence of years from 2014 to the current year
+years <- 2014:current_year
 
+# Iterate over the years
+for (year in years) {
+  remote_file <- file.path(
+    output_bucket, "major_road",
+    paste0("year=", year),
+    paste0("major_road-", year, ".parquet")
+  )
 
-if (!aws.s3::object_exists(remote_file)) {
-  osm_roads <- opq(bbox = "Cook County, IL") %>%
-    add_osm_feature(
-      key = "highway",
-      value = c("motorway", "trunk", "primary")
-    ) %>%
-    osmdata_sf() %>%
-    .$osm_lines %>%
-    select(osm_id, name) %>%
-    st_transform(4326) %>%
-    mutate(geometry_3435 = st_transform(geometry, 3435))
+  if (!aws.s3::object_exists(remote_file)) {
+    # Update the datetime in the opq function
+    osm_roads <- opq(bbox = "Cook County, IL",
+                     datetime = paste0(year, "-01-01T00:00:00Z"),
+                     timeout = 900) %>%
+      add_osm_feature(
+        key = "highway",
+        value = c("motorway", "trunk", "primary")
+      ) %>%
+      osmdata_sf() %>%
+      .$osm_lines %>%
+      select(osm_id, name) %>%
+      st_transform(4326) %>%
+      mutate(geometry_3435 = st_transform(geometry, 3435))
 
-  geoarrow::write_geoparquet(osm_roads, remote_file)
+    geoarrow::write_geoparquet(osm_roads, remote_file)
+  }
 }
