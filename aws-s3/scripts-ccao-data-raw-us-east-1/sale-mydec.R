202585ae909a5bc0f09ff294647218f5c1c2554d
William Ridgeway <10358980+wrridgeway@users.noreply.github.com>
2023-11-14 15:22:33 -0600

    Remove 2015 Lake Michigan coastline artifacts (#222)
    * Remove odd 2015 coastline artifacts
    
    * Linting
    
    * Linting
    
    * Linting
    
    * Linting
    
    * Update aws-s3/scripts-ccao-data-warehouse-us-east-1/spatial-environment.R
    
    happy to
    
    Co-authored-by: Dan Snow <31494343+dfsnow@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Dan Snow <31494343+dfsnow@users.noreply.github.com>

--- aws-s3/scripts-ccao-data-raw-us-east-1/sale-mydec.R
+++ aws-s3/scripts-ccao-data-raw-us-east-1/sale-mydec.R
@@ -14,24 +14,27 @@ AWS_S3_RAW_BUCKET <- Sys.getenv("AWS_S3_RAW_BUCKET")
 output_bucket <- file.path(AWS_S3_RAW_BUCKET, "sale", "mydec")
 
 # Mydec file addresses
-files <- xml2::read_html('https://tax.illinois.gov/localgovernments/property/mydecdatafiles.html') %>%
-  html_nodes('a') %>%
+files <- xml2::read_html(
+  "https://tax.illinois.gov/localgovernments/property/mydecdatafiles.html"
+  ) %>%
+  html_nodes("a") %>%
   html_attr("href") %>%
   str_subset("ptax203")
 
 # Function to scrape IDOR data and write to S3
 down_up <- function(x) {
-
   year <- str_extract(x, pattern = "[0-9]{4}")
 
-  if (!aws.s3::object_exists(file.path(output_bucket, glue("{year}.parquet")))) {
-
+  if (
+    !aws.s3::object_exists(file.path(output_bucket, glue("{year}.parquet")))
+    ) {
     print(glue("Uploading data for: {year}"))
 
     tmp1 <- tempfile(fileext = ".zip")
     tmp2 <- tempfile()
 
-    # Grab zipped file - https://tax.illinois.gov/localgovernments/property/mydecdatafiles.html
+    # Grab zipped file
+    # https://tax.illinois.gov/localgovernments/property/mydecdatafiles.html
     download.file(glue("https://tax.illinois.gov{x}"), destfile = tmp1)
 
     # Unzip
@@ -40,9 +43,7 @@ down_up <- function(x) {
     # upload to S3
     readr::read_delim(list.files(tmp2, full.names = TRUE), delim = "\t") %>%
       write_parquet(file.path(output_bucket, glue("{year}.parquet")))
-
   }
-
 }
 
 
