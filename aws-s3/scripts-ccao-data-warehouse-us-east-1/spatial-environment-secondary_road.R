00c49fadea41e25dac22c549f3e52d1163a77e15
Michael Wagner <wagnerleemichael@gmail.com>
2023-11-21 16:29:40 +0000

    Add subset for testing

--- aws-s3/scripts-ccao-data-warehouse-us-east-1/spatial-environment-secondary_road.R
+++ aws-s3/scripts-ccao-data-warehouse-us-east-1/spatial-environment-secondary_road.R
@@ -40,8 +40,8 @@ for (year in years) {
     paste0("secondary_road-", year, ".parquet"))
 
   # Simplify linestrings
-  current_data <- read_geoparquet_sf(ingest_file_secondary) %>%
-    mutate(geometry_3435 = st_simplify(geometry_3435, dTolerance = 10))
+  current_data <- read_geoparquet_sf(ingest_file_secondary) #%>%
+    #mutate(geometry_3435 = st_simplify(geometry_3435, dTolerance = 10))
 
   # Initiate master data set with first available year, add column for de-duping
   if (is.null(master_dataset)) {
@@ -71,7 +71,7 @@ for (year in years) {
 
         # Ingest Major roads data for the next year
         major_roads_post <- read_geoparquet_sf(ingest_file_major_post)
-# st_difference - sf
+
         # Apply filter for both prior and post year major roads, this filter
         # accounts for the case where:
         # - A previously major road becomes secondary
@@ -105,53 +105,6 @@ for (year in years) {
     master_dataset <- dedup_data %>%
       mutate(temporal = 0)
 
-
-    # This code is an imperfect solution to the changing osm_id problem. We
-    # condense road names into singular roads if they overlap. If they don't
-    # overlap, they remain as two distinct geometries with the same road name.
-    # This allows us to filter on distance, it doesn't catch everything, but
-    # does catch a large majority of floating geometries.
-
-    processed_data <- dedup_data %>%
-      # Group the data by the 'name' field
-      group_by(name) %>%
-      # Apply a function to each group (each unique name)
-      group_map(~ {
-        # '.x' is the subset of data for the current group
-        df <- .x
-        # Calculate intersections of geometries within each group
-        interactions <- st_intersects(df$geometry)
-
-        # Map over each row in the group's data frame
-        map_df(seq_len(nrow(df)), function(i) {
-          # If the current row does not intersect with any other,
-          # return it as is. This check avoids self-intersection
-          if (!any(interactions[[i]] != i)) {
-            return(df[i, ])
-          }
-          # Identify indices of geometries that intersect with the current one,
-          # excluding the current geometry itself
-          connected <- unlist(interactions[i]) %>% setdiff(i)
-          # If there are connected geometries, merge them
-          if (length(connected) > 0) {
-            # Merge the current geometry with all connected ones
-            merged_geom <- st_union(df$geometry[c(i, connected)])
-            # Return a tibble with the merged geometry and the group name.
-            return(tibble(name = df$name[i], geometry = merged_geom))
-          } else {
-            # If no connected geometries, return the current geometry as is
-            return(df[i, ])
-          }
-        })
-      }) %>%
-      # Combine all the processed data frames into one data frame
-      bind_rows()
-      mutate(length = st_length(geometry),
-             geometry_3435 = st_transform(geometry, 3435)) %>%
-      st_as_sf(sf_column_name = "geometry")  %>%
-      filter(length > units::set_units(250, "m")) %>%
-      select(-length)
-
   }
 
   # Define the output file path for the data to write
@@ -166,6 +119,51 @@ for (year in years) {
 
 }
 
+
+#
+# Difference method
+#
+dedup_data_union <- dedup_data %>%
+  #mutate(geometry = st_buffer(geo))
+  summarise(geometry = st_union(geometry))
+
+major_roads_post_union <- major_roads_post %>%
+  #select(geometry) %>%
+  summarise((geometry = st_union(geometry)))
+
+result <- st_difference(st_buffer(dedup_data_union, 1),
+                        st_buffer(major_roads_post_union, 1))
+
+#
+# Intersection method
+#
+intersection_result <- st_intersection(dedup_data_union, major_roads_post_union)
+data2_modified <- st_difference(dedup_data_union, intersection_result)
+
+#
+# Isolated Ashland differences / test buffer
+#
+new_bbox <- st_bbox(c(xmin = -87.68, ymin = 41.885,
+                      xmax = -87.655, ymax = 42),
+                    crs = st_crs(dedup_data))
+
+subset_dedup_data <- st_intersection(dedup_data, st_as_sfc(new_bbox))
+subset_major_roads_post <- st_intersection(major_roads_post, st_as_sfc(new_bbox))
+
+subset_dedup_data_union <- subset_dedup_data %>%
+  summarise((geometry = st_union(geometry)))
+subset_major_roads_post_union <- subset_major_roads_post %>%
+  summarise((geometry = st_union(geometry)))
+
+result_subset <- st_difference(subset_dedup_data_union,
+                               #subset_major_roads_prior_union
+                               subset_major_roads_post_union)
+
+result_subset <- st_difference(st_buffer(subset_dedup_data_union, 0.00001),
+                                 st_buffer(subset_major_roads_post_union, 0.00001))
+
+
+#
 
 
 
