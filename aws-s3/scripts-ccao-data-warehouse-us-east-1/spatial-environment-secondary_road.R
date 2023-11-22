be52ce5fde340f5c653cd7dde58bc040fa571db4
Michael Wagner <wagnerleemichael@gmail.com>
2023-11-22 17:11:04 +0000

    Add troubleshooting checkpoint

--- aws-s3/scripts-ccao-data-warehouse-us-east-1/spatial-environment-secondary_road.R
+++ aws-s3/scripts-ccao-data-warehouse-us-east-1/spatial-environment-secondary_road.R
@@ -4,6 +4,8 @@ library(dplyr)
 library(geoarrow)
 library(osmdata)
 library(sf)
+library(sfnetworks)
+library(tidygraph)
 source("utils.R")
 
 # This script is designed to ingest spatial data on secondary roads for each
@@ -16,10 +18,11 @@ source("utils.R")
 # addition will become our 2015 secondary roads data. If there are identical
 # osm_id observations between 2014 and 2015, we preserve the data from 2014.
 #
-# Note: We only add new secondary roads if they were NOT previously major roads,
-# and if they don't become major roads in the future.
-#
-# This is to prevent classifying a road as both major and secondary
+# We only add new secondary roads if they were not previously major roads, are
+# not in the corresponding year data in our additive major roads, and if they
+# don't become major roads in the future. This is accomplished by filtering on
+# osm_id and name, along with some spatial network cleaning to clean up what was
+# missed. This is to prevent classifying a road as both major and secondary.
 
 # Instantiate S3 bucket names
 AWS_S3_RAW_BUCKET <- Sys.getenv("AWS_S3_RAW_BUCKET")
@@ -40,8 +43,8 @@ for (year in years) {
     paste0("secondary_road-", year, ".parquet"))
 
   # Simplify linestrings
-  current_data <- read_geoparquet_sf(ingest_file_secondary) #%>%
-    #mutate(geometry_3435 = st_simplify(geometry_3435, dTolerance = 10))
+  current_data <- read_geoparquet_sf(ingest_file_secondary) %>%
+    mutate(geometry_3435 = st_simplify(geometry_3435, dTolerance = 10))
 
   # Initiate master data set with first available year, add column for de-duping
   if (is.null(master_dataset)) {
@@ -57,8 +60,15 @@ for (year in years) {
       paste0("year=", year - 1),
       paste0("major_road-", year - 1, ".parquet"))
 
+    ingest_file_major_current <- file.path(
+      AWS_S3_WAREHOUSE_BUCKET, "spatial",
+      "environment", "major_road",
+      paste0("year=", year),
+      paste0("major_road-", year, ".parquet"))
+
     # Ingest Major roads data for the prior year
     major_roads_prior <- read_geoparquet_sf(ingest_file_major_prior)
+    major_roads_current <- read_geoparquet_sf(ingest_file_major_current)
 
       # This if/else block prevents us from indexing a future
       # year that doesn't exist yet
@@ -81,12 +91,14 @@ for (year in years) {
         current_data <-
           current_data %>%
           filter(!osm_id %in% major_roads_prior$osm_id,
-                 !osm_id %in% major_roads_post$osm_id)
+                 !osm_id %in% major_roads_post$osm_id,
+                 !name %in% major_roads_current$name)
       } else {
         # Apply filter only for prior year major roads
         current_data <-
           current_data %>%
-          filter(!osm_id %in% major_roads_prior$osm_id)
+          filter(!osm_id %in% major_roads_prior$osm_id,
+                 !name %in% major_roads_current$name)
       }
 
     # Create temporal column to preserve earliest data
@@ -105,6 +117,47 @@ for (year in years) {
     master_dataset <- dedup_data %>%
       mutate(temporal = 0)
 
+    # - - - - - - - - - - - - - -
+    # Use networks to trim remaining islands
+    # presumably resulting from OSM tag mismatches
+    # - - - - - - - - - - - - - -
+    dedup_network <- dedup_data %>% as_sfnetwork(directed=F)
+
+    # Identify connected components in the network
+    network <- dedup_network %>%
+      activate(nodes) %>%
+      mutate(component = as.factor(group_components()))
+
+    # Create a mapping of node components
+    node_component <- as_tibble(network, active = "nodes")
+
+    # Map component data to edges
+    network <- network %>%
+      activate(edges) %>%
+      mutate(from_component = node_component$component[from],
+             to_component = node_component$component[to])
+
+    # Here we assume that an edge belongs to a component if both its nodes do
+    network <- network %>%
+      mutate(edge_component = ifelse(from_component == to_component,
+                                     from_component,
+                                     NA))
+
+    # First convert the network with edge components into a regular tibble
+    network_edges <- as_tibble(network, active = "edges")
+
+    # Now calculate the size of each component
+    component_sizes <- network_edges %>%
+      filter(!is.na(edge_component)) %>%
+      group_by(edge_component) %>%
+      summarise(size = n())
+
+    # Trim islands
+    data_to_write <- component_sizes %>%
+      filter(size > 5) %>%
+      mutate(geoemetry_3435 = st_transform(geometry, 3435)) %>%
+      select(-edge_component, -size)
+
   }
 
   # Define the output file path for the data to write
@@ -115,58 +168,25 @@ for (year in years) {
     paste0("secondary_road-", year, ".parquet")
   )
 
-  #geoarrow::write_geoparquet(processed_data, output_file)
-
+  #geoarrow::write_geoparquet(data_to_write, output_file)
 }
 
 
-#
-# Difference method
-#
-dedup_data_union <- dedup_data %>%
-  #mutate(geometry = st_buffer(geo))
-  summarise(geometry = st_union(geometry))
+intersected_data <- st_intersection(dedup_data, data_to_write)
 
-major_roads_post_union <- major_roads_post %>%
-  #select(geometry) %>%
-  summarise((geometry = st_union(geometry)))
-
-result <- st_difference(st_buffer(dedup_data_union, 1),
-                        st_buffer(major_roads_post_union, 1))
-
-#
-# Intersection method
-#
-intersection_result <- st_intersection(dedup_data_union, major_roads_post_union)
-data2_modified <- st_difference(dedup_data_union, intersection_result)
-
-#
-# Isolated Ashland differences / test buffer
-#
-new_bbox <- st_bbox(c(xmin = -87.68, ymin = 41.885,
-                      xmax = -87.655, ymax = 42),
-                    crs = st_crs(dedup_data))
-
-subset_dedup_data <- st_intersection(dedup_data, st_as_sfc(new_bbox))
-subset_major_roads_post <- st_intersection(major_roads_post, st_as_sfc(new_bbox))
-
-subset_dedup_data_union <- subset_dedup_data %>%
-  summarise((geometry = st_union(geometry)))
-subset_major_roads_post_union <- subset_major_roads_post %>%
-  summarise((geometry = st_union(geometry)))
-
-result_subset <- st_difference(subset_dedup_data_union,
-                               #subset_major_roads_prior_union
-                               subset_major_roads_post_union)
-
-result_subset <- st_difference(st_buffer(subset_dedup_data_union, 0.00001),
-                                 st_buffer(subset_major_roads_post_union, 0.00001))
-
-
-#
+intersected_data_cast <- st_cast(intersected_data, "MULTIPOLYGON")
 
 
 
 
+
+# # Create an interactive map
+leaflet() %>%
+  addTiles() %>%
+  addPolylines(data = intersected_data,
+               color = "blue",
+               weight = 2,
+               opacity = 1.0) %>%
+  setView(lng = -87.6298, lat = 41.8781, zoom = 10)
 
 
