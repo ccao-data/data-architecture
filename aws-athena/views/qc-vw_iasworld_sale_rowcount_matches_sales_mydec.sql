533c95134794a636bf55daa8d053b94a9e94eb52
Damonamajor <damon.major@cookcountyil.gov>
2023-12-04 21:57:21 +0000

    Black Changes

--- aws-athena/views/qc-vw_iasworld_sale_rowcount_matches_sales_mydec.sql
+++ aws-athena/views/qc-vw_iasworld_sale_rowcount_matches_sales_mydec.sql
@@ -16,7 +16,7 @@ mydec_cte AS (
     SELECT
         SUBSTR(m.year_of_sale, 1, 4) AS year,
         COUNT(DISTINCT m.document_number) AS my_dec_sales
-    FROM sale.mydec m
+    FROM sale.mydec AS m
     WHERE CAST(SUBSTR(m.year_of_sale, 1, 4) AS INTEGER) >= 2014
     GROUP BY SUBSTR(m.year_of_sale, 1, 4)
     ORDER BY SUBSTR(m.year_of_sale, 1, 4)
@@ -28,12 +28,14 @@ comparison AS (
         i.iasworld_sales,
         m.my_dec_sales,
         CASE
-            WHEN i.iasworld_sales > 1.05 * m.my_dec_sales THEN 'IasWorld 5% Higher'
+            WHEN
+                i.iasworld_sales > 1.05 * m.my_dec_sales
+                THEN 'IasWorld 5% Higher'
             WHEN m.my_dec_sales > 1.05 * i.iasworld_sales THEN 'Mydec 5% Higher'
             ELSE 'No significant difference'
         END AS comparison
-    FROM iasworld_sales_cte i
-    INNER JOIN mydec_cte m ON i.year = m.year
+    FROM iasworld_sales_cte AS i
+    INNER JOIN mydec_cte AS m ON i.year = m.year
 )
 
 SELECT * FROM comparison;
