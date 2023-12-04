0c7cf75c99f4d3a942b60c340af3aa87be1d02fd
Damonamajor <damon.major@cookcountyil.gov>
2023-12-04 21:56:32 +0000

    modified to fix linting

--- aws-athena/views/qc-vw_iasworld_sale_rowcount_matches_sales_mydec.sql
+++ aws-athena/views/qc-vw_iasworld_sale_rowcount_matches_sales_mydec.sql
@@ -1,41 +1,39 @@
-WITH sales_cte AS (
+WITH iasworld_sales_cte AS (
     SELECT
-        SUBSTR(sales.saledt, 1, 4) AS year,
-        COUNT(DISTINCT sales.instruno) AS iasworld_sales
+        SUBSTR(iasworld.sales.saledt, 1, 4) AS year,
+        COUNT(DISTINCT iasworld.sales.instruno) AS iasworld_sales
     FROM iasworld.sales
     WHERE
-        sales.deactivat IS NULL
-        AND sales.cur = 'Y'
-        AND sales.instruno IS NOT NULL
-        AND CAST(SUBSTR(sales.saledt, 1, 4) AS INTEGER) >= 2014
-    GROUP BY SUBSTR(sales.saledt, 1, 4)
-    ORDER BY SUBSTR(sales.saledt, 1, 4)
+        iasworld.sales.deactivat IS NULL
+        AND iasworld.sales.cur = 'Y'
+        AND iasworld.sales.instruno IS NOT NULL
+        AND CAST(SUBSTR(iasworld.sales.saledt, 1, 4) AS INTEGER) >= 2014
+    GROUP BY SUBSTR(iasworld.sales.saledt, 1, 4)
+    ORDER BY SUBSTR(iasworld.sales.saledt, 1, 4)
 ),
 
 mydec_cte AS (
     SELECT
-        SUBSTR(year_of_sale, 1, 4) AS year,
-        COUNT(DISTINCT document_number) AS my_dec_sales
-    FROM sale.mydec
-    WHERE CAST(SUBSTR(year_of_sale, 1, 4) AS INTEGER) >= 2014
-    GROUP BY SUBSTR(year_of_sale, 1, 4)
-    ORDER BY SUBSTR(year_of_sale, 1, 4)
+        SUBSTR(m.year_of_sale, 1, 4) AS year,
+        COUNT(DISTINCT m.document_number) AS my_dec_sales
+    FROM sale.mydec m
+    WHERE CAST(SUBSTR(m.year_of_sale, 1, 4) AS INTEGER) >= 2014
+    GROUP BY SUBSTR(m.year_of_sale, 1, 4)
+    ORDER BY SUBSTR(m.year_of_sale, 1, 4)
 ),
 
 comparison AS (
     SELECT
-        s.year,
-        s.iasworld_sales,
+        i.year AS comparison_year,
+        i.iasworld_sales,
         m.my_dec_sales,
         CASE
-            WHEN
-                s.iasworld_sales > 1.05 * m.my_dec_sales
-                THEN 'IasWorld 5% Higher'
-            WHEN m.my_dec_sales > 1.05 * s.iasworld_sales THEN 'Mydec 5% Higher'
+            WHEN i.iasworld_sales > 1.05 * m.my_dec_sales THEN 'IasWorld 5% Higher'
+            WHEN m.my_dec_sales > 1.05 * i.iasworld_sales THEN 'Mydec 5% Higher'
             ELSE 'No significant difference'
         END AS comparison
-    FROM sales_cte AS s
-    INNER JOIN mydec_cte AS m ON s.year = m.year
+    FROM iasworld_sales_cte i
+    INNER JOIN mydec_cte m ON i.year = m.year
 )
 
-SELECT * FROM comparison
+SELECT * FROM comparison;
