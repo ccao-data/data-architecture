a12692a467976a2dec028f3b48e2af3a0ca5ab34
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2023-12-06 21:19:40 +0000

    Clean up Δ high/low value

--- aws-athena/views/qc-vw_iasworld_change_in_high_low_value_sales.sql
+++ aws-athena/views/qc-vw_iasworld_change_in_high_low_value_sales.sql
@@ -1,3 +1,5 @@
+-- View that counts the number of sales with a price less than 10k or greater
+-- than 1m for each year. Includes the previous year's count for comparison.
 WITH sales_cte AS (
     SELECT
         SUBSTR(sales.saledt, 1, 4) AS year,
@@ -5,34 +7,27 @@ WITH sales_cte AS (
             AS price_less_than_10k_count,
         COUNT(CASE WHEN sales.price > 1000000 THEN 1 END)
             AS price_greater_than_1m_count
-    FROM iasworld.sales
+    FROM {{ source('iasworld', 'sales') }} AS sales
     WHERE
         sales.deactivat IS NULL
         AND sales.cur = 'Y'
         AND sales.price IS NOT NULL
         AND sales.instruno IS NOT NULL
-        AND CAST(SUBSTR(sales.saledt, 1, 4) AS INTEGER) BETWEEN 2014 AND 2040
+        AND CAST(SUBSTR(sales.saledt, 1, 4) AS INTEGER) BETWEEN 2014 AND YEAR(
+            CURRENT_DATE
+        )
     GROUP BY SUBSTR(sales.saledt, 1, 4)
-    ORDER BY SUBSTR(sales.saledt, 1, 4)
 )
 
 SELECT
     year,
     price_less_than_10k_count,
     price_greater_than_1m_count,
-    prev_year_price_less_than_10k_count,
-    prev_year_price_greater_than_1m_count
-FROM (
-    SELECT
-        year,
-        price_less_than_10k_count,
-        price_greater_than_1m_count,
-        LAG(price_less_than_10k_count)
-            OVER (ORDER BY year)
-            AS prev_year_price_less_than_10k_count,
-        LAG(price_greater_than_1m_count)
-            OVER (ORDER BY year)
-            AS prev_year_price_greater_than_1m_count
-    FROM sales_cte
-) AS result
-WHERE CAST(year AS INTEGER) > 2014;
+    LAG(price_less_than_10k_count)
+        OVER (ORDER BY year)
+        AS prev_year_price_less_than_10k_count,
+    LAG(price_greater_than_1m_count)
+        OVER (ORDER BY year)
+        AS prev_year_price_greater_than_1m_count
+FROM sales_cte
+WHERE year > '2014'
