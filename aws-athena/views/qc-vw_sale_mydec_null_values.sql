c9e1fe3284b37ec659a8fb43ad81fa5749060908
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2023-12-11 01:28:19 +0000

    Use dbt_utils.not_null_proportion for mydec

--- aws-athena/views/qc-vw_sale_mydec_null_values.sql
+++ aws-athena/views/qc-vw_sale_mydec_null_values.sql
@@ -1,18 +1,12 @@
 -- View that identifies sales with null values in key fields.
 SELECT
-    SUBSTR(year_of_sale, 1, 4) AS year,
-    COUNT(CASE WHEN line_11_full_consideration IS NULL THEN 1 END)
-        AS price_null_count,
-    COUNT(CASE WHEN full_address IS NULL THEN 1 END) AS address_null_count,
-    COUNT(CASE WHEN line_5_instrument_type IS NULL THEN 1 END)
-        AS deed_null_count,
-    COUNT(
-        CASE WHEN LENGTH(buyer_name) < 2 OR buyer_name IS NULL THEN 1 END
-    ) AS buyer_null_count,
-    COUNT(
-        CASE WHEN LENGTH(seller_name) < 2 OR seller_name IS NULL THEN 1 END
-    ) AS seller_null_count
-FROM {{ source('sale', 'mydec') }}
-WHERE SUBSTR(year_of_sale, 1, 4) >= '2014'
-GROUP BY SUBSTR(year_of_sale, 1, 4)
-ORDER BY SUBSTR(year_of_sale, 1, 4)
+    SUBSTR(mydec.year_of_sale, 1, 4) AS year,
+    mydec.line_11_full_consideration AS price,
+    mydec.full_address AS address,
+    mydec.line_5_instrument_type AS deed_type,
+    CASE WHEN LENGTH(mydec.buyer_name) < 2 THEN NULL ELSE mydec.buyer_name END
+        AS buyer,
+    CASE WHEN LENGTH(mydec.seller_name) < 2 THEN NULL ELSE mydec.seller_name END
+        AS seller
+FROM {{ source('sale', 'mydec') }} AS mydec
+WHERE SUBSTR(mydec.year_of_sale, 1, 4) >= '2014'
