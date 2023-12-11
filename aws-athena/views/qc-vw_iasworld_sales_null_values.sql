4cfe4c800f5bd52d24f009576120cd97877932b6
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2023-12-11 23:14:31 +0000

    Remove upper limit for sale range

--- aws-athena/views/qc-vw_iasworld_sales_null_values.sql
+++ aws-athena/views/qc-vw_iasworld_sales_null_values.sql
@@ -10,6 +10,4 @@ WHERE
     sales.deactivat IS NULL
     AND sales.cur = 'Y'
     AND sales.instruno IS NOT NULL
-    AND CAST(SUBSTR(sales.saledt, 1, 4) AS INTEGER) BETWEEN 2014 AND YEAR(
-        CURRENT_DATE
-    )
+    AND SUBSTR(sales.saledt, 1, 4) >= '2014'
