945ceea2e68594700d3d51d3af79054f23b5fdd2
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2023-12-11 00:38:52 +0000

    Refactor nulls view

--- aws-athena/views/qc-vw_iasworld_sales_null_values.sql
+++ aws-athena/views/qc-vw_iasworld_sales_null_values.sql
@@ -1,15 +1,10 @@
 -- View that identifies sales with null values in key fields.
 SELECT
     SUBSTR(sales.saledt, 1, 4) AS year,
-    COUNT(CASE WHEN sales.price IS NULL THEN 1 END) AS price_null_count,
-    COUNT(CASE WHEN sales.instrtyp IS NULL THEN 1 END) AS deed_null_count,
-    COUNT(CASE WHEN LENGTH(sales.own1) < 2 OR sales.own1 IS NULL THEN 1 END)
-        AS buyer_null_count,
-    COUNT(
-        CASE
-            WHEN LENGTH(sales.oldown) < 2 OR sales.oldown IS NULL THEN 1
-        END
-    ) AS seller_null_count
+    sales.price,
+    sales.instrtyp AS deed_type,
+    CASE WHEN LENGTH(sales.own1) < 2 THEN NULL ELSE sales.own1 END AS buyer,
+    CASE WHEN LENGTH(sales.oldown) < 2 THEN NULL ELSE sales.oldown END AS seller
 FROM {{ source('iasworld', 'sales') }} AS sales
 WHERE
     sales.deactivat IS NULL
@@ -18,5 +13,3 @@ WHERE
     AND CAST(SUBSTR(sales.saledt, 1, 4) AS INTEGER) BETWEEN 2014 AND YEAR(
         CURRENT_DATE
     )
-GROUP BY SUBSTR(sales.saledt, 1, 4)
-ORDER BY SUBSTR(sales.saledt, 1, 4)
