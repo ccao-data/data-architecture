2a4e9b2f5f5d955f6a6393a38cc0f8b0c4f5e161
Jean Cochrane <jean@jeancochrane.com>
2023-12-21 17:02:27 -0600

    Add descriptions and categories for remaining iasworld tests

--- dbt/tests/generic/test_not_accepted_values.sql
+++ dbt/tests/generic/test_not_accepted_values.sql
@@ -1,13 +1,15 @@
--- Override built-in accepted_values generic so that it can return extra
+-- Override `dbt_utils.not_accepted_values` generic so that it can return extra
 -- columns for debugging
-{% test accepted_values(model, column_name, values, quote=True, select_columns=[]) %}
+{% test not_accepted_values(
+    model, column_name, values, quote=True, select_columns=[]
+) %}
 
     {%- set columns_csv = select_columns | join(", ") %}
 
     select {{ column_name }}{%- if columns_csv %}, {{ columns_csv }}{% endif %}
     from {{ model }}
     where
-        {{ column_name }} not in (
+        {{ column_name }} in (
             {% for value in values -%}
                 {% if quote -%}'{{ value }}'
                 {%- else -%}{{ value }}
