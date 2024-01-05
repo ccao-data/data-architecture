cf794511b82edd657787347afb818835698bc31a
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2024-01-05 23:04:22 +0000

    Adjust census PUMA data reference year

--- aws-athena/ctas/location-census_2010.sql
+++ aws-athena/ctas/location-census_2010.sql
@@ -64,7 +64,7 @@ distinct_joined AS (
             ST_POINT(dp.x_3435, dp.y_3435),
             ST_GEOMFROMBINARY(cen.geometry_3435)
         )
-    WHERE cen.year = '2021'
+    WHERE cen.year = '2012' -- PUMA spatial data isn't available before 2012
     GROUP BY dp.x_3435, dp.y_3435, cen.year
 )
 
