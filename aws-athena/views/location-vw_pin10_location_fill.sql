a6264165bca83a2d43f56413263ff2e5ca88bc69
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2024-01-08 20:49:02 +0000

    Language correction

--- aws-athena/views/location-vw_pin10_location_fill.sql
+++ aws-athena/views/location-vw_pin10_location_fill.sql
@@ -170,7 +170,7 @@ LEFT JOIN {{ ref('location.environment') }} AS env_flood_fs
 LEFT JOIN {{ ref('location.environment') }} AS env_ohare_noise_contour
     ON pin.pin10 = env_ohare_noise_contour.pin10
     AND cyf.env_ohare_noise_contour_data_year = env_ohare_noise_contour.year
--- Airport noise is joined differently year since it's filled during ingest and
+-- Airport noise is joined differently since it's filled during ingest and
 -- values for env_airport_noise_data_year won't match values for year
 LEFT JOIN {{ ref('location.environment') }} AS env_airport_noise
     ON pin.pin10 = env_airport_noise.pin10
