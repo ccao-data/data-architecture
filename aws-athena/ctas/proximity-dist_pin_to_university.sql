61affd122e70731087dead9c6eaadd5ee75d61da
Damonamajor <damon.major@cookcountyil.gov>
2024-01-18 18:49:18 +0000

    Remove code

--- aws-athena/ctas/proximity-dist_pin_to_university.sql
+++ aws-athena/ctas/proximity-dist_pin_to_university.sql
@@ -10,8 +10,6 @@
 
 SELECT
     pcl.pin10,
-    CAST(CAST(ARBITRARY(xy.gniscode) AS BIGINT) AS VARCHAR)
-        AS nearest_university_gnis_code,
     ARBITRARY(xy.name) AS nearest_university_name,
     ARBITRARY(xy.dist_ft) AS nearest_university_dist_ft,
     ARBITRARY(xy.year) AS nearest_university_data_year,
@@ -20,7 +18,7 @@ FROM {{ source('spatial', 'parcel') }} AS pcl
 INNER JOIN
     ( {{ dist_to_nearest_geometry(source('spatial', 'school_location')) }} )
         AS xy
-    ON CAST(pcl.x_3435 AS FLOAT) = ST_X(xy.geometry_3435)
-    AND CAST(pcl.y_3435 AS FLOAT) = ST_Y(xy.geometry_3435)
+    ON pcl.x_3435 = xy.x_3435
+    AND pcl.y_3435 = xy.y_3435
     AND pcl.year = xy.pin_year
 GROUP BY pcl.pin10, pcl.year
