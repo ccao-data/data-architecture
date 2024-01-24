bb0925c18f2d728dc72eb8279c2187e37aeeee2d
Dan Snow <daniel.snow@cookcountyil.gov>
2024-01-24 23:59:24 +0000

    Fix join and star select

--- aws-athena/ctas/proximity-dist_pin_to_pin.sql
+++ aws-athena/ctas/proximity-dist_pin_to_pin.sql
@@ -1,10 +1,11 @@
 -- View that finds the 3 nearest neighbor PINs for every PIN for every year
-SELECT * FROM {{ ref('proximity.dist_pin_to_pin_1km') }}
+SELECT * FROM proximity.dist_pin_to_pin_1km
 UNION
 -- Anti-join is necessary here because the 10km table sometimes contains
 -- slightly different distances which don't get de-duped via UNION
-SELECT *
-FROM {{ ref('proximity.dist_pin_to_pin_10km') }} AS km10
-LEFT JOIN {{ ref('proximity.dist_pin_to_pin_1km') }} AS km1
-USING (pin10, year)
+SELECT km10.*
+FROM proximity.dist_pin_to_pin_10km AS km10
+LEFT JOIN proximity.dist_pin_to_pin_1km AS km1
+    ON km10.pin10 = km1.pin10
+    AND km10.year = km1.year
 WHERE km1.pin10 IS NULL
