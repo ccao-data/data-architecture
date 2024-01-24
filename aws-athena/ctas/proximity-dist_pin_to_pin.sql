c184f99c678a6e69c1a34fa3ac607367e4fd899d
Dan Snow <daniel.snow@cookcountyil.gov>
2024-01-24 23:52:36 +0000

    Add anti-join to dedupe pin proximity view

--- aws-athena/ctas/proximity-dist_pin_to_pin.sql
+++ aws-athena/ctas/proximity-dist_pin_to_pin.sql
@@ -1,4 +1,10 @@
 -- View that finds the 3 nearest neighbor PINs for every PIN for every year
 SELECT * FROM {{ ref('proximity.dist_pin_to_pin_1km') }}
 UNION
-SELECT * FROM {{ ref('proximity.dist_pin_to_pin_10km') }}
+-- Anti-join is necessary here because the 10km table sometimes contains
+-- slightly different distances which don't get de-duped via UNION
+SELECT *
+FROM {{ ref('proximity.dist_pin_to_pin_10km') }} AS km10
+LEFT JOIN {{ ref('proximity.dist_pin_to_pin_1km') }} AS km1
+USING (pin10, year)
+WHERE km1.pin10 IS NULL
