7fe6c33f6b77fc4de468003c278d8d0bb10fbebc
William Ridgeway <10358980+wrridgeway@users.noreply.github.com>
2024-03-14 14:43:01 -0500

    Remove dummy PINs from `vw_pin_universe` (#342)
    * Test where clause filtering performance
    
    * Linting
    
    * Fix conditional
    
    * Adjust test temporarily
    
    * Test alternate syntax performance
    
    * Run more tests on first specification
    
    * Simplify test pin filter
    
    * Remove test pins from other views
    
    * Revert lin endings
    
    * Check dbt test
    
    * Revert dbt test
    
    * Add numeric pins dbt test
    
    * Correct dbt test languange
    
    * Add context commenting, improve nonnumeric pin field test
    
    * Add pin length condition to dbt tests
    
    * Remove pins based on different criteria
    
    * Fix line endings
    
    * Correct parid alias
    
    * Remove appeal view from test pin testing

--- aws-athena/views/default-vw_pin_universe.sql
+++ aws-athena/views/default-vw_pin_universe.sql
@@ -140,3 +140,7 @@ LEFT JOIN {{ source('ccao', 'corner_lot') }} AS lot
 
 WHERE par.cur = 'Y'
     AND par.deactivat IS NULL
+    -- Remove any parcels with non-numeric characters
+    -- or that are not 14 characters long
+    AND REGEXP_COUNT(par.parid, '[a-zA-Z]') = 0
+    AND LENGTH(par.parid) = 14
