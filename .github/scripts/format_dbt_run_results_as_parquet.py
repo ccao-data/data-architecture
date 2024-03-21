bcbe71555a25563de693e8c50b3f0762a7529bc6
Jean Cochrane <jean@jeancochrane.com>
2024-03-21 17:23:24 -0500

    Sketch out solution for saving and uploading dbt run results to S3

---
+++ .github/scripts/format_dbt_run_results_as_parquet.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#
+# Parse a dbt run_results.json file and reformat it as parquet so that it
+# can be analyzed in Athena.
+#
+# Example usage:
+#
+#   python3 format_dbt_run_results_as_parquet.py \
+#       target/run_results.json > target/run_results.parquet
+
+if __name__ == "__main__":
+    # TODO: Flesh this out
+    pass
