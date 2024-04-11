14095e2a9bb15106ae95807f2e8302a34a3b7e45
Damonamajor <56321109+Damonamajor@users.noreply.github.com>
2024-04-11 15:31:27 -0500

    Add Chicago cbd boundary to `default.vw_pin_universe` (#372)
    * Initial test
    
    * replace file year
    
    * Add cleaning function
    
    * Invoke cbd function in clean_economy function
    
    * Initial work on cta
    
    * Changes to cta
    
    * Add cbd num
    
    * Add vw_pin_universe
    
    * Switch ctas to num
    
    * Add to location view
    
    * Add to location fill
    
    * Add cbd to dbt docs
    
    * Remove Chicago
    
    * Move to econ
    
    * Add join
    
    * Change to data_year
    
    * Add to crosswalk year fill
    
    * Add join
    
    * lintr
    
    * lint
    
    * lintr
    
    ---------
    
    Co-authored-by: Sweaty Handshake <william.ridgeway@cookcountyil.gov>

--- aws-s3/scripts-ccao-data-raw-us-east-1/spatial-economy.R
+++ aws-s3/scripts-ccao-data-raw-us-east-1/spatial-economy.R
@@ -58,14 +58,37 @@ if (!aws.s3::object_exists(remote_file_qualified_opportunity_zone)) {
 
   # Only keep Cook County, save as geojson
   st_read(grep(
-      "shp",
-      list.files(tmp2, recursive = TRUE, full.names = TRUE),
-      value = TRUE
-    )) %>%
+    "shp",
+    list.files(tmp2, recursive = TRUE, full.names = TRUE),
+    value = TRUE
+  )) %>%
     filter(STATENAME == "Illinois" & COUNTYNAME == "Cook") %>%
     st_write(tmp3)
 
   # Upload, clean
   save_local_to_s3(remote_file_qualified_opportunity_zone, tmp3)
   file.remove(c(tmp1, tmp3, list.files(tmp2, full.names = TRUE)))
+}
+
+##### CENTRAL BUSINESS DISTRICT
+remote_file_central_business_district <- file.path(
+  output_bucket, "central_business_district",
+  paste0(2016, ".geojson")
+)
+
+# Write file to S3 if it doesn't already exist
+if (!aws.s3::object_exists(remote_file_central_business_district)) {
+  tmp_file_central_business_district <- tempfile(fileext = ".geojson")
+  download.file(
+    paste0(
+      "https://data.cityofchicago.org/api/geospatial/",
+      "tksj-nvsw?method=export&format=GeoJSON"
+    ),
+    tmp_file_central_business_district
+  )
+  save_local_to_s3(
+    remote_file_central_business_district,
+    tmp_file_central_business_district
+  )
+  file.remove(tmp_file_central_business_district)
 }
