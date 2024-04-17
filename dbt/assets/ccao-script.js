677913e0cc37dff6af0f4d2cd165c42dc8d8243e
Dan Snow <dan@sno.ws>
2024-04-16 22:01:59 -0500

    Fix JS lint errors

--- dbt/assets/ccao-script.js
+++ dbt/assets/ccao-script.js
@@ -1,18 +1,17 @@
 // Drops the useless "Group" tab from dbt docs
-var groupTab = document.querySelector('[ng-class="{active: (nav_selected == \'group\')}"]');
+const groupTab = document.querySelector('[ng-class="{active: (nav_selected == \'group\')}"]');
 if (groupTab) {
-    groupTab.parentNode.remove()
+  groupTab.parentNode.remove();
 }
 
 // Open the "Database" tab and "awsdatacatalog" database by default
-var databaseTab = document.querySelector('[ng-class="{active: (nav_selected == \'database\')}"]');
+const databaseTab = document.querySelector('[ng-class="{active: (nav_selected == \'database\')}"]');
 if (databaseTab) {
-    databaseTab.click();
+  databaseTab.click();
 }
-var targetTable = Array
-    .from(document.querySelectorAll('span.filename-normal'))
-    .find(span => span.textContent.trim() === 'awsdatacatalog');
+const targetTable = Array
+  .from(document.querySelectorAll('span.filename-normal'))
+  .find(span => span.textContent.trim() === 'awsdatacatalog');
 if (targetTable) {
-    targetTable.click();
+  targetTable.click();
 }
-
