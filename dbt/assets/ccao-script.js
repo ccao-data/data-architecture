dd0ec5eb12f3ce0ca0c6eed9c77b7724a8ea494a
Dan Snow <daniel.snow@cookcountyil.gov>
2024-04-17 15:43:13 +0000

    Fix script linting error

--- dbt/assets/ccao-script.js
+++ dbt/assets/ccao-script.js
@@ -1,5 +1,5 @@
 // Use event listener to wait until DOM is loaded
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   // Drops the useless "Group" tab from dbt docs
   const groupTab = document.querySelector('[ng-class="{active: (nav_selected == \'group\')}"]')
   if (groupTab) {
