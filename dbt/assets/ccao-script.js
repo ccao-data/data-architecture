d2b9ff8a9e250d1557b2710f0f1629ac09653dcb
Dan Snow <daniel.snow@cookcountyil.gov>
2024-04-17 15:26:17 +0000

    Add DOM event listener for dbt page cleaner

--- dbt/assets/ccao-script.js
+++ dbt/assets/ccao-script.js
@@ -1,17 +1,20 @@
-// Drops the useless "Group" tab from dbt docs
-const groupTab = document.querySelector('[ng-class="{active: (nav_selected == \'group\')}"]')
-if (groupTab) {
-  groupTab.parentNode.remove()
-}
+// Use event listener to wait until DOM is loaded
+document.addEventListener('DOMContentLoaded', function() {
+  // Drops the useless "Group" tab from dbt docs
+  const groupTab = document.querySelector('[ng-class="{active: (nav_selected == \'group\')}"]')
+  if (groupTab) {
+    groupTab.parentNode.remove()
+  }
 
-// Open the "Database" tab and "awsdatacatalog" database by default
-const databaseTab = document.querySelector('[ng-class="{active: (nav_selected == \'database\')}"]')
-if (databaseTab) {
-  databaseTab.click()
-}
-const targetTable = Array
-  .from(document.querySelectorAll('span.filename-normal'))
-  .find(span => span.textContent.trim() === 'awsdatacatalog')
-if (targetTable) {
-  targetTable.click()
+  // Open the "Database" tab and "awsdatacatalog" database by default
+  const databaseTab = document.querySelector('[ng-class="{active: (nav_selected == \'database\')}"]')
+  if (databaseTab) {
+    databaseTab.click()
+  }
+  const targetTable = Array
+    .from(document.querySelectorAll('span.filename-normal'))
+    .find(span => span.textContent.trim() === 'awsdatacatalog')
+  if (targetTable) {
+    targetTable.click()
+  }
 }
