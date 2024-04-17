ade91d17fc9755d0aab36fe33b463a20cb761812
Dan Snow <31494343+dfsnow@users.noreply.github.com>
2024-04-17 10:08:00 -0500

    Tweak dbt data docs sidebar (#389)
    * Tweak dbt data docs sidebar
    
    * Fix JS lint errors
    
    * Drop semi-colons :(

---
+++ dbt/assets/ccao-script.js
@@ -0,0 +1,17 @@
+// Drops the useless "Group" tab from dbt docs
+const groupTab = document.querySelector('[ng-class="{active: (nav_selected == \'group\')}"]')
+if (groupTab) {
+  groupTab.parentNode.remove()
+}
+
+// Open the "Database" tab and "awsdatacatalog" database by default
+const databaseTab = document.querySelector('[ng-class="{active: (nav_selected == \'database\')}"]')
+if (databaseTab) {
+  databaseTab.click()
+}
+const targetTable = Array
+  .from(document.querySelectorAll('span.filename-normal'))
+  .find(span => span.textContent.trim() === 'awsdatacatalog')
+if (targetTable) {
+  targetTable.click()
+}
