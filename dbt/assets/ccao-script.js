1c99c9648740a9d08035e48d671c7004e313b849
Dan Snow <daniel.snow@cookcountyil.gov>
2024-04-17 15:18:36 +0000

    Fixup docs JS cleaner script
    Add DOM event listener for dbt page cleaner
    
    Add missing paren to dbt JS script
    
    Move script to end of body
    
    Fix script linting error

--- dbt/assets/ccao-script.js
+++ dbt/assets/ccao-script.js
@@ -1,17 +1,24 @@
-// Drops the useless "Group" tab from dbt docs
-const groupTab = document.querySelector('[ng-class="{active: (nav_selected == \'group\')}"]')
-if (groupTab) {
-  groupTab.parentNode.remove()
-}
+// Use event listener to wait until DOM is loaded
+document.addEventListener('DOMContentLoaded', function () {
+  // Drops the useless "Group" tab from dbt docs
+  const groupTab = document.querySelector('[ng-class="{active: (nav_selected == \'group\')}"]')
+  if (groupTab) {
+    groupTab.parentNode.remove()
+  }
 
-// Open the "Database" tab and "awsdatacatalog" database by default
-const databaseTab = document.querySelector('[ng-class="{active: (nav_selected == \'database\')}"]')
-if (databaseTab) {
-  databaseTab.click()
-}
-const targetTable = Array
-  .from(document.querySelectorAll('span.filename-normal'))
-  .find(span => span.textContent.trim() === 'awsdatacatalog')
-if (targetTable) {
-  targetTable.click()
-}
+  // Open the "Database" tab and "awsdatacatalog" database by default
+  const databaseTab = document.querySelector('[ng-class="{active: (nav_selected == \'database\')}"]')
+  if (databaseTab) {
+    databaseTab.click()
+  }
+
+  // Wait until all other JS is executed before clicking the table
+  setTimeout(function () {
+    const targetTable = Array
+      .from(document.querySelectorAll('span.filename-normal'))
+      .find(span => span.textContent.trim() === 'awsdatacatalog')
+    if (targetTable) {
+      targetTable.click()
+    }
+  }, 200)
+})
