0b0f80f2b939eee7cad24c92736297e53194dfbe
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2024-04-22 14:43:39 +0000

    Refactor vw_pin_township_class to use ccao.class_dict

--- aws-athena/views/reporting-vw_pin_township_class.sql
+++ aws-athena/views/reporting-vw_pin_township_class.sql
@@ -20,30 +20,8 @@ SELECT
     leg.user1 AS township_code,
     SUBSTR(correct.nbhd, 3, 3) AS nbhd,
     correct.class,
-    CASE
-        WHEN SUBSTR(correct.class, 1, 2) IN ('EX', 'RR') THEN correct.class
-        -- OA classes contain their major class as the third and final
-        -- character
-        WHEN SUBSTR(correct.class, 1, 2) = 'OA' THEN SUBSTR(correct.class, 3, 1)
-        WHEN correct.class IN (
-                '500', '535', '501', '516', '517', '522', '523',
-                '526', '527', '528', '529', '530', '531', '532',
-                '533', '535', '590', '591', '592', '597', '599'
-            ) THEN '5A'
-        WHEN correct.class IN (
-                '550', '580', '581', '583', '587', '589', '593'
-            ) THEN '5B'
-        ELSE SUBSTR(correct.class, 1, 1)
-    END AS major_class,
-    CASE WHEN correct.class IN ('299', '399') THEN 'CONDO'
-        WHEN correct.class IN ('211', '212') THEN 'MF'
-        WHEN
-            correct.class IN (
-                '202', '203', '204', '205', '206', '207',
-                '208', '209', '210', '234', '278', '295'
-            )
-            THEN 'SF'
-    END AS property_group,
+    groups.major_class,
+    groups.modeling_group AS property_group,
     CASE
         WHEN
             MOD(CAST(correct.taxyr AS INT), 3) = 0
@@ -67,5 +45,7 @@ LEFT JOIN {{ source('iasworld', 'legdat') }} AS leg
     AND leg.deactivat IS NULL
 LEFT JOIN {{ source('spatial', 'township') }} AS town
     ON leg.user1 = town.township_code
+LEFT JOIN {{ source('ccao', 'class_dict') }} AS groups
+    ON correct.class = groups.class_code
 WHERE correct.cur = 'Y'
     AND correct.deactivat IS NULL
