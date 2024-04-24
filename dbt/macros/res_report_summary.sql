fb35d7c5cdc1776500b3c97da0205728220778ea
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2024-04-24 18:49:03 +0000

    Consolidate macros

--- dbt/macros/res_report_summary.sql
+++ dbt/macros/res_report_summary.sql
@@ -1,10 +1,5 @@
--- Macro that takes a model and returns its row count grouped and
--- sorted by a given column. The sort order for the results can be specified
--- with the `ordering` argument, defaulting to "asc".
---
--- If the `print` argument is set to True (default is False), the macro will
--- print the results of the query to stdout, which allows this macro to be used
--- by scripts to return data.
+-- Macros that takes summarize FMVs and sales by different groupings for
+-- reporting.res_report_summary.
 {% macro res_report_summarize_values(geo_type, prop_group) %}
     select
         triad,
@@ -31,5 +26,28 @@
         {% endif %}
         {% if prop_group %} year, property_group
         {% elif not prop_group %} year
+        {% endif %}
+{% endmacro %}
+
+{% macro res_report_summarize_sales(geo_type, prop_group) %}
+    select
+        {% if geo_type == "Town" %} township_code as geography_id,
+        {% elif geo_type == "TownNBHD" %} townnbhd as geography_id,
+        {% endif %}
+        sale_year,
+        {% if prop_group %} property_group,
+        {% elif not prop_group %} 'ALL REGRESSION' as property_group,
+        {% endif %}
+        min(sale_price) as sale_min,
+        approx_percentile(sale_price, 0.5) as sale_median,
+        max(sale_price) as sale_max,
+        count(*) as sale_n
+    from sales
+    group by
+        {% if geo_type == "Town" %} township_code,
+        {% elif geo_type == "TownNBHD" %} townnbhd,
+        {% endif %}
+        {% if prop_group %} sale_year, property_group
+        {% elif not prop_group %} sale_year
         {% endif %}
 {% endmacro %}
