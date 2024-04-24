f65baf25475cb6399f35096b7b1127924e23c109
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2024-04-24 16:53:30 +0000

    Linting

--- dbt/macros/res_report_summarize_values.sql
+++ dbt/macros/res_report_summarize_values.sql
@@ -8,37 +8,28 @@
 {% macro res_report_summarize_values(geo_type, prop_group) %}
     select
         triad,
-        '{{ geo_type }}' AS geography_type,
-        {% if prop_group %}
-        property_group,
-        {% else %}
-        'ALL REGRESSION' as property_group,
+        '{{ geo_type }}' as geography_type,
+        {% if prop_group %} property_group,
+        {% else %} 'ALL REGRESSION' as property_group,
         {% endif %}
         assessment_stage,
-        {% if geo_type == "Town" %}
-        township_code as geography_id,
-        {% elif geo_type == "TownNBHD" %}
-        townnbhd as geography_id,
+        {% if geo_type == "Town" %} township_code as geography_id,
+        {% elif geo_type == "TownNBHD" %} townnbhd as geography_id,
         {% endif %}
         year,
-        APPROX_PERCENTILE(total, 0.5) as fmv_median,
-        COUNT(*) as pin_n,
-        APPROX_PERCENTILE(total_land_sf, 0.5) as land_sf_median,
-        APPROX_PERCENTILE(total_bldg_sf, 0.5) as bldg_sf_median,
-        APPROX_PERCENTILE(yrblt, 0.5) as yrblt_median
+        approx_percentile(total, 0.5) as fmv_median,
+        count(*) as pin_n,
+        approx_percentile(total_land_sf, 0.5) as land_sf_median,
+        approx_percentile(total_bldg_sf, 0.5) as bldg_sf_median,
+        approx_percentile(yrblt, 0.5) as yrblt_median
     from all_values
     group by
         assessment_stage,
         triad,
-        {% if geo_type == "Town" %}
-        township_code,
-        {% elif geo_type == "TownNBHD" %}
-        townnbhd,
+        {% if geo_type == "Town" %} township_code,
+        {% elif geo_type == "TownNBHD" %} townnbhd,
         {% endif %}
-        {% if prop_group %}
-        year,
-        property_group
-        {% else %}
-        year
+        {% if prop_group %} year, property_group
+        {% else %} year
         {% endif %}
 {% endmacro %}
