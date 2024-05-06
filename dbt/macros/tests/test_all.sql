a6c6e1fa0df06ef174cd6dd522e46aefa6fe4554
Dan Snow <daniel.snow@cookcountyil.gov>
2024-05-06 16:02:26 +0000

    Drop slugify test

--- dbt/macros/tests/test_all.sql
+++ dbt/macros/tests/test_all.sql
@@ -1,7 +1,6 @@
 -- dbt macro unit testing inspired by this blog post:
 -- https://docs.getdbt.com/blog/unit-testing-dbt-packages
 {% macro test_all() %}
-    {% do test_slugify() %}
     {% do test_generate_schema_name() %}
     {% do test_generate_alias_name() %}
     {% do test_format_additional_select_columns() %}
