d43fa6e9168d9a0198853068ba24c14bbefef14e
Jean Cochrane <jean@jeancochrane.com>
2024-05-15 17:32:46 -0500

    Try moving sc.addPyFile() call above model() definition in reporting.ratio_stats

--- dbt/models/reporting/reporting.ratio_stats.py
+++ dbt/models/reporting/reporting.ratio_stats.py
@@ -1,12 +1,14 @@
 # pylint: skip-file
+
+# Load dependency bundle so that we can import deps
+# type: ignore
+sc.addPyFile(  # noqa: F821
+    "s3://ccao-dbt-athena-ci-us-east-1/dependencies/jeancochrane_439_refactor_reportingratio_stats_to_use_new_pattern_for_python_model_dependencies/reporting.ratio_stats.requirements.zip"  # noqa: E501
+)
+
+
 def model(dbt, spark_session):
     dbt.config(materialized="table")
-
-    # Load dependency bundle so that we can import deps
-    # type: ignore
-    sc.addPyFile(  # noqa: F821
-        "s3://ccao-dbt-athena-ci-us-east-1/dependencies/jeancochrane_439_refactor_reportingratio_stats_to_use_new_pattern_for_python_model_dependencies/reporting.ratio_stats.requirements.zip"  # noqa: E501
-    )
 
     import numpy as np  # noqa: E402 F401
     import pandas as pd  # noqa: E402 F401
