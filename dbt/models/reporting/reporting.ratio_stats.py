04f00153b62d3cb0a418f2618c3de3d4c305f2df
Jean Cochrane <jean@jeancochrane.com>
2024-05-15 17:36:09 -0500

    Temporarily try pointing to different package bundle in reporting.ratio_stats

--- dbt/models/reporting/reporting.ratio_stats.py
+++ dbt/models/reporting/reporting.ratio_stats.py
@@ -3,7 +3,7 @@
 # Load dependency bundle so that we can import deps
 # type: ignore
 sc.addPyFile(  # noqa: F821
-    "s3://ccao-dbt-athena-ci-us-east-1/dependencies/jeancochrane_439_refactor_reportingratio_stats_to_use_new_pattern_for_python_model_dependencies/reporting.ratio_stats.requirements.zip"  # noqa: E501
+    "s3://ccao-dbt-athena-dev-us-east-1/packages/spark-packages.zip"
 )
 
 
