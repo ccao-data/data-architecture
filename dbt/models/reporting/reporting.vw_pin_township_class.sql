154c51085b17032ca634db832ae02a0fd5adfa2b
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2024-05-20 21:56:08 +0000

    Commenting

--- dbt/models/reporting/reporting.vw_pin_township_class.sql
+++ dbt/models/reporting/reporting.vw_pin_township_class.sql
@@ -18,7 +18,10 @@ year while the view is. Therefore, if any part of a parcel (card, lline, etc.)
 triggers AHSAP status, the parcel as a whole will be identified as AHSAP (since
 TRUE > FALSE). user columns with "AI" prefixes that trigger AHSAP status are
 'alternative CDU' and user columns with "SAP" prefixes that trigger AHSAP status
-are 'incentive number'. comdat.user13 is 'subclass 2'. */
+are 'incentive number'. comdat.user13 is 'subclass 2'.
+
+For an explanation of AHSAP and why it involves so many different iasWorld
+tables, see: https://www.cookcountyassessor.com/affordable-housing */
 ahsap AS (
     SELECT
         par.parid,
