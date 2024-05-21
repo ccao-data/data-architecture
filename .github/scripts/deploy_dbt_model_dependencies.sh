7e823f97302d4d69301e962874f11d8951e41188
Jean Cochrane <jeancochrane@users.noreply.github.com>
2024-05-21 11:35:05 -0500

    Refactor Python model dependencies to use global package repository (#461)
    * Use python_model_dependency view to pull dependency bundle in reporting.ratio_stats
    
    * Make python_model_dependency a table instead of a view
    
    * Use correct path to remote dependency bundle in reporting.ratio_stats model
    
    * Temporarily simplify reporting.ratio_stats for the purposes of testing
    
    * Temporarily try calling sc.addPyFile on the fully qualified path for debugging
    
    * Try moving sc.addPyFile() call above model() definition in reporting.ratio_stats
    
    * Temporarily try pointing to different package bundle in reporting.ratio_stats
    
    * Remove numpy and pandas from reporting.ratio_stats packages list
    
    * Revert "Temporarily try pointing to different package bundle in reporting.ratio_stats"
    
    This reverts commit 04f00153b62d3cb0a418f2618c3de3d4c305f2df.
    
    * Revert "Try moving sc.addPyFile() call above model() definition in reporting.ratio_stats"
    
    This reverts commit d43fa6e9168d9a0198853068ba24c14bbefef14e.
    
    * Revert "Temporarily try calling sc.addPyFile on the fully qualified path for debugging"
    
    This reverts commit 3dc48940be8d74864ab573b57f60f64ce4cd2abe.
    
    * Update deploy_dbt_model_dependencies.sh to remove top level of zip archive
    
    * Remove numpy from package bundle in deploy_dbt_model_dependencies.sh
    
    * Appease shellcheck by fixing glob syntax in deploy_dbt_model_dependencies.sh
    
    * Exclude pandas as a preinstalled package in deploy_dbt_model_dependencies.sh
    
    * Generalize the preinstalled_packages pattern in deploy_dbt_model_dependencies.sh
    
    * Run pip install with --no-deps in deploy_dbt_model_dependencies.sh
    
    * Revert "Temporarily simplify reporting.ratio_stats for the purposes of testing"
    
    This reverts commit 58e415ba468c241bf0d773744949bb53af773622.
    
    * Move ccao.python_model_dependency from aws-athena/ctas/ to dbt/models/ directory
    
    * Switch reporting.ratio_stats to new Python model dependency design
    
    * WIP refactor deploy_dbt_model_dependencies.sh to follow new Python model dependency design
    
    * Finish refactor of deploy_dbt_model_dependencies.sh to follow new model deployment scheme
    
    * Slight refactor to deploy_dbt_model_dependencies.sh to warn faster on no deps found
    
    * Reformat reporting.ratio_stats.py
    
    * Fix remote dependency dir location in reporting.ratio_stats.py
    
    * Temporarily add attrs dependencies to reporting tables to test multiple packages
    
    * Simplify deploy_dbt_model_dependencies.sh
    
    * Remove deprecated ccao.python_model_dependency model
    
    * Switch back to correct package list for reporting.ratio_stats

--- .github/scripts/deploy_dbt_model_dependencies.sh
+++ .github/scripts/deploy_dbt_model_dependencies.sh
@@ -1,20 +1,38 @@
 #!/usr/bin/env bash
-# Parse, bundle, and upload dbt Python model requirements to S3.
+# Parse, install, zip, and upload dbt Python model requirements to S3.
 #
-# Takes one or more optional positional arguments representing the models for
-# which you would like to upload dependencies. For example, the following
-# command will only upload dependencies for the `reporting.ratio_stats` model:
+# This script checks for Python model dependencies by searching every
+# model node for a `config.packages` attribute and uploading a zip archive
+# for every version of every package that is specified in one or more such
+# attributes. It expects attribute values to be well-formed Python requirement
+# specifiers that are pinned to a specific version of the package, e.g.
+# `assesspy==1.0.1` is allowed but not `assesspy==1.0.*` or `assesspy~=1.0.1`.
 #
-# ../.github/scripts/deploy_dbt_model_dependencies.sh reporting.ratio_stats
+# If a zip archive for a given version of a dependency is already present in
+# the remote dependency dir on S3, the script will skip installing, zipping,
+# and uploading that dependency version.
 #
-# When no arguments are provided, the script will upload dependencies for all
-# models:
+# Zip archives uploaded using this script can be used to import packages
+# into a Python model script with a set of import calls like so:
 #
-# ../.github/scripts/deploy_dbt_model_dependencies.sh
+#   sc.addPyFile(f"{s3_dependency_dir}/attrs==23.2.0.zip")  # noqa: F821
+#   sc.addPyFile(f"{s3_dependency_dir}/assesspy==1.1.0.zip")  # noqa: F821
+#   import attrs  # noqa: E402
+#   import assesspy  # noqa: E402
+#
+# Dependencies are installed using the `--no-deps` flag for `pip install`,
+# meaning that your dependency's dependencies will not be installed or
+# uploaded by default. Make sure you check to see what additional dependencies
+# are required for your dependency and include those in the `config.packages`
+# array.
 #
 # Assumes that dbt, python, and jq are installed and available on the caller's
 # path. Also assumes that it is being run in a directory containing a
 # dbt project.
+#
+# Example usage:
+#
+#   ../.github/scripts/deploy_dbt_model_dependencies.sh
 set -euo pipefail
 
 # Fall back to local development if a dbt $TARGET is not specified
@@ -22,15 +40,7 @@ target=${TARGET:-"dev"}
 echo "Parsing dbt Python model dependencies for target '$target'"
 
 # Determine the remote location where bundled dependencies will be deployed
-s3_dependency_dir=$(dbt run-operation print_s3_dependency_dir --quiet \
-    --target "$target")
-
-# Parse optional positional arguments representing a restricted list of
-# models for which to upload dependencies
-declare -a specified_models
-if [ $# -gt 0 ]; then
-    specified_models=("$@")
-fi
+s3_dependency_dir="s3://ccao-athena-dependencies-us-east-1"
 
 # Compile the DAG so that we have up-to-date info on dependencies
 dbt compile --quiet --target "$target"
@@ -38,147 +48,102 @@ dbt compile --quiet --target "$target"
 # Extract the config.packages attribute from only the models
 # where it is set
 packages_json=$(jq '
-    .nodes
-    | with_entries(
-        select(
-            .value.config.packages != null and
-            (.value.config.packages | length) > 0
-        )
-    )
-    | with_entries(
-        .value = .value.config.packages
-    )
+    .nodes |
+        map(
+            select(
+                .config.packages != null and (.config.packages | length) > 0
+            ) |
+            .config.packages
+        ) |
+        flatten |
+        unique
 ' target/manifest.json)
+
+echo "Extracted the following requirements from the dbt DAG:"
+echo "$packages_json"
+
+# Convert the JSON array to a bash array
+mapfile -t packages_array < <(echo "$packages_json" | jq -r '.[]')
 
 # Set a flag to check whether any dependencies were found so we can log
 # a warning if none are found
-dependencies_found=false
+new_dependencies_found=false
+
+# Before moving forward, validate the package names to ensure that they
+# match our requirements for deterministic installation. We want to do this as
+# soon as possible to quickly return to the user in case of a formatting error
+for package_name in "${packages_array[@]}"; do
+    if [[ "$package_name" != *'=='* ]] || [[ "$package_name" == *'*'* ]]; then
+        echo "Error: Requirement identifier '${package_name}' must include '==' and must not include '*'."
+        echo "This is necessary to ensure that dependency installs are deterministic."
+        echo "Please edit the config.packages attribute containing this string and try again."
+        exit 1
+    fi
+    # Set the flag to confirm new dependencies were found, since if we reach
+    # this point it means that packages_array is non-empty
+    new_dependencies_found=true
+done
+
+# Warn and exit early if no dependencies were found
+if [ "$new_dependencies_found" == "false" ]; then
+    echo "No Python model dependencies found"
+    exit 0
+fi
+
+# Define the name of the virtualenv and set a flag to record whether it
+# has been created
+venv_name="deploy-dbt-model-dependencies-venv"
+venv_created=false
 
 # Iterate over each key-value pair representing a set of package
-# dependencies and output those dependencies to a requirements file.
-# Note that the input to the `read` call is passed in using process
-# substitution so that we can avoid a subshell and thereby modify the
-# global $dependencies_found variable in the context of the loop
-while read -r item; do
-    # Extract the name of the model and its list of dependencies
-    model_name=$(echo "$item" | jq -r '.model_name')
-    dependencies=$(echo "$item" | jq -r '.dependencies[]')
-
-    # Split the model name by '.' and take the last two elements to
-    # generate an ID, since model names always have extraneous prefixes
-    # in their DAG representation
-    model_identifier=$(echo "$model_name" | awk -F. '{print $(NF-1)"."$NF}')
-
-    # If a list of models was specified in the positional args, skip
-    # any models that are not in the list.
-    # The preliminary -n check is necessary to support older versions of bash
-    # that treat empty arrays as unbound variables
-    if [ -n "${specified_models+x}" ] && [ ${#specified_models[@]} -gt 0 ]; then
-        should_process=false
-        for specified_model in "${specified_models[@]}"; do
-            if [ "$specified_model" == "$model_identifier" ]; then
-                should_process=true
-                break
-            fi
-        done
-
-        if [ "$should_process" == "false" ]; then
-            continue
-        fi
+# dependencies and output those dependencies to a requirements file
+for package_name in "${packages_array[@]}"; do
+    # Check to see if the package already exists in S3
+    zip_archive_name="${package_name}.zip"
+    existing_package_url="${s3_dependency_dir}/${zip_archive_name}"
+    if aws s3 ls "$existing_package_url" > /dev/null 2>&1; then
+        echo "$package_name already exists at ${existing_package_url}, skipping upload"
+        continue
     fi
 
-    # Set the flag to confirm dependencies were found, since if we reach this
-    # point it means that A) a line of input was present that triggered the
-    # `read` loop and B) the model represented by the line was not excluded
-    # via the `specified_models` input arguments
-    dependencies_found=true
-
-    # Define the filename for the requirements file
-    requirements_filename="${model_identifier}.requirements.txt"
-
-    # Write the list of dependencies to the requirements file
-    echo "$dependencies" | tr ' ' '\n' > "$requirements_filename"
-    echo "Python requirements file $requirements_filename created with contents:"
-    cat "$requirements_filename"
-
-    # Check if the archive already exists on S3
-    existing_requirements_file_url="${s3_dependency_dir}/${requirements_filename}"
-    if aws s3 ls "$existing_requirements_file_url" > /dev/null 2>&1; then
-        echo "Diffing against $existing_requirements_file_url to check for changes"
-
-        # Create a temporary directory to download the S3 file
-        temp_dir=$(mktemp -d)
-        existing_requirements_filename="${temp_dir}/${requirements_filename}"
-
-        # Download the S3 file to the temporary directory
-        aws s3 cp "$existing_requirements_file_url" "$existing_requirements_filename" --no-progress
-
-        # Sort and compare the contents of the requirements files
-        skip_upload=false
-        if diff <(sort "$requirements_filename") <(sort "$existing_requirements_filename") > /dev/null; then
-            echo "Skipping upload for $requirements_filename since it has not changed"
-            skip_upload=true
-        else
-            echo "Proceeding with upload for $requirements_filename since it has changed"
-        fi
-
-        # Clean up the temporary directory, since it's no longer needed
-        rm -rf "$temp_dir"
-
-        if [ "$skip_upload" == "true" ]; then
-            # Normally we clean up the requirements file after upload, but if
-            # we're not going to upload the file, we need to clean it up now
-            rm "$requirements_filename"
-            continue
-        fi
+    # Create and activate a Python virtual environment for installing
+    # dependencies. Only do this once we know that we have at least one
+    # dependency to upload, since virtualenv creation takes a few seconds
+    if [ "$venv_created" == "false" ]; then
+        echo "Creating and activating virtualenv at $venv_name"
+        python3 -m venv "$venv_name"
+        # Stop shellcheck from getting mad that the virtualenv dir doesn't exist at
+        # compile time. It's not a problem since the Python `venv` command will
+        # take care of creating it
+        # shellcheck disable=SC1091
+        source "${venv_name}/bin/activate"
+        venv_created=true
     fi
-
-    # Create and activate a Python virtual environment for installing dependencies
-    venv_name="${model_identifier}.venv"
-    echo "Creating and activating virtualenv at $venv_name"
-    python3 -m venv "$venv_name"
-    # Stop shellcheck from getting mad that the virtualenv dir doesn't exist at
-    # compile time. It's not a problem since the Python `venv` command will
-    # take care of creating it
-    # shellcheck disable=SC1091
-    source "${venv_name}/bin/activate"
 
     # Install dependencies into a subdirectory that we can use for bundling
-    subdirectory_name="${model_identifier}/"
+    subdirectory_name="${package_name}/"
     mkdir -p "$subdirectory_name"
-    echo "Installing dependencies from $requirements_filename into $subdirectory_name"
-    pip install -t "$subdirectory_name" -r "$requirements_filename" --no-deps
+    echo "Installing '${package_name}' into $subdirectory_name"
+    pip install -t "$subdirectory_name" "$package_name" --no-deps
 
     # Create a zip archive from the contents of the subdirectory
-    zip_archive_name="${model_identifier}.requirements.zip"
     echo "Creating zip archive $zip_archive_name from $subdirectory_name"
     cd "$subdirectory_name" && zip -q -r9 "../$zip_archive_name" ./* && cd ..
 
     # Upload the zip archive and the requirements file to S3
-    echo "Uploading $zip_archive_name and $requirements_filename to S3"
+    echo "Uploading $zip_archive_name to S3"
     aws s3 cp "$zip_archive_name" "${s3_dependency_dir}/" --no-progress
-    aws s3 cp "$requirements_filename" "${s3_dependency_dir}/" --no-progress
 
-    # Cleanup the intermediate artifacts. This isn't important on CI but
+    # Clean up intermediate artifacts. This isn't important on CI but
     # it's helpful when developing locally
-    echo "Cleaning up intermediate artifacts"
-    deactivate
-    rm "$requirements_filename"
-    rm -rf "$venv_name"
+    echo "Cleaning up package directory and zip archive"
     rm -rf "$subdirectory_name"
     rm "$zip_archive_name"
+done
 
-done < <(
-    # Parse the model name and list of dependencies for each model to
-    # pass into the script.
-    echo "$packages_json" | \
-        # The -rc flag ensures that jq excludes extraneous quotes and
-        # outputs one line for each model, which is an expectation of the
-        # `read` loop above
-        jq -rc 'to_entries[] | {model_name: .key, dependencies: .value}'
-)
-
-# Warn if no dependencies were found
-if [ "$dependencies_found" == "false" ]; then
-    echo "No Python model dependencies found"
+# Cleanup the virtualenv
+if [ "$venv_created" == "true" ]; then
+    echo "Cleaning up virtualenv"
+    deactivate
+    rm -rf "$venv_name"
 fi
--- dbt/models/reporting/reporting.ratio_stats.py
+++ dbt/models/reporting/reporting.ratio_stats.py
@@ -1,234 +1,237 @@
 # pylint: skip-file
-def model(dbt, spark_session):
-    dbt.config(materialized="table")
+# type: ignore
+sc.addPyFile(  # noqa: F821
+    "s3://ccao-athena-dependencies-us-east-1/assesspy==1.1.0.zip"
+)
 
-    # Load dependency bundle so that we can import deps
-    python_model_dependency = dbt.ref("ccao.python_model_dependency")
-    s3_dependency_dir = python_model_dependency.first()["s3_dependency_dir"]
-    # type: ignore
-    sc.addPyFile(  # noqa: F821
-        f"{s3_dependency_dir}/reporting.ratio_stats.requirements.zip"
+import numpy as np  # noqa: E402
+import pandas as pd  # noqa: E402
+from assesspy import boot_ci  # noqa: E402
+from assesspy import cod  # noqa: E402
+from assesspy import prd_met  # noqa: E402
+from assesspy import cod_ci as cod_boot  # noqa: E402
+from assesspy import cod_met, mki, mki_met, prb, prb_met, prd  # noqa: E402
+from assesspy import prd_ci as prd_boot  # noqa: E402
+
+
+def median_boot(ratio, nboot=100, alpha=0.05):
+    return boot_ci(np.median, nboot=nboot, alpha=alpha, ratio=ratio)
+
+
+def ccao_median(x):
+    # Remove top and bottom 5% of ratios as per CCAO Data Department SOPs
+    no_outliers = x.between(
+        x.quantile(0.05), x.quantile(0.95), inclusive="neither"
     )
 
-    import numpy as np  # noqa: E402
-    import pandas as pd  # noqa: E402
-    from assesspy import boot_ci  # noqa: E402
-    from assesspy import cod  # noqa: E402
-    from assesspy import prd_met  # noqa: E402
-    from assesspy import cod_ci as cod_boot  # noqa: E402
-    from assesspy import cod_met, mki, mki_met, prb, prb_met, prd  # noqa: E402
-    from assesspy import prd_ci as prd_boot  # noqa: E402
+    x_no_outliers = x[no_outliers]
 
-    def median_boot(ratio, nboot=100, alpha=0.05):
-        return boot_ci(np.median, nboot=nboot, alpha=alpha, ratio=ratio)
+    median_n = sum(no_outliers)
 
-    def ccao_median(x):
-        # Remove top and bottom 5% of ratios as per CCAO Data Department SOPs
-        no_outliers = x.between(
-            x.quantile(0.05), x.quantile(0.95), inclusive="neither"
-        )
+    median_val = np.median(x_no_outliers)
+    median_ci = median_boot(x_no_outliers, nboot=1000)
+    median_ci = f"{median_ci[0]}, {median_ci[1]}"
 
-        x_no_outliers = x[no_outliers]
+    out = [median_val, median_ci, median_n]
 
-        median_n = sum(no_outliers)
+    return out
 
-        median_val = np.median(x_no_outliers)
-        median_ci = median_boot(x_no_outliers, nboot=1000)
-        median_ci = f"{median_ci[0]}, {median_ci[1]}"
 
-        out = [median_val, median_ci, median_n]
+def ccao_mki(fmv, sale_price):
+    ratio = fmv / sale_price
 
-        return out
+    # Remove top and bottom 5% of ratios as per CCAO Data Department SOPs
+    no_outliers = ratio.between(
+        ratio.quantile(0.05), ratio.quantile(0.95), inclusive="neither"
+    )
 
-    def ccao_mki(fmv, sale_price):
-        ratio = fmv / sale_price
+    fmv_no_outliers = fmv[no_outliers]
+    sale_price_no_outliers = sale_price[no_outliers]
 
-        # Remove top and bottom 5% of ratios as per CCAO Data Department SOPs
-        no_outliers = ratio.between(
+    mki_n = sum(no_outliers)
+
+    if mki_n >= 20:
+        mki_val = mki(fmv_no_outliers, sale_price_no_outliers)
+        met = mki_met(mki_val)
+
+        out = [mki_val, met, mki_n]
+
+    else:
+        out = [None, None, mki_n]
+
+    return out
+
+
+def ccao_cod(ratio):
+    # Remove top and bottom 5% of ratios as per CCAO Data Department SOPs
+    no_outliers = ratio[
+        ratio.between(
             ratio.quantile(0.05), ratio.quantile(0.95), inclusive="neither"
         )
+    ]
 
-        fmv_no_outliers = fmv[no_outliers]
-        sale_price_no_outliers = sale_price[no_outliers]
+    cod_n = no_outliers.size
 
-        mki_n = sum(no_outliers)
+    if cod_n >= 20:
+        cod_val = cod(no_outliers)
+        cod_ci = cod_boot(no_outliers, nboot=1000)
+        cod_ci = f"{cod_ci[0]}, {cod_ci[1]}"
+        met = cod_met(cod_val)
+        out = [cod_val, cod_ci, met, cod_n]
 
-        if mki_n >= 20:
-            mki_val = mki(fmv_no_outliers, sale_price_no_outliers)
-            met = mki_met(mki_val)
+    else:
+        out = [None, None, None, cod_n]
 
-            out = [mki_val, met, mki_n]
+    return out
 
-        else:
-            out = [None, None, mki_n]
 
-        return out
+def ccao_prd(fmv, sale_price):
+    ratio = fmv / sale_price
 
-    def ccao_cod(ratio):
-        # Remove top and bottom 5% of ratios as per CCAO Data Department SOPs
-        no_outliers = ratio[
-            ratio.between(
-                ratio.quantile(0.05), ratio.quantile(0.95), inclusive="neither"
-            )
+    # Remove top and bottom 5% of ratios as per CCAO Data Department SOPs
+    no_outliers = ratio.between(
+        ratio.quantile(0.05), ratio.quantile(0.95), inclusive="neither"
+    )
+
+    fmv_no_outliers = fmv[no_outliers]
+    sale_price_no_outliers = sale_price[no_outliers]
+
+    prd_n = sum(no_outliers)
+
+    if prd_n >= 20:
+        prd_val = prd(fmv_no_outliers, sale_price_no_outliers)
+        prd_ci = prd_boot(fmv_no_outliers, sale_price_no_outliers, nboot=1000)
+        prd_ci = f"{prd_ci[0]}, {prd_ci[1]}"
+        met = prd_met(prd_val)
+
+        out = [prd_val, prd_ci, met, prd_n]
+
+    else:
+        out = [None, None, None, prd_n]
+
+    return out
+
+
+def ccao_prb(fmv, sale_price):
+    ratio = fmv / sale_price
+
+    # Remove top and bottom 5% of ratios as per CCAO Data Department SOPs
+    no_outliers = ratio.between(
+        ratio.quantile(0.05), ratio.quantile(0.95), inclusive="neither"
+    )
+
+    fmv_no_outliers = fmv[no_outliers]
+    sale_price_no_outliers = sale_price[no_outliers]
+
+    prb_n = sum(no_outliers)
+
+    if prb_n >= 20:
+        prb_model = prb(fmv_no_outliers, sale_price_no_outliers)
+        prb_val = prb_model["prb"]
+        prb_ci = prb_model["95% ci"]
+        prb_ci = f"{prb_ci[0]}, {prb_ci[1]}"
+        met = prb_met(prb_val)
+
+        out = [prb_val, prb_ci, met, prb_n]
+
+    else:
+        out = [None, None, None, prb_n]
+
+    return out
+
+
+def report_summarise(df, geography_id, geography_type):
+    """
+    Aggregates data and calculates summary statistics for given groupings
+    """
+
+    group_cols = [
+        "year",
+        "triad",
+        "geography_type",
+        "property_group",
+        "assessment_stage",
+        "geography_id",
+        "sale_year",
+    ]
+
+    df["geography_id"] = df[geography_id].astype(str)
+    df["geography_type"] = geography_type
+
+    # Remove groups with less than three observations
+    # TODO: Remove/upgrade detect_chasing output
+    df["n"] = df.groupby(group_cols)["ratio"].transform("count")
+    df = df[df["n"] > 3]
+    df = df.groupby(group_cols).apply(
+        lambda x: pd.Series(
+            {
+                "sale_n": np.size(x["triad"]),
+                "ratio": ccao_median(x["ratio"]),
+                "cod": ccao_cod(ratio=x["ratio"]),
+                "mki": ccao_mki(fmv=x["fmv"], sale_price=x["sale_price"]),
+                "prd": ccao_prd(fmv=x["fmv"], sale_price=x["sale_price"]),
+                "prb": ccao_prb(fmv=x["fmv"], sale_price=x["sale_price"]),
+                "detect_chasing": False,
+                "within_20_pct": sum(abs(1 - x["ratio"]) <= 0.20),
+                "within_10_pct": sum(abs(1 - x["ratio"]) <= 0.10),
+                "within_05_pct": sum(abs(1 - x["ratio"]) <= 0.05),
+            }
+        )
+    )
+
+    df[["median_ratio", "median_ratio_ci", "median_ratio_n"]] = pd.DataFrame(
+        df.ratio.tolist(), index=df.index
+    )
+    df[["cod", "cod_ci", "cod_met", "cod_n"]] = pd.DataFrame(
+        df.cod.tolist(), index=df.index
+    )
+    df[["mki", "mki_met", "mki_n"]] = pd.DataFrame(
+        df.mki.tolist(), index=df.index
+    )
+    df[["prd", "prd_ci", "prd_met", "prd_n"]] = pd.DataFrame(
+        df.prd.tolist(), index=df.index
+    )
+    df[["prb", "prb_ci", "prb_met", "prb_n"]] = pd.DataFrame(
+        df.prb.tolist(), index=df.index
+    )
+    df["ratio_met"] = abs(1 - df["median_ratio"]) <= 0.05
+    df["vertical_equity_met"] = df.prd_met | df.prb_met
+
+    # Arrange output columns
+    df = df[
+        [
+            "sale_n",
+            "median_ratio",
+            "median_ratio_ci",
+            "cod",
+            "cod_ci",
+            "cod_n",
+            "prd",
+            "prd_ci",
+            "prd_n",
+            "prb",
+            "prb_ci",
+            "prb_n",
+            "mki",
+            "mki_n",
+            "detect_chasing",
+            "ratio_met",
+            "cod_met",
+            "prd_met",
+            "prb_met",
+            "mki_met",
+            "vertical_equity_met",
+            "within_20_pct",
+            "within_10_pct",
+            "within_05_pct",
         ]
+    ].reset_index()
 
-        cod_n = no_outliers.size
+    return df
 
-        if cod_n >= 20:
-            cod_val = cod(no_outliers)
-            cod_ci = cod_boot(no_outliers, nboot=1000)
-            cod_ci = f"{cod_ci[0]}, {cod_ci[1]}"
-            met = cod_met(cod_val)
-            out = [cod_val, cod_ci, met, cod_n]
 
-        else:
-            out = [None, None, None, cod_n]
-
-        return out
-
-    def ccao_prd(fmv, sale_price):
-        ratio = fmv / sale_price
-
-        # Remove top and bottom 5% of ratios as per CCAO Data Department SOPs
-        no_outliers = ratio.between(
-            ratio.quantile(0.05), ratio.quantile(0.95), inclusive="neither"
-        )
-
-        fmv_no_outliers = fmv[no_outliers]
-        sale_price_no_outliers = sale_price[no_outliers]
-
-        prd_n = sum(no_outliers)
-
-        if prd_n >= 20:
-            prd_val = prd(fmv_no_outliers, sale_price_no_outliers)
-            prd_ci = prd_boot(
-                fmv_no_outliers, sale_price_no_outliers, nboot=1000
-            )
-            prd_ci = f"{prd_ci[0]}, {prd_ci[1]}"
-            met = prd_met(prd_val)
-
-            out = [prd_val, prd_ci, met, prd_n]
-
-        else:
-            out = [None, None, None, prd_n]
-
-        return out
-
-    def ccao_prb(fmv, sale_price):
-        ratio = fmv / sale_price
-
-        # Remove top and bottom 5% of ratios as per CCAO Data Department SOPs
-        no_outliers = ratio.between(
-            ratio.quantile(0.05), ratio.quantile(0.95), inclusive="neither"
-        )
-
-        fmv_no_outliers = fmv[no_outliers]
-        sale_price_no_outliers = sale_price[no_outliers]
-
-        prb_n = sum(no_outliers)
-
-        if prb_n >= 20:
-            prb_model = prb(fmv_no_outliers, sale_price_no_outliers)
-            prb_val = prb_model["prb"]
-            prb_ci = prb_model["95% ci"]
-            prb_ci = f"{prb_ci[0]}, {prb_ci[1]}"
-            met = prb_met(prb_val)
-
-            out = [prb_val, prb_ci, met, prb_n]
-
-        else:
-            out = [None, None, None, prb_n]
-
-        return out
-
-    def report_summarise(df, geography_id, geography_type):
-        """
-        Aggregates data and calculates summary statistics for given groupings
-        """
-
-        group_cols = [
-            "year",
-            "triad",
-            "geography_type",
-            "property_group",
-            "assessment_stage",
-            "geography_id",
-            "sale_year",
-        ]
-
-        df["geography_id"] = df[geography_id].astype(str)
-        df["geography_type"] = geography_type
-
-        # Remove groups with less than three observations
-        # TODO: Remove/upgrade detect_chasing output
-        df["n"] = df.groupby(group_cols)["ratio"].transform("count")
-        df = df[df["n"] > 3]
-        df = df.groupby(group_cols).apply(
-            lambda x: pd.Series(
-                {
-                    "sale_n": np.size(x["triad"]),
-                    "ratio": ccao_median(x["ratio"]),
-                    "cod": ccao_cod(ratio=x["ratio"]),
-                    "mki": ccao_mki(fmv=x["fmv"], sale_price=x["sale_price"]),
-                    "prd": ccao_prd(fmv=x["fmv"], sale_price=x["sale_price"]),
-                    "prb": ccao_prb(fmv=x["fmv"], sale_price=x["sale_price"]),
-                    "detect_chasing": False,
-                    "within_20_pct": sum(abs(1 - x["ratio"]) <= 0.20),
-                    "within_10_pct": sum(abs(1 - x["ratio"]) <= 0.10),
-                    "within_05_pct": sum(abs(1 - x["ratio"]) <= 0.05),
-                }
-            )
-        )
-
-        df[
-            ["median_ratio", "median_ratio_ci", "median_ratio_n"]
-        ] = pd.DataFrame(df.ratio.tolist(), index=df.index)
-        df[["cod", "cod_ci", "cod_met", "cod_n"]] = pd.DataFrame(
-            df.cod.tolist(), index=df.index
-        )
-        df[["mki", "mki_met", "mki_n"]] = pd.DataFrame(
-            df.mki.tolist(), index=df.index
-        )
-        df[["prd", "prd_ci", "prd_met", "prd_n"]] = pd.DataFrame(
-            df.prd.tolist(), index=df.index
-        )
-        df[["prb", "prb_ci", "prb_met", "prb_n"]] = pd.DataFrame(
-            df.prb.tolist(), index=df.index
-        )
-        df["ratio_met"] = abs(1 - df["median_ratio"]) <= 0.05
-        df["vertical_equity_met"] = df.prd_met | df.prb_met
-
-        # Arrange output columns
-        df = df[
-            [
-                "sale_n",
-                "median_ratio",
-                "median_ratio_ci",
-                "cod",
-                "cod_ci",
-                "cod_n",
-                "prd",
-                "prd_ci",
-                "prd_n",
-                "prb",
-                "prb_ci",
-                "prb_n",
-                "mki",
-                "mki_n",
-                "detect_chasing",
-                "ratio_met",
-                "cod_met",
-                "prd_met",
-                "prb_met",
-                "mki_met",
-                "vertical_equity_met",
-                "within_20_pct",
-                "within_10_pct",
-                "within_05_pct",
-            ]
-        ].reset_index()
-
-        return df
+def model(dbt, spark_session):
+    dbt.config(materialized="table")
 
     input = dbt.ref("reporting.ratio_stats_input")
 
