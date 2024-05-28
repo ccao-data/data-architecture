6489a481b3004448bbfef3f9f9f5ba60673e4f1b
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2024-05-28 18:06:32 +0000

    Testing mypy linting workaround

--- etl/scripts-ccao-data-warehouse-us-east-1/housing/housing-ari_index.py
+++ etl/scripts-ccao-data-warehouse-us-east-1/housing/housing-ari_index.py
@@ -12,7 +12,7 @@ s3 = boto3.client("s3")
 temp_file = tempfile.NamedTemporaryFile(suffix=".xlsx", delete=False)
 
 load_dotenv("etl/.Renviron")
-AWS_S3_RAW_BUCKET = os.getenv("AWS_S3_RAW_BUCKET")[5:]
+AWS_S3_RAW_BUCKET = os.getenv("AWS_S3_RAW_BUCKET")[5:]  # type: ignore
 file_key = os.path.join("housing", "ari_index", "2023-ARI.xlsx")
 
 s3.download_file(AWS_S3_RAW_BUCKET, file_key, temp_file.name)
@@ -42,7 +42,9 @@ def upload_df_to_s3_as_parquet(df, bucket, file_name):
     s3.put_object(Bucket=bucket, Key=file_name, Body=parquet_buffer.getvalue())
 
 
-AWS_S3_WAREHOUSE_BUCKET = os.getenv("AWS_S3_WAREHOUSE_BUCKET")[5:]
+AWS_S3_WAREHOUSE_BUCKET = os.getenv("AWS_S3_WAREHOUSE_BUCKET")[
+    5:
+]  # type: ignore
 
 # Upload the Parquet file to S3
 upload_df_to_s3_as_parquet(data, AWS_S3_WAREHOUSE_BUCKET, file_key)
