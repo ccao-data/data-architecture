7959f8002d0b06c351bcd7f22afb6fea6da43523
Damonamajor <damon.major@cookcountyil.gov>
2024-05-29 21:27:17 +0000

    Fix where clause

--- dbt/models/model/model.vw_pin_shared_input.sql
+++ dbt/models/model/model.vw_pin_shared_input.sql
@@ -75,7 +75,7 @@ affordability_risk_index AS (
     FROM {{ source('other', 'ari') }} AS ari
     LEFT JOIN {{ ref('location.census') }} AS tract
         ON ari.geoid = tract.census_tract_geoid
-        AND tract.year = '2023'
+    WHERE tract.year = '2023'
 ),
 
 tax_bill_amount AS (
