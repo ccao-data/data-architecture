a96be69c046f6b907cb6cda8367afacf52740655
Damonamajor <damon.major@cookcountyil.gov>
2024-05-31 20:15:51 +0000

    Working proximity view

--- dbt/models/proximity/proximity.dist_pin_to_stadium.sql
+++ dbt/models/proximity/proximity.dist_pin_to_stadium.sql
@@ -1,17 +1,22 @@
--- CTAS to create a table of distance to the nearest stadium for each PIN
-{{
-    config(
-        materialized='table',
-        partitioned_by=['year'],
-        bucketed_by=['pin10'],
-        bucket_count=1
-    )
-}}
+-- Create a table of distance to the nearest stadium for each PIN
+
+{{ config(
+    materialized='table',
+    partitioned_by=['year'],
+    bucketed_by=['pin10'],
+    bucket_count=1
+) }}
 
 WITH stadiums AS (
-    SELECT *
-    FROM {{ source('spatial', 'parcel') }}
-    WHERE pin10 IN (
+    SELECT
+        pin10 AS pin10_stadium,
+        x_3435 AS x_3435_stadium,
+        y_3435 AS y_3435_stadium,
+        year
+    FROM
+        {{ source('spatial', 'parcel') }}
+    WHERE
+        pin10 IN (
             '1420227002',
             '1718201035',
             '1722110002',
@@ -27,13 +32,23 @@ WITH stadiums AS (
 
 SELECT
     pcl.pin10,
-    ARBITRARY(xy.name) AS nearest_stadium_name,
-    ARBITRARY(xy.dist_ft) AS nearest_stadium_dist_ft,
-    ARBITRARY(xy.year) AS nearest_stadium_data_year,
+    MIN(output.dist_ft) AS nearest_stadium_dist_ft,
     pcl.year
 FROM {{ source('spatial', 'parcel') }} AS pcl
-INNER JOIN ( {{ dist_to_nearest_geometry('stadiums') }} ) AS xy
-    ON pcl.x_3435 = xy.x_3435
-    AND pcl.y_3435 = xy.y_3435
-    AND pcl.year = xy.pin_year
-GROUP BY pcl.pin10, pcl.year
+INNER JOIN (
+    SELECT
+        st.pin10_stadium,
+        pcl.pin10 AS parcel_pin10,
+        st.year,
+        SQRT(
+            POW(st.x_3435_stadium - pcl.x_3435, 2)
+            + POW(st.y_3435_stadium - pcl.y_3435, 2)
+        ) AS dist_ft
+    FROM
+        stadiums AS st, {{ source('spatial', 'parcel') }} AS pcl
+    WHERE
+        pcl.year = st.year
+) AS output
+    ON pcl.pin10 = output.parcel_pin10
+    AND pcl.year = output.year
+GROUP BY pcl.pin10, pcl.year;
