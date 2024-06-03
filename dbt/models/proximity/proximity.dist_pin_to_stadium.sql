7565e68b31f4ee787a307ece3f487c7338bce45f
Damonamajor <damon.major@cookcountyil.gov>
2024-06-03 22:40:18 +0000

    Rework structure

--- dbt/models/proximity/proximity.dist_pin_to_stadium.sql
+++ dbt/models/proximity/proximity.dist_pin_to_stadium.sql
@@ -1,94 +1,108 @@
-{{ config(
-    materialized='table',
-    partitioned_by=['year'],
-    bucketed_by=['pin10'],
-    bucket_count=1
-) }}
+-- CTAS to create a table of distance to the stadiums for each PIN
+{{
+    config(
+        materialized='table',
+        partitioned_by=['year'],
+        bucketed_by=['pin10'],
+        bucket_count=1
+    )
+}}
 
-WITH stadiums AS (
-    SELECT
-        CASE
-            WHEN pin10 = '1420227002' THEN 'Wrigley'
-            WHEN pin10 = '1718201035' THEN 'United Center'
-            WHEN pin10 = '1722110002' THEN 'Soldier Field'
-            WHEN pin10 = '1733400049' THEN 'Guaranteed Rate Field'
-            WHEN pin10 = '1722320018' THEN 'Wintrust Arena'
-            WHEN pin10 = '1717239022' THEN 'UIC Pavilion'
-        END AS stadium_name,
-        pin10 AS pin10_stadium,
-        x_3435 AS x_3435_stadium,
-        y_3435 AS y_3435_stadium,
+WITH distinct_pins AS (
+    SELECT DISTINCT
+        pin10,
+        x_3435,
+        y_3435,
         year
-    FROM
-        {{ source('spatial', 'parcel') }}
-    WHERE
-        pin10 IN (
-            '1420227002',  -- Wrigley
-            '1718201035',  -- United Center
-            '1722110002',  -- Soldier Field
-            '1733400049',  -- Guaranteed Rate Field
-            '1722320018',  -- Wintrust Arena
-            '1717239022'   -- UIC Pavilion
-        )
-        AND year >= '2017'
+    FROM {{ source('spatial', 'parcel') }}
 ),
 
-distances AS (
+xy_to_stadium_dist AS (
     SELECT
-        pcl.pin10,
-        st.stadium_name,
-        st.pin10_stadium,
-        SQRT(
-            POW(st.x_3435_stadium - pcl.x_3435, 2)
-            + POW(st.y_3435_stadium - pcl.y_3435, 2)
-        ) AS dist_ft,
-        pcl.year
-    FROM
-        stadiums AS st
-    CROSS JOIN {{ source('spatial', 'parcel') }} AS pcl
-    WHERE
-        pcl.year = st.year
+        dp.pin10,
+        dp.x_3435,
+        dp.y_3435,
+        dp.year,
+        'Soldier Field' AS stadium_name,
+        ST_DISTANCE(
+            ST_POINT(dp.x_3435, dp.y_3435), ST_POINT(1179603, 1893122)
+        ) AS stadium_dist_ft
+    FROM distinct_pins AS dp
+    UNION ALL
+    SELECT
+        dp.pin10,
+        dp.x_3435,
+        dp.y_3435,
+        dp.year,
+        'Wintrust Arena' AS stadium_name,
+        ST_DISTANCE(
+            ST_POINT(dp.x_3435, dp.y_3435), ST_POINT(1178259, 1889955)
+        ) AS stadium_dist_ft
+    FROM distinct_pins AS dp
+    UNION ALL
+    SELECT
+        dp.pin10,
+        dp.x_3435,
+        dp.y_3435,
+        dp.year,
+        'Guaranteed Rate Field' AS stadium_name,
+        ST_DISTANCE(
+            ST_POINT(dp.x_3435, dp.y_3435), ST_POINT(1174703, 1881406)
+        ) AS stadium_dist_ft
+    FROM distinct_pins AS dp
+    UNION ALL
+    SELECT
+        dp.pin10,
+        dp.x_3435,
+        dp.y_3435,
+        dp.year,
+        'Wrigley Field' AS stadium_name,
+        ST_DISTANCE(
+            ST_POINT(dp.x_3435, dp.y_3435), ST_POINT(1168634, 1924426)
+        ) AS stadium_dist_ft
+    FROM distinct_pins AS dp
+    UNION ALL
+    SELECT
+        dp.pin10,
+        dp.x_3435,
+        dp.y_3435,
+        dp.year,
+        'United Center' AS stadium_name,
+        ST_DISTANCE(
+            ST_POINT(dp.x_3435, dp.y_3435), ST_POINT(1163584, 1899780)
+        ) AS stadium_dist_ft
+    FROM distinct_pins AS dp
+    UNION ALL
+    SELECT
+        dp.pin10,
+        dp.x_3435,
+        dp.y_3435,
+        dp.year,
+        'UIC Pavilion' AS stadium_name,
+        ST_DISTANCE(
+            ST_POINT(dp.x_3435, dp.y_3435), ST_POINT(1168816, 1897726)
+        ) AS stadium_dist_ft
+    FROM distinct_pins AS dp
 ),
 
-nearest_stadiums AS (
+min_stadium_dist AS (
     SELECT
         pin10,
+        x_3435,
+        y_3435,
+        year,
         stadium_name,
-        dist_ft,
-        year,
-        ROW_NUMBER() OVER (PARTITION BY pin10, year ORDER BY dist_ft) AS rn
-    FROM
-        distances
-),
-
-deduplicated AS (
-    SELECT
-        pin10,
-        nearest_stadium_dist_ft,
-        nearest_stadium_name,
-        year,
+        stadium_dist_ft,
         ROW_NUMBER()
-            OVER (PARTITION BY year ORDER BY nearest_stadium_dist_ft)
-            AS year_rn
-    FROM (
-        SELECT
-            ns.pin10,
-            ns.dist_ft AS nearest_stadium_dist_ft,
-            ns.stadium_name AS nearest_stadium_name,
-            ns.year
-        FROM
-            nearest_stadiums AS ns
-        WHERE
-            ns.rn = 1
-    ) AS base_query
+            OVER (PARTITION BY pin10, year ORDER BY stadium_dist_ft)
+            AS rnk
+    FROM xy_to_stadium_dist
 )
 
 SELECT
     pin10,
-    nearest_stadium_dist_ft,
-    nearest_stadium_name,
+    stadium_name,
+    stadium_dist_ft AS min_stadium_dist_ft,
     year
-FROM
-    deduplicated
-WHERE
-    year_rn = 1
+FROM min_stadium_dist
+WHERE rnk = 1;
