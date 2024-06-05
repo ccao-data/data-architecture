eb289162105d4f7fd3f2fdf3cc12d4df17e185fe
Damonamajor <damon.major@cookcountyil.gov>
2024-06-05 20:44:37 +0000

    Revert geojson file

--- etl/scripts-ccao-data-warehouse-us-east-1/export/export-geojson.R
+++ etl/scripts-ccao-data-warehouse-us-east-1/export/export-geojson.R
@@ -31,21 +31,13 @@ remote_file_tract_2022_warehouse <- file.path(
 remote_file_tract_2022_export <- file.path(
   output_bucket, "geojson", "census-tract-2022.geojson"
 )
-remote_file_ari_warehouse <- file.path(
-  AWS_S3_WAREHOUSE_BUCKET, "housing", "ari",
-  "year=2024", "part-0.parquet"
-)
 
 if (!aws.s3::object_exists(remote_file_tract_2022_export)) {
-  ari <- read_parquet(remote_file_ari_warehouse)
   tracts_2022 <- read_geoparquet_sf(remote_file_tract_2022_warehouse) %>%
     filter(geoid != "17031990000") %>%
     select(geoid, geometry) %>%
     mutate(year = "2022") %>%
-    left_join(ari %>%
-                distinct(geoid, name), by = "geoid") %>%
     st_transform(4326) %>%
-    st_intersection(cook_boundary) %>%
     rmapshaper::ms_simplify(keep = 0.7, keep_shapes = TRUE)
 
   # Write geojson, then upload to S3
@@ -75,8 +67,7 @@ if (!aws.s3::object_exists(remote_file_puma_2021_export)) {
     filter(geoid %in% c(ihs_index$geoid, "1703525")) %>%
     select(geoid, geometry) %>%
     mutate(year = "2021") %>%
-    left_join(ihs_index %>%
-                distinct(geoid, name), by = "geoid") %>%
+    left_join(ihs_index %>% distinct(geoid, name), by = "geoid") %>%
     st_transform(4326) %>%
     st_intersection(cook_boundary) %>%
     rmapshaper::ms_simplify(keep = 0.7, keep_shapes = TRUE)
@@ -160,8 +151,7 @@ remote_file_municipality_2022_export <- file.path(
 )
 
 if (!aws.s3::object_exists(remote_file_municipality_2022_export)) {
-  municipality_2022 <-
-    read_geoparquet_sf(remote_file_municipality_2022_warehouse) %>%
+  municipality_2022 <- read_geoparquet_sf(remote_file_municipality_2022_warehouse) %>%
     mutate(year = "2022") %>%
     st_transform(4326) %>%
     rmapshaper::ms_simplify(keep = 0.7, keep_shapes = TRUE)
@@ -184,8 +174,7 @@ remote_file_school_elem_2022_export <- file.path(
 )
 
 if (!aws.s3::object_exists(remote_file_school_elem_2022_export)) {
-  school_elem_2022 <-
-    read_geoparquet_sf(remote_file_school_elem_2022_warehouse) %>%
+  school_elem_2022 <- read_geoparquet_sf(remote_file_school_elem_2022_warehouse) %>%
     select(geoid, name, school_num, is_attendance_boundary) %>%
     mutate(year = "2022") %>%
     st_transform(4326) %>%
@@ -209,8 +198,7 @@ remote_file_school_sec_2022_export <- file.path(
 )
 
 if (!aws.s3::object_exists(remote_file_school_sec_2022_export)) {
-  school_sec_2022 <-
-    read_geoparquet_sf(remote_file_school_sec_2022_warehouse) %>%
+  school_sec_2022 <- read_geoparquet_sf(remote_file_school_sec_2022_warehouse) %>%
     select(geoid, name, school_num, is_attendance_boundary) %>%
     mutate(year = "2022") %>%
     st_transform(4326) %>%
@@ -234,8 +222,7 @@ remote_file_school_unif_2022_export <- file.path(
 )
 
 if (!aws.s3::object_exists(remote_file_school_unif_2022_export)) {
-  school_unif_2022 <-
-    read_geoparquet_sf(remote_file_school_unif_2022_warehouse) %>%
+  school_unif_2022 <- read_geoparquet_sf(remote_file_school_unif_2022_warehouse) %>%
     select(geoid, name, school_num, is_attendance_boundary) %>%
     mutate(year = "2022") %>%
     st_transform(4326) %>%
