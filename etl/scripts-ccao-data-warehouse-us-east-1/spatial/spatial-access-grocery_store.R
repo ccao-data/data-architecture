f86ede17b156a007f591e094aadc4b96c6046452
Damonamajor <damon.major@cookcountyil.gov>
2024-06-13 21:56:25 +0000

    Dan edits

--- etl/scripts-ccao-data-warehouse-us-east-1/spatial/spatial-access-grocery_store.R
+++ etl/scripts-ccao-data-warehouse-us-east-1/spatial/spatial-access-grocery_store.R
@@ -8,16 +8,17 @@ source("utils.R")
 # This script queries OpenStreetMap for grocery stores in Cook County and
 # saves them as a spatial parquet
 AWS_S3_WAREHOUSE_BUCKET <- Sys.getenv("AWS_S3_WAREHOUSE_BUCKET")
-output_bucket <- file.path(AWS_S3_WAREHOUSE_BUCKET, "spatial", "environment")
+output_bucket <- file.path(AWS_S3_WAREHOUSE_BUCKET, "spatial", "access")
 current_year <- strftime(Sys.Date(), "%Y")
 
 ##### Grocery Stores #####
+
 # Query OpenStreetMap API for grocery stores in Cook County
 # Create a sequence of years from 2014 to the current year
 years <- 2014:current_year
 
 # Iterate over the years
-test <- for (year in years) {
+for (year in years) {
   remote_file <- file.path(
     output_bucket, "grocery_store",
     paste0("year=", year),
@@ -26,9 +27,11 @@ test <- for (year in years) {
 
   if (!aws.s3::object_exists(remote_file)) {
     # Update the datetime in the opq function
-    supermarkets <- opq(bbox = "Cook County, IL",
-                        datetime = paste0(year, "-01-01T00:00:00Z"),
-                        timeout = 900) %>%
+    supermarkets <- opq(
+      bbox = "Cook County, IL",
+      datetime = paste0(year, "-01-01T00:00:00Z"),
+      timeout = 900
+    ) %>%
       add_osm_features(features = c(
         "\"shop\"=\"supermarket\"",
         "\"shop\"=\"wholesale\"",
@@ -37,10 +40,10 @@ test <- for (year in years) {
       osmdata_sf()
 
     supermarkets_polygons <- supermarkets$osm_polygons %>%
-      select(osm_id, shop, name)
+      select(osm_id, category = shop, name)
 
     supermarkets_points <- supermarkets$osm_points %>%
-      select(osm_id, shop, name)
+      select(osm_id, category = shop, name)
 
     all_supermarkets <- rbind(supermarkets_polygons, supermarkets_points) %>%
       st_centroid()
