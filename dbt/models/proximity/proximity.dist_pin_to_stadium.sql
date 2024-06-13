0084f257b4dee256760238b384cab6cb1c027763
Damonamajor <damon.major@cookcountyil.gov>
2024-06-13 19:36:09 +0000

    use seed in query

--- dbt/models/proximity/proximity.dist_pin_to_stadium.sql
+++ dbt/models/proximity/proximity.dist_pin_to_stadium.sql
@@ -1,38 +1,5 @@
--- CTAS to create a table of distance to the stadiums for each PIN
-{{
-    config(
-        materialized='table',
-        partitioned_by=['year'],
-        bucketed_by=['pin10'],
-        bucket_count=1
-    )
-}}
-
--- Define stadium names and locations
-WITH stadiums AS (
-    SELECT
-        'Soldier Field' AS stadium_name,
-        ST_POINT(1179603, 1893122) AS stadium_xy
-    UNION ALL
-    SELECT
-        'Wintrust Arena' AS stadium_name,
-        ST_POINT(1178259, 1889955) AS stadium_xy
-    UNION ALL
-    SELECT
-        'Guaranteed Rate Field' AS stadium_name,
-        ST_POINT(1174703, 1881406) AS stadium_xy
-    UNION ALL
-    SELECT
-        'Wrigley Field' AS stadium_name,
-        ST_POINT(1168634, 1924426) AS stadium_xy
-    UNION ALL
-    SELECT
-        'United Center' AS stadium_name,
-        ST_POINT(1163584, 1899780) AS stadium_xy
-    UNION ALL
-    SELECT
-        'UIC Pavilion' AS stadium_name,
-        ST_POINT(1168816, 1897726) AS stadium_xy
+WITH stadium AS (
+    SELECT * FROM {{ ref('spatial.stadium') }}
 ),
 
 -- Calculate distance between every distinct parcel and each stadium
@@ -42,12 +9,13 @@ xy_to_stadium_dist AS (
         parcel.x_3435,
         parcel.y_3435,
         parcel.year,
-        stadiums.stadium_name,
+        stadium.name AS stadium_name,
         ST_DISTANCE(
-            ST_POINT(parcel.x_3435, parcel.y_3435), stadiums.stadium_xy
+            ST_POINT(parcel.x_3435, parcel.y_3435),
+            ST_POINT(stadium.lon, stadium.lat)
         ) AS stadium_dist_ft
     FROM spatial.parcel
-    CROSS JOIN stadiums
+    CROSS JOIN stadium
 ),
 
 -- Rank distance to stadium within parcel
@@ -60,8 +28,7 @@ min_stadium_dist AS (
         stadium_name,
         stadium_dist_ft,
         ROW_NUMBER()
-            OVER (PARTITION BY pin10, year ORDER BY stadium_dist_ft)
-            AS rnk
+            OVER (PARTITION BY pin10, year ORDER BY stadium_dist_ft) AS rnk
     FROM xy_to_stadium_dist
 )
 
@@ -70,7 +37,6 @@ SELECT
     pin10,
     stadium_name AS nearest_stadium_name,
     stadium_dist_ft AS nearest_stadium_dist_ft,
-    year AS nearest_stadium_data_year,
-    year
+    year AS nearest_stadium_data_year
 FROM min_stadium_dist
 WHERE rnk = 1
