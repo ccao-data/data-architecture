189b03c5f0c78c545770cb777ac9b1d6766afb37
Michael Wagner <wagnerleemichael@gmail.com>
2024-06-13 16:38:51 +0000

    Fix errors

--- dbt/models/sale/sale.vw_ias_salesval_upload.sql
+++ dbt/models/sale/sale.vw_ias_salesval_upload.sql
@@ -11,7 +11,7 @@ max_version AS (
     SELECT
         meta_sale_document_num,
         MAX(version) AS max_version
-    FROM "z_ci_0002-update-outlier-column-structure-w-iasworld-2024-update_sale'.'new_prod_data"
+    FROM "z_ci_0002-update-outlier-column-structure-w-iasworld-2024-update_sale"."new_prod_data"
     GROUP BY meta_sale_document_num
 )
 
@@ -23,7 +23,7 @@ SELECT
     sf.sv_outlier_reason3,
     sf.run_id
 FROM ias_sales
-INNER JOIN source("z_ci_0002-update-outlier-column-structure-w-iasworld-2024-update_sale"."new_prod_data") AS sf
+INNER JOIN "z_ci_0002-update-outlier-column-structure-w-iasworld-2024-update_sale"."new_prod_data" AS sf
     ON ias_sales.instruno_clean = sf.meta_sale_document_num
 INNER JOIN max_version AS mv
     ON sf.meta_sale_document_num = mv.meta_sale_document_num
