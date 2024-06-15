151a097bb17676c69779046d07bc7044344a10d0
Damonamajor <damon.major@cookcountyil.gov>
2024-06-15 01:01:28 +0000

    Test hie

--- dbt/models/proximity/proximity.dist_pin_to_hie.sql
+++ dbt/models/proximity/proximity.dist_pin_to_hie.sql
@@ -7,32 +7,49 @@
     )
 }}
 
-WITH new_construction AS (  -- noqa: ST03
+WITH new_permit AS (
     SELECT
         parcel.pin10,
         ST_ASBINARY(ST_POINT(parcel.x_3435, parcel.y_3435)) AS geometry_3435,
         parcel.year,
-        vw_card_res_char.char_yrblt
+        iasworld.permdt AS permit_date,
+        iasworld.amount
     FROM spatial.parcel AS parcel
-    INNER JOIN default.vw_card_res_char AS vw_card_res_char
-        ON parcel.pin10 = vw_card_res_char.pin10
-        AND CAST(parcel.year AS INT) = CAST(vw_card_res_char.year AS INT)
+    INNER JOIN iasworld.permit AS iasworld
+        ON parcel.pin10 = iasworld.parid
         AND CAST(parcel.year AS INT)
-        <= CAST(vw_card_res_char.char_yrblt AS INT) + 2
+        >= CAST(SUBSTRING(iasworld.permdt, 1, 4) AS INT)
+),
 
+min_year_per_pin AS (
+    SELECT
+        pin10,
+        MIN(year) AS min_year
+    FROM new_permit
+    GROUP BY pin10
 )
 
 SELECT
     pcl.pin10,
-    ARBITRARY(xy.pin10) AS nearest_new_construction_pin10,
-    ARBITRARY(xy.dist_ft) AS nearest_new_construction_dist_ft,
-    ARBITRARY(xy.year) AS nearest_new_construction_data_year,
-    ARBITRARY(xy.char_yrblt) AS nearest_new_construction_char_yrblt,
+    ARBITRARY(xy.pin10) AS nearest_permit_pin10,
+    ARBITRARY(xy.dist_ft) AS nearest_permit_dist_ft,
+    ARBITRARY(xy.year) AS nearest_permit_data_year,
+    ARBITRARY(xy.permit_date) AS nearest_permit_year,
     pcl.year
 FROM {{ source('spatial', 'parcel') }} AS pcl
-INNER JOIN
-    ( {{ dist_to_nearest_geometry('new_construction') }} ) AS xy
+INNER JOIN (
+    SELECT
+        np.pin10,
+        np.geometry_3435,
+        np.year,
+        np.permit_date,
+        np.amount
+    FROM new_permit AS np
+    INNER JOIN min_year_per_pin AS myp
+        ON np.pin10 = myp.pin10
+        AND np.year = myp.min_year
+) AS xy
     ON pcl.x_3435 = xy.x_3435
     AND pcl.y_3435 = xy.y_3435
-    AND pcl.year = xy.pin_year
+    AND pcl.year = xy.year
 GROUP BY pcl.pin10, pcl.year
