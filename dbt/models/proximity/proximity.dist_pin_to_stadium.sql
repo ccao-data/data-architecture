557a214c9f7a8ec3d4ebb072b42d4685249d669f
Damonamajor <damon.major@cookcountyil.gov>
2024-06-18 13:30:01 +0000

    Modify stadiums query

--- dbt/models/proximity/proximity.dist_pin_to_stadium.sql
+++ dbt/models/proximity/proximity.dist_pin_to_stadium.sql
@@ -8,25 +8,17 @@
     )
 }}
 
-WITH stadium AS (  -- noqa: ST03
-    SELECT
-        ST_ASBINARY(ST_POINT(stadium.x_3435, stadium.y_3435)) AS geometry_3435,
-        stadium.name,
-        stadium.date_opened,
-        stadium.year
-    FROM {{ ref('spatial.stadium') }}
-)
-
 SELECT
     pcl.pin10,
     ARBITRARY(xy.name) AS nearest_stadium_name,
     ARBITRARY(xy.dist_ft) AS nearest_stadium_dist_ft,
     ARBITRARY(xy.year) AS nearest_stadium_data_year,
-    ARBITRARY(xy.date_opened) AS nearest_stadium_date_opened,
     pcl.year
 FROM {{ source('spatial', 'parcel') }} AS pcl
-INNER JOIN ( {{ dist_to_nearest_geometry('stadium') }} ) AS xy
+INNER JOIN
+    ( {{ dist_to_nearest_geometry(source('spatial', 'spatial_stadium')) }} )
+        AS xy
     ON pcl.x_3435 = xy.x_3435
     AND pcl.y_3435 = xy.y_3435
-WHERE CAST(pcl.year AS INTEGER) >= CAST(xy.date_opened AS INTEGER)
-GROUP BY pcl.pin10, pcl.year;
+    AND pcl.year = xy.pin_year
+GROUP BY pcl.pin10, pcl.year
