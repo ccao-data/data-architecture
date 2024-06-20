f89a32a4de802d8f4b1b30bd4361eba42c50df09
Damonamajor <damon.major@cookcountyil.gov>
2024-06-20 16:09:53 +0000

    Finalize

--- dbt/models/spatial/spatial.stadium.sql
+++ dbt/models/spatial/spatial.stadium.sql
@@ -6,5 +6,5 @@ SELECT
     stadium.name,
     ST_ASBINARY(ST_POINT(stadium.lon, stadium.lat)) AS geometry,
     ST_ASBINARY(ST_POINT(stadium.x_3435, stadium.y_3435)) AS geometry_3435,
-    CAST(stadium.year AS VARCHAR) AS stadium_year
+    CAST(stadium.year AS VARCHAR) AS year
 FROM {{ ref('spatial.stadium_raw') }} AS stadium
