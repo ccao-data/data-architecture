eb32e2f0d39888c692d6cad2b2630aa747093a75
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2024-07-10 15:52:46 +0000

    Add requirements.txt to etl workspace

--- etl/scripts-ccao-data-warehouse-us-east-1/housing/housing-ari.py
+++ etl/scripts-ccao-data-warehouse-us-east-1/housing/housing-ari.py
@@ -5,6 +5,7 @@ import boto3
 import pandas as pd
 from dotenv import load_dotenv
 
+load_dotenv(".Renviron")
 AWS_S3_WAREHOUSE_BUCKET = os.getenv("AWS_S3_WAREHOUSE_BUCKET")
 AWS_S3_RAW_BUCKET = os.getenv("AWS_S3_RAW_BUCKET")[5:]  # type: ignore
 
@@ -12,7 +13,6 @@ AWS_S3_RAW_BUCKET = os.getenv("AWS_S3_RAW_BUCKET")[5:]  # type: ignore
 s3 = boto3.client("s3")
 temp_file = tempfile.NamedTemporaryFile(suffix=".xlsx", delete=False)
 
-load_dotenv("etl/.Renviron")
 # Manually include the year of file construction.
 file_key = os.path.join("housing", "ari", "2023-ARI.xlsx")
 
