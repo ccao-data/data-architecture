9b58d161c85c189dadaf9baf5a1236749d9f35f5
Jean Cochrane <jean@jeancochrane.com>
2024-07-11 10:47:31 -0500

    Optionally rebuild models and override vars in scripts/qc_report.py

--- dbt/scripts/qc_report.py
+++ dbt/scripts/qc_report.py
@@ -1,4 +1,4 @@
-# Query dbt models to generate QC reports.
+# Export dbt models to Excel files ("reports").
 #
 # Run `python scripts/qc_report.py --help` for details.
 
@@ -17,30 +17,27 @@ from openpyxl.utils import get_column_letter
 from openpyxl.worksheet.table import Table, TableStyleInfo
 
 DBT = dbtRunner()
-CLI_DESCRIPTION = """Query dbt models to generate QC reports.
+CLI_DESCRIPTION = """Export dbt models to Excel files ("reports").
 
 A few configuration values can be set on any model used to generate reports:
 
-    * config.meta.report_name (optional): The name of the report to generate.
-      This will be used to set the filename for the output report. File
-      extensions are not necessary since all reports are output as .xlsx files.
-      If unset, defaults to the name of the model.
-    * config.meta.template (optional): The filename an Excel template to use
-      when generating the report. Templates should be stored in the
-      reports/templates/ directory and should include header rows. If unset,
-      will search for a template with the same name as the model; if no
-      template is found, defaults to a naive Excel layout with filterable
-      columns.
-"""
+    * config.meta.report_name (optional): The name of the report to generate. This will be used to set the filename for the output report. File
+      extensions are not necessary since all reports are output as .xlsx files. If unset, defaults to the name of the model.
+
+    * config.meta.template (optional): The filename an Excel template to use when generating the report. Templates should be stored in the
+      reports/templates/ directory and should include header rows. If unset, will search for a template with the same name as the model; if no
+      template is found, defaults to a simple layout with filterable columns and striped rows.
+"""  # noqa: E501
+CLI_EXAMPLE = """Example usage to output the qc_report_town_close report for Hyde Park township:
+
+    python scripts/qc_report.py --select tag:qc_report_town_close --vars '{qc_report_town_code: 70}' --rebuild
+"""  # noqa: E501
 
 
 def main():
     parser = argparse.ArgumentParser(
         description=CLI_DESCRIPTION,
-        epilog=(
-            "Example usage: python scripts/generate_qc_report.py --target dev "
-            "--select tag:qc_report_town_close"
-        ),
+        epilog=CLI_EXAMPLE,
         # Parse the description and epilog as raw text so that newlines
         # get preserved
         formatter_class=argparse.RawTextHelpFormatter,
@@ -57,15 +54,52 @@ def main():
         default="dev",
         help="dbt target to use for querying data, defaults to 'dev'",
     )
+    parser.add_argument(
+        "--rebuild",
+        action=argparse.BooleanOptionalAction,
+        default=True,
+        help="Rebuild models before exporting reports",
+    )
+    parser.add_argument(
+        "--vars",
+        required=False,
+        help=(
+            "Variable overrides to pass to `dbt build`. Expects a YAML "
+            "string. Useful for e.g. overriding the township that is used "
+            "to generate a report. Requires that --rebuild be set"
+        ),
+    )
 
     args = parser.parse_args()
     target = args.target
     select = args.select
+    rebuild = args.rebuild
+    vars = args.vars
 
-    print(f"Checking for models matching these select statements: {select}")
+    if vars and not rebuild:
+        raise ValueError("--rebuild must be set if --vars is set")
+
+    if rebuild:
+        print("Rebuilding models")
+        dbt_run_args = [
+            "run",
+            "--target",
+            target,
+            "--select",
+            *select,
+        ]
+        if vars:
+            dbt_run_args += ["--vars", vars]
+
+        dbt_run_result = DBT.invoke(dbt_run_args)
+        if not dbt_run_result.success:
+            print("Encountered error in `dbt run` call")
+            raise ValueError(dbt_run_result.exception)
+
+    print("Listing models to select for export")
     dbt_output = io.StringIO()
     with contextlib.redirect_stdout(dbt_output):
-        dbt_result = DBT.invoke(
+        dbt_list_result = DBT.invoke(
             [
                 "--quiet",
                 "list",
@@ -84,9 +118,9 @@ def main():
             ]
         )
 
-    if not dbt_result.success:
-        print("Encountered error in dbt call")
-        raise ValueError(dbt_result.exception)
+    if not dbt_list_result.success:
+        print("Encountered error in `dbt list` call")
+        raise ValueError(dbt_list_result.exception)
 
     # Output is formatted as a list of newline-separated JON objects
     models = [
@@ -98,6 +132,11 @@ def main():
 
     if not models:
         raise ValueError(f"No models found for the --select value '{select}'")
+
+    print(
+        "The following models will be exported to reports: "
+        f"{', '.join(model['name'] for model in models)}"
+    )
 
     conn = pyathena.connect(
         s3_staging_dir=os.getenv(
@@ -135,7 +174,7 @@ def main():
             print("No template file exists; creating a workbook from scratch")
 
         writer_kwargs = (
-            {"model": "a", "if_sheet_exists": "overlay"}
+            {"mode": "a", "if_sheet_exists": "overlay"}
             if template_exists
             else {}
         )
