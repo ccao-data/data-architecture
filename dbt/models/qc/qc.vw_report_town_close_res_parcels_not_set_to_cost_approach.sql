f47872d801173bea82566a22b2a46df98610cbb3
Jean Cochrane <jean.cochrane@cookcountyil.gov>
2024-07-18 19:47:45 +0000

    Migrate "Res parcels not set to cost approach" QC report

---
+++ dbt/models/qc/qc.vw_report_town_close_res_parcels_not_set_to_cost_approach.sql
@@ -0,0 +1,27 @@
+SELECT
+    pardat.parid,
+    pardat.taxyr,
+    legdat.user1 AS township_code,
+    pardat.class,
+    aprval.revcode,
+    aprval.reascd,
+    aprval.aprland,
+    aprval.aprbldg,
+    aprval.aprtot,
+    aprval.who,
+    aprval.wen
+FROM {{ source('iasworld', 'aprval') }} AS aprval
+LEFT JOIN {{ source('iasworld', 'pardat') }} AS pardat
+    ON aprval.parid = pardat.parid
+    AND aprval.taxyr = pardat.taxyr
+    AND pardat.cur = 'Y'
+    AND pardat.deactivat IS NULL
+LEFT JOIN {{ source('iasworld', 'legdat') }} AS legdat
+    ON aprval.parid = legdat.parid
+    AND aprval.taxyr = legdat.taxyr
+    AND legdat.cur = 'Y'
+    AND legdat.deactivat IS NULL
+WHERE aprval.cur = 'Y'
+    AND aprval.deactivat IS NULL
+    AND pardat.class LIKE '2%'
+    AND (aprval.revcode IS NULL OR aprval.revcode != '1')
