2ddbf20f393cde8140769517bbed9b093051ce6a
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2024-07-23 19:40:21 +0000

    Add raw script

---
+++ etl/scripts-ccao-data-raw-us-east-1/ccbor/ccbor-appeals.R
@@ -0,0 +1,42 @@
+library(arrow)
+library(glue)
+library(dplyr)
+library(jsonlite)
+library(purrr)
+library(RSocrata)
+
+# This script retrieves Board of Review appeals data from the county's open
+# data portal
+AWS_S3_RAW_BUCKET <- Sys.getenv("AWS_S3_RAW_BUCKET")
+output_bucket <- file.path(AWS_S3_RAW_BUCKET, "ccbor", "appeals")
+
+years <- read_json(
+  glue(
+    "https://datacatalog.cookcountyil.gov/resource/7pny-nedm.json?",
+    "$select=distinct(tax_year)"
+  ),
+  simplifyVector = TRUE
+) %>%
+  pull() %>%
+  as.numeric()
+
+# Gather BOR appeals data by year
+walk(years, \(x) {
+
+  remote_path <- file.path(output_bucket, paste0(x, '.parquet'))
+
+  # Only gathers data if it doesn't already exist or is from the last two years
+  if (!aws.s3::object_exists(remote_path) | x >= (max(years) - 1)) {
+    print(paste0("Fetching BOR Appeals Data for ", x))
+    read.socrata(
+      glue(
+        "https://datacatalog.cookcountyil.gov/resource/7pny-nedm.json?",
+        "$select=tax_year={x}"
+      ),
+      app_token = Sys.getenv("SOCRATA_APP_TOKEN"),
+      email     = Sys.getenv("SOCRATA_EMAIL"),
+      password  = Sys.getenv("SOCRATA_PASSWORD")
+    ) %>%
+      write_parquet(remote_path)
+  }
+}, .progress = TRUE)
