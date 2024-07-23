3e9beebef76c481df6412497649c5c5730f195ec
Damonamajor <damon.major@cookcountyil.gov>
2024-07-23 19:22:37 +0000

    Fix query

--- dbt/models/spatial/spatial.stadium.sql
+++ dbt/models/spatial/spatial.stadium.sql
@@ -6,5 +6,11 @@ SELECT
     stadium.name,
     ST_ASBINARY(ST_POINT(stadium.lon, stadium.lat)) AS geometry,
     ST_ASBINARY(ST_POINT(stadium.x_3435, stadium.y_3435)) AS geometry_3435,
+    ST_ASBINARY(
+        ST_POINT(CAST(stadium.x_3435 AS DOUBLE), CAST(stadium.y_3435 AS DOUBLE))
+    ) AS x_3435,
+    ST_ASBINARY(
+        ST_POINT(CAST(stadium.y_3435 AS DOUBLE), CAST(stadium.y_3435 AS DOUBLE))
+    ) AS y_3435,
     CAST(stadium.year AS VARCHAR) AS year
 FROM {{ ref('spatial.stadium_raw') }} AS stadium
