7436d412151229d4e54dd0b18386aebf11adb890
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2024-07-23 21:49:40 +0000

    Linting

--- etl/scripts-ccao-data-warehouse-us-east-1/ccbor/ccbor-appeals.R
+++ etl/scripts-ccao-data-warehouse-us-east-1/ccbor/ccbor-appeals.R
@@ -12,15 +12,18 @@ input_bucket <- file.path(AWS_S3_RAW_BUCKET, "ccabor", "appeals")
 output_bucket <- file.path(AWS_S3_WAREHOUSE_BUCKET, "ccbor", "appeals")
 
 # Grab the file paths for the raw data on S3
-raw_paths <- aws.s3::get_bucket_df(AWS_S3_RAW_BUCKET, prefix = "ccbor/appeals/") %>%
+raw_paths <- aws.s3::get_bucket_df(
+  AWS_S3_RAW_BUCKET,
+  prefix = "ccbor/appeals/"
+) %>%
   filter(str_detect(Key, "parquet")) %>%
   pull(Key)
 
 # Load the raw appeals data, rename and clean up columns, then write to S3
 # partitioned by year
-map(raw_paths, \(x){
+map(raw_paths, \(x) {
   read_parquet(file.path(AWS_S3_RAW_BUCKET, x))
-}) %>%
+} ) %>%
   bind_rows() %>%
   select(-c(pin10:centroid_geom.coordinates)) %>%
   rename(taxyr = tax_year) %>%
