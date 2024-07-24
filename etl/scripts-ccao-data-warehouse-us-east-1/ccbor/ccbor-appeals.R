65925c49a34b21dfc54a38bc921471a3648b72bb
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2024-07-24 17:13:18 +0000

    Rename columns and add descriptions

--- etl/scripts-ccao-data-warehouse-us-east-1/ccbor/ccbor-appeals.R
+++ etl/scripts-ccao-data-warehouse-us-east-1/ccbor/ccbor-appeals.R
@@ -25,8 +25,39 @@ map(raw_paths, \(x) {
   read_parquet(file.path(AWS_S3_RAW_BUCKET, x))
 }) %>%
   bind_rows() %>%
-  select(-c(pin10:centroid_geom.coordinates)) %>%
-  rename(taxyr = tax_year) %>%
+  select(
+    appeal_id = appealid,
+    pin,
+    taxyr = tax_year,
+    class,
+    township_code,
+    tax_code = taxcode,
+    appeal_trk = appealtrk,
+    appeal_seq = appealseq,
+    appeal_type = appealtype,
+    appeal_type_desc = appealtypedescription,
+    assessor_land_value = assessor_landvalue,
+    assessor_improvement_value = assessor_improvementvalue,
+    assessor_total_value = assessor_totalvalue,
+    bor_land_value = bor_landvalue,
+    bor_improvement_value = bor_improvementvalue,
+    bor_total_value = bor_totalvalue,
+    result,
+    change_reason = changereason,
+    change_reason_desc = changereasondescription,
+    no_change_reason = nochangereason,
+    no_change_reason_desc = nochangereasondescription,
+    appellant,
+    appellant_address,
+    appellant_city,
+    appellant_state,
+    appellant_zip,
+    attorney_code = attorneycode,
+    attorney_id = attny,
+    attorney_first_name = attorney_firstname,
+    attorney_last_name = attorney_lastname,
+    attorney_firm_name = attorney_firmname
+  ) %>%
   group_by(taxyr) %>%
   write_partitions_to_s3(
     output_bucket,
