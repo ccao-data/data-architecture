663dede00cbf02e9cfd8dbd0cc7aeefe127a5f61
Jean Cochrane <jean@jeancochrane.com>
2024-07-24 12:27:03 -0500

    Fix "Possibly not at 100%" QC reports

--- dbt/models/qc/qc.vw_report_town_close_prior_year_card_code_5s_comdat.sql
+++ dbt/models/qc/qc.vw_report_town_close_prior_year_card_code_5s_comdat.sql
@@ -1,28 +1,86 @@
+WITH comdat_change AS (
+    SELECT
+        comdat_prev.parid,
+        -- If there is a record for a card in the prior year but not in the
+        -- current year, infer the current year based on the prior year
+        COALESCE(
+            comdat.taxyr,
+            CAST(CAST(comdat_prev.taxyr AS INT) + 1 AS VARCHAR)
+        ) AS taxyr,
+        comdat_prev.card,
+        comdat_prev.chgrsn,
+        comdat.bldgval,
+        comdat_prev.bldgval AS bldgval_prev,
+        -- The data switch from mktadj to external_occpct around 2023 but are
+        -- not consistent about that switch, so consolidate the two fields
+        ROUND(
+            COALESCE(comdat_prev.external_occpct, comdat_prev.mktadj),
+            1
+        ) AS external_occpct_prev,
+        comdat.external_occpct,
+        CASE
+            WHEN comdat.bldgval != 0
+                THEN CAST(
+                    ROUND(
+                        (comdat_prev.bldgval / CAST(comdat.bldgval AS DOUBLE))
+                        * 100,
+                        1
+                    )
+                    AS VARCHAR
+                )
+            ELSE 'N/A'
+        END AS pct_prev_of_cur_bldgval,
+        CASE
+            WHEN comdat.bldgval != 0
+                THEN CAST(
+                    ROUND(
+                        ROUND(
+                            (
+                                comdat_prev.bldgval
+                                / CAST(comdat.bldgval AS DOUBLE)
+                            )
+                            * 100,
+                            1
+                        )
+                        - COALESCE(
+                            comdat_prev.external_occpct,
+                            comdat_prev.mktadj
+                        ),
+                        1
+                    )
+                    AS VARCHAR
+                )
+            ELSE 'N/A'
+        END AS difference_in_pct
+    -- Select from the prior year of data as the base of the query so that we
+    -- can preserve parcels that may have changed in the following year
+    FROM {{ source('iasworld', 'comdat') }} AS comdat_prev
+    LEFT JOIN {{ source('iasworld', 'comdat') }} AS comdat
+        ON comdat_prev.parid = comdat.parid
+        AND comdat_prev.card = comdat.card
+        AND CAST(comdat.taxyr AS INT) = CAST(comdat_prev.taxyr AS INT) + 1
+        AND comdat.cur = 'Y'
+        AND comdat.deactivat IS NULL
+    WHERE comdat_prev.cur = 'Y'
+        AND comdat_prev.deactivat IS NULL
+)
+
 SELECT
-    asmt.parid,
-    -- If there is a record for a card in the prior year but not in the
-    -- current year, infer the current year based on the prior year
-    COALESCE(
-        comdat.taxyr,
-        CAST(CAST(comdat_prev.taxyr AS INT) + 1 AS VARCHAR)
-    ) AS taxyr,
+    comdat_change.parid,
+    comdat_change.taxyr,
     asmt.township_code,
     asmt.class,
     asmt.own1,
     asmt.reascd,
     asmt.who,
-    comdat_prev.card,
-    comdat_prev.chgrsn,
-    -- The data switch from mktadj to external_occpct around 2023 but are
-    -- not consistent about that switch, so consolidate the two fields
-    COALESCE(comdat_prev.external_occpct, comdat_prev.mktadj)
-        AS external_occpct_prev,
-    comdat.external_occpct,
-    comdat.external_occpct
-    - COALESCE(comdat_prev.external_occpct, comdat_prev.mktadj, 0)
-        AS external_occpct_difference,
-    comdat_prev.bldgval AS bldgval_prev,
-    comdat.bldgval,
+    comdat_change.card,
+    comdat_change.chgrsn,
+    comdat_change.external_occpct_prev,
+    comdat_change.external_occpct,
+    comdat_change.pct_prev_of_cur_bldgval,
+    comdat_change.difference_in_pct,
+    comdat_change.bldgval_prev,
+    comdat_change.bldgval,
     asmt.valapr1_prev,
     asmt.valapr2_prev,
     asmt.valapr3_prev,
@@ -35,18 +93,9 @@ SELECT
     asmt.valasm1,
     asmt.valasm2,
     asmt.valasm3
--- Select from the prior year of data as the base of the query so that we
--- can preserve parcels that may have changed in the following year
-FROM {{ source('iasworld', 'comdat') }} AS comdat_prev
-LEFT JOIN {{ source('iasworld', 'comdat') }} AS comdat
-    ON comdat.parid = comdat_prev.parid
-    AND comdat.card = comdat_prev.card
-    AND CAST(comdat.taxyr AS INT) = CAST(comdat_prev.taxyr AS INT) + 1
-    AND comdat.cur = 'Y'
-    AND comdat.deactivat IS NULL
+FROM comdat_change
 LEFT JOIN {{ ref('qc.vw_iasworld_asmt_all_with_prior_year_values') }} AS asmt
-    ON asmt.parid = comdat_prev.parid
-    AND asmt.taxyr = comdat_prev.taxyr
-WHERE comdat_prev.cur = 'Y'
-    AND comdat_prev.deactivat IS NULL
-    AND comdat_prev.chgrsn IN ('5', '5B')
+    ON comdat_change.parid = asmt.parid
+    AND comdat_change.taxyr = asmt.taxyr
+WHERE comdat_change.chgrsn IN ('5', '5B')
+    AND comdat_change.difference_in_pct != '0.0'
--- dbt/models/qc/qc.vw_report_town_close_prior_year_card_code_5s_dweldat.sql
+++ dbt/models/qc/qc.vw_report_town_close_prior_year_card_code_5s_dweldat.sql
@@ -1,28 +1,89 @@
+WITH dweldat_change AS (
+    SELECT
+        dweldat_prev.parid,
+        -- If there is a record for a card in the prior year but not in the
+        -- current year, infer the current year based on the prior year
+        COALESCE(
+            dweldat.taxyr,
+            CAST(CAST(dweldat_prev.taxyr AS INT) + 1 AS VARCHAR)
+        ) AS taxyr,
+        dweldat_prev.card,
+        dweldat_prev.mktrsn,
+        dweldat.adjrcnld,
+        dweldat_prev.adjrcnld AS adjrcnld_prev,
+        -- The data switch from mktadj to external_occpct around 2023 but are
+        -- not consistent about that switch, so consolidate the two fields
+        ROUND(
+            COALESCE(dweldat_prev.external_occpct, dweldat_prev.mktadj),
+            1
+        ) AS external_occpct_prev,
+        dweldat.external_occpct,
+        CASE
+            WHEN dweldat.adjrcnld != 0
+                THEN CAST(
+                    ROUND(
+                        (
+                            dweldat_prev.adjrcnld
+                            / CAST(dweldat.adjrcnld AS DOUBLE)
+                        )
+                        * 100,
+                        1
+                    )
+                    AS VARCHAR
+                )
+            ELSE 'N/A'
+        END AS pct_prev_of_cur_adjrcnld,
+        CASE
+            WHEN dweldat.adjrcnld != 0
+                THEN CAST(
+                    ROUND(
+                        ROUND(
+                            (
+                                dweldat_prev.adjrcnld
+                                / CAST(dweldat.adjrcnld AS DOUBLE)
+                            )
+                            * 100,
+                            1
+                        )
+                        - COALESCE(
+                            dweldat_prev.external_occpct,
+                            dweldat_prev.mktadj
+                        ),
+                        1
+                    )
+                    AS VARCHAR
+                )
+            ELSE 'N/A'
+        END AS difference_in_pct
+    -- Select from the prior year of data as the base of the query so that we
+    -- can preserve parcels that may have changed in the following year
+    FROM {{ source('iasworld', 'dweldat') }} AS dweldat_prev
+    LEFT JOIN {{ source('iasworld', 'dweldat') }} AS dweldat
+        ON dweldat_prev.parid = dweldat.parid
+        AND dweldat_prev.card = dweldat.card
+        AND CAST(dweldat.taxyr AS INT) = CAST(dweldat_prev.taxyr AS INT) + 1
+        AND dweldat.cur = 'Y'
+        AND dweldat.deactivat IS NULL
+    WHERE dweldat_prev.cur = 'Y'
+        AND dweldat_prev.deactivat IS NULL
+)
+
 SELECT
-    asmt.parid,
-    -- If there is a record for a card in the prior year but not in the
-    -- current year, infer the current year based on the prior year
-    COALESCE(
-        dweldat.taxyr,
-        CAST(CAST(dweldat_prev.taxyr AS INT) + 1 AS VARCHAR)
-    ) AS taxyr,
+    dweldat_change.parid,
+    dweldat_change.taxyr,
     asmt.township_code,
     asmt.class,
     asmt.own1,
     asmt.reascd,
     asmt.who,
-    dweldat_prev.card,
-    dweldat_prev.mktrsn,
-    -- The data switch from mktadj to external_occpct around 2023 but are
-    -- not consistent about that switch, so consolidate the two fields
-    COALESCE(dweldat_prev.external_occpct, dweldat_prev.mktadj)
-        AS external_occpct_prev,
-    dweldat.external_occpct,
-    dweldat.external_occpct
-    - COALESCE(dweldat_prev.external_occpct, dweldat_prev.mktadj, 0)
-        AS external_occpct_difference,
-    dweldat_prev.adjrcnld AS adjrcnld_prev,
-    dweldat.adjrcnld,
+    dweldat_change.card,
+    dweldat_change.mktrsn,
+    dweldat_change.external_occpct_prev,
+    dweldat_change.external_occpct,
+    dweldat_change.pct_prev_of_cur_adjrcnld,
+    dweldat_change.difference_in_pct,
+    dweldat_change.adjrcnld_prev,
+    dweldat_change.adjrcnld,
     asmt.valapr1_prev,
     asmt.valapr2_prev,
     asmt.valapr3_prev,
@@ -35,18 +96,9 @@ SELECT
     asmt.valasm1,
     asmt.valasm2,
     asmt.valasm3
--- Select from the prior year of data as the base of the query so that we
--- can preserve parcels that may have changed in the following year
-FROM {{ source('iasworld', 'dweldat') }} AS dweldat_prev
-LEFT JOIN {{ source('iasworld', 'dweldat') }} AS dweldat
-    ON dweldat.parid = dweldat_prev.parid
-    AND dweldat.card = dweldat_prev.card
-    AND CAST(dweldat.taxyr AS INT) = CAST(dweldat_prev.taxyr AS INT) + 1
-    AND dweldat.cur = 'Y'
-    AND dweldat.deactivat IS NULL
+FROM dweldat_change
 LEFT JOIN {{ ref('qc.vw_iasworld_asmt_all_with_prior_year_values') }} AS asmt
-    ON asmt.parid = dweldat_prev.parid
-    AND asmt.taxyr = dweldat_prev.taxyr
-WHERE dweldat_prev.cur = 'Y'
-    AND dweldat_prev.deactivat IS NULL
-    AND dweldat_prev.mktrsn IN ('5', '5B')
+    ON dweldat_change.parid = asmt.parid
+    AND dweldat_change.taxyr = asmt.taxyr
+WHERE dweldat_change.mktrsn IN ('5', '5B')
+    AND dweldat_change.difference_in_pct != '0.0'
