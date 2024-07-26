7d22a9d8895661353485070b1033e205f9559f15
Jean Cochrane <jean.cochrane@cookcountyil.gov>
2024-07-26 20:58:56 +0000

    Refactor qc.vw_report_town_close_500k_increase_1m_decrease to use shared asmt view

--- dbt/models/qc/qc.vw_report_town_close_500k_increase_1m_decrease.sql
+++ dbt/models/qc/qc.vw_report_town_close_500k_increase_1m_decrease.sql
@@ -51,24 +51,24 @@ WITH card_code_info AS (
 SELECT
     asmt.parid,
     asmt.taxyr,
-    legdat.user1 AS township_code,
-    pardat.class,
-    aprval.reascd,
-    asmt_prev.valapr1 AS valapr1_prev,
-    asmt_prev.valapr2 AS valapr2_prev,
-    asmt_prev.valapr3 AS valapr3_prev,
+    asmt.township_code,
+    asmt.class,
+    asmt.reascd,
+    asmt.valapr1_prev,
+    asmt.valapr2_prev,
+    asmt.valapr3_prev,
     asmt.valapr1,
     asmt.valapr2,
     asmt.valapr3,
-    asmt.valapr3 - asmt_prev.valapr3 AS difference,
+    asmt.valapr3 - asmt.valapr3_prev AS difference,
     CASE
-        WHEN asmt_prev.valapr3 != 0
+        WHEN asmt.valapr3_prev != 0
             THEN CONCAT(
                 CAST(
                     ROUND(
                         (
-                            (asmt.valapr3 - asmt_prev.valapr3)
-                            / CAST(asmt_prev.valapr3 AS DOUBLE)
+                            (asmt.valapr3 - asmt.valapr3_prev)
+                            / CAST(asmt.valapr3_prev AS DOUBLE)
                         )
                         * 100,
                         2
@@ -80,37 +80,13 @@ SELECT
     code.dweldat_code_5,
     code.comdat_code_5,
     code.oby_code_5
-FROM {{ source('iasworld', 'asmt_all') }} AS asmt
-LEFT JOIN {{ source('iasworld', 'asmt_all') }} AS asmt_prev
-    ON asmt.parid = asmt_prev.parid
-    AND CAST(asmt.taxyr AS INT) = CAST(asmt_prev.taxyr AS INT) + 1
-    AND asmt_prev.cur = 'Y'
-    AND asmt_prev.deactivat IS NULL
-    AND asmt_prev.valclass IS NULL
+FROM {{ ref('qc.vw_iasworld_asmt_all_with_prior_year_values') }} AS asmt
 LEFT JOIN card_code_info AS code
-    ON asmt_prev.parid = code.parid
+    ON asmt.parid = code.parid
     -- Join to prior year card code info, since we want to know the code value
     -- last year
-    AND asmt_prev.taxyr = code.taxyr
-LEFT JOIN {{ source('iasworld', 'legdat') }} AS legdat
-    ON asmt.parid = legdat.parid
-    AND asmt.taxyr = legdat.taxyr
-    AND legdat.cur = 'Y'
-    AND legdat.deactivat IS NULL
-LEFT JOIN {{ source('iasworld', 'pardat') }} AS pardat
-    ON asmt.parid = pardat.parid
-    AND asmt.taxyr = pardat.taxyr
-    AND pardat.cur = 'Y'
-    AND pardat.deactivat IS NULL
-LEFT JOIN {{ source('iasworld', 'aprval') }} AS aprval
-    ON asmt.parid = aprval.parid
-    AND asmt.taxyr = aprval.taxyr
-    AND aprval.cur = 'Y'
-    AND aprval.deactivat IS NULL
-WHERE asmt.cur = 'Y'
-    AND asmt.deactivat IS NULL
-    AND asmt.valclass IS NULL
-    AND (
-        asmt.valapr3 - asmt_prev.valapr3 >= 500000
-        OR asmt.valapr3 - asmt_prev.valapr3 <= -1000000
-    )
+    AND CAST(CAST(asmt.taxyr AS INT) - 1 AS VARCHAR) = code.taxyr
+WHERE (
+    asmt.valapr3 - asmt.valapr3_prev >= 500000
+    OR asmt.valapr3 - asmt.valapr3_prev <= -1000000
+)
