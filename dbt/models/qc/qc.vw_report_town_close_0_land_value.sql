1f04f90217e5b544e3c3b6d37822d243859a6dc9
Jean Cochrane <jean@jeancochrane.com>
2024-07-31 16:39:13 -0500

    Filter out leasehold properties from 0 land value QC report

--- dbt/models/qc/qc.vw_report_town_close_0_land_value.sql
+++ dbt/models/qc/qc.vw_report_town_close_0_land_value.sql
@@ -21,3 +21,8 @@ SELECT
 FROM {{ ref('qc.vw_iasworld_asmt_all_with_prior_year_values') }}
 WHERE valasm1 = 0
     AND class NOT IN ('EX', 'RR')
+    -- Filter out leasehold parcels. We are not yet sure if there is a
+    -- definitive field for marking leasehold parcels, but in the meantime
+    -- the reports have historically followed the heuristic of filtering out
+    -- parcel IDs where the 11th digit is an 8
+    AND SUBSTR(parid, 11, 1) != '8'
