c5468e49df5d44a6d62cdb60738cfb3fa6b6f630
Jean Cochrane <jean.cochrane@cookcountyil.gov>
2024-08-26 19:37:05 +0000

    WIP Factor out SNS notifications to a separate script, and use it for test notifs

---
+++ .github/actions/publish_sns_topic/publish.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Publish a message to an SNS topic
+#
+# Raise on all errors, and print all commands to the console
+set -euxo pipefail
+
+# Mask topic ARN
+echo "::add-mask::$1"
+
+# Publish to SNS topic
+aws sns publish --topic-arn "$1" --subject "$2" --message "$3"
