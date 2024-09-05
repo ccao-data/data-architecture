d9199dae1c3e195652a8863a2c2329a662b6af2e
Damonamajor <damon.major@cookcountyil.gov>
2024-09-05 14:52:42 +0000

    Commit quick report

--- populate_211_and_212s/Missing_units.sql
+++ populate_211_and_212s/Missing_units.sql
@@ -1,5 +1,12 @@
-SELECT pin
-FROM vw_card_res_char
-WHERE (char_apts = '0' OR char_apts IS NULL OR char_apts = '6')
-    AND year = '2023'
-    AND (class = '211' OR class = '212')
+SELECT
+    rc.pin,
+    pa.prop_address_street_name,
+    pa.prop_address_street_number,
+    pa.prop_address_city_name
+FROM vw_card_res_char AS rc
+INNER JOIN vw_pin_address AS pa
+    ON rc.pin = pa.pin
+WHERE
+    (rc.char_apts = '0' OR rc.char_apts IS NULL OR rc.char_apts = '6')
+    AND rc.year = '2023'
+    AND (rc.class = '211' OR rc.class = '212')
