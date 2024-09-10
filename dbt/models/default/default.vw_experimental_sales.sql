c2f99cc184813055f3114b4a594a1fb58ffed6c3
Michael Wagner <wagnerleemichael@gmail.com>
2024-09-10 20:49:43 +0000

    Fix coalesce

--- dbt/models/default/default.vw_experimental_sales.sql
+++ dbt/models/default/default.vw_experimental_sales.sql
@@ -1,6 +1,4 @@
 WITH 
--- "nopar" isn't entirely accurate for sales associated with only one parcel,
--- so we create our own counter
 calculated AS (
     SELECT
         instruno,
@@ -9,40 +7,42 @@ calculated AS (
         SELECT DISTINCT
             parid,
             NULLIF(REPLACE(instruno, 'D', ''), '') AS instruno
-        FROM {{ source('iasworld', 'sales') }}
+        FROM iasworld.sales
         WHERE deactivat IS NULL
             AND cur = 'Y'
-    )
+    ) AS distinct_sales
     GROUP BY instruno
 ),
 
 ias_sales AS (
     SELECT
-        parid as pin,
-        NULLIF(REPLACE(sales.instruno, 'D', ''), '') as doc_no,
+        sales.parid AS pin,
+        NULLIF(REPLACE(sales.instruno, 'D', ''), '') AS doc_no,
         DATE_PARSE(SUBSTR(sales.saledt, 1, 10), '%Y-%m-%d') AS sale_date,
         CAST(sales.price AS BIGINT) AS sale_price,
         COALESCE(
-            sales.nopar > 1 OR calculated.nopar_calculated > 1,
+            (sales.nopar > 1 OR calculated.nopar_calculated > 1),
             FALSE
         ) AS is_multisale,
         CASE
-            WHEN sales.nopar > 1 THEN sales.nopar ELSE
-                calculated.nopar_calculated
+            WHEN sales.nopar > 1 THEN sales.nopar 
+            ELSE calculated.nopar_calculated
         END AS num_parcels_sale,
-        CASE WHEN TRIM(oldown) IN ('', 'MISSING SELLER NAME')
+        CASE 
+            WHEN TRIM(sales.oldown) IN ('', 'MISSING SELLER NAME')
                 THEN NULL   
-            ELSE oldown
+            ELSE sales.oldown
         END AS seller_name,
-        CASE WHEN TRIM(own1) IN ('', 'MISSING BUYER NAME')
+        CASE 
+            WHEN TRIM(sales.own1) IN ('', 'MISSING BUYER NAME')
                 THEN NULL
-            ELSE own1
+            ELSE sales.own1
         END AS buyer_name
-    FROM iasworld.sales
+    FROM iasworld.sales AS sales
+    LEFT JOIN calculated ON calculated.instruno = NULLIF(REPLACE(sales.instruno, 'D', ''), '')
     WHERE
-        deactivat IS null
-        and cur = 'Y'
-    LEFT JOIN calculated.doc_no = calculated.instruno
+        sales.deactivat IS NULL
+        AND sales.cur = 'Y'
 ),
 mydec_sales AS (
     SELECT
@@ -53,14 +53,14 @@ mydec_sales AS (
         NULLIF(TRIM(seller_name), '') AS seller_name,
         NULLIF(TRIM(buyer_name), '') AS buyer_name,
         COALESCE(
-            line_2_total_parcels > 1
+            line_2_total_parcels > 1,
             FALSE
         ) AS is_multisale,
         line_2_total_parcels as num_parcels_sale
     FROM sale.mydec
 ),
 
-WITH combined_sales AS (
+combined_sales AS (
     -- Select all rows from ias_sales
     SELECT 
         ias.pin,
@@ -70,8 +70,8 @@ WITH combined_sales AS (
         ias.sale_price,
         ias.seller_name,
         ias.buyer_name,
-        ias.is_multisale
-        ias.num_parcels_sale
+        ias.is_multisale,
+        ias.num_parcels_sale,
         'iasworld' AS source
     FROM ias_sales ias
     LEFT JOIN mydec_sales mydec ON ias.doc_no = mydec.doc_no
@@ -87,7 +87,8 @@ WITH combined_sales AS (
         mydec.sale_price,
         mydec.seller_name,
         mydec.buyer_name,
-        mydec.num_parcels_sale
+        mydec.is_multisale,
+        mydec.num_parcels_sale,
         'mydec' AS source
     FROM mydec_sales mydec
     LEFT JOIN ias_sales ias ON mydec.doc_no = ias.doc_no
