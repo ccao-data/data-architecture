21aa4382520ff844c8f24efe3bb7b3c6272134e0
Damonamajor <damon.major@cookcountyil.gov>
2024-10-07 20:56:49 +0000

    Use correct buckets

--- etl/scripts-ccao-data-warehouse-us-east-1/spatial/spatial-environment-traffic.R
+++ etl/scripts-ccao-data-warehouse-us-east-1/spatial/spatial-environment-traffic.R
@@ -4,8 +4,8 @@ library(sf)
 library(geoarrow)
 
 # Define the S3 bucket and folder path
-AWS_S3_RAW_BUCKET <- "ccao-data-raw-us-east-1"
-AWS_S3_WAREHOUSE_BUCKET <- "ccao-data-warehouse-us-east-1"
+AWS_S3_RAW_BUCKET <- Sys.getenv("AWS_S3_RAW_BUCKET")
+AWS_S3_WAREHOUSE_BUCKET <- Sys.getenv("AWS_S3_WAREHOUSE_BUCKET")
 s3_folder <- "spatial/environment/traffic/"
 output_bucket <- file.path(AWS_S3_WAREHOUSE_BUCKET, "spatial", "environment", "traffic")
 
