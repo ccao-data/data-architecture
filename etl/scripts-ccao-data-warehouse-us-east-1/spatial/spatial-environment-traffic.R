1e4475199712bee29b1ebef0d08053fb662bfff0
Damonamajor <damon.major@cookcountyil.gov>
2024-10-07 20:54:46 +0000

    text edits

--- etl/scripts-ccao-data-warehouse-us-east-1/spatial/spatial-environment-traffic.R
+++ etl/scripts-ccao-data-warehouse-us-east-1/spatial/spatial-environment-traffic.R
@@ -9,10 +9,9 @@ AWS_S3_WAREHOUSE_BUCKET <- "ccao-data-warehouse-us-east-1"
 s3_folder <- "spatial/environment/traffic/"
 output_bucket <- file.path(AWS_S3_WAREHOUSE_BUCKET, "spatial", "environment", "traffic")
 
-# List all the files in the S3 folder
 files_in_s3 <- get_bucket_df(bucket = AWS_S3_RAW_BUCKET, prefix = s3_folder)
 
-# Get the 'Key' (file path) for all files
+# Get the 'Key'
 parquet_files <- files_in_s3 %>%
   pull(Key)
 
@@ -25,7 +24,7 @@ for (file_key in parquet_files) {
   # Convert the S3 object into raw data and read using geoarrow
   shapefile_data <- geoarrow::read_geoparquet(rawConnection(obj))
 
-  # Ensure geometry column is in 'sf' format
+  # Convert geometry column to 'sf' format
   shapefile_data$geometry <- st_as_sfc(shapefile_data$geometry)
 
   shapefile_data <- shapefile_data %>%
@@ -33,7 +32,8 @@ for (file_key in parquet_files) {
     st_transform(4326) %>%
     mutate(geometry_3435 = st_transform(geometry, 3435))
 
-  # Define the columns you want to select
+  # Define the columns you want to select. We do this because some columns are not present in older
+  # versions of the data
   required_columns <- c("LNS", "SURF_TYP", "SURF_WTH", "SRF_YR", "AADT", "CRS_WITH", "CRS_OPP", "CRS_YR",
                         "ROAD_NAME", "DTRESS_WTH", "DTRESS_OPP", "SP_LIM", "INVENTORY", "geometry_3435")
 
