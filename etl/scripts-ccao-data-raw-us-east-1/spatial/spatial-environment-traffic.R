c6961d08baecce5226492ba38c8b65212a97d5f5
Damonamajor <damon.major@cookcountyil.gov>
2024-10-08 16:39:44 +0000

    Add if-else statement for County

--- etl/scripts-ccao-data-raw-us-east-1/spatial/spatial-environment-traffic.R
+++ etl/scripts-ccao-data-raw-us-east-1/spatial/spatial-environment-traffic.R
@@ -61,8 +61,9 @@ process_shapefiles_for_year <- map(years, \(x) {
     if (length(shp_file_for_year) == 1) {
       # Read the shapefile into the environment using sf::st_read
       shapefile_data <- sf::st_read(shp_file_for_year) %>%
-        # Add filter for Cook County
-        filter(INV_CO == '016')
+        # Add filter for Cook County. The name changes in different years
+        filter(if ("COUNTY" %in% names(df))
+          COUNTY == '016' else INV_CO == '016')
 
       # Save the shapefile as a GeoParquet file
       geoarrow::write_geoparquet(shapefile_data, remote_file_path)
