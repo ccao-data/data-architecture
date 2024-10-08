de905ef91d4ea5d12d47e8474aa668e8384e82eb
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2024-10-08 15:48:08 +0000

    Change geoparquet function

--- etl/scripts-ccao-data-warehouse-us-east-1/spatial/spatial-environment-traffic.R
+++ etl/scripts-ccao-data-warehouse-us-east-1/spatial/spatial-environment-traffic.R
@@ -1,65 +1,53 @@
 library(aws.s3)
 library(dplyr)
+library(purrr)
 library(sf)
 library(geoarrow)
 
 # Define the S3 bucket and folder path
 AWS_S3_RAW_BUCKET <- Sys.getenv("AWS_S3_RAW_BUCKET")
 AWS_S3_WAREHOUSE_BUCKET <- Sys.getenv("AWS_S3_WAREHOUSE_BUCKET")
-s3_folder <- "spatial/environment/traffic/"
-output_bucket <- file.path(AWS_S3_WAREHOUSE_BUCKET,
-                           "spatial", "environment", "traffic")
-
-files_in_s3 <- get_bucket_df(
-                             bucket = AWS_S3_RAW_BUCKET, prefix = s3_folder)
+output_bucket <- file.path(
+  AWS_S3_WAREHOUSE_BUCKET, "spatial", "environment", "traffic"
+  )
 
 # Get the 'Key'
-parquet_files <- files_in_s3 %>%
+parquet_files <- get_bucket_df(
+  bucket = AWS_S3_RAW_BUCKET, prefix = s3_folder
+  ) %>%
   pull(Key)
 
 # Loop through each parquet file and process it
-for (file_key in parquet_files) {
+walk(parquet_files, \(file_key) {
 
-  # Read the parquet file directly from S3 using aws.s3 functions
-  obj <- get_object(object = file_key, bucket = AWS_S3_RAW_BUCKET)
+  if (!aws.s3::object_exists(file.path(AWS_S3_WAREHOUSE_BUCKET, file_key))) {
 
-  # Convert the S3 object into raw data and read using geoarrow
-  shapefile_data <- geoarrow::read_geoparquet(rawConnection(obj))
+    print(paste("Cleaning", file_key))
 
-  # Convert geometry column to 'sf' format
-  shapefile_data$geometry <- st_as_sfc(shapefile_data$geometry)
+    # Convert the S3 object into raw data and read using geoarrow
+    shapefile_data <- geoarrow::read_geoparquet_sf(
+      file.path(AWS_S3_RAW_BUCKET, file_key)
+    ) %>%
+      st_transform(4326) %>%
+      mutate(geometry_3435 = st_transform(geometry, 3435))
 
-  shapefile_data <- shapefile_data %>%
-    st_as_sf() %>%
-    st_transform(4326) %>%
-    mutate(geometry_3435 = st_transform(geometry, 3435))
+    # We do this because some columns are not present in
+    # older versions of the data
+    required_columns <- c("LNS", "SURF_TYP", "SURF_WTH", "SRF_YR", "AADT",
+                          "CRS_WITH", "CRS_OPP", "CRS_YR",
+                          "ROAD_NAME", "DTRESS_WTH", "DTRESS_OPP",
+                          "SP_LIM", "INVENTORY", "geometry_3435")
 
-  # We do this because some columns are not present in
-  # older versions of the data
-  required_columns <- c("LNS", "SURF_TYP", "SURF_WTH", "SRF_YR", "AADT",
-                        "CRS_WITH", "CRS_OPP", "CRS_YR",
-                        "ROAD_NAME", "DTRESS_WTH", "DTRESS_OPP",
-                        "SP_LIM", "INVENTORY", "geometry_3435")
+    # Select only the non-geometry columns that exist in the dataset
+    existing_columns <- intersect(required_columns, colnames(shapefile_data))
+    shapefile_data %>%
+      select(all_of(existing_columns)) %>%
+      geoarrow::write_geoparquet(
+        file.path(AWS_S3_WAREHOUSE_BUCKET, file_key)
+      )
 
-  # Select only the non-geometry columns that exist in the dataset
-  existing_columns <- intersect(required_columns,
-                                colnames(shapefile_data))
-  selected_columns <- shapefile_data %>%
-    select(all_of(existing_columns))
+    print(paste(file_key, "cleaned and uploaded."))
 
-  # Create a temporary file for saving the processed data
-  output_file <- tempfile(fileext = ".parquet")
+  }
 
-  # Write the selected columns to a new parquet file
-  geoarrow::write_geoparquet(selected_columns, output_file)
-
-  # Define the output file path in the S3 bucket
-  output_key <- file.path(output_bucket, basename(file_key))
-
-  # Upload the processed file to the S3 output bucket
-  put_object(file = output_file, object = output_key,
-             bucket = AWS_S3_WAREHOUSE_BUCKET)
-
-  # Clean up the temporary files
-  unlink(output_file)
-}
+})
