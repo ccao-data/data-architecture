603ffcb62355dd07b745da0cc26877294cf22bd8
Michael Wagner <wagnerleemichael@gmail.com>
2024-11-06 21:14:06 +0000

    Check alternative solution

--- dbt/models/default/default.vw_pin_sale.sql
+++ dbt/models/default/default.vw_pin_sale.sql
@@ -282,19 +282,12 @@ combined_sales AS (
             ELSE COALESCE(uq_sales.sale_date, md_sales.sale_date)
         END AS sale_date_coalesced,
         CASE
-            -- If uq_sales.doc_no is not NULL, apply the COALESCE logic
-            WHEN uq_sales.doc_no IS NOT NULL THEN
-                CASE
-                    WHEN COALESCE(
-                            md_sales.sale_date IS NOT NULL
-                            OR YEAR(uq_sales.sale_date) >= 2021,
-                            FALSE
-                        ) THEN TRUE
-                    ELSE FALSE
-                END
-            -- If uq_sales.doc_no is NULL, set is_mydec_date to TRUE
-            ELSE
-                TRUE
+            WHEN (uq_sales.year < '2021' OR uq_sales.sale_date IS NULL)
+                AND md_sales.sale_date IS NOT NULL
+                THEN TRUE
+            WHEN (uq_sales.year >= '2021' OR md_sales.sale_date IS NULL)
+                AND uq_sales.sale_date IS NOT NULL --noqa
+                THEN FALSE
         END AS is_mydec_date,
         COALESCE(uq_sales.sale_price, md_sales.sale_price)
             AS sale_price_coalesced, --noqa
