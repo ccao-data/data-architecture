1a3f32ebd8628506aa7492194ad5f9726d10a096
Damonamajor <damon.major@cookcountyil.gov>
2024-11-13 19:30:52 +0000

    Remove test macros

--- dbt/macros/split_data.sql
+++
@@ -1,7 +0,0 @@
-{% macro create_dist_to_nearest_cte(township_code) %}
-    filtered_joined_traffic_township_{{ township_code }} as (
-        select *
-        from {{ dist_to_nearest_geometry("filtered_joined_traffic_township") }}
-        where township_code = {{ township_code }}
-    )
-{% endmacro %}
