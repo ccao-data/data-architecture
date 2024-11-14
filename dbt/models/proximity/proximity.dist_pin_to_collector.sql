e415509f498d361d369377f1078a14ed281972f6
Damonamajor <damon.major@cookcountyil.gov>
2024-11-14 20:46:42 +0000

    Add highways/collector

---
+++ dbt/models/proximity/proximity.dist_pin_to_collector.sql
@@ -0,0 +1,31 @@
+{{
+    config(
+        materialized='table',
+        partitioned_by=['year'],
+        bucketed_by=['pin10'],
+        bucket_count=1
+    )
+}}
+
+WITH collector AS (  -- noqa: ST03
+    SELECT *
+    FROM {{ source('spatial', 'traffic') }}
+    WHERE road_type = 'Major Collector'
+        OR road_type = 'Minor Collector'
+),
+
+SELECT
+    pcl.pin10,
+    xy.year,
+    ARBITRARY(xy.road_name) AS nearest_collector_road_name,
+    ARBITRARY(xy.dist_ft) AS nearest_collector_road_dist_ft,
+    ARBITRARY(xy.year) AS nearest_collector_road_data_year,
+    ARBITRARY(xy.daily_traffic) AS nearest_collector_road_daily_traffic,
+    ARBITRARY(xy.speed_limit) AS nearest_collector_road_speed_limit,
+    ARBITRARY(xy.surface_type) AS nearest_collector_road_surface_type,
+    ARBITRARY(xy.lanes) AS nearest_collector_road_lanes
+FROM distinct_pins AS pcl
+INNER JOIN ( {{ dist_to_nearest_geometry('collector') }} ) AS xy
+    ON pcl.x_3435 = xy.x_3435
+    AND pcl.y_3435 = xy.y_3435
+GROUP BY pcl.pin10, xy.year
