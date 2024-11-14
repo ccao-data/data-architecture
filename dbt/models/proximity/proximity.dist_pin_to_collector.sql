48603c09c2b6f2e04dfc908025078a7ee6651737
Damonamajor <damon.major@cookcountyil.gov>
2024-11-14 21:20:09 +0000

    Fix query

--- dbt/models/proximity/proximity.dist_pin_to_collector.sql
+++ dbt/models/proximity/proximity.dist_pin_to_collector.sql
@@ -12,21 +12,21 @@ WITH collector AS (  -- noqa: ST03
     FROM {{ source('spatial', 'traffic') }}
     WHERE road_type = 'Major Collector'
         OR road_type = 'Minor Collector'
-),
+)
 
 SELECT
     pcl.pin10,
-    xy.year,
     ARBITRARY(xy.road_name) AS nearest_collector_road_name,
     ARBITRARY(xy.dist_ft) AS nearest_collector_road_dist_ft,
-    ARBITRARY(xy.year) AS nearest_collector_road_data_year,
     ARBITRARY(xy.daily_traffic) AS nearest_collector_road_daily_traffic,
     ARBITRARY(xy.speed_limit) AS nearest_collector_road_speed_limit,
     ARBITRARY(xy.surface_type) AS nearest_collector_road_surface_type,
-    ARBITRARY(xy.lanes) AS nearest_collector_road_lanes
+    ARBITRARY(xy.lanes) AS nearest_collector_road_lanes,
+    ARBITRARY(xy.year) AS nearest_collector_road_data_year,
+    pcl.year
 FROM {{ source('spatial', 'parcel') }} AS pcl
 INNER JOIN ( {{ dist_to_nearest_geometry('collector') }} ) AS xy
     ON pcl.x_3435 = xy.x_3435
     AND pcl.y_3435 = xy.y_3435
     AND pcl.year = xy.pin_year
-GROUP BY pcl.pin10, xy.year
+GROUP BY pcl.pin10, pcl.year
--- dbt/models/proximity/proximity.dist_pin_to_highway.sql
+++ dbt/models/proximity/proximity.dist_pin_to_highway.sql
@@ -12,21 +12,21 @@ WITH highway AS (  -- noqa: ST03
     FROM {{ source('spatial', 'traffic') }}
     WHERE road_type = 'Interstate'
         OR road_type = 'Freeway and Expressway'
-),
+)
 
 SELECT
     pcl.pin10,
-    xy.year,
     ARBITRARY(xy.road_name) AS nearest_highway_road_name,
     ARBITRARY(xy.dist_ft) AS nearest_highway_road_dist_ft,
-    ARBITRARY(xy.year) AS nearest_highway_road_data_year,
     ARBITRARY(xy.daily_traffic) AS nearest_highway_road_daily_traffic,
     ARBITRARY(xy.speed_limit) AS nearest_highway_road_speed_limit,
     ARBITRARY(xy.surface_type) AS nearest_highway_road_surface_type,
-    ARBITRARY(xy.lanes) AS nearest_highway_road_lanes
+    ARBITRARY(xy.lanes) AS nearest_highway_road_lanes,
+    ARBITRARY(xy.year) AS nearest_highway_road_data_year,
+    pcl.year
 FROM {{ source('spatial', 'parcel') }} AS pcl
 INNER JOIN ( {{ dist_to_nearest_geometry('highway') }} ) AS xy
     ON pcl.x_3435 = xy.x_3435
     AND pcl.y_3435 = xy.y_3435
     AND pcl.year = xy.pin_year
-GROUP BY pcl.pin10, xy.year
+GROUP BY pcl.pin10, pcl.year
