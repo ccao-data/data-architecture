7e09272dfd22fe01811b02279706e73b8815c515
Jean Cochrane <jean.cochrane@cookcountyil.gov>
2024-11-15 20:49:44 +0000

    Add comment to pre_stage_filters macro explaining `cur = 'Y'`

--- dbt/macros/pre_stage_filters.sql
+++ dbt/macros/pre_stage_filters.sql
@@ -1,5 +1,11 @@
 -- Macros to simplify the filter conditions that produce pre-mailed and
--- pre-certified values
+-- pre-certified values.
+--
+-- Note that the `cur = 'Y'` filter is not necessary in the
+-- default.vw_pin_value query that is the main consumer of these macros, since
+-- that query already filters rows where `cur = 'Y'`; however, we leave the
+-- filter in here so that it might make the macro more generally useful in
+-- other instances where we may not filter queries the same way
 {% macro pre_stage_filters(tablename, stage_name) %}
     {{ tablename }}.procname is null
     and {{ tablename }}.cur = 'Y'
