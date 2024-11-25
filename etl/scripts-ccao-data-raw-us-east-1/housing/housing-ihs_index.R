3e329453491717a9f47ca0842b4a55eed7245e0e
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2024-11-25 23:58:15 +0000

    Minor code cleaning

--- etl/scripts-ccao-data-raw-us-east-1/housing/housing-ihs_index.R
+++ etl/scripts-ccao-data-raw-us-east-1/housing/housing-ihs_index.R
@@ -26,13 +26,13 @@ remote_file <- file.path(output_bucket, paste0("ihs_price_index_data.parquet"))
 
 # Grab the data, clean it just a bit, and write if it doesn't already exist
 data.frame(t(
-  openxlsx::read.xlsx(most_recent_ihs_data_url, sheet = 2) %>%
-    dplyr::select(-c("X2", "X3", "X4"))
+  read.xlsx(most_recent_ihs_data_url, sheet = 2) %>%
+    select(-c("X2", "X3", "X4"))
 )) %>%
   # Names and columns are kind of a mess after the transpose,
   # shift up first row, shift over column names
-  janitor::row_to_names(1) %>%
-  dplyr::mutate(puma = rownames(.)) %>%
-  dplyr::relocate(puma, .before = "YEARQ") %>%
-  dplyr::rename(name = "YEARQ") %>%
-  arrow::write_parquet(remote_file)
+  row_to_names(1) %>%
+  mutate(puma = rownames(.)) %>%
+  relocate(puma, .before = "YEARQ") %>%
+  rename(name = "YEARQ") %>%
+  write_parquet(remote_file)
