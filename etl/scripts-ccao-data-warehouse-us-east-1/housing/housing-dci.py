952b4b8cf0827cb249ff66b289a8c099d7b29212
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2024-12-17 22:15:16 +0000

    Housing

--- etl/scripts-ccao-data-warehouse-us-east-1/housing/housing-dci.py
+++ etl/scripts-ccao-data-warehouse-us-east-1/housing/housing-dci.py
@@ -1,5 +1,6 @@
 import os
 import tempfile
+from datetime import datetime
 
 import boto3
 import pandas as pd
@@ -39,6 +40,8 @@ data = data[
 )
 data["geoid"] = data["geoid"].astype(str)
 
+# Upload the Parquet file to S3
+data["loaded_at"] = str(datetime.now())
 data.to_parquet(
     os.path.join(
         os.environ["AWS_S3_WAREHOUSE_BUCKET"],
