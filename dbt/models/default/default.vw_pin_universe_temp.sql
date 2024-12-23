eb16ee785b08437475607c48fbe2fc659afb363c
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2024-12-23 22:49:16 +0000

    Correct ref

--- dbt/models/default/default.vw_pin_universe_temp.sql
+++ dbt/models/default/default.vw_pin_universe_temp.sql
@@ -1,3 +1,3 @@
 -- Small, managable version of pin universe for the purporse of updating the
 -- Parcel Universe asset on open data.
-SELECT * FROM {{ ref('location.vw_pin10_location') }} LIMIT 100000
+SELECT * FROM {{ ref('default.vw_pin_universe') }} LIMIT 100000
