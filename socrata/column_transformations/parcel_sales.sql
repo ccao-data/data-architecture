92431d1ab5f1217da429410175fb81c95d5bb1c4
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2025-01-06 17:30:42 +0000

    Add sales asset re-codes

--- socrata/column_transformations/parcel_sales.sql
+++ socrata/column_transformations/parcel_sales.sql
@@ -1,1 +1,12 @@
 left_pad(`pin`, 14, '0')
+
+CASE
+  WHEN (`deed_type` = "01") THEN "Warranty"
+  WHEN (`deed_type` = "02") THEN "Trustee"
+  WHEN (`deed_type` = "03") THEN "Quit claim"
+  WHEN (`deed_type` = "04") THEN "Executor"
+  WHEN (`deed_type` = "05") THEN "Other"
+  WHEN (`deed_type` = "06") THEN "Beneficiary"
+  WHEN (`deed_type` = "99") THEN "Unknown"
+  ELSE null
+END
