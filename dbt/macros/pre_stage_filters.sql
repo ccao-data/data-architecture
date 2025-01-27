62bf0e0718f566e6e9e12dc540c53febdb486b20
Jean Cochrane <jean.cochrane@cookcountyil.gov>
2025-01-27 17:35:14 +0000

    Fixup pre stage filters to accommodate rollover in `default.vw_pin_value`

--- dbt/macros/pre_stage_filters.sql
+++ dbt/macros/pre_stage_filters.sql
@@ -1,21 +1,18 @@
 -- Macros to simplify the filter conditions that produce pre-mailed and
--- pre-certified values.
+-- pre-certified values in the `default.vw_pin_value` view.
 --
 -- Note that the `cur = 'Y'` filter is not necessary in the
 -- default.vw_pin_value query that is the main consumer of these macros, since
--- that query already filters rows where `cur = 'Y'`; however, we leave the
--- filter in here so that it might make the macro more generally useful in
--- other instances where we may not filter queries the same way
-{% macro pre_stage_filters(tablename, stage_name) %}
+-- that query already filters rows where `cur = 'Y'`
+{% macro pre_mailed_filters(tablename) %}
     {{ tablename }}.procname is null
     and {{ tablename }}.cur = 'Y'
-    and not contains(stages.procnames, '{{ stage_name }}')
-{% endmacro %}
-
-{% macro pre_mailed_filters(tablename) %}
-    {{- pre_stage_filters(tablename, "CCAOVALUE") -}}
+    and cardinality(stages.procnames) = 0
 {% endmacro %}
 
 {% macro pre_certified_filters(tablename) %}
-    {{- pre_stage_filters(tablename, "CCAOFINAL") -}}
+    {{ tablename }}.procname is null
+    and {{ tablename }}.cur = 'Y'
+    and cardinality(stages.procnames) > 0
+    and not contains(stages.procnames, 'CCAOVALUE')
 {% endmacro %}
--- dbt/models/default/default.vw_pin_value.sql
+++ dbt/models/default/default.vw_pin_value.sql
@@ -16,10 +16,25 @@ WITH stages AS (
     SELECT
         parid,
         taxyr,
-        ARRAY_AGG(procname) AS procnames
+        -- Force an empty array representing the procnames for any PIN/year
+        -- combo that has no procnames yet, so that our `CONTAINS()` check
+        -- in subsequent queries that join to this stage do not have to worry
+        -- about side effects from null comparisons
+        ARRAY_REMOVE(
+            ARRAY_AGG(
+                CASE
+                    WHEN procname IN ('CCAOVALUE', 'CCAOFINAL', 'BORVALUE')
+                        THEN procname
+                    -- Can't use null to indicate missing data here, since a
+                    -- null comparison in the outer `ARRAY_REMOVE` call would
+                    -- always cast the array to null
+                    ELSE ''
+                END
+            ),
+            ''
+        ) AS procnames
     FROM {{ source('iasworld', 'asmt_all') }}
-    WHERE procname IN ('CCAOVALUE', 'CCAOFINAL', 'BORVALUE')
-        AND rolltype != 'RR'
+    WHERE rolltype != 'RR'
         AND deactivat IS NULL
         AND valclass IS NULL
     GROUP BY parid, taxyr
