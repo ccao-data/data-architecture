d76db0bd415d8f64189d9b3921f5aa3f73f316c1
Jean Cochrane <jeancochrane@users.noreply.github.com>
2025-02-13 13:20:08 -0600

    Add assessable permits flag to `vw_pin_status` view (#733)
    * Add assessable permits flag to `vw_pin_status` view
    
    * Rename assessable permit field and document it
    
    * Add data test to ensure `has_recent_assessable_permit` is not null
    
    * Remove unnecessary GROUP BY in CTE that powers `has_recent_assessable_permit` flag in `vw_pin_status`

--- dbt/models/default/default.vw_pin_status.sql
+++ dbt/models/default/default.vw_pin_status.sql
@@ -87,6 +87,31 @@ WITH ahsap AS (
     WHERE par.cur = 'Y'
         AND par.deactivat IS NULL
     GROUP BY par.parid, par.taxyr
+),
+
+-- Get a table with the number of assessable permits in the past 3 years for
+-- all PINs in all years, which we use to power a "recent assessable permit"
+-- flag
+assessable_permits_past_3_years AS (
+    SELECT
+        pardat.parid AS pin,
+        pardat.taxyr AS year,
+        SUM(
+            CASE WHEN assessable_permit.year IS NULL THEN 0 ELSE 1 END
+        ) AS num_assessable_permits
+    FROM {{ source('iasworld', 'pardat') }} AS pardat
+    LEFT JOIN (
+        SELECT
+            pin,
+            SUBSTR(date_issued, 1, 4) AS year
+        FROM {{ ref('default.vw_pin_permit') }}
+        WHERE assessable = 'A'
+            AND date_issued IS NOT NULL
+    ) AS assessable_permit
+        ON pardat.parid = assessable_permit.pin
+        AND CAST(assessable_permit.year AS INT)
+        BETWEEN CAST(pardat.taxyr AS INT) - 2 AND CAST(pardat.taxyr AS INT)
+    GROUP BY pardat.parid, pardat.taxyr
 )
 
 SELECT
@@ -118,7 +143,8 @@ SELECT
     ddat.cdu_description AS dweldat_cdu_description,
     pdat.note2 AS pardat_note,
     pdat.class = '999' AS is_filler_class,
-    pdat.parid LIKE '%999%' AS is_filler_pin
+    pdat.parid LIKE '%999%' AS is_filler_pin,
+    permit.num_assessable_permits > 0 AS has_recent_assessable_permit
 FROM {{ source('iasworld', 'pardat') }} AS pdat
 LEFT JOIN {{ source('spatial', 'corner') }} AS colo
     ON SUBSTR(pdat.parid, 1, 10) = colo.pin10
@@ -169,5 +195,8 @@ LEFT JOIN ({{ aggregate_cdu(
 LEFT JOIN {{ ref('ccao.pin_test') }} AS ptst
     ON pdat.parid = ptst.pin
     AND pdat.taxyr = ptst.year
+LEFT JOIN assessable_permits_past_3_years AS permit
+    ON pdat.parid = permit.pin
+    AND pdat.taxyr = permit.year
 WHERE pdat.cur = 'Y'
     AND pdat.deactivat IS NULL
