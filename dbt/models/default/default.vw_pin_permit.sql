5ed7282f7ab48970ceb4555f5fb6e69a8fc8a28e
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2025-02-27 17:14:34 +0000

    Make year varchar

--- dbt/models/default/default.vw_pin_permit.sql
+++ dbt/models/default/default.vw_pin_permit.sql
@@ -25,7 +25,7 @@ permit_addresses AS (
 
 SELECT
     permit.parid AS pin,
-    YEAR(CAST(permit.permdt AS TIMESTAMP)) AS year,
+    SUBSTR(permit.permdt, 1, 4) AS year,
     permit.num AS permit_number,
     permit.user28 AS local_permit_number,
     permit.permdt AS date_issued,
