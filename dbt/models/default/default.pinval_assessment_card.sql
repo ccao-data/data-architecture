73751aedc69dcab4ef916fc1fa518525b12568a9
Michael Wagner <wagnerleemichael@gmail.com>
2025-04-24 20:35:09 +0000

    Remove source to test

--- dbt/models/default/default.pinval_assessment_card.sql
+++ dbt/models/default/default.pinval_assessment_card.sql
@@ -28,7 +28,7 @@ final_model_run AS (
     SELECT
         year,
         SUBSTRING(run_id, 1, 10) AS final_model_run_date
-    FROM {{ source('model', 'final_model') }}
+    FROM model.final_model
     WHERE type = 'res'
         AND is_final
 )
