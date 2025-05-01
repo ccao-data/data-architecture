b2ba372478bb297e26619a6eea06affb8d875977
Jean Cochrane <jeancochrane@users.noreply.github.com>
2025-05-01 10:00:45 -0500

    Final tweaks to new data integrity tests (#797)
    * Fix bug preventing CI from running DI tests that reference iasWorld models
    
    * Tweak `row_count` generic dbt test to support `equals` parameter
    
    * Run tests on every PR, not just branches off of main branch
    
    * Fix some more row_count data integrity tests
    
    * Fix new proximity tests and remove unnecessary `load_manual` tags
    
    * Remove docs for testing missing vacant/new construction parcels
    
    * Revert "Run tests on every PR, not just branches off of main branch"
    
    This reverts commit 9a41019dee73c078039a483ec74302f941bce135.
    
    * Fix typo in `test_row_count` generic test
    
    * Clear up comment on default_vw_pin_appeal_row_count
    
    * Switch a couple more `row_count` tests to use `equals` instead of `above`
    
    * Temporarily enable tests to run on non-main branches
    
    * Revert "Temporarily enable tests to run on non-main branches"
    
    This reverts commit fe0f52d24d58bfd3e02ba2ea2a1292794f8d886f.

--- dbt/tests/generic/test_row_count.sql
+++ dbt/tests/generic/test_row_count.sql
@@ -1,11 +1,26 @@
--- Test that row counts are above a certain value.
--- This includes an implicit test to pass when row counts match.
-{% test row_count(model, column_name, above) %}
+-- Test that row counts are equal to or above a certain value.
+-- Note that `above` is greater-than-or-equal-to, so equal values will pass.
+{% test row_count(model, column_name, above=none, equals=none) %}
+    {%- if above is none and equals is none -%}
+        {{
+            exceptions.raise_compiler_error(
+                "One of the arguments 'above' or 'equals' is required"
+            )
+        }}
+    {%- elif above is not none and equals is not none -%}
+        {{
+            exceptions.raise_compiler_error(
+                "The arguments 'above' and 'equals' cannot both be set"
+            )
+        }}
+    {%- endif -%}
+    {%- set comparison_operator = "<" if above is not none else "!=" -%}
+    {%- set comparison_value = above or equals -%}
     {%- if column_name is defined %} {%- set count_col = column_name %}
     {%- else %} {%- set count_col = "*" %}
     {%- endif %}
 
     select row_count
     from (select count({{ count_col }}) as row_count from {{ model }})
-    where row_count < {{ above }}
+    where row_count {{ comparison_operator }} {{ comparison_value }}
 {% endtest %}
