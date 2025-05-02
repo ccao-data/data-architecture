370cecece60323b2e55e6b6b4fba4435708cb3de
Damonamajor <damon.major@cookcountyil.gov>
2025-05-02 16:24:02 +0000

    update docs

--- etl/scripts-ccao-data-warehouse-us-east-1/model/model-training_data.R
+++ etl/scripts-ccao-data-warehouse-us-east-1/model/model-training_data.R
@@ -7,12 +7,14 @@ library(noctua)
 library(purrr)
 library(stringr)
 
-# Once original data has been uploaded, we should upload the current year
+# Once original data has been uploaded, we should do it for the current year
 run_year <- format(Sys.Date(), "%Y")
 
 # Connect to Athena
 noctua_options(cache_size = 10)
 conn <- dbConnect(noctua::athena(), rstudio_conn_tab = FALSE)
+AWS_S3_WAREHOUSE_BUCKET <- "s3://ccao-data-warehouse-us-east-1"
+output_bucket <- file.path(AWS_S3_WAREHOUSE_BUCKET, "model", "training_data")
 
 # Query final model metadata
 metadata <- dbGetQuery(
@@ -46,12 +48,10 @@ for (i in seq_len(nrow(metadata))) {
 
   # Build DVC path depending on year
   # The dvc path changes in 2023
-  dvc_path <- if (as.integer(year) < 2023) {
-    glue("s3://ccao-data-dvc-us-east-1/{substr(dvc_hash, 1, 2)}/
-         {substr(dvc_hash, 3, 32)}")
+  dvc_path <- if (as.integer(year) <= 2023) {
+    glue("s3://ccao-data-dvc-us-east-1/{substr(dvc_hash, 1, 2)}/{substr(dvc_hash, 3, 32)}") # nolint: line_length_linter
   } else {
-    glue("s3://ccao-data-dvc-us-east-1/files/md5/{substr(dvc_hash, 1, 2)}
-         /{substr(dvc_hash, 3, 32)}")
+    glue("s3://ccao-data-dvc-us-east-1/files/md5/{substr(dvc_hash, 1, 2)}/{substr(dvc_hash, 3, 32)}") # nolint: line_length_linter
   }
 
   message(glue("Processing run_id: {run_id}, year: {year}"))
@@ -59,16 +59,24 @@ for (i in seq_len(nrow(metadata))) {
   # Read and filter training data
   df <- open_dataset(dvc_path) %>%
     select(meta_pin, meta_card_num, all_of(predictor_vars)) %>%
-    collect() %>%
-    mutate(run_id = run_id)
+    collect()
 
-  # Save to S3, partitioned by run_id
-  write_dataset(
-    df,
-    path = "s3://ccao-data-warehouse-us-east-1/model/training_data",
-    format = "parquet",
-    partitioning = c("run_id"),
-    existing_data_behavior = "overwrite",
-    basename_template = paste0(run_id, ".parquet")
-  )
+  # Ensure known type mismatches are cast consistently
+  if ("ccao_is_active_exe_homeowner" %in% names(df)) {
+    df <- df %>%
+      mutate(
+        ccao_is_active_exe_homeowner =
+          as.logical(ccao_is_active_exe_homeowner)
+      )
+  }
+
+  # Add run_id after cleaning types
+  df <- df %>%
+    mutate(run_id = run_id) %>%
+    group_by(run_id) %>%
+    write_partitions_to_s3(
+      output_bucket,
+      is_spatial = FALSE,
+      overwrite = TRUE
+    )
 }
