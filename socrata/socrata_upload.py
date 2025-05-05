8f734eaa2b4aed89237d8447711cf5203cfa7d3e
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2025-05-05 18:13:30 +0000

    Make sure deleted indicator is included in upload

--- socrata/socrata_upload.py
+++ socrata/socrata_upload.py
@@ -209,7 +209,7 @@ def build_query_dict(athena_asset, asset_id, years=None):
         .split(",")
     )
     # row id won't show up here since it's hidden on the open data portal assets
-    asset_columns += ["row_id"]
+    asset_columns += ["row_id", ":deleted"]
     asset_columns.sort()
 
     # If there are columns on Socrata that are not in Athena, abort upload and
