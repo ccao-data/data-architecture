4c88fa166adae47578535e8e084bfe6d93fc9c11
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2025-05-09 15:19:06 +0000

    Commenting

--- dbt/macros/open_data_columns.sql
+++ dbt/macros/open_data_columns.sql
@@ -2,8 +2,9 @@
 Creates syntax for open data view columns year, row_id, and ":deleted". The
 row_id is a unique identifier for each row in the open data views, while the
 ":deleted" column indicates whether a row has been deleted from
-`iasworld.pardat` and also needs to be flagged for deletion from the open data
-portal assets.
+`iasworld.pardat` (or other tables that collectively define a given asset's
+universe along with `pardat`) and also needs to be flagged for deletion from the
+open data portal assets.
 
 The only real complication here is that feeder views can have different columns
 that define row_id. Currently, the only case we are accomodating is res sf/mf
