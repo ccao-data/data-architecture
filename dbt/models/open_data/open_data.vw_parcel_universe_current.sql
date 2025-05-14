b9a37823522a446fcba906339eace17ab6bba1ae
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2025-05-14 18:26:12 +0000

    Correct view ref

--- dbt/models/open_data/open_data.vw_parcel_universe_current.sql
+++ dbt/models/open_data/open_data.vw_parcel_universe_current.sql
@@ -2,7 +2,10 @@
 -- Universe (Current Year)" open data asset.
 -- Some columns from the feeder view may not be present in this view.
 
-SELECT feeder.*
-FROM {{ ref('vw_parcel_universe_historical') }} AS feeder
-WHERE feeder.year
-    = (SELECT MAX(year) FROM {{ ref('vw_parcel_universe_historical') }})
+SELECT *
+FROM {{ ref('open_data.vw_parcel_universe_historical') }}
+WHERE year
+    = (
+        SELECT MAX(year)
+        FROM {{ ref('open_data.vw_parcel_universe_historical') }}
+    )
