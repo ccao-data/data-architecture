face30551a0386009aa2cd159dfd2c1007d9e4ea
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2025-05-20 17:02:49 +0000

    Appeal fix

--- dbt/macros/open_data_join_rows_to_delete.sql
+++ dbt/macros/open_data_join_rows_to_delete.sql
@@ -16,36 +16,56 @@ how to construct the approriate universe of rows to purge.
 {% macro open_data_join_rows_to_delete(allow_999=false, condo=false, addn_table=none) %}
     full outer join
         (
-            select
-            {% if addn_table == "sales" %}
-                    salekey as row_id,
-                    substr(saledt, 1, 4) as year,
-                    true as ":deleted"
+            {% if addn_table == "htpar" %}
+                with
+                    entire_case as (
+                        select
+                            addndat.parid || addndat.taxyr || addndat.caseno as row_id,
+                            addndat.taxyr as year,
+                            true as ":deleted",
+                            avg(
+                                case
+                                    when addndat.deactivat is not null then 1 else 0
+                                end
+                            ) as deactivat
+                        from {{ source("iasworld", addn_table) }} as addndat
+                        left join
+                            {{ source("iasworld", "pardat") }} as pdat
+                            on addndat.parid = pdat.parid
+                            and addndat.taxyr = pdat.taxyr
+                        where addndat.caseno is not null or pdat.deactivat is not null
+                        group by
+                            addndat.parid || addndat.taxyr || addndat.caseno,
+                            addndat.taxyr
+                    )
+                select row_id, year, ":deleted"
+                from entire_case
+                where deactivat = 1
+            {% elif addn_table == "sales" %}
+                select
+                    salekey as row_id, substr(saledt, 1, 4) as year, true as ":deleted"
                 from {{ source("iasworld", addn_table) }}
                 where deactivat is not null
             {% elif addn_table == "permit" %}
+                select
                     parid || coalesce(num, '') || coalesce(permdt, '') as row_id,
                     substr(permdt, 1, 4) as year,
                     true as ":deleted"
                 from {{ source("iasworld", addn_table) }}
                 where deactivat is not null
             {% else %}
+                select
                     {% if addn_table == "dweldat" %}
                         pdat.parid
                         || cast(addndat.card as varchar)
                         || pdat.taxyr as row_id,
-                    -- Unfortunately, some iasworld tables are not unique by the columns
-                    -- we expect them to be when look at deactivated rows, so we need to
-                    -- use distinct.
-                    {% elif addn_table == "htpar" %}
-                        distinct pdat.parid || pdat.taxyr || addndat.caseno as row_id,
                     {% else %} pdat.parid || pdat.taxyr as row_id,
                     {% endif %}
                     pdat.taxyr as year,
                     true as ":deleted"
                 from {{ source("iasworld", "pardat") }} as pdat
                 {% if addn_table is not none %}
-                    {% if addn_table in ["dweldat", "htpar"] %} inner join
+                    {% if addn_table == "dweldat" %} inner join
                     {% else %} left join
                     {% endif %}
                         {{ source("iasworld", addn_table) }} as addndat
@@ -54,11 +74,6 @@ how to construct the approriate universe of rows to purge.
                 {% endif %}
                 where
                     pdat.deactivat is not null
-                    {% if addn_table == "htpar" %}
-                        or (
-                            addndat.deactivat is not null and addndat.caseno is not null
-                        )
-                    {% endif %}
                     {% if addn_table == "dweldat" %} or addndat.deactivat is not null
                     {% endif %}
                     {% if addn_table == "owndat" %} or addndat.ownnum is null
