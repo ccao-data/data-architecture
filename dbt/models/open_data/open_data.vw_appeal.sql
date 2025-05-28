9fc4c796b05821ce2b1637971c2302139680fcc1
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2025-05-28 18:43:31 +0000

    Appeals

--- dbt/models/open_data/open_data.vw_appeal.sql
+++ dbt/models/open_data/open_data.vw_appeal.sql
@@ -25,4 +25,41 @@ SELECT
     feeder.status,
     {{ open_data_columns(row_id_cols=['pin', 'year', 'case_no']) }}
 FROM {{ ref('default.vw_pin_appeal') }} AS feeder
-{{ open_data_join_rows_to_delete(addn_table="htpar") }}
+FULL OUTER JOIN
+    (
+
+        -- htpar is by far the most complex here since row_id's can show
+        -- up multiple times, sometimes being deactivated and sometimes
+        -- not.
+        WITH
+        entire_case AS (
+            SELECT
+                addndat.parid || addndat.taxyr || addndat.caseno AS row_id,
+                addndat.taxyr AS year,
+                TRUE AS deleted,
+                AVG(
+                    CASE
+                        WHEN addndat.deactivat IS NOT NULL THEN 1 ELSE 0
+                    END
+                ) AS deactivat
+            FROM {{ source("iasworld", "htpar") }} AS addndat
+            LEFT JOIN
+                {{ source("iasworld", "pardat") }} AS pdat
+                ON addndat.parid = pdat.parid
+                AND addndat.taxyr = pdat.taxyr
+            WHERE addndat.caseno IS NOT NULL OR pdat.deactivat IS NOT NULL
+            GROUP BY
+                addndat.parid || addndat.taxyr || addndat.caseno,
+                addndat.taxyr
+        )
+
+        SELECT
+            row_id,
+            year,
+            deleted AS ":deleted" -- noqa: RF05
+        FROM entire_case
+        WHERE deactivat = 1
+
+    ) AS deleted_rows
+    ON feeder.pin || feeder.year || feeder.case_no
+    = deleted_rows.row_id
