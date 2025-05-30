c4b1a8f51023b82b0185e323737cef1e664abe42
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2025-05-30 21:33:58 +0000

    Swicth from dev to ci

--- socrata/socrata_upload.py
+++ socrata/socrata_upload.py
@@ -237,7 +237,7 @@ def build_query_dict(athena_asset, asset_id, years=None):
     print(f"The following columns will be updated for {athena_asset}:")
     print(columns)
 
-    query = f"""SELECT {", ".join(columns["column"])} FROM {athena_asset.replace("open_data", "z_dev_wridgeway_open_data")}"""
+    query = f"""SELECT {", ".join(columns["column"])} FROM {athena_asset.replace("open_data", "z_ci_alternate_open_data_delete_solution_open_data")}"""
 
     # Build a dictionary with queries for each year requested, or no years
     if not years:
