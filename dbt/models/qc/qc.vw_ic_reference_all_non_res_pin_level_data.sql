3ac804dfca59d7ee0fc20ff104376e423ef9562e
Jean Cochrane <jean@jeancochrane.com>
2025-06-12 12:33:09 -0500

    Add `owndat` and `asmt` fields to IC PIN-level reference file output

--- dbt/models/qc/qc.vw_ic_reference_all_non_res_pin_level_data.sql
+++ dbt/models/qc/qc.vw_ic_reference_all_non_res_pin_level_data.sql
@@ -14,14 +14,21 @@ SELECT
     legdat.adrdir,
     legdat.adrstr,
     legdat.user1 AS township_code,
-    legdat.cityname,
+    legdat.cityname AS leg_cityname,
     legdat.taxdist,
-    legdat.zip1,
+    legdat.zip1 AS leg_zip1,
     {{ insert_hyphens("pardat.nbhd", 2) }} AS nbhd,
     pardat.note1,
     pardat.user1,
     pardat.zoning,
-    owndat.own1
+    owndat.own1,
+    owndat.addr1,
+    owndat.cityname AS own_cityname,
+    owndat.statecode,
+    owndat.zip1 AS own_zipcode,
+    asmt.valasm1,
+    asmt.valasm2,
+    asmt.valasm3
 FROM {{ source('iasworld', 'aprval') }} AS aprval
 LEFT JOIN {{ source('iasworld', 'pardat') }} AS pardat
     ON aprval.parid = pardat.parid
@@ -38,6 +45,16 @@ LEFT JOIN {{ source('iasworld', 'owndat') }} AS owndat
     AND pardat.taxyr = owndat.taxyr
     AND owndat.cur = 'Y'
     AND owndat.deactivat IS NULL
+LEFT JOIN (
+    SELECT asmt_all.* FROM {{ source('iasworld', 'asmt_all') }} AS asmt_all
+    EXCEPT
+    SELECT asmt_hist.* FROM {{ source('iasworld', 'asmt_hist') }} AS asmt_hist
+) AS asmt
+    ON aprval.parid = asmt.parid
+    AND aprval.taxyr = asmt.taxyr
+    AND asmt.cur = 'Y'
+    AND asmt.deactivat IS NULL
+    AND asmt.valclass IS NULL
 WHERE aprval.cur = 'Y'
     AND aprval.deactivat IS NULL
     AND pardat.class NOT BETWEEN '200' AND '299'
