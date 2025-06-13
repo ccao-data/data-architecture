0d56d9e512af82ae0a6d1b9204e1a6d78d6c7cd4
Michael Wagner <wagnerleemichael@gmail.com>
2025-06-13 21:17:44 +0000

    Add crosswalk

--- dbt/models/pinval/pinval.vw_assessment_card.sql
+++ dbt/models/pinval/pinval.vw_assessment_card.sql
@@ -2,17 +2,10 @@ WITH runs_to_include AS (
     SELECT
         run_id,
         model_predictor_all_name,
-        assessment_year
+        assessment_year,
+        assessment_triad
     FROM {{ source('model', 'metadata') }}
-    WHERE run_id = 'comps'
-),
-
-school_district AS (
-    SELECT
-        geoid,
-        name,
-        year
-    FROM {{ source('spatial', 'school_district') }}
+    WHERE run_id = '2025-02-11-charming-eric'
 ),
 
 final_model_run AS (
@@ -22,18 +15,18 @@ final_model_run AS (
         SUBSTRING(run_id, 1, 10) AS final_model_run_date
     FROM {{ ref('model.final_model') }}
     WHERE type = 'res'
-      AND is_final
+        AND is_final
 )
 
 SELECT
     ac.*,
     ap.pred_pin_final_fmv_round,
     ap.loc_property_address AS property_address,
-
     elem_sd.name AS school_elementary_district_name,
-    sec_sd.name  AS school_secondary_district_name,
-
+    sec_sd.name AS school_secondary_district_name,
     run.model_predictor_all_name,
+    run.assessment_triad,
+    run.assessment_year,
     final.final_model_run_date
 FROM runs_to_include AS run
 INNER JOIN model.assessment_card AS ac
@@ -41,10 +34,10 @@ INNER JOIN model.assessment_card AS ac
 LEFT JOIN model.assessment_pin AS ap
     ON ac.meta_pin = ap.meta_pin
     AND ac.run_id = ap.run_id
-LEFT JOIN school_district AS elem_sd
+LEFT JOIN spatial.school_district AS elem_sd
     ON ac.loc_school_elementary_district_geoid = elem_sd.geoid
     AND ac.meta_year = elem_sd.year
-LEFT JOIN school_district AS sec_sd
+LEFT JOIN spatial.school_district AS sec_sd
     ON ac.loc_school_secondary_district_geoid = sec_sd.geoid
     AND ac.meta_year = sec_sd.year
 LEFT JOIN final_model_run AS final
