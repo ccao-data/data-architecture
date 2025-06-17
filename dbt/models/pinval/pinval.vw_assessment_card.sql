9df3429ce868cd51aafbcc54e909522c6d81ab46
Michael Wagner <wagnerleemichael@gmail.com>
2025-06-17 15:01:47 +0000

    Update query so schools and muni rely on pin fill table

--- dbt/models/pinval/pinval.vw_assessment_card.sql
+++ dbt/models/pinval/pinval.vw_assessment_card.sql
@@ -22,9 +22,9 @@ SELECT
     ac.*,
     ap.pred_pin_final_fmv_round,
     ap.loc_property_address AS property_address,
-    elem_sd.name AS school_elementary_district_name,
-    sec_sd.name AS school_secondary_district_name,
-    loc.tax_municipality_name,
+    elem_sd.school_elementary_district_name,
+    sec_sd.school_secondary_district_name,
+    mun.tax_municipality_name,
     run.model_predictor_all_name,
     run.assessment_triad,
     run.assessment_year,
@@ -35,15 +35,15 @@ INNER JOIN model.assessment_card AS ac
 LEFT JOIN model.assessment_pin AS ap
     ON ac.meta_pin = ap.meta_pin
     AND ac.run_id = ap.run_id
-LEFT JOIN spatial.school_district AS elem_sd
+LEFT JOIN location.vw_pin10_location_fill AS elem_sd
     ON ac.loc_school_elementary_district_geoid = elem_sd.geoid
     AND ac.meta_year = elem_sd.year
-LEFT JOIN spatial.school_district AS sec_sd
+LEFT JOIN location.vw_pin10_location_fill AS sec_sd
     ON ac.loc_school_secondary_district_geoid = sec_sd.geoid
     AND ac.meta_year = sec_sd.year
-LEFT JOIN location.vw_pin10_location AS loc
-    ON SUBSTRING(ac.meta_pin, 1, 10) = loc.pin10
-    AND ac.meta_year = loc.year
+LEFT JOIN location.vw_pin10_location_fill AS mun
+    ON SUBSTRING(ac.meta_pin, 1, 10) = mun.pin10
+    AND ac.meta_year = mun.year
 LEFT JOIN final_model_run AS final
     ON run.assessment_year = final.year
 WHERE ap.meta_triad_code = final.triad_code
