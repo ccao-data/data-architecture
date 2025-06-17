838ed37dc69ab24d6e3e79594772a1f6e0a3187c
wagnerlmichael <93889413+wagnerlmichael@users.noreply.github.com>
2025-06-17 13:52:15 -0500

    Add more data for pinval tables (#812)
    * Attempt to add sale_year_range
    
    * Fix comma
    
    * Add date format
    
    * Add column for triad name
    
    * Add assessment_triad to vw_comp
    
    * Add assessment year
    
    * Fix alias length
    
    * Fix alias refs
    
    * Format
    
    * Use different source for school district data
    
    * Revert
    
    * Test join for school district names
    
    * Change ref to source
    
    * Add crosswalk
    
    * Add municipality
    
    * Switch string cut
    
    * Update dbt/models/pinval/pinval.vw_assessment_card.sql
    
    Co-authored-by: Jean Cochrane <jeancochrane@users.noreply.github.com>
    
    * Update query so schools and muni rely on pin fill table
    
    * Switch column names
    
    * Fix column ref for vw_pin_location
    
    * Use spatial table cta to get school districts
    
    * Fix column ref
    
    * Remove municipality
    
    * Switch out school district calculation
    
    ---------
    
    Co-authored-by: Jean Cochrane <jeancochrane@users.noreply.github.com>

--- dbt/models/pinval/pinval.vw_assessment_card.sql
+++ dbt/models/pinval/pinval.vw_assessment_card.sql
@@ -2,21 +2,10 @@ WITH runs_to_include AS (
     SELECT
         run_id,
         model_predictor_all_name,
-        assessment_year
+        assessment_year,
+        assessment_triad
     FROM {{ source('model', 'metadata') }}
-    -- This will eventually grab all run_ids where
-    -- run_type == comps
     WHERE run_id = '2025-02-11-charming-eric'
-),
-
-school_data AS (
-    SELECT
-        pin10 AS school_pin,
-        year,
-        school_elementary_district_name,
-        school_secondary_district_name
-    FROM {{ ref('location.school') }}
-    WHERE year > '2014'
 ),
 
 final_model_run AS (
@@ -27,16 +16,27 @@ final_model_run AS (
     FROM {{ ref('model.final_model') }}
     WHERE type = 'res'
         AND is_final
+),
+
+school_districts AS (
+    SELECT
+        geoid,
+        year,
+        MAX(name) AS name
+    FROM spatial.school_district
+    WHERE geoid IS NOT NULL
+    GROUP BY geoid, year
 )
 
 SELECT
     ac.*,
     ap.pred_pin_final_fmv_round,
     ap.loc_property_address AS property_address,
-    school.school_elementary_district_name
-        AS loc_school_elementary_district_name,
-    school.school_secondary_district_name AS loc_school_secondary_district_name,
+    elem_sd.name AS school_elementary_district_name,
+    sec_sd.name AS school_secondary_district_name,
     run.model_predictor_all_name,
+    run.assessment_triad,
+    run.assessment_year,
     final.final_model_run_date
 FROM runs_to_include AS run
 INNER JOIN model.assessment_card AS ac
@@ -44,9 +44,12 @@ INNER JOIN model.assessment_card AS ac
 LEFT JOIN model.assessment_pin AS ap
     ON ac.meta_pin = ap.meta_pin
     AND ac.run_id = ap.run_id
-LEFT JOIN school_data AS school
-    ON SUBSTRING(ac.meta_pin, 1, 10) = school.school_pin
-    AND ac.meta_year = school.year
+LEFT JOIN school_districts AS elem_sd
+    ON ac.loc_school_elementary_district_geoid = elem_sd.geoid
+    AND ac.meta_year = elem_sd.year
+LEFT JOIN school_districts AS sec_sd
+    ON ac.loc_school_secondary_district_geoid = sec_sd.geoid
+    AND ac.meta_year = sec_sd.year
 LEFT JOIN final_model_run AS final
     ON run.assessment_year = final.year
 WHERE ap.meta_triad_code = final.triad_code
