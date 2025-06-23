2509e404c78f7e07fa3b6fa1cca15d0622b2eb65
sweatyhandshake <wridgeway@cookcountyassessor.com>
2025-06-23 10:31:52 -0500

    Remove old table, rerun build to gen error log

--- dbt/models/reporting/reporting.sot_assessment_roll.py
+++ dbt/models/reporting/reporting.sot_assessment_roll.py
@@ -2,10 +2,11 @@
 # number of geographies, class combinations, and time.
 
 # Import libraries
-import pandas as pd
+from functools import reduce
 
-# Declare class groupings
-groups = ["no_group", "class", "major_class", "modeling_group", "res_other"]
+import pandas as pd
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import lit
 
 
 # Define aggregation functions. These are just wrappers for basic python
@@ -35,330 +36,126 @@ def first(x):
     return output
 
 
-more_stats = [
-    "min",
-    q10,
-    q25,
-    "median",
-    q75,
-    q90,
-    "max",
-    "mean",
-    "sum",
+def reassessment_year(year, geography, triad):
+    if geography in ["triad", "township", "nbhd"]:
+        year = int(year) % 3
+
+        if (
+            ((year == 0) & (triad == "North"))
+            | ((year == 1) & (triad == "South"))
+            | ((year == 2) & (triad == "City"))
+        ):
+            out = "Yes"
+        else:
+            out = "No"
+    else:
+        out = ""
+
+    return out
+
+
+def aggregate(key, pdf):
+    columns = ["av_tot", "av_bldg", "av_land"]
+
+    out = ()
+    out += (
+        reassessment_year(pdf["year"][0], geography, pdf["triad"][0]),  # noqa: F821
+        first(pdf[years[geography]]),  # noqa: F821
+        len(pdf["av_tot"]),
+        pdf["av_tot"].count(),
+        pdf["av_tot"].count() / pdf["av_tot"].size,
+    )
+    for column in columns:
+        out += (
+            pdf[column].min(),
+            q10(pdf[column]),
+            q25(pdf[column]),
+            pdf[column].median(),
+            q75(pdf[column]),
+            q90(pdf[column]),
+            pdf[column].max(),
+            pdf[column].mean(),
+            pdf[column].sum(),
+        )
+
+    return pd.DataFrame([key + out])
+
+
+groups = [
+    "res_other",
+    "major_class",
+    "no_group",
+    "class",
+    "modeling_group",
 ]
 
-stats = {
-    "av_tot": ["size", "count"] + more_stats,
-    "av_bldg": more_stats,
-    "av_land": more_stats,
-    "triad": [first],
-    "geography_data_year": [first],
+years = {
+    "county": "year",
+    "triad": "year",
+    "township": "year",
+    "nbhd": "year",
+    "tax_code": "year",
+    "zip_code": "year",
+    "community_area": "community_area_data_year",
+    "census_place": "census_data_year",
+    "census_tract": "census_data_year",
+    "census_congressional_district": "census_data_year",
+    "census_zcta": "census_data_year",
+    "cook_board_of_review_district": "cook_board_of_review_district_data_year",
+    "cook_commissioner_district": "cook_commissioner_district_data_year",
+    "cook_judicial_district": "cook_judicial_district_data_year",
+    "ward_num": "ward_data_year",
+    "police_district": "police_district_data_year",
+    "school_elementary_district": "school_data_year",
+    "school_secondary_district": "school_data_year",
+    "school_unified_district": "school_data_year",
+    "tax_municipality": "tax_data_year",
+    "tax_park_district": "tax_data_year",
+    "tax_library_district": "tax_data_year",
+    "tax_fire_protection_district": "tax_data_year",
+    "tax_community_college_district": "tax_data_year",
+    "tax_sanitation_district": "tax_data_year",
+    "tax_special_service_area": "tax_data_year",
+    "tax_tif_district": "tax_data_year",
+    "central_business_district": "central_business_district_data_year",
 }
 
+geographies = list(years.keys())
 
-def aggregrate(data, geography_type, group_type, stats):
-    """
-    Function to group a dataframe by whichever geography and group types it is
-    passed and output aggregate stats for that grouping.
-    """
-
-    print(geography_type, group_type)
-
-    group = [geography_type, group_type, "year", "stage_name"]
-    summary = data.groupby(group).agg(stats).round(2)
-    summary["geography_type"] = geography_type
-    summary["group_type"] = group_type
-    summary.index.names = ["geography_id", "group_id", "year", "stage_name"]
-    summary = summary.reset_index().set_index(
-        [
-            "geography_type",
-            "geography_id",
-            "group_type",
-            "group_id",
-            "year",
-            "stage_name",
-        ]
-    )
-
-    return summary
-
-
-def assemble(df, geos, groups):
-    """
-    Function that loops over predefined geography and class groups and passes
-    them to the aggregate function. Returns stacked output from the aggregate
-    function.
-    """
-
-    # Create an empty dataframe to fill with output
-    output = pd.DataFrame()
-
-    # Loop through group combinations and stack output
-    for key, value in geos.items():
-        df["geography_data_year"] = df[key]
-
-        for x in value:
-            for z in groups:
-                output = pd.concat([output, aggregrate(df, x, z, stats=stats)])
-
-    # Flatten multi-index
-    output.columns = ["_".join(col) for col in output.columns]
-    output = output.reset_index()
-    output = output.rename(columns={"triad_first": "triad"})
-
-    # Create additional stat columns post-aggregation
-    output["av_tot_pct_w_value"] = (
-        output["av_tot_count"] / output["av_tot_size"]
-    )
-
-    output = output.sort_values("year")
-
-    diff_cols = [
-        "geography_id",
-        "group_id",
-        "stage_name",
-        "av_tot_median",
-        "av_tot_mean",
-        "av_tot_sum",
-        "av_bldg_median",
-        "av_bldg_mean",
-        "av_bldg_sum",
-        "av_land_median",
-        "av_land_mean",
-        "av_land_sum",
-    ]
-
-    output[
-        [
-            "av_tot_delta_median",
-            "av_tot_delta_mean",
-            "av_tot_delta_sum",
-            "av_bldg_delta_median",
-            "av_bldg_delta_mean",
-            "av_bldg_delta_sum",
-            "av_land_delta_median",
-            "av_land_delta_mean",
-            "av_land_delta_sum",
-        ]
-    ] = (
-        output[diff_cols]
-        .groupby(["geography_id", "group_id", "stage_name"])
-        .diff()
-    )
-
-    output[
-        [
-            "av_tot_delta_pct_median",
-            "av_tot_delta_pct_mean",
-            "av_tot_delta_pct_sum",
-            "av_bldg_delta_pct_median",
-            "av_bldg_delta_pct_mean",
-            "av_bldg_delta_pct_sum",
-            "av_land_delta_pct_median",
-            "av_land_delta_pct_mean",
-            "av_land_delta_pct_sum",
-        ]
-    ] = (
-        output[diff_cols]
-        .groupby(["geography_id", "group_id", "stage_name"])
-        .pct_change()
-    )
-
-    output["year"] = output["year"].astype(int)
-    output["triennial"] = output["geography_type"].isin(
-        ["triad", "township", "nbhd"]
-    )
-
-    # Reassessment year is constructed as a string rather than a boolean to
-    # avoid PySpark errors with nullable booleans that can likely be resolved.
-    output["reassessment_year"] = ""
-    output.loc[
-        (output["triennial"] == True), "reassessment_year"  # noqa: E712
-    ] = "No"
-    output.loc[
-        ((output["year"] % 3 == 0) & (output["triad"] == "North"))
-        | ((output["year"] % 3 == 1) & (output["triad"] == "South"))
-        | ((output["year"] % 3 == 2) & (output["triad"] == "City"))
-        & (output["triennial"] == True),  # noqa: E712
-        "reassessment_year",
-    ] = "Yes"
-    output = output.drop(["triennial", "triad"], axis=1)
-
-    output = clean_names(output)
-
-    return output
-
-
-def clean_names(x):
-    """
-    Function to rename and reorder columns.
-    """
-
-    output = x.rename(
-        columns={
-            "av_tot_size": "pin_n_tot",
-            "av_tot_count": "pin_n_w_value",
-            "av_tot_pct_w_value": "pin_pct_w_value",
-            "geography_data_year_first": "geography_data_year",
-        }
-    )
-
-    output = output[
-        [
-            "geography_type",
-            "geography_id",
-            "geography_data_year",
-            "group_type",
-            "group_id",
-            "year",
-            "reassessment_year",
-            "stage_name",
-            "pin_n_tot",
-            "pin_n_w_value",
-            "pin_pct_w_value",
-            "av_tot_min",
-            "av_tot_q10",
-            "av_tot_q25",
-            "av_tot_median",
-            "av_tot_q75",
-            "av_tot_q90",
-            "av_tot_max",
-            "av_tot_mean",
-            "av_tot_sum",
-            "av_tot_delta_median",
-            "av_tot_delta_mean",
-            "av_tot_delta_sum",
-            "av_tot_delta_pct_median",
-            "av_tot_delta_pct_mean",
-            "av_tot_delta_pct_sum",
-            "av_bldg_min",
-            "av_bldg_q10",
-            "av_bldg_q25",
-            "av_bldg_median",
-            "av_bldg_q75",
-            "av_bldg_q90",
-            "av_bldg_max",
-            "av_bldg_mean",
-            "av_bldg_sum",
-            "av_bldg_delta_median",
-            "av_bldg_delta_mean",
-            "av_bldg_delta_sum",
-            "av_bldg_delta_pct_median",
-            "av_bldg_delta_pct_mean",
-            "av_bldg_delta_pct_sum",
-            "av_land_min",
-            "av_land_q10",
-            "av_land_q25",
-            "av_land_median",
-            "av_land_q75",
-            "av_land_q90",
-            "av_land_max",
-            "av_land_mean",
-            "av_land_sum",
-            "av_land_delta_median",
-            "av_land_delta_mean",
-            "av_land_delta_sum",
-            "av_land_delta_pct_median",
-            "av_land_delta_pct_mean",
-            "av_land_delta_pct_sum",
-        ]
-    ]
-
-    return output
-
-
-def ingest_geos(geos):
-    """
-    Function to convert dbt seed into a dictionary that can be iterated over.
-    """
-
-    geos = geos.toPandas()
-    output = {
-        k: list(geos[k].unique()[pd.notnull(geos[k].unique())])
-        for k in geos.columns
-    }
-
-    return output
+output_schema = "stage_name string, group_id string, geography_id string, year string, reassessment_year string, geography_data_year string, pin_n_tot int, pin_n_w_value int, pin_pct_w_value double, min_av_tot double, q10_av_tot double, q25_av_tot double, median_av_tot double, q75_av_tot double, q90_av_tot double, max_av_tot double, mean_av_tot double, sum_av_tot double, min_av_bldg double, q10_av_bldg double, q25_av_bldg double, median_av_bldg double, q75_av_bldg double, q90_av_bldg double, max_av_bldg double, mean_av_bldg double, sum_av_bldg double, min_av_land double, q10_av_land double, q25_av_land double, median_av_land double, q75_av_land double, q90_av_land double, max_av_land double, mean_av_land double, sum_av_land double"
 
 
 def model(dbt, spark_session):
-    """
-    Function to build a dbt python model using PySpark.
-    """
-    dbt.config(materialized="table")
-
-    # Ingest geographies and their associated data years
-    geos = ingest_geos(dbt.ref("reporting.sot_data_years"))
+    dbt.config(materialized="table", engine_config={"MaxConcurrentDpus": 40})
+    athena_user_logger.info("Loading assessment roll input table")
 
     input = dbt.ref("reporting.sot_assessment_roll_input")
-
-    # Convert the Spark input dataframe to Pandas for
-    # compatibility with assesspy functions
-    input = input.toPandas()
-
-    df = assemble(input, geos=geos, groups=groups)
-    # %%
-    schema = {
-        "geography_type": "string",
-        "geography_id": "string",
-        "geography_data_year": "string",
-        "group_type": "string",
-        "group_id": "string",
-        "year": "string",
-        "reassessment_year": "string",
-        "stage_name": "string",
-        "pin_n_tot": "bigint",
-        "pin_n_w_value": "bigint",
-        "pin_pct_w_value": "double",
-        "av_tot_min": "double",
-        "av_tot_q10": "double",
-        "av_tot_q25": "double",
-        "av_tot_median": "double",
-        "av_tot_q75": "double",
-        "av_tot_q90": "double",
-        "av_tot_max": "double",
-        "av_tot_mean": "double",
-        "av_tot_sum": "double",
-        "av_tot_delta_median": "double",
-        "av_tot_delta_mean": "double",
-        "av_tot_delta_sum": "double",
-        "av_tot_delta_pct_median": "double",
-        "av_tot_delta_pct_mean": "double",
-        "av_tot_delta_pct_sum": "double",
-        "av_bldg_min": "double",
-        "av_bldg_q10": "double",
-        "av_bldg_q25": "double",
-        "av_bldg_median": "double",
-        "av_bldg_q75": "double",
-        "av_bldg_q90": "double",
-        "av_bldg_max": "double",
-        "av_bldg_mean": "double",
-        "av_bldg_sum": "double",
-        "av_bldg_delta_median": "double",
-        "av_bldg_delta_mean": "double",
-        "av_bldg_delta_sum": "double",
-        "av_bldg_delta_pct_median": "double",
-        "av_bldg_delta_pct_mean": "double",
-        "av_bldg_delta_pct_sum": "double",
-        "av_land_min": "double",
-        "av_land_q10": "double",
-        "av_land_q25": "double",
-        "av_land_median": "double",
-        "av_land_q75": "double",
-        "av_land_q90": "double",
-        "av_land_max": "double",
-        "av_land_mean": "double",
-        "av_land_sum": "double",
-        "av_land_delta_median": "double",
-        "av_land_delta_mean": "double",
-        "av_land_delta_sum": "double",
-        "av_land_delta_pct_median": "double",
-        "av_land_delta_pct_mean": "double",
-        "av_land_delta_pct_sum": "double",
-    }
-    # %%
-    spark_df = spark_session.createDataFrame(
-        df, schema=", ".join(f"{key}: {val}" for key, val in schema.items())
+    spark_schema = dict.fromkeys(input.columns, "string")
+    spark_schema |= dict.fromkeys(["av_tot", "av_bldg", "av_land"], "double")
+    spark_schema = ", ".join(
+        f"{key} {val}" for key, val in spark_schema.items()
     )
 
-    return spark_df
+    spark_df = spark_session.createDataFrame(input, schema=spark_schema)
+
+    athena_user_logger.info("Dope stuff is happening... maybe?")
+
+    output = []
+    for group in groups:
+        for geography in geographies:
+            output += [
+                spark_df.groupby(["stage_name", group, geography, "year"])
+                .applyInPandas(
+                    aggregate,
+                    schema=output_schema,
+                )
+                .select(
+                    "*",
+                    lit(group).alias("group_type"),
+                    lit(geography).alias("geography_type"),
+                )
+            ]
+
+    df = reduce(DataFrame.unionByName, output)
+
+    return df
