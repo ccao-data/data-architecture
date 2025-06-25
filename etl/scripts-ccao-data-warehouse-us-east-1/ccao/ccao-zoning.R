624afb454d17cfdf45422e3ba3febc3ec7b22dbd
Damonamajor <damon.major@cookcountyil.gov>
2025-06-25 20:57:58 +0000

    remove pin10

--- etl/scripts-ccao-data-warehouse-us-east-1/ccao/ccao-zoning.R
+++ etl/scripts-ccao-data-warehouse-us-east-1/ccao/ccao-zoning.R
@@ -38,13 +38,6 @@ township_specs <- tibble::tibble(
   file_path = township_paths,
   file_name = basename(township_paths),
   folder = basename(dirname(township_paths)),
-  pin_col = c(
-    "Barrington_PIN10", "ElkGrove_PIN10", "Evanston_PIN10",
-    "Hanover_PIN10", "PARID", "Maine_PIN10", "NewTrier_PIN10",
-    "Niles_PIN10", "Northfield_PIN10", "NorwoodPark_PIN10",
-    "PalatineTwp_PIN10", "Schaumburg_PIN10", "Wheeling_PIN10",
-    "Pin10"
-  ),
   pin14_col = c(
     "PARID", "PARID", "PARID", "PARID", "PARID", "PARID", "PARID",
     "PARID", "PARID", "PIN14", "PARID", "PARID", "PARID", "PIN14"
@@ -68,43 +61,28 @@ township_specs <- tibble::tibble(
 read_and_standardize <- function(file_path,
                                  file_name,
                                  folder,
-                                 pin_col,
                                  pin14_col,
                                  zone_col,
                                  special_case) {
-  # Read Excel file and coerce all columns to character
   df <- read_excel(file_path) %>%
     mutate(across(everything(), as.character))
 
-  # Standardize to pin10 / pin / zoning_code
-  df <- if (file_name == "Leyden.xlsx") {
-    df %>%
-      mutate(
-        pin10 = str_sub(!!sym(pin_col), 1, 10),
-        pin = !!sym(pin14_col)
-      ) %>%
-      select(pin10, pin, zoning_code = !!sym(zone_col))
-  } else if (special_case) {
-    df %>%
-      rename(pin10 = !!sym(pin_col)) %>%
+  if (file_name == "Leyden.xlsx") {
+    df <- df %>%
       mutate(pin = !!sym(pin14_col)) %>%
-      select(pin10, pin, zoning_code = !!sym(zone_col))
+      select(pin, zoning_code = !!sym(zone_col))
   } else {
-    df %>%
-      transmute(
-        pin10 = !!sym(pin_col),
-        pin = !!sym(pin14_col),
-        zoning_code = !!sym(zone_col)
-      )
+    df <- df %>%
+      mutate(pin = !!sym(pin14_col)) %>%
+      select(pin, zoning_code = !!sym(zone_col))
   }
 
   df %>%
     mutate(
-      pin10 = as.character(pin10),
       pin = as.character(pin),
       zoning_code = as.character(zoning_code)
     ) %>%
-    filter(!is.na(pin10), !is.na(zoning_code))
+    filter(!is.na(pin), !is.na(zoning_code))
 }
 
 # === Read and standardize all zoning files ===
@@ -120,7 +98,6 @@ zoning <- bind_rows(township_data) %>%
   ) %>%
   mutate(year = "2025") %>%
   group_by(year)
-
 
 write_partitions_to_s3(
   df = zoning,
