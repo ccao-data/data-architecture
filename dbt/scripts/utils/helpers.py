b2b40cb6b33fdc83ca33b96258e7c9c1750e22af
Jean Cochrane <jeancochrane@users.noreply.github.com>
2025-08-12 10:47:21 -0500

    Simplify IC reference file logging config (#874)
    * Simplify IC reference file logging config
    
    * Don't push extra timestamps to CloudWatch

--- dbt/scripts/utils/helpers.py
+++ dbt/scripts/utils/helpers.py
@@ -28,14 +28,25 @@ def create_logger(
     if log_file_path is not None and os.path.exists(log_file_path):
         os.remove(log_file_path)
 
-    # Create and start the default stream logger
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s - %(levelname)s - %(message)s",
-        datefmt="%Y-%m-%d_%H:%M:%S.%f",
-        force=True,
+    formatter = logging.Formatter(
+        fmt="%(asctime)s.%(msecs)03d - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d_%H:%M:%S",
     )
+
+    # Create logger and add custom handlers
     logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    # Clear handlers in case we accidentally call this function multiple times
+    logger.handlers.clear()
+
+    # Always log to the console
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+
+    # Turn off log propagation to the root logger to avoid duplication with
+    # the stream handler
+    logger.propagate = False
 
     # Log to CloudWatch if desired
     if log_group_name is not None:
@@ -45,10 +56,18 @@ def create_logger(
             log_group_name=log_group_name,
             stream_name=stream_name,
         )
+
+        # Don't push timestamps to CloudWatch, since CloudWatch adds its own
+        # timestamps
+        cw_handler.setFormatter(
+            logging.Formatter(fmt="%(levelname)s - %(message)s")
+        )
         logger.addHandler(cw_handler)
 
     # Add a file handler to write logs to a local file if specified
     if log_file_path is not None:
-        logger.addHandler(logging.FileHandler(log_file_path))
+        file_handler = logging.FileHandler(log_file_path)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
 
     return logger
