a6dab593003a60afda0d356efa661478190cf804
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2025-09-10 19:05:03 +0000

    Add ptax_exe

--- dbt/models/default/default.vw_pin_exemption.sql
+++ dbt/models/default/default.vw_pin_exemption.sql
@@ -4,6 +4,19 @@ SELECT
     det.taxyr AS year,
     det.excode AS exemption_code,
     code.descr AS exemption_description,
+    CASE WHEN det.excode LIKE '%DP%' THEN 'exe_disabled'
+        WHEN det.excode LIKE '%SF%' THEN 'exe_freeze'
+        WHEN det.excode LIKE '%HO%' THEN 'exe_homeowner'
+        WHEN det.excode LIKE '%LT%' THEN 'exe_longtime_homeowner'
+        WHEN det.excode LIKE '%SR%' THEN 'exe_senior'
+        WHEN det.excode = 'MUNI' THEN 'exe_muni_built'
+        WHEN det.excode LIKE '%DV1%' THEN 'exe_vet_dis_lt50'
+        WHEN det.excode LIKE '%DV2%' THEN 'exe_vet_dis_50_69'
+        WHEN det.excode LIKE '%DV3%' THEN 'exe_vet_dis_ge70'
+        WHEN det.excode LIKE '%DV4%' THEN 'exe_vet_dis_100'
+        WHEN det.excode LIKE '%RTV%' THEN 'exe_vet_returning'
+        WHEN det.excode = 'WW2' THEN 'exe_wwii'
+    END AS ptax_exe,
     CAST(det.apother AS INT) AS exemption_amount
 FROM {{ source('iasworld', 'exdet') }} AS det
 LEFT JOIN {{ source('iasworld', 'excode') }} AS code
