55d959d79c644c4f4b415cfbe7b66cb197f3c537
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2025-09-16 20:11:53 +0000

    Explicit codes

--- dbt/models/default/default.vw_pin_exe.sql
+++ dbt/models/default/default.vw_pin_exe.sql
@@ -10,24 +10,20 @@ WITH long AS (
     SELECT
         det.parid AS pin,
         det.taxyr AS year,
-        CASE WHEN det.excode LIKE '%DP%' THEN 'exe_disabled'
-            WHEN det.excode LIKE '%SF%' THEN 'exe_freeze'
-            WHEN det.excode LIKE '%HO%' THEN 'exe_homeowner'
-            WHEN det.excode LIKE '%LT%' THEN 'exe_longtime_homeowner'
+        CASE WHEN det.excode IN ('DP', 'C-DP', 'DPHE') THEN 'exe_disabled'
+            WHEN det.excode IN ('SF', 'C-SF') THEN 'exe_freeze'
+            WHEN det.excode IN ('HO', 'C-HO') THEN 'exe_homeowner'
             WHEN
-                (det.excode LIKE '%SR%' AND det.excode != 'I-SRO')
-                OR (det.excode LIKE '%SC%' AND det.excode != 'SCAFHE')
+                det.excode IN ('LT', 'C-LT', 'LT1', 'LT2')
+                THEN 'exe_longtime_homeowner'
+            WHEN det.excode IN ('SR', 'C-SR', 'SC', 'SCHEM', 'SCHE', 'SCHE')
                 THEN 'exe_senior'
             WHEN det.excode = 'MUNI' THEN 'exe_muni_built'
-            WHEN
-                det.excode LIKE '%DV1%' AND det.excode != 'RDV1'
-                THEN 'exe_vet_dis_lt50'
-            WHEN
-                det.excode LIKE '%DV2%' AND det.excode != 'RDV2'
-                THEN 'exe_vet_dis_50_69'
-            WHEN det.excode LIKE '%DV3%' THEN 'exe_vet_dis_ge70'
-            WHEN det.excode LIKE '%DV4%' THEN 'exe_vet_dis_100'
-            WHEN det.excode LIKE '%RTV%' THEN 'exe_vet_returning'
+            WHEN det.excode IN ('DV1', 'C-DV1') THEN 'exe_vet_dis_lt50'
+            WHEN det.excode IN ('DV2', 'C-DV2') THEN 'exe_vet_dis_50_69'
+            WHEN det.excode IN ('DV3', 'DV3-M', 'DV30') THEN 'exe_vet_dis_ge70'
+            WHEN det.excode IN ('DV4', 'DV4-M') THEN 'exe_vet_dis_100'
+            WHEN det.excode = 'RTV' THEN 'exe_vet_returning'
             WHEN det.excode = 'WW2' THEN 'exe_wwii'
         END AS ptax_exe,
         CAST(det.apother AS INT) AS exemption_amount
