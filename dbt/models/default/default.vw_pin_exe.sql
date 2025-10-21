de1eae508bbc6b2477373f010e3be19600947573
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2025-10-21 14:23:50 +0000

    Remove taxable eav from vw_pin_exe

--- dbt/models/default/default.vw_pin_exe.sql
+++ dbt/models/default/default.vw_pin_exe.sql
@@ -1,4 +1,4 @@
--- View to collect pin-level exemptions and taxable EAV
+-- View to collect pin-level exemptions
 {% set exes = [
     'exe_disabled', 'exe_freeze', 'exe_homeowner',
     'exe_longtime_homeowner', 'exe_senior', 'exe_muni_built', 'exe_vet_dis_lt50',
@@ -40,49 +40,16 @@ WITH long AS (
         AND code.deactivat IS NULL
     WHERE det.deactivat IS NULL
         AND det.cur = 'Y'
-),
-
--- pin-level taxable EAV by stage
-asmt AS (
-    SELECT
-        parid AS pin,
-        taxyr AS year,
-        MAX(CASE WHEN procname = 'CCAOVALUE' THEN tot51 END) AS mailed_eav,
-        MAX(CASE WHEN procname = 'CCAOFINAL' THEN tot51 END)
-            AS certified_eav,
-        MAX(CASE WHEN procname = 'BORVALUE' THEN tot51 END) AS board_eav
-    FROM {{ source('iasworld', 'asmt_all') }}
-    WHERE rolltype != 'RR'
-        AND deactivat IS NULL
-        AND procname IN ('CCAOVALUE', 'CCAOFINAL', 'BORVALUE')
-        AND valclass IS NULL
-        -- Class 999 are test pins
-        AND class NOT IN ('999')
-    GROUP BY parid, taxyr
-
-
-),
-
--- Widen the exemptions
-wide AS (
-    SELECT
-        pin,
-        year,
-    {%- for exe_ in exes %}
-        CAST(SUM(CASE
-            WHEN ptax_exe = '{{ exe_ }}' THEN exemption_amount ELSE 0
-            END) AS INT)
-            AS {{ exe_ }}{%- if not loop.last -%},{%- endif -%}
-    {% endfor %}
-    FROM long
-    GROUP BY pin, year
 )
 
--- Join exemptions and EAVs
 SELECT
-    wide.*,
-    asmt.mailed_eav,
-    asmt.certified_eav,
-    asmt.board_eav
-FROM wide
-LEFT JOIN asmt ON wide.pin = asmt.pin AND wide.year = asmt.year
+    pin,
+    year,
+{%- for exe_ in exes %}
+    CAST(SUM(CASE
+        WHEN ptax_exe = '{{ exe_ }}' THEN exemption_amount ELSE 0
+        END) AS INT)
+        AS {{ exe_ }}{%- if not loop.last -%},{%- endif -%}
+{% endfor %}
+FROM long
+GROUP BY pin, year
