346af3cf3d97880a2b9631b12fbada36a762334a
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2025-10-28 15:53:37 +0000

    Remove tot32 from logic

--- dbt/models/default/default.vw_pin_tax_roll.sql
+++ dbt/models/default/default.vw_pin_tax_roll.sql
@@ -5,43 +5,17 @@ WITH asmt AS (
     SELECT
         parid AS pin,
         taxyr AS year,
-        MAX(
-            CASE WHEN
-                    procname = 'CCAOVALUE'
-                    AND COALESCE(tot30, 0) + COALESCE(tot31, 0) > 0
-                    THEN tot32
-                WHEN
-                    procname = 'CCAOVALUE'
-                    AND COALESCE(tot30, 0) + COALESCE(tot31, 0) = 0
-                    THEN tot13
-            END
-        ) AS mailed_taxable_av,
-        MAX(
-            CASE WHEN
-                    procname = 'CCAOFINAL'
-                    AND COALESCE(tot30, 0) + COALESCE(tot31, 0) > 0
-                    THEN tot32
-                WHEN
-                    procname = 'CCAOFINAL'
-                    AND COALESCE(tot30, 0) + COALESCE(tot31, 0) = 0
-                    THEN tot13
-            END
-        ) AS certified_taxable_av,
-        MAX(
-            CASE WHEN
-                    procname = 'BORVALUE'
-                    AND COALESCE(tot30, 0) + COALESCE(tot31, 0) > 0
-                    THEN tot32
-                WHEN
-                    procname = 'BORVALUE'
-                    AND COALESCE(tot30, 0) + COALESCE(tot31, 0) = 0
-                    THEN tot13
-            END
-        ) AS board_taxable_av,
-        MAX(CASE WHEN procname = 'CCAOVALUE' THEN tot51 END) AS mailed_eav,
-        MAX(CASE WHEN procname = 'CCAOFINAL' THEN tot51 END)
+        ARBITRARY(CASE WHEN procname = 'CCAOVALUE' THEN tot13 END)
+            AS mailed_taxable_av,
+        ARBITRARY(CASE WHEN procname = 'CCAOFINAL' THEN tot13 END)
+            AS certified_taxable_av,
+        ARBITRARY(CASE WHEN procname = 'BORVALUE' THEN tot13 END)
+            AS board_taxable_av,
+        ARBITRARY(CASE WHEN procname = 'CCAOVALUE' THEN tot51 END)
+            AS mailed_eav,
+        ARBITRARY(CASE WHEN procname = 'CCAOFINAL' THEN tot51 END)
             AS certified_eav,
-        MAX(CASE WHEN procname = 'BORVALUE' THEN tot51 END) AS board_eav
+        ARBITRARY(CASE WHEN procname = 'BORVALUE' THEN tot51 END) AS board_eav
     FROM {{ source('iasworld', 'asmt_all') }}
     WHERE rolltype != 'RR'
         AND deactivat IS NULL
