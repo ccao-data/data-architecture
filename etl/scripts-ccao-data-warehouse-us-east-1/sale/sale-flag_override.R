a1d46d72b0685cb0e6f8c438d449fbc611d67b5a
Michael Wagner <wagnerleemichael@gmail.com>
2025-12-22 21:04:49 +0000

    Lint

--- etl/scripts-ccao-data-warehouse-us-east-1/sale/sale-flag_override.R
+++ etl/scripts-ccao-data-warehouse-us-east-1/sale/sale-flag_override.R
@@ -7,7 +7,7 @@ library(aws.s3)
 df <-
   read_excel(
     here::here(
-      "scripts-ccao-data-warehouse-us-east-1/sale/res_characteristic_potentials_reviewed.xlsx"
+      "scripts-ccao-data-warehouse-us-east-1/sale/res_characteristic_potentials_reviewed.xlsx" # nonlint
     )
   ) %>%
   janitor::clean_names() %>%
@@ -19,15 +19,14 @@ df <-
 df <- df %>%
   mutate(
     exclude_sale =
-      coalesce(sale_is_arms_length == "NO", FALSE) |
-        coalesce(flips == "YES", FALSE) |
-        coalesce(grepl("YES", class_change, ignore.case = TRUE), FALSE) |
-        coalesce(grepl("YES", field_check, ignore.case = TRUE), FALSE)
+    coalesce(
+      sale_is_arms_length == "NO", FALSE) |
+      coalesce(flips == "YES", FALSE) |
+      coalesce(grepl("YES", class_change, ignore.case = TRUE), FALSE) |
+      coalesce(grepl("YES", field_check, ignore.case = TRUE), FALSE)
   )
-
-# df %>% select(sale_is_arms_length, flips, class_change, field_check, exclude_sale)
 
 write_parquet(
   df %>% select(doc_no, exclude_sale),
-  "s3://ccao-data-warehouse-dev-us-east-1/z_dev_miwagne_sale/flag_override/res_characteristic_potentials_reviewed.parquet"
+  "s3://ccao-data-warehouse-dev-us-east-1/z_dev_miwagne_sale/flag_override/res_characteristic_potentials_reviewed.parquet" # nolint
 )
