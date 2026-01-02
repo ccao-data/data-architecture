e2956f0e74c573b2e332aa789ba0531e55676b76
Michael Wagner <wagnerleemichael@gmail.com>
2026-01-02 20:50:34 +0000

    Rework script

--- etl/scripts-ccao-data-warehouse-us-east-1/sale/sale-flag_override.R
+++ etl/scripts-ccao-data-warehouse-us-east-1/sale/sale-flag_override.R
@@ -32,19 +32,49 @@ for (obj in objs) {
   df <- read_excel(local_path)
 
   df_name <- make.names(tools::file_path_sans_ext(file_name))
+
+  df <- df %>% mutate(source_file = df_name)
+
   dfs[[df_name]] <- df
 }
-# ------------------------------------------------------------------------------
-# Transform: one function applied to every dataframe
-#   - If name contains "dont_use_char_col", we don't utilize the characteristic
-#     change column, since we re-scoped it after realizing excluding minor
-#     characteristic changes are too prohitive. We rely on the other columns for
-#     this file like flips, class_change, etc.
-#   - Otherwise use characteristic_change regex matching strategy to account
-#     for human input errors
-# ------------------------------------------------------------------------------
 
-add_is_valid_for_modeling <- function(df, use_characteristic_regex = TRUE) {
+# We have a hard-coded exception for valuations_sale_review_2025.12.16,
+# since unlike the rest of the files, we aren't using the `characteristic_change`
+# column. In the first round of this collaboration with valuations, we weren't
+# distinguishing between minor and major characteristic changes. For example,
+# a `YES` in `characteristic_change` could have represented a full new floor, or
+# it could have represented a fireplace addition. We don't want to exclude all
+# the sales from the training data with very minor characteristic changes, so
+# we re-scoped and added a minor/major option for this column. As such, for this
+# file specifically we are relying on other columns
+
+dfs$valuations_sale_review_2025.12.16 <-
+  dfs$valuations_sale_review_2025.12.16 %>%
+  janitor::clean_names() %>%
+  rename(
+    sale_is_arms_length = sale_is_arm_s_length,
+    doc_no = sale_doc_no
+  ) %>%
+  mutate(
+    is_arms_length =
+      coalesce(sale_is_arms_length == "YES", FALSE),
+    is_flip =
+      coalesce(flip == "YES", FALSE),
+    has_class_change =
+      coalesce(
+        grepl("YES", class_change, ignore.case = TRUE),
+        FALSE
+      ),
+    has_characteristic_change =
+      NA,
+    requires_field_check =
+      coalesce(
+        grepl("YES", field_check, ignore.case = TRUE),
+        FALSE
+      )
+  )
+
+clean_columns <- function(df) {
   df %>%
     janitor::clean_names() %>%
     rename(
@@ -52,65 +82,82 @@ add_is_valid_for_modeling <- function(df, use_characteristic_regex = TRUE) {
       doc_no = sale_doc_no
     ) %>%
     mutate(
-      is_valid_for_modeling = !(
-        coalesce(sale_is_arms_length == "NO", FALSE) | # nolint
-          coalesce(flip == "YES", FALSE) |
-          coalesce(grepl("YES", class_change, ignore.case = TRUE), FALSE) |
-          if (use_characteristic_regex) {
-            # Regex statement explained:
-            # - YES - string match
-            # - [- ]? - matches a space, a hyphen, and doesn't disqualify
-            #   omission (YES-MAJOR, YES MAJOR, YESMAJOR)
-            # - MAJ - string match # [OA] - matches O or A
-            # - [REOT] - matches R, E, O, or T (we see data like 'YES-MAJOE'
-            #   and 'YES-MAJOE')
-            coalesce(
-              grepl(
-                "YES[- ]?MAJ[OA][REOT]",
-                characteristic_change,
-                ignore.case = TRUE
-              ),
-              FALSE
-            )
-          } else {
-            coalesce(grepl("YES", field_check, ignore.case = TRUE), FALSE)
-          })
+      is_arms_length =
+        coalesce(sale_is_arms_length == "YES", FALSE),
+      is_flip =
+        coalesce(flip == "YES", FALSE),
+      has_class_change =
+        coalesce(
+          grepl("YES", class_change, ignore.case = TRUE),
+          FALSE
+        ),
+      has_characteristic_change = case_when(
+        grepl(
+          "YES[- ]?MAJ[OA][REOT]",
+          characteristic_change,
+          ignore.case = TRUE
+        ) ~ "yes_major",
+        grepl(
+          "YES[- ]?MIN(?:OR|OT)",
+          characteristic_change,
+          ignore.case = TRUE
+        ) ~ "yes_minor",
+        TRUE ~ "no"
+      ),
+      requires_field_check =
+        coalesce(
+          grepl("YES", field_check, ignore.case = TRUE),
+          FALSE
+        )
     )
 }
 
+# Keep exception separate from this loop
+exception_df <- "valuations_sale_review_2025.12.16"
 dfs_processed <- imap(
-  dfs,
-  ~ add_is_valid_for_modeling(
-    .x,
-    use_characteristic_regex = !grepl(
-      "dont_use_char_col", .y,
-      ignore.case = TRUE
-    )
-  )
+  dfs[names(dfs) != exception_df],
+  ~ clean_columns(.x)
 )
+dfs_processed[[exception_df]] <- dfs[[exception_df]]
 
-# Bind all processed dfs together and write to S3 as parquet
-data_to_write <- purrr::imap_dfr(
+dfs_ready_to_write <- purrr::imap(
   dfs_processed,
-  ~ .x %>% select(doc_no, is_valid_for_modeling),
-  .id = "source_file"
-) %>%
-  select(-source_file)
-
-out_uri <- "s3://ccao-data-warehouse-us-east-1/sale/flag_override/flag_override.parquet" # nolint
-
-tmp_file <- tempfile(fileext = ".parquet")
-
-arrow::write_parquet(
-  data_to_write,
-  tmp_file,
-  compression = "snappy"
+  ~ dplyr::select(
+    .x,
+    doc_no,
+    is_arms_length,
+    is_flip,
+    has_class_change,
+    has_characteristic_change,
+    requires_field_check,
+    source_file
+  ) %>%
+    mutate(
+      loaded_at = Sys.time()
+    )
 )
 
-save_local_to_s3(
-  s3_uri = out_uri,
-  path = tmp_file,
-  overwrite = FALSE
-)
+# Output dir
+out_dir <- "s3://ccao-data-warehouse-us-east-1/sale/flag_override/"
 
-unlink(tmp_file)
+purrr::iwalk(
+  dfs_ready_to_write,
+  ~ {
+    out_uri <- paste0(out_dir, .y, ".parquet")
+    tmp_file <- tempfile(fileext = ".parquet")
+
+    arrow::write_parquet(
+      .x,
+      tmp_file,
+      compression = "snappy"
+    )
+
+    save_local_to_s3(
+      s3_uri = out_uri,
+      path = tmp_file,
+      overwrite = FALSE
+    )
+
+    unlink(tmp_file)
+  }
+)
