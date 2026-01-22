02c993c3ab1567be73afaa58eeb49bc1b0f0e57d
Jean Cochrane <jean@jeancochrane.com>
2026-01-22 13:26:34 -0600

    Eliminate duplicates in `default.vw_pin_exe_long`

--- dbt/models/default/default.vw_pin_exe_long.sql
+++ dbt/models/default/default.vw_pin_exe_long.sql
@@ -6,51 +6,71 @@
 -- see `default.vw_pin_exe`.
 
 -- Gather PIN-level exemptions from the exdet table. Exdet contains a row for
--- each exemption applied to a PIN in a year.
+-- each exemption applied to a PIN in a year. There are some duplicates due
+-- to data errors, so we start with a raw CTE that we can then aggregate in
+-- the final view
+WITH exe_raw AS (
+    SELECT
+        det.parid AS pin,
+        det.taxyr AS year,
+        CASE WHEN det.excode IN ('DP', 'C-DP', 'DPHE') THEN 'exe_disabled'
+            WHEN det.excode IN ('SF', 'C-SF') THEN 'exe_freeze'
+            WHEN det.excode IN ('HO', 'C-HO') THEN 'exe_homeowner'
+            WHEN
+                det.excode IN ('LT', 'C-LT', 'LT1', 'LT2')
+                THEN 'exe_longtime_homeowner'
+            WHEN det.excode IN ('SR', 'C-SR', 'SC', 'SCHE')
+                THEN 'exe_senior'
+            WHEN det.excode = 'MUNI' THEN 'exe_muni_built'
+            WHEN
+                det.excode IN ('DV1', 'C-DV1', 'DV0', 'C-DV0', 'DV-1')
+                THEN 'exe_vet_dis_lt50'
+            WHEN det.excode IN ('DV2', 'C-DV2', 'DV-2') THEN 'exe_vet_dis_50_69'
+            WHEN det.excode IN ('DV3', 'DV3-M', 'DV-3') THEN 'exe_vet_dis_ge70'
+            WHEN det.excode IN ('DV4', 'DV4-M', 'DV-4') THEN 'exe_vet_dis_100'
+            WHEN
+                det.excode IN ('RTV', 'C-RTV', 'RDV1', 'RV1', 'RDV2')
+                THEN 'exe_vet_returning'
+            WHEN det.excode = 'WW2' THEN 'exe_wwii'
+        END AS exemption_type,
+        CAST(det.apother AS INT) AS exemption_amount,
+        CASE
+            WHEN TRIM(UPPER(SPLIT_PART(admn.user126, ':', 1))) = 'Y' THEN TRUE
+            WHEN TRIM(UPPER(SPLIT_PART(admn.user126, ':', 1))) = 'N' THEN FALSE
+        END AS is_cofe,
+        DATE_PARSE(SUBSTR(admn.udate9, 1, 10), '%Y-%m-%d') AS cofe_date
+    FROM {{ source('iasworld', 'exdet') }} AS det
+    -- Ensure only approved exemptions are pulled
+    INNER JOIN {{ source('iasworld', 'exadmn') }} AS admn
+        ON det.parid = admn.parid
+        AND det.caseno = admn.caseno
+        AND det.taxyr = admn.taxyr
+        AND det.excode = admn.excode
+        AND admn.cur = 'Y'
+        AND admn.deactivat IS NULL
+        AND admn.exstat = 'A'
+    -- Ensure we are only pulling valid exemption codes
+    INNER JOIN {{ source('iasworld', 'excode') }} AS code
+        ON det.excode = code.excode
+        AND det.taxyr = code.taxyr
+        AND code.cur = 'Y'
+        AND code.deactivat IS NULL
+    WHERE det.deactivat IS NULL
+        AND det.cur = 'Y'
+)
+
 SELECT
-    det.parid AS pin,
-    det.taxyr AS year,
-    CASE WHEN det.excode IN ('DP', 'C-DP', 'DPHE') THEN 'exe_disabled'
-        WHEN det.excode IN ('SF', 'C-SF') THEN 'exe_freeze'
-        WHEN det.excode IN ('HO', 'C-HO') THEN 'exe_homeowner'
-        WHEN
-            det.excode IN ('LT', 'C-LT', 'LT1', 'LT2')
-            THEN 'exe_longtime_homeowner'
-        WHEN det.excode IN ('SR', 'C-SR', 'SC', 'SCHE')
-            THEN 'exe_senior'
-        WHEN det.excode = 'MUNI' THEN 'exe_muni_built'
-        WHEN
-            det.excode IN ('DV1', 'C-DV1', 'DV0', 'C-DV0', 'DV-1')
-            THEN 'exe_vet_dis_lt50'
-        WHEN det.excode IN ('DV2', 'C-DV2', 'DV-2') THEN 'exe_vet_dis_50_69'
-        WHEN det.excode IN ('DV3', 'DV3-M', 'DV-3') THEN 'exe_vet_dis_ge70'
-        WHEN det.excode IN ('DV4', 'DV4-M', 'DV-4') THEN 'exe_vet_dis_100'
-        WHEN
-            det.excode IN ('RTV', 'C-RTV', 'RDV1', 'RV1', 'RDV2')
-            THEN 'exe_vet_returning'
-        WHEN det.excode = 'WW2' THEN 'exe_wwii'
-    END AS exemption_type,
-    CAST(det.apother AS INT) AS exemption_amount,
-    CASE
-        WHEN TRIM(UPPER(SPLIT_PART(admn.user126, ':', 1))) = 'Y' THEN TRUE
-        WHEN TRIM(UPPER(SPLIT_PART(admn.user126, ':', 1))) = 'N' THEN FALSE
-    END AS is_cofe,
-    DATE_PARSE(SUBSTR(admn.udate9, 1, 10), '%Y-%m-%d') AS cofe_date
-FROM {{ source('iasworld', 'exdet') }} AS det
--- Ensure only approved exemptions are pulled
-INNER JOIN {{ source('iasworld', 'exadmn') }} AS admn
-    ON det.parid = admn.parid
-    AND det.caseno = admn.caseno
-    AND det.taxyr = admn.taxyr
-    AND det.excode = admn.excode
-    AND admn.cur = 'Y'
-    AND admn.deactivat IS NULL
-    AND admn.exstat = 'A'
--- Ensure we are only pulling valid exemption codes
-INNER JOIN {{ source('iasworld', 'excode') }} AS code
-    ON det.excode = code.excode
-    AND det.taxyr = code.taxyr
-    AND code.cur = 'Y'
-    AND code.deactivat IS NULL
-WHERE det.deactivat IS NULL
-    AND det.cur = 'Y'
+    pin,
+    year,
+    exemption_type,
+    -- Use the larger exemption_amount when duplicates have different values.
+    -- This decision is arbitrary, we just want to choose one deterministically
+    MAX(exemption_amount) AS exemption_amount,
+    -- Prefer true over false or null for is_cofe, since true values are less
+    -- common
+    MAX(is_cofe) AS is_cofe,
+    -- Choose any non-null cofe_date, since we don't know which one is correct.
+    -- This decision is also arbitrary
+    MAX(cofe_date) AS cofe_date
+FROM exe_raw
+GROUP BY pin, year, exemption_type
