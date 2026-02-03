3c7c0109c195074d7f2268c7767d7d512f922786
Michael Wagner <wagnerleemichael@gmail.com>
2026-02-03 17:02:47 +0000

    Fix quotes

--- etl/scripts-ccao-data-warehouse-us-east-1/sale/sale-flag_review.R
+++ etl/scripts-ccao-data-warehouse-us-east-1/sale/sale-flag_review.R
@@ -50,7 +50,7 @@ for (obj in objs) {
 # file valuations_sale_review_2025.12.16
 dfs$valuations_sale_review_01_30_2026_diane <-
   dfs$valuations_sale_review_01_30_2026_diane %>%
-  filter(`Sale Doc No` != '2516802041')
+  filter(`Sale Doc No` != "2516802041")
 
 clean_columns_and_whitespace <- function(df) {
   df %>%
