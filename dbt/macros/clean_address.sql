a5363ce51fb2bbb5f5a50723ae9e1b912cbe83ce
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2026-03-25 17:36:42 +0000

    Use concat_ws

--- dbt/macros/clean_address.sql
+++ dbt/macros/clean_address.sql
@@ -2,6 +2,11 @@
 -- spaces with a single space, removing null characters, and returning null
 -- if the result is an empty string.
 {% macro clean_address(address_columns) %}
-    {% set column_name = address_columns | join(" || ' ' || ") %}
-    nullif(replace(regexp_replace({{ column_name }}, '\s+', ' '), chr(0), ''), '')
+    {% set column_name = address_columns | join(", ") %}
+    nullif(
+        replace(
+            regexp_replace(concat_ws(' ', {{ column_name }}), '\s+', ' '), chr(0), ''
+        ),
+        ''
+    )
 {% endmacro %}
