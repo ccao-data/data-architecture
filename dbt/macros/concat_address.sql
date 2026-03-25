2ed49503ab69b63de8f82c373be1598c6a058fd6
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2026-03-25 19:17:54 +0000

    Rejigger replacement values and order

--- dbt/macros/concat_address.sql
+++ dbt/macros/concat_address.sql
@@ -1,11 +1,13 @@
 -- Macro that cleans and concatenates address columns by replacing multiple
 -- spaces with a single space, removing null characters, and returning null
 -- if the result is an empty string.
-{% macro clean_address(address_columns) %}
+{% macro concat_address(address_columns) %}
     {% set column_name = address_columns | join(", ") %}
     nullif(
-        replace(
-            regexp_replace(concat_ws(' ', {{ column_name }}), '\s+', ' '), chr(0), ''
+        regexp_replace(
+            regexp_replace(concat_ws(' ', {{ column_name }}), '[[:cntrl:]]', ''),
+            '\s+',
+            ' '
         ),
         ''
     )
--- dbt/models/default/default.vw_pin_address.sql
+++ dbt/models/default/default.vw_pin_address.sql
@@ -9,8 +9,8 @@ WITH mail AS (
     SELECT
         parid,
         taxyr,
-        {{ clean_address(['mail1', 'mail2']) }} AS mail_address_name,
-        {{ clean_address(['maddr1', 'maddr2']) }} AS mail_address_full,
+        {{ concat_address(['mail1', 'mail2']) }} AS mail_address_name,
+        {{ concat_address(['maddr1', 'maddr2']) }} AS mail_address_full,
         mcityname AS mail_address_city_name,
         mstatecode AS mail_address_state,
         NULLIF(mzip1, '00000') AS mail_address_zipcode_1,
