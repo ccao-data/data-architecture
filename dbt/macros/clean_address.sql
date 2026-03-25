e344c670c601073344f889ba0f4d12c7a4d10221
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2026-03-25 17:22:59 +0000

    Commenting

--- dbt/macros/clean_address.sql
+++ dbt/macros/clean_address.sql
@@ -1,4 +1,6 @@
--- Macro that aggregates CDUs to PIN-level. Strips out duplicate CDUs.
+-- Macro that cleans and concatenates address columns by replacing multiple
+-- spaces with a single space, removing null characters, and returning null
+-- if the result is an empty string.
 {% macro clean_address(address_columns) %}
     {% set column_name = address_columns | join(" || ' ' || ") %}
     nullif(replace(regexp_replace({{ column_name }}, '\s+', ' '), chr(0), ''), '')
