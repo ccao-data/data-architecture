22e20aff185317c2bac9ca2ed6b7ef58b635eb65
wagnerlmichael <93889413+wagnerlmichael@users.noreply.github.com>
2026-04-14 12:56:22 -0500

    Update etl/scripts-ccao-data-warehouse-us-east-1/sale/sale-flag_review.R
    Co-authored-by: William Ridgeway <10358980+wrridgeway@users.noreply.github.com>

--- etl/scripts-ccao-data-warehouse-us-east-1/sale/sale-flag_review.R
+++ etl/scripts-ccao-data-warehouse-us-east-1/sale/sale-flag_review.R
@@ -232,16 +232,9 @@ dfs$valuations_sale_review_01_30_2026_peter <-
 dfs$valuations_sale_review_03_05_2026_jon <-
   dfs$valuations_sale_review_03_05_2026_jon %>%
   mutate(
-    `Characteristic Change` = if_else(
-      `Sale Doc No` == "2516820281",
-      "YES-MAJOR",
-      `Characteristic Change`
-    ),
-    `Class Change` = if_else(
-      `Sale Doc No` == "2529320200",
-      "YES",
-      `Class Change`
-    )
+    `Characteristic Change` = str_replace(`Characteristic Change`, "YES-FIRE DAMAGE", "YES-MAJOR") ,
+    `Class Change` = str_replace(`Class Change`, "I CHANGED TO 2-10", "YES")
+  )
   )
 
 # Remove doc_nos that appear in both lydia_monica and monica files,
