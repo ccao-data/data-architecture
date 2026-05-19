52fc60fff8ba59aba1e2cec03d0661c4676045be
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2026-05-19 20:59:31 +0000

    Only include desired values for procname

--- dbt/models/default/default.vw_pin_value.sql
+++ dbt/models/default/default.vw_pin_value.sql
@@ -39,6 +39,10 @@ WITH stages AS (
         AND valclass IS NULL
         -- Class 999 are test pins
         AND class NOT IN ('999')
+        AND (
+            procname IN ('CCAOVALUE', 'CCAOFINAL', 'BORVALUE')
+            OR procname IS NULL
+        )
     GROUP BY parid, taxyr
 ),
 
