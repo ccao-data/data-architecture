bd84a429603b786c52083fe03f77060e5298d277
Jean Cochrane <jean@jeancochrane.com>
2026-06-22 11:54:45 -0500

    Add co-op version of `exe_vet_dis_ge70` excode to exemption views

--- dbt/models/default/default.vw_pin_exe_long.sql
+++ dbt/models/default/default.vw_pin_exe_long.sql
@@ -27,7 +27,9 @@ WITH exe_raw AS (
                 det.excode IN ('DV1', 'C-DV1', 'DV0', 'C-DV0', 'DV-1')
                 THEN 'exe_vet_dis_lt50'
             WHEN det.excode IN ('DV2', 'C-DV2', 'DV-2') THEN 'exe_vet_dis_50_69'
-            WHEN det.excode IN ('DV3', 'DV3-M', 'DV-3') THEN 'exe_vet_dis_ge70'
+            WHEN
+                det.excode IN ('DV3', 'C-DV3', 'DV3-M', 'DV-3')
+                THEN 'exe_vet_dis_ge70'
             WHEN det.excode IN ('DV4', 'DV4-M', 'DV-4') THEN 'exe_vet_dis_100'
             WHEN det.excode = 'DV5' THEN 'exe_vet_dis_100_idor'
             WHEN
