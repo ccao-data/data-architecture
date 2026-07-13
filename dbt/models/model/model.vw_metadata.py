d68412dfcddb16c312c14117415bcad9efb0f19a
Damonamajor <damon.major@cookcountyil.gov>
2026-07-13 21:17:37 +0000

    simplify to run_id run_type and year

--- dbt/models/model/model.vw_metadata.py
+++
@@ -1,4 +0,0 @@
-def model(dbt, session):
-    prod = dbt.source("model", "metadata")
-    dev = dbt.source("z_dev_model", "metadata")
-    return prod.unionByName(dev, allowMissingColumns=True)
---
+++ dbt/models/model/model.vw_metadata.sql
@@ -0,0 +1,3 @@
+SELECT run_id, run_type, year FROM {{ source('model', 'metadata') }}
+UNION ALL
+SELECT run_id, run_type, year FROM {{ source('z_dev_model', 'metadata') }}
