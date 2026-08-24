3d29a0c36cfc188c235f5b14007880f1651e5e1b
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2026-08-24 14:01:09 +0000

    Change to meta_township_code for training_data

--- dbt/models/model/model.training_data_final.sql
+++ dbt/models/model/model.training_data_final.sql
@@ -4,4 +4,4 @@ FROM {{ ref('model.training_data') }} AS td
 INNER JOIN {{ ref('model.final_model') }} AS fm
     ON td.run_id = fm.run_id
     -- Model runs are specific to townships
-    AND CONTAINS(fm.township_code_coverage, td.township_code)
+    AND CONTAINS(fm.township_code_coverage, td.meta_township_code)
