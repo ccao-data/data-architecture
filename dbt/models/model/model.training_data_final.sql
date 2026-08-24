8e335477f34bc4ea7c2f872844f7e3d8e0f3f4b5
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2026-08-24 13:55:36 +0000

    Change training data to ref

--- dbt/models/model/model.training_data_final.sql
+++ dbt/models/model/model.training_data_final.sql
@@ -1,6 +1,6 @@
 -- This view collects training data for each PIN, only for final model runs
 SELECT td.*
-FROM {{ source('model', 'training_data') }} AS td
+FROM {{ ref('model.training_data') }} AS td
 INNER JOIN {{ ref('model.final_model') }} AS fm
     ON td.run_id = fm.run_id
     -- Model runs are specific to townships
