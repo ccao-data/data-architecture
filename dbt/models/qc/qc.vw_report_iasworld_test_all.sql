7ad12280c5bea6c95654fcfb12c53de126ecddd7
Damonamajor <damon.major@cookcountyil.gov>
2026-08-25 18:52:13 +0000

    lintr

--- dbt/models/qc/qc.vw_report_iasworld_test_all.sql
+++ dbt/models/qc/qc.vw_report_iasworld_test_all.sql
@@ -1,3 +1,9 @@
-SELECT *, 'pardat' AS source_table FROM {{ ref('qc.vw_report_iasworld_test_pardat') }}
+SELECT
+    *,
+    'pardat' AS source_table
+FROM {{ ref('qc.vw_report_iasworld_test_pardat') }}
 UNION ALL
-SELECT *, 'owndat' AS source_table FROM {{ ref('qc.vw_report_iasworld_test_owndat') }}
+SELECT
+    *,
+    'owndat' AS source_table
+FROM {{ ref('qc.vw_report_iasworld_test_owndat') }}
