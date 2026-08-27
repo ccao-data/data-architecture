932656d413f09136d5308cb224687b2dd849351d
Sweaty Handshake <william.ridgeway@cookcountyil.gov>
2026-08-27 19:07:44 +0000

    Use full syntax for corner pull

--- etl/scripts-ccao-data-warehouse-us-east-1/spatial/spatial-ccao-corner.R
+++ etl/scripts-ccao-data-warehouse-us-east-1/spatial/spatial-ccao-corner.R
@@ -31,8 +31,12 @@ for (iter_year in parcel_years) {
   tictoc::tic(paste("Finished processing corners for:", iter_year))
 
   # Load the full year's parcel file to iterate though by township
-  parcels <- open_s3_geodataset(parcel_path) %>%
-    filter(year == iter_year)
+  parcels <- open_dataset(parcel_path) %>%
+    filter(year == iter_year) %>%
+    collect() %>%
+    st_as_sf(sf_column_name = "geometry") %>%
+    st_set_crs(unique(.$crs)) %>%
+    select(-crs)
 
   for (iter_town in ccao::town_dict$township_code) {
     print(paste("Now processing township:", iter_town))
