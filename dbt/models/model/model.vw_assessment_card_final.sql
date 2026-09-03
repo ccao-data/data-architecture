171d4a528a05c6809d63c7790386b1b603cbb373
Jean Cochrane <jean@jeancochrane.com>
2026-09-03 11:00:38 -0500

    Add `meta_triad_code` to `model.vw_assessment_card_final` for the purposes of testing

--- dbt/models/model/model.vw_assessment_card_final.sql
+++ dbt/models/model/model.vw_assessment_card_final.sql
@@ -2,9 +2,12 @@
 -- runs
 SELECT
     ac.*,
+    town.triad_code AS meta_triad_code,
     fm.type AS model_type
 FROM {{ source('model', 'assessment_card') }} AS ac
 INNER JOIN {{ ref('model.final_model') }} AS fm
     ON ac.run_id = fm.run_id
     -- Model runs are specific to townships
     AND CONTAINS(fm.township_code_coverage, ac.township_code)
+LEFT JOIN {{ source('spatial', 'township') }} AS town
+    ON ac.township_code = town.township_code
