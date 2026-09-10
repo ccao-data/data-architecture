6174243c9a5a555171fdfb9421bb7cfa63f7fd7c
Damonamajor <damon.major@cookcountyil.gov>
2026-09-10 14:54:05 +0000

    add any join for pardat and NULL pin

--- dbt/models/qc/qc.vw_report_iasworld_test_sales.sql
+++ dbt/models/qc/qc.vw_report_iasworld_test_sales.sql
@@ -58,7 +58,7 @@
         CAST(NULL AS INTEGER) AS card,
         CAST(NULL AS INTEGER) AS lline,
         legdat.user1 AS township_code,
-        pardat.class,
+        CAST(NULL AS VARCHAR) AS class,
         sales.who,
         sales.wen,
         -- Columns to test
@@ -77,16 +77,13 @@
         AND SUBSTR(sales.saledt, 1, 4) = legdat.taxyr
         AND legdat.cur = 'Y'
         AND legdat.deactivat IS NULL
-    LEFT JOIN LATERAL (
-        SELECT parid, class
-        FROM {{ source('iasworld', 'pardat') }} AS pardat_src
-        WHERE pardat_src.parid = sales.parid
-            AND pardat_src.taxyr <= SUBSTR(sales.saledt, 1, 4)
-            AND pardat_src.cur = 'Y'
-            AND pardat_src.deactivat IS NULL
-        ORDER BY pardat_src.taxyr DESC
-        LIMIT 1
-    ) AS pardat ON TRUE
+    LEFT JOIN (
+        SELECT DISTINCT parid
+        FROM {{ source('iasworld', 'pardat') }}
+        WHERE cur = 'Y'
+            AND deactivat IS NULL
+    ) AS pardat
+        ON sales.parid = pardat.parid
     WHERE sales.cur = 'Y'
         AND sales.deactivat IS NULL
 {% endset %}
