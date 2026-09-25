2bfe8f541addbddcc5926881d65283e3824138c6
Damonamajor <56321109+Damonamajor@users.noreply.github.com>
2026-09-25 14:48:00 -0500

    add iasworld.dweldat to tests (#1107)
    * initial test
    
    * Delete dbt/analyses/iasworld_dweldat_test_comparison.sql
    
    * remove array
    
    * lintr
    
    * update to pardat inner join
    
    * test new query structure
    
    * Update qc.vw_report_iasworld_test_dweldat.sql
    
    * revert unique conditions
    
    * Apply batched suggestions from code review
    
    Co-authored-by: Jean Cochrane <jeancochrane@users.noreply.github.com>
    
    * line length
    
    * remove duplicated NULL tests
    
    * test aggregated table
    
    * switch to array_join
    
    * switch out class join
    
    * remove NULL testing
    
    * add excluding class 211, 2122
    
    * remove NOT class filter
    
    * split sql to one line each
    
    * update base on class
    
    ---------
    
    Co-authored-by: Jean Cochrane <jeancochrane@users.noreply.github.com>

--- dbt/models/qc/qc.vw_report_iasworld_test_all.sql
+++ dbt/models/qc/qc.vw_report_iasworld_test_all.sql
@@ -1,12 +1,22 @@
 SELECT
     *,
-    'pardat' AS source_table
-FROM {{ ref('qc.vw_report_iasworld_test_pardat') }}
+    'dweldat' AS source_table
+FROM {{ ref('qc.vw_report_iasworld_test_dweldat') }}
+UNION ALL
+SELECT
+    *,
+    'dweldat' AS source_table
+FROM {{ ref('qc.vw_report_iasworld_test_dweldat_agg') }}
 UNION ALL
 SELECT
     *,
     'owndat' AS source_table
 FROM {{ ref('qc.vw_report_iasworld_test_owndat') }}
+UNION ALL
+SELECT
+    *,
+    'pardat' AS source_table
+FROM {{ ref('qc.vw_report_iasworld_test_pardat') }}
 UNION ALL
 SELECT
     *,
---
+++ dbt/models/qc/qc.vw_report_iasworld_test_dweldat_agg.sql
@@ -0,0 +1,95 @@
+{%- set tests = [
+    {
+        "name": "iasworld_dweldat_class_matches_pardat_class",
+        "description": "at least one class should match pardat class",
+        "category": "class_mismatch_or_issue",
+        "condition": (
+            "NOT any_class_is_regression_class "
+            "OR any_class_matches_pardat_class"
+        ),
+        "additional_select_columns": ["pardat_class", "classes"]
+    },
+    {
+        "name": "iasworld_dweldat_exempt_classes_match_pardat_class",
+        "description": (
+            "at least one class should be exempt or omitted if pardat is "
+            "exempt"
+        ),
+        "category": "class_mismatch_or_issue",
+        "condition": "pardat_class != 'EX' OR any_class_is_exempt_or_omitted",
+        "additional_select_columns": ["pardat_class", "classes"]
+    },
+    {
+        "name": "iasworld_dweldat_class_in_ccao_class_dict",
+        "description": "class code must be valid for every card",
+        "category": "class_mismatch_or_issue",
+        "condition": "NOT any_class_invalid",
+        "additional_select_columns": ["classes"]
+    }
+] -%}
+
+{%- set base_query %}
+    SELECT
+        -- Identifying columns
+        dweldat.parid,
+        dweldat.taxyr,
+        CAST(NULL AS INTEGER) AS card,
+        CAST(NULL AS INTEGER) AS lline,
+        MAX(legdat.user1) AS township_code,
+        CAST(NULL AS VARCHAR) AS class,
+        MAX(dweldat.who) AS who,
+        MAX(dweldat.wen) AS wen,
+        -- Computed columns for tests
+        MAX(pardat.class) AS pardat_class,
+        ARRAY_JOIN(ARRAY_AGG(dweldat.class), ', ') AS classes,
+        -- Only regression classes are subject to the pardat class match
+        -- test. Excludes non-regression classes.
+        BOOL_OR(
+            dweldat.class NOT IN (
+                '201', '213', '218', '219', '220', '221', '224', '225',
+                '236', '240', '241', '290', '294', '297'
+            )
+        ) AS any_class_is_regression_class,
+        BOOL_OR(
+            dweldat.class NOT IN (
+                '201', '213', '218', '219', '220', '221', '224', '225',
+                '236', '240', '241', '290', '294', '297'
+            )
+            AND SUBSTR(dweldat.class, 1, 3) = SUBSTR(pardat.class, 1, 3)
+        ) AS any_class_matches_pardat_class,
+        BOOL_OR(
+            dweldat.class LIKE 'OA%' OR dweldat.class = 'EX'
+        ) AS any_class_is_exempt_or_omitted,
+        BOOL_OR(
+            dweldat.class NOT IN ('EX', 'RR') AND class_dict.class_code IS NULL
+        ) AS any_class_invalid
+    FROM {{ source('iasworld', 'dweldat') }} AS dweldat
+    LEFT JOIN {{ source('iasworld', 'legdat') }} AS legdat
+        ON dweldat.parid = legdat.parid
+        AND dweldat.taxyr = legdat.taxyr
+        AND legdat.cur = 'Y'
+        AND legdat.deactivat IS NULL
+    INNER JOIN {{ source('iasworld', 'pardat') }} AS pardat
+        ON dweldat.parid = pardat.parid
+        AND dweldat.taxyr = pardat.taxyr
+        AND pardat.cur = 'Y'
+        AND pardat.deactivat IS NULL
+    LEFT JOIN {{ ref('ccao.class_dict') }} AS class_dict
+        ON dweldat.class = class_dict.class_code
+    -- Excludes mixed-use/commercial parcels, which are not subject to these
+    -- residential class checks
+    LEFT JOIN (
+        SELECT DISTINCT parid, taxyr
+        FROM {{ source('iasworld', 'comdat') }}
+        WHERE cur = 'Y'
+            AND deactivat IS NULL
+    ) AS comdat
+        ON dweldat.parid = comdat.parid
+        AND dweldat.taxyr = comdat.taxyr
+    WHERE dweldat.cur = 'Y'
+        AND dweldat.deactivat IS NULL
+        AND comdat.parid IS NULL
+    GROUP BY dweldat.parid, dweldat.taxyr
+{% endset %}
+
+{{ generate_iasworld_qc_test_view(base_query, tests) }}
